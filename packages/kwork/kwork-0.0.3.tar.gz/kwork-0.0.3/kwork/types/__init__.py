5cdf7654d5a4dd04ed6e7aa171420140f190aad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:54 +0000

    Add package kwork/0.0.3/kwork-0.0.3.tar.gz

---
+++ packages/kwork/kwork-0.0.3.tar.gz/kwork-0.0.3/kwork/types/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .actor import Actor
+from .message import Message, MessageModel
+from .dialog import Dialog, LastMessage
+from .event import BaseEvent, EventType, Notify
+from .category import Category
+from .project import Project
