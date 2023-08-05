53619e9a684a0956aef46a1176e8677d142eb7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:47 +0000

    Add package CionAttack/0.1.3/CionAttack-0.1.3.tar.gz

---
+++ packages/CionAttack/CionAttack-0.1.3.tar.gz/CionAttack-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup,find_packages
+
+setup(
+    name="CionAttack",
+    version="0.1.3",
+    description="A",
+    long_description="C",
+
+    long_description_content_type="text/markdown",
+    author="io_O",
+    author_email="fastorder9@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=['CionAttack'],
+    include_package_data=True
+)
