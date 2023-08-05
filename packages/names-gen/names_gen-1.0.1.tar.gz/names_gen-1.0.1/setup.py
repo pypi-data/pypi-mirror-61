a2d464a243c7acca45d28af564cfc67df5b5665c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package names-gen/1.0.1/names_gen-1.0.1.tar.gz

---
+++ packages/names-gen/names_gen-1.0.1.tar.gz/names_gen-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+with open(path.join(here, 'VERSION'), encoding='utf-8') as f:
+	version = f.read()
+
+setup(
+	name = 'names_gen',
+	version = version,
+	description = "Generate random names with lenght",
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+    url="https://github.com/treyhunner/names",
+	author = 'Michal Kopys',
+	author_email = 'mikopys@gmail.com',
+    include_package_data=True,
+	keywords = 'random names',
+	packages = find_packages(),
+    install_requires = ['names']
+)
