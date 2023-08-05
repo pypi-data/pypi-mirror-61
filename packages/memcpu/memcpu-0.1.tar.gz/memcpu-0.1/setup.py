e09374ffff044605f5ce1e3c597afe5dfaad599d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:31 +0000

    Add package memcpu/0.1/memcpu-0.1.tar.gz

---
+++ packages/memcpu/memcpu-0.1.tar.gz/memcpu-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='memcpu',
+      version='0.1',
+      description='The world',
+      url='https://www.memcpu.com/',
+      author='zqian',
+      author_email='zqian@memcpu.com',
+      license='MIT',
+      packages=['memcpu'],
+      zip_safe=False)
