628cbe002bc8e95aab641a204e60d3b6f40baa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:30 +0000

    Add package pypackproxy/0.2.0/pypackproxy-0.2.0.tar.gz

---
+++ packages/pypackproxy/pypackproxy-0.2.0.tar.gz/pypackproxy-0.2.0/CHANGELOG.md
@@ -0,0 +1,8 @@
+History
+-------
+
+**2020-02-11 (0.2.0)**
+ - Can now run without external package repository
+
+**2020-02-08 (0.1.0)**
+ - First public release
---
+++ packages/pypackproxy/pypackproxy-0.2.0.tar.gz/pypackproxy-0.2.0/src/pypackproxy/__init__.py
@@ -0,0 +1,13 @@
+"""PyPackProxy package."""
+
+import os
+
+__version__ = '0.2.0'
+
+PROG_NAME = 'PyPackProxy'
+
+PYPP_DEBUG = bool(os.getenv('PYPP_DEBUG'))
+
+ROOT_PATH = '/'
+PACKS_PATH = '/packs'
+SIMPLE_PATH = '/simple'
