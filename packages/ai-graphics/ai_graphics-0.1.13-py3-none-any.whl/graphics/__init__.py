334708fb1e17aa22c4a8d37a405b8a970f88300e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:28:03 +0000

    Add package ai-graphics/0.1.13/ai_graphics-0.1.13-py3-none-any.whl

---
+++ packages/ai-graphics/ai_graphics-0.1.13-py3-none-any.whl/graphics/__init__.py
@@ -0,0 +1,11 @@
+from .app import Graphics, VideoProcess, FontWriter, InsightFace, FaceDetector, FaceRecognition
+from .app import ImageCrop, Classifier
+
+__all__ = ['Graphics',
+           'VideoProcess',
+           'FontWriter',
+           'InsightFace',
+           'FaceDetector',
+           'FaceRecognition',
+           'ImageCrop',
+           'Classifier']
