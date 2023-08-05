b4fe5732369bd155c4794552661d4f9725241357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package skymapping-tools/0.12.0/skymapping_tools-0.12.0.tar.gz

---
+++ packages/skymapping-tools/skymapping_tools-0.12.0.tar.gz/skymapping_tools-0.12.0/skymapper/__init__.py
@@ -0,0 +1,31 @@
+from . import core
+from .config import VERSION
+
+
+__version__ = VERSION
+
+
+originInstance = core.Initialization()
+
+# environment setting
+gpu = originInstance.choose_gpu
+
+# data input
+reader = originInstance.skymapper_read_mtx
+get_dim = originInstance.get_dim
+change_dim = originInstance.change_dim
+
+# To view the data
+ploter = originInstance.make_sample_plots
+video_maker = originInstance.make_sample_video
+
+# to convert the data
+converter = originInstance.prepare
+
+# to train the data
+set_train_size = originInstance.set_train_size
+trainer = originInstance.train
+
+# to analyze the data
+
+analyzer = originInstance.analyze
---
+++ packages/skymapping-tools/skymapping_tools-0.12.0.tar.gz/skymapping_tools-0.12.0/skymapper/config.py
@@ -0,0 +1,4 @@
+
+
+VERSION = '0.12.0'
+BASE_URL = 'https://github.com/zwang2019/Skymapping_Tools'
