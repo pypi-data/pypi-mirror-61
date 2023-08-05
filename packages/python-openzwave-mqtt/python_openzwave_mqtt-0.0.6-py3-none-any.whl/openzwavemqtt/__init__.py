221793c5f956eeb35494b41c84d7035a36fd1b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package python-openzwave-mqtt/0.0.6/python_openzwave_mqtt-0.0.6-py3-none-any.whl

---
+++ packages/python-openzwave-mqtt/python_openzwave_mqtt-0.0.6-py3-none-any.whl/openzwavemqtt/__init__.py
@@ -0,0 +1,4 @@
+"""Python listener implementation of the OpenZWave MQTT Server."""
+
+from .options import OZWOptions  # noqa
+from .manager import OZWManager  # noqa
---
+++ packages/python-openzwave-mqtt/python_openzwave_mqtt-0.0.6-py3-none-any.whl/openzwavemqtt/const.py
@@ -0,0 +1,39 @@
+import logging
+
+LOGGER = logging.getLogger("openzwavemqtt")
+
+EVENT_PLACEHOLDER = "missing"
+
+EVENT_COMMAND_CLASS_ADDED = "command_class_added"
+EVENT_COMMAND_CLASS_CHANGED = "command_class_changed"
+EVENT_COMMAND_CLASS_REMOVED = "command_class_removed"
+
+EVENT_INSTANCE_ADDED = "instance_added"
+EVENT_INSTANCE_CHANGED = "instance_changed"
+EVENT_INSTANCE_REMOVED = "instance_removed"
+EVENT_INSTANCE_EVENT = "instance_event"
+
+EVENT_INSTANCE_STATISTICS_CHANGED = "instance_statistics_changed"
+EVENT_INSTANCE_STATUS_CHANGED = "instance_status_changed"
+
+EVENT_NODE_ADDED = "node_added"
+EVENT_NODE_CHANGED = "node_changed"
+EVENT_NODE_REMOVED = "node_removed"
+EVENT_NODE_INSTANCE_ADDED = "node_instance_added"
+EVENT_NODE_INSTANCE_CHANGED = "node_instance_changed"
+EVENT_NODE_INSTANCE_REMOVED = "node_instance_removed"
+EVENT_NODE_ASSOCIATION_ADDED = "node_association_added"
+EVENT_NODE_ASSOCIATION_CHANGED = "node_association_changed"
+EVENT_NODE_ASSOCIATION_REMOVED = "node_association_removed"
+EVENT_NODE_STATISTICS_CHANGED = "node_statistics_changed"
+
+EVENT_VALUE_ADDED = "value_added"
+EVENT_VALUE_CHANGED = "value_changed"
+EVENT_VALUE_REMOVED = "value_removed"
+
+EMPTY = {}
+
+TYPE_BOOL = "Bool"
+TYPE_LIST = "List"
+TYPE_STRING = "String"
+TYPE_DECIMAL = "Decimal"
