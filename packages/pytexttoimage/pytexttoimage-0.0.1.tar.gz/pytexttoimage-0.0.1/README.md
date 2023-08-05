2a57e9def61523dc9ea45f100b6defadee6e20e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:11 +0000

    Add package pytexttoimage/0.0.1/pytexttoimage-0.0.1.tar.gz

---
+++ packages/pytexttoimage/pytexttoimage-0.0.1.tar.gz/pytexttoimage-0.0.1/README.md
@@ -0,0 +1,2 @@
+# Text_To_Image
+Method to convert text to image in python using opencv2
---
+++ packages/pytexttoimage/pytexttoimage-0.0.1.tar.gz/pytexttoimage-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytexttoimage", # Replace with your own username
+    version="0.0.1",
+    author="Saurabh Kumar",
+    author_email="skp1535@gmail.com",
+    description="Package to convert text data into image",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SKP1535/pytext2image.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
