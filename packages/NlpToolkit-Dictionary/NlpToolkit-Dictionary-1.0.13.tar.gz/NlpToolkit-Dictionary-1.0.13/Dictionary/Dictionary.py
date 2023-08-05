3162e837b0ea80ef797c72e963769d2ae61d2140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:12 +0000

    Add package NlpToolkit-Dictionary/1.0.13/NlpToolkit-Dictionary-1.0.13.tar.gz

---
+++ packages/NlpToolkit-Dictionary/NlpToolkit-Dictionary-1.0.13.tar.gz/NlpToolkit-Dictionary-1.0.13/Dictionary/Dictionary.py
@@ -0,0 +1,136 @@
+from bisect import bisect_left
+from Dictionary.Word import Word
+
+
+class Dictionary:
+
+    words: list
+    filename: str
+
+    """
+    An empty constructor of Dictionary class.
+    """
+    def __init__(self):
+        self.words = []
+        self.filename = ""
+
+    """
+    The getWord method takes a String name as an input and performs binary search within words list and assigns the 
+    result to integer variable middle. If the middle is greater than 0, it returns the item at index middle of words 
+    list, None otherwise.
+
+    PARAMETERS
+    ----------
+    name : str
+        String input.
+        
+    RETURNS
+    -------
+    Word
+        the item at found index of words {@link ArrayList}, null if cannot be found.
+    """
+    def getWord(self, name: str) -> Word:
+        word = Word(name)
+        middle = bisect_left(self.words, word)
+        if self.words[middle] == word:
+            return self.words[middle]
+        return None
+
+    """
+    The getWordIndex method takes a String name as an input and performs binary search within words list and assigns 
+    the result to integer variable middle. If the middle is greater than 0, it returns the index middle, -1 otherwise.
+
+    PARAMETERS
+    ----------
+    name : str
+        String input.
+        
+    RETURNS
+    -------
+    int
+        found index of words list, -1 if cannot be found.
+    """
+    def getWordIndex(self, name: str) -> int:
+        word = Word(name)
+        middle = bisect_left(self.words, word)
+        if self.words[middle] == word:
+            return middle
+        return -1
+
+    """
+    RemoveWord removes a word with the given name
+    
+    PARAMETERS
+    ----------
+    name : str
+        Name of the word to be removed.
+    """
+    def removeWord(self, name: str):
+        index = self.getWordIndex(name)
+        if index != -1:
+            self.words.pop(index)
+
+    """
+    The size method returns the size of the words list.
+
+    RETURNS
+    -------
+    int
+        The size of the words list.
+    """
+    def size(self) -> int:
+        return len(self.words)
+
+    """
+    The getWordWithIndex method which takes an index as an input and returns the value at given index of words list.
+
+    PARAMETERS
+    ----------
+    index : int
+        index to get the value.
+        
+    RETURNS
+    -------
+    Word
+        The value at given index of words list.
+    """
+    def getWordWithIndex(self, index : int) -> Word:
+        return self.words[index]
+
+    """
+    The longestWordSize method loops through the words list and returns the item with the maximum word length.
+
+    RETURNS
+    -------
+    int
+        The item with the maximum word length.
+    """
+    def longestWordSize(self) -> int:
+        maxLength = 0
+        for word in self.words:
+            if len(word) > maxLength:
+                maxLength = len(word)
+        return maxLength
+
+    """
+    The getWordStartingWith method takes a String hash as an input and performs binary search within words list and 
+    assigns the result to integer variable middle. If the middle is greater than 0, it returns the index middle, 
+    -middle-1 otherwise.
+
+    PARAMETERS
+    ----------
+    hash : str
+        String input.
+        
+    RETURNS
+    -------
+    int
+        Found index of words list, -middle-1 if cannot be found.
+    """
+    def getWordStartingWith(self, hash : str) -> int:
+        word = Word(hash)
+        middle = bisect_left(self.words, word)
+        if self.words[middle] == word:
+            return middle
+        else:
+            return -middle - 1
---
+++ packages/NlpToolkit-Dictionary/NlpToolkit-Dictionary-1.0.13.tar.gz/NlpToolkit-Dictionary-1.0.13/Dictionary/ExceptionalWord.py
@@ -0,0 +1,49 @@
+from Dictionary.Word import Word
+from Dictionary.Pos import Pos
+
+
+class ExceptionalWord(Word):
+
+    __root: str
+    __pos: Pos
+
+    """
+    A constructor of ExceptionalWord class which takes a Pos as a  part of speech and two Strings; name
+    and root as inputs. Then, calls its super class Word with given name and initialises root and pos variables
+    with given inputs.
+
+    PARAMETERS
+    ----------
+    name : str
+        String input.
+    root : str
+        String input.
+    pos : Pos
+        Pos type input.
+    """
+    def __init__(self, name: str, root: str, pos: Pos):
+        super().__init__(name)
+        self.__root = root
+        self.__pos = pos
+
+    """
+    Getter for the root variable.
+
+    RETURNS
+    -------
+    str
+        root variable.
+    """
+    def getRoot(self) -> str:
+        return self.__root
+
+    """
+    Getter for the pos variable.
+
+    RETURNS
+    -------
+    Pos
+        pos variable.
+    """
+    def getPos(self) -> Pos:
+        return self.__pos
