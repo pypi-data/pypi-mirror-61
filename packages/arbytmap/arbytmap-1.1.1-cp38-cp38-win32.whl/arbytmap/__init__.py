1c1be5f8046014c5740fca1e1914b3f60cca9903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:49 +0000

    Add package arbytmap/1.1.1/arbytmap-1.1.1-cp38-cp38-win32.whl

---
+++ packages/arbytmap/arbytmap-1.1.1-cp38-cp38-win32.whl/arbytmap/__init__.py
@@ -0,0 +1,16 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla"
+#           YYYY.MM.DD
+__date__ = "2020.02.14"
+__version__ = (1, 1, 1)
+
+from arbytmap import constants
+from arbytmap.arby import *
+from arbytmap import bitmap_io, constants, dds_defs, ext,\
+     format_defs, swizzler
+
+
+__all__ = ("bitmap_io", "constants", "dds_defs", "ext",
+           "format_defs", "swizzler")
