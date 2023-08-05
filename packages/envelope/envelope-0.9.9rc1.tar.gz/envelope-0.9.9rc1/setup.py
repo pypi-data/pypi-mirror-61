d5a2030b1e07f1bedc872d6512a333b61bad1e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package envelope/0.9.9rc1/envelope-0.9.9rc1.tar.gz

---
+++ packages/envelope/envelope-0.9.9rc1.tar.gz/envelope-0.9.9rc1/setup.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from setuptools import setup
+
+# using the same libraries in requirements.txt because after many articles I didn't understand any good reason why I shouldn't
+requirements = ""
+p = Path("requirements.txt")
+if p.exists():  # stand-alone install
+    requirements = p.read_text()
+else:  # PyPi install
+    p = Path("envelope.egg-info/requires.txt")
+    if p.exists():
+        requirements = p.read_text()
+
+# load long description
+p = Path("README.md")
+if p.exists():
+    long_description = p.read_text()
+
+setup(
+    name='envelope',
+    version='0.9.9rc1',
+    packages=['envelope'],
+    author='Edvard Rejthar',
+    author_email='edvard.rejthar@nic.cz',
+    url='https://github.com/CZ-NIC/envelope',
+    license='GNU GPLv3',
+    description='Insert a message and attachments and send e-mail / sign / encrypt contents by a single line.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[requirements.split("\n")],
+    extras_require={
+        "smime": ["M2Crypto"]
+    },
+    entry_points={
+        'console_scripts': [
+            'envelope=envelope:_cli',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
