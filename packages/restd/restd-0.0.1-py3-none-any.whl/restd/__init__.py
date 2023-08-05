3d734678b6b06b9972458c526466641233e179bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:28:27 +0000

    Add package restd/0.0.1/restd-0.0.1-py3-none-any.whl

---
+++ packages/restd/restd-0.0.1-py3-none-any.whl/restd/__init__.py
@@ -0,0 +1,9 @@
+"""
+restd is a REST service that routes incoming data to plugins.
+The goal is to provide a lightweight, flexible, and extensible
+REST framework manged by service configs
+"""
+import os
+from . import restd
+
+name = "restd"
