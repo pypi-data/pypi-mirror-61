1365bf6618e22a22938848e5bd03474916ea9144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:22 +0000

    Add package django-initial-setup/0.0.6/django-initial-setup-0.0.6.tar.gz

---
+++ packages/django-initial-setup/django-initial-setup-0.0.6.tar.gz/django-initial-setup-0.0.6/first_setup/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from first_setup.views import (
+    WelcomeView,
+    InitialSetupView,
+    DatabaseView
+)
+
+
+urlpatterns = [
+    path('initial_user_setup/welcome', WelcomeView.as_view(), name='welcome'),
+    path('initial_user_setup/migration', DatabaseView.as_view(), name='migration'),
+    path('initial_user_setup/superuser', InitialSetupView.as_view(), name='superuser'),
+    path('initial_user_setup/confirm', InitialSetupView.as_view(), name='confirm'),
+]
---
+++ packages/django-initial-setup/django-initial-setup-0.0.6.tar.gz/django-initial-setup-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from first_setup.version import version
+setup(
+    name='django-initial-setup',
+    version=version,
+    description='Django Initial Setup provides inital setup screen developed with Django for first launch.',
+    long_description=open('README.md').read(),
+    author='Shota Shimazu',
+    author_email='hornet.live.mf@gmail.com',
+    url='https://github.com/shotastage/django-initial-setup',
+    download_url='https://pypi.python.org/pypi/django-initial-setup',
+    license='MIT',
+    packages=find_packages(exclude=('tests')),
+    install_requires=[
+        'django>=2.0',
+    ],
+    extras_require={
+    },
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+)
