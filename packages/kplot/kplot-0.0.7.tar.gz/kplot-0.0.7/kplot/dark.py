b5fab1849cfaf000ac10877f923513baca0d308a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:08 +0000

    Add package kplot/0.0.7/kplot-0.0.7.tar.gz

---
+++ packages/kplot/kplot-0.0.7.tar.gz/kplot-0.0.7/kplot/dark.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return str(os.path.join(root, name))
+            
+            
+dark = find('dark.mplstyle', dir_path)
+print(dark)
+plt.style.use(dark)
+plt.plot([1,2],[1,2])
+plt.show()
---
+++ packages/kplot/kplot-0.0.7.tar.gz/kplot-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kplot",
+    version="0.0.7",
+    author="Keyan Gootkin",
+    author_email="KeyanGootkin@gmail.com",
+    description="A small package for making matplotlib plots pretty <3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KeyanGootkin/kplot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
