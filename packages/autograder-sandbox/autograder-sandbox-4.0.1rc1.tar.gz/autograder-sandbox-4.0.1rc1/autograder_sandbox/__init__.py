db48e86444353c1d6c23005bffb72bf8e373a685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:34 +0000

    Add package autograder-sandbox/4.0.1rc1/autograder-sandbox-4.0.1rc1.tar.gz

---
+++ packages/autograder-sandbox/autograder-sandbox-4.0.1rc1.tar.gz/autograder-sandbox-4.0.1rc1/autograder_sandbox/__init__.py
@@ -0,0 +1,9 @@
+from .autograder_sandbox import (
+    AutograderSandbox,
+    CompletedCommand,
+    SandboxCommandError,
+    VERSION,
+    SANDBOX_USERNAME,
+    SANDBOX_HOME_DIR_NAME,
+    SANDBOX_DOCKER_IMAGE
+)
---
+++ packages/autograder-sandbox/autograder-sandbox-4.0.1rc1.tar.gz/autograder-sandbox-4.0.1rc1/setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+from setuptools import setup  # type: ignore
+import autograder_sandbox
+
+setup(name='autograder-sandbox',
+      version=autograder_sandbox.VERSION,
+      description=('Python library for running untrusted '
+                   'code in Docker containers'),
+      author='James Perretta',
+      author_email='jameslp@umich.edu',
+      packages=["autograder_sandbox"],
+      include_package_data=True,
+      url='https://github.com/eecs280staff/autograder-sandbox',
+      license='GNU Lesser General Public License v3',
+      install_requires=['redis>=2.10.5'],
+      classifiers=['Programming Language :: Python :: 3.5'])
