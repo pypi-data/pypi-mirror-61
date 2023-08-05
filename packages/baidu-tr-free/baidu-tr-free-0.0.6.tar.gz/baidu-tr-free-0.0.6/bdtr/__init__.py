a222a417a6b224f95582539bf8ce1e7904e8560b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:01 +0000

    Add package baidu-tr-free/0.0.6/baidu-tr-free-0.0.6.tar.gz

---
+++ packages/baidu-tr-free/baidu-tr-free-0.0.6.tar.gz/baidu-tr-free-0.0.6/bdtr/__init__.py
@@ -0,0 +1,7 @@
+from .bdtr import bdtr, TOKEN, BAIDUID, GTK
+from .js2py_sign import js2py_sign
+
+# version__ = '0.0.4'
+# version__ = '0.0.5'  # 2019-11-21
+__version__ = '0.0.6'  # 2020-02-11
+VERSION = tuple(__version__.split('.')
