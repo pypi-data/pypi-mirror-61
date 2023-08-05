f06a615ea926a788dc96681fa5566afb28ef2fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:32 +0000

    Add package KinetiKit/0.1.1/KinetiKit-0.1.1.tar.gz

---
+++ packages/KinetiKit/KinetiKit-0.1.1.tar.gz/KinetiKit-0.1.1/setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="KinetiKit",
+    packages=find_packages(exclude=("tests", "tests.*")),
+    python_requires=">=3.6",
+    install_requires=[
+        "matplotlib>=3.0",
+        "numpy>=1.15.0",
+        "scipy",
+    ],
+    version='0.1.1',
+    description="Tools for comparing and kinetically simulating time-resolved data",
+)
