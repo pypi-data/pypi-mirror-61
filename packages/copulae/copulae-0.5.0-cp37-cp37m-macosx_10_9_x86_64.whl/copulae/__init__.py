3f0d7e413774eb950f0cfa942073887f48e011fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package copulae/0.5.0/copulae-0.5.0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/copulae/copulae-0.5.0-cp37-cp37m-macosx_10_9_x86_64.whl/copulae/__init__.py
@@ -0,0 +1,13 @@
+from copulae._version import get_versions
+from copulae.archimedean import *
+from copulae.elliptical import *
+from copulae.empirical import *
+from copulae.indep import *
+
+__version__ = get_versions()['version']
+del get_versions
+
+
+def doc():
+    import webbrowser
+    webbrowser.open('https://copulae.readthedocs.io/en/latest')
