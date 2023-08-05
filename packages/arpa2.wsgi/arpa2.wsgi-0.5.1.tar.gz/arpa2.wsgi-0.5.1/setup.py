0406f7c27b44d2b584f65ce4b249b16ca5d36b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:58 +0000

    Add package arpa2.wsgi/0.5.1/arpa2.wsgi-0.5.1.tar.gz

---
+++ packages/arpa2.wsgi/arpa2.wsgi-0.5.1.tar.gz/arpa2.wsgi-0.5.1/setup.py
@@ -0,0 +1,57 @@
+# setup.py for ARPA2 WSGI Middleware
+#
+# Package name "arpa2wsgi"
+#
+# From: Rick van Rein <rick@openfortress.nl>
+
+import setuptools
+from os import path
+
+#
+# Preparation
+#
+here = path.dirname (path.realpath (__file__))
+
+
+#
+# Packaging Instructions -- arpa2wsgi
+#
+readme = open (path.join (here, 'README.MD')).read ()
+setuptools.setup (
+
+	# What?
+	name = 'arpa2.wsgi',
+	version = '0.5.1',
+	url = 'https://github.com/arpa2/wsgi-middleware',
+	description = 'WSGI Middleware to "Bring Your Own IDentity" to a web server',
+	long_description = readme,
+	long_description_content_type = 'text/markdown',
+
+	# Who?
+	author = 'Rick van Rein (ARPA2 developer)',
+	author_email = 'rick@openfortress.nl',
+
+	# Where?
+	namespace_packages = [
+		'arpa2',
+		'arpa2.wsgi',
+	],
+	packages = [
+		'arpa2',
+		'arpa2.wsgi',
+		'arpa2.wsgi.byoid',
+	],
+	package_dir = {
+		'arpa2'           : path.join (here,'arpa2'),
+		'arpa2.wsgi'      : path.join (here,'arpa2','wsgi'),
+		'arpa2.wsgi.byoid': path.join (here,'arpa2','wsgi','byoid'),
+	},
+
+	# How?
+	# entry_points = ... Not for WSGI, right?
+
+        # Requirements
+        # install_requires = [ ... ],
+
+)
+
