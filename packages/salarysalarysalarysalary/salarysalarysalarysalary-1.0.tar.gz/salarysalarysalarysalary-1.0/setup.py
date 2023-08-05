b7d40452d2242d7ce4831db4497eb3ea09f4f5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:10 +0000

    Add package salarysalarysalarysalary/1.0/salarysalarysalarysalary-1.0.tar.gz

---
+++ packages/salarysalarysalarysalary/salarysalarysalarysalary-1.0.tar.gz/salarysalarysalarysalary-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name='salarysalarysalarysalary',
+version='1.0',
+description='just test',
+author='swh',
+author_email='8888888@qq.com',
+py_modules=['salarysalarysalarysalary'],
+url='http://baidu.com'
+)
