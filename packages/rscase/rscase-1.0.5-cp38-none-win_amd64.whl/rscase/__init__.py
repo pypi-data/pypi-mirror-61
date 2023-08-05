ac7f2d35a70cc5125a1424d7bb719ad556ef5fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:43 +0000

    Add package rscase/1.0.5/rscase-1.0.5-cp38-none-win_amd64.whl

---
+++ packages/rscase/rscase-1.0.5-cp38-none-win_amd64.whl/rscase/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+rsCase is a simple python package implemented in Rust for string case translation.
+"""
+
+
+__author__ = "Sondre Lillebø Gundersen <sondrelg@live.no>"
+__version__ = "1.0.5"
+__all__ = ["rscase"]
