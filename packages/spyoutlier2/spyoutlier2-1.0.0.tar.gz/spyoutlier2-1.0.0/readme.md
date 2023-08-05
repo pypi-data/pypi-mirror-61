aa1d736a26f2c9455eeb022d70643eb0ebe98657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:14 +0000

    Add package spyoutlier2/1.0.0/spyoutlier2-1.0.0.tar.gz

---
+++ packages/spyoutlier2/spyoutlier2-1.0.0.tar.gz/spyoutlier2-1.0.0/readme.md
@@ -0,0 +1,9 @@
+TOPSIS
+A Python package to get outlier result
+
+Usage
+Following query on terminal will provide you the topsis detail.
+Usages:
+ python outlier.py
+ 
+Example: python outlier.py myData.csv a.txt
---
+++ packages/spyoutlier2/spyoutlier2-1.0.0.tar.gz/spyoutlier2-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="spyoutlier2",
+    version="1.0.0",
+    description="'depicting no of rows removed by outlier row removal method'",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/manavsharma136",
+    author="Manav Sharma",
+    author_email="manavsharma136@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["outlier"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "spyoutlier2==outlier.outlier:main",
+        ]
+    },
+)
