1cce4655d5e107918ff854406c301c70a47ab594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package google-sign/0.0.2/google_sign-0.0.2-py3-none-any.whl

---
+++ packages/google-sign/google_sign-0.0.2-py3-none-any.whl/google_sign/__init__.py
@@ -0,0 +1,8 @@
+from .google_sign import google_sign, get_tkk, TKK
+from .js2py_sign import js2py_sign
+
+GTK = '320305.131321201'  # for signing baidu
+
+# version__ = '0.0.1'  # '2020.1.31'
+__version__ = '0.0.2'
+VERSION = tuple(__version__.split('.'))
