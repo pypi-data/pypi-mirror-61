0502f03af09c3cf96472f08e4abd8bcee8bfcbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package fa-common/0.2.1/fa-common-0.2.1.tar.gz

---
+++ packages/fa-common/fa-common-0.2.1.tar.gz/fa-common-0.2.1/README.md
@@ -0,0 +1,7 @@
+# FastAPI Common Framework
+
+## Version: 0.2.1
+
+This is the common API framework used by the CSIRO geoanalytics software team. Not currently designed for use outside of the CSIRO geoanalytics team.
+
+Code and concepts borrow heavily from [FastAPI Contrib](https://pypi.org/project/fastapi-contrib/) I suggest you check them out if you are looking for a user friendly open source FastAPI Utility framework.
---
+++ packages/fa-common/fa-common-0.2.1.tar.gz/fa-common-0.2.1/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fa-common",
+    description="FastAPI Common Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.2.1",
+    packages=["fa_common"],
+    maintainer="Samuel Bradley",
+    maintainer_email="sam.bradley@csiro.au",
+    python_requires=">=3.5, <4",
+    install_requires=[
+        "pyjwt",
+        "loguru",
+        "python-multipart",
+        "email-validator",
+        "six",
+        "fastapi",
+        "aiohttp",
+    ],
+    extras_require={
+        "sentry": ["sentry_sdk"],
+        "mongo": ["motor", "pymongo"],
+        "gcp": [
+            "google-cloud-storage",
+            "google-cloud-logging",
+            "firebase-admin",
+            "requests",
+        ],
+        "minio": ["minio"],
+        "windows": ["win32-setctime"],
+    },
+    include_package_data=True,
+    license="MIT",
+    url="https://gitlab.com/csiro-geoanalytics/python-shared/fastapi-common-framework",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Science/Research",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
