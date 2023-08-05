51a24642a36fb22e1117e9c635d9b0fa41b1ec10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:52 +0000

    Add package imgviz/0.11.1/imgviz-0.11.1.tar.gz

---
+++ packages/imgviz/imgviz-0.11.1.tar.gz/imgviz-0.11.1/imgviz/__init__.py
@@ -0,0 +1,37 @@
+# flake8: noqa
+
+__version__ = '0.11.1'
+
+# -----------------------------------------------------------------------------
+
+from . import data
+
+from . import draw
+
+from . import _io as io
+
+# -----------------------------------------------------------------------------
+
+# visualization targets
+from .color import asgray
+from .color import gray2rgb
+from .color import rgb2gray
+from .color import rgb2rgba
+from .color import rgb2hsv
+from .color import rgba2rgb
+from .color import hsv2rgb
+from .depth import Depth2RGB
+from .depth import depth2rgb
+from .flow import flow2rgb
+from .instances import instances2rgb
+from .label import label_colormap
+from .label import label2rgb
+from .nchannel import Nchannel2RGB
+from .nchannel import nchannel2rgb
+from .trajectory import plot_trajectory
+
+# visualization operations
+from .centerize import centerize
+from .normalize import normalize
+from .resize import resize
+from .tile import tile
