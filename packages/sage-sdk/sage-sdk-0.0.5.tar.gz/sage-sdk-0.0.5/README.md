312747de269d150842c54ff8aa4a76e96c6ba822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package sage-sdk/0.0.5/sage-sdk-0.0.5.tar.gz

---
+++ packages/sage-sdk/sage-sdk-0.0.5.tar.gz/sage-sdk-0.0.5/README.md
@@ -0,0 +1,7 @@
+# sage-sdk
+
+
+install this from internal PYPI.
+
+请使用内部 PyPi 安装。
+
---
+++ packages/sage-sdk/sage-sdk-0.0.5.tar.gz/sage-sdk-0.0.5/setup.py
@@ -0,0 +1,38 @@
+# -*-coding:utf-8-*-
+import io
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class InstallWrapper(install):
+
+    def run(self):
+        raise Exception("using internal PyPi to install sage-sdk, 请使用内网 PyPi 安装。")
+
+
+with io.open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sage-sdk',
+    version='0.0.5',
+    description="sage sdk",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'future',
+        'python-dateutil',
+        'wrapt',
+        'pandas>=0.22.0',
+        'numpy>=1.14.2',
+        'enum34',
+        'tzlocal',
+        'Flask',
+    ],
+    cmdclass={
+        'install': InstallWrapper
+    },
+    python_requires='>=3.7',
+)
