9ec8356a50fab822d2f1cd083a2eb03023e359dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:47 +0000

    Add package brainlit/0.0.1/brainlit-0.0.1.tar.gz

---
+++ packages/brainlit/brainlit-0.0.1.tar.gz/brainlit-0.0.1/brainlit/__init__.py
@@ -0,0 +1,13 @@
+import warnings
+
+import brainlit.algorithms
+import brainlit.plot
+import brainlit.preprocessing
+import brainlit.utils
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+warnings.simplefilter("always", category=UserWarning)
+
+
+__version__ = "0.0.1"
