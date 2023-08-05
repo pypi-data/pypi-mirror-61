4e15e67abf2ab960f3edf0a1e47f2046e4e4b917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:53 +0000

    Add package snnw/0.2.1/snnw-0.2.1-py2.py3-none-any.whl

---
+++ packages/snnw/snnw-0.2.1-py2.py3-none-any.whl/snnw/train.py
@@ -0,0 +1,17 @@
+import os
+
+import numpy as np
+
+from snnw.nn.model import Model
+from snnw.nn.config.load_config import load_config
+
+
+def train(model_dir, config_path, image_path, label_path, steps=60000, learning_rate=5e-4):
+
+    model_config = load_config(os.path.realpath(config_path))
+
+    train_images = np.load(os.path.realpath(image_path))
+    train_labels = np.load(os.path.realpath(label_path))
+
+    train_model = Model(x_input=train_images, y_true=train_labels, model_config=model_config)
+    train_model.train(steps=steps, learning_rate=learning_rate, model_dir=model_dir)
