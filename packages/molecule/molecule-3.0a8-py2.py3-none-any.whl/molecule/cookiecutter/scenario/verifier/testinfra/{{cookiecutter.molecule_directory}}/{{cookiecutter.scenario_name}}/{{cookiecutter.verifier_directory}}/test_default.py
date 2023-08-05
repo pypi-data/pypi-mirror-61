9ac9c71905739ab7595f6593bf531fb8b50688a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:05 +0000

    Add package molecule/3.0a8/molecule-3.0a8-py2.py3-none-any.whl

---
+++ packages/molecule/molecule-3.0a8-py2.py3-none-any.whl/molecule/cookiecutter/scenario/verifier/testinfra/{{cookiecutter.molecule_directory}}/{{cookiecutter.scenario_name}}/{{cookiecutter.verifier_directory}}/test_default.py
@@ -0,0 +1,17 @@
+"""Role testing files using testinfra."""
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
