dfd958021918fd2523c45385eec32eb859e863ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:08 +0000

    Add package pyorphans/0.2.2/pyorphans-0.2.2.tar.gz

---
+++ packages/pyorphans/pyorphans-0.2.2.tar.gz/pyorphans-0.2.2/pyorphans/cli.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import sys
+
+from .utils import find_orphan_dirs
+
+
+CTRL_RED = '\033[91m'
+CTRL_END = '\033[0m'
+
+
+def main(*root_dirs):
+    """
+    Main method for command-line execution. Prints details to stdout.
+
+    Args:
+        *root_dirs (str): paths to root packages of project to scan
+    """
+    orphan_dir = None
+
+    for root_dir in root_dirs:
+        orphans_generator = find_orphan_dirs(root_dir)
+        while True:
+            try:
+                orphan_dir, orphan_files = next(orphans_generator)
+            except StopIteration:
+                break
+
+            print(orphan_dir)
+            for orphan in orphan_files:
+                print('-> %s' % orphan)
+            if orphan_files:
+                print('')
+
+    if orphan_dir is None:
+        # no orphans found!
+        sys.exit(0)
+    else:
+        # orphans found
+        sys.exit(
+            CTRL_RED +
+            'If these are valid standalone python scripts, add their '
+            'parent dirs to the .pyorphans_ignore file.' +
+            CTRL_END
+        )
+
+
+def entrypoint():
+    """
+    Entrypoint for command-line execution. Parses args.
+    """
+    main(*sys.argv[1:])
