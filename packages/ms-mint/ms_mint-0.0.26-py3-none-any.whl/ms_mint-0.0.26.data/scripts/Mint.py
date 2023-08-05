6fda2b03a9f3adf6488f2e723b153e380df1f199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:47 +0000

    Add package ms-mint/0.0.26/ms_mint-0.0.26-py3-none-any.whl

---
+++ packages/ms-mint/ms_mint-0.0.26-py3-none-any.whl/ms_mint-0.0.26.data/scripts/Mint.py
@@ -0,0 +1,33 @@
+#!python
+
+import sys
+import pandas as pd
+from os.path import isfile
+from glob import glob
+
+from ms_mint.dash_gui import app, mint
+
+if __name__ == '__main__':
+    
+    args = sys.argv
+    
+    if '--debug' in args:
+        DEBUG = True
+        mint.verbose = True
+        mint.peaklist_files = ['/home/swacker/data/metabolomics_storage/MINT/MINT_peaklists/MINT-peaklist__TR-13-biomarkers.csv']
+        mint.files = glob('/data/metabolomics_storage/MINT/MINT_demofiles/**/*.mzXML', recursive=True)
+        print('MINT files:', mint.files)
+        print('MINTegration list:')
+        print(mint.peaklist.to_string())
+    else:
+        DEBUG = False
+
+    if '--data' in args:
+        if isfile('/tmp/mint_results.csv'):
+            mint._results = pd.read_csv('/tmp/mint_results.csv')
+        for i in mint.files:
+            assert isfile(i)
+        for i in mint.peaklist_files:
+            assert isfile(i)
+
+    app.run_server(debug=DEBUG, port=9999)
