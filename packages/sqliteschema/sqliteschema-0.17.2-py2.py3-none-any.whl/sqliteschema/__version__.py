f264899161258891d564d150438944db08b35292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:55 +0000

    Add package sqliteschema/0.17.2/sqliteschema-0.17.2-py2.py3-none-any.whl

---
+++ packages/sqliteschema/sqliteschema-0.17.2-py2.py3-none-any.whl/sqliteschema/__version__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+
+__author__ = "Tsuyoshi Hombashi"
+__copyright__ = "Copyright 2016, {}".format(__author__)
+__license__ = "MIT License"
+__version__ = "0.17.2"
+__maintainer__ = __author__
+__email__ = "tsuyoshi.hombashi@gmail.com"
