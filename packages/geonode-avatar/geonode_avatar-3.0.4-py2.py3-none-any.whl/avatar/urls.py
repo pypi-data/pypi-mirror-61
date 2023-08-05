e5defd68d150b00704ee339ed8d6081c41a4a816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:47 +0000

    Add package geonode-avatar/3.0.4/geonode_avatar-3.0.4-py2.py3-none-any.whl

---
+++ packages/geonode-avatar/geonode_avatar-3.0.4-py2.py3-none-any.whl/avatar/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+
+from . import views
+
+urlpatterns = [  # 'avatar.views',
+    url('^add/$', views.add, name='avatar_add'),
+    url('^change/$', views.change, name='avatar_change'),
+    url('^delete/$', views.delete, name='avatar_delete'),
+    url('^render_primary/(?P<user>[\+\w]+)/(?P<size>[\d]+)/$', views.render_primary, name='avatar_render_primary'),
+    url('^comments/', include('django_comments.urls')),
+]
