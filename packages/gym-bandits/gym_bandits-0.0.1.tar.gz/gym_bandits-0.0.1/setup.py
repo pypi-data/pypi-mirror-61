ceaab44446933d7fdcf5ec7017b81ba3835d41ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:12 +0000

    Add package gym-bandits/0.0.1/gym_bandits-0.0.1.tar.gz

---
+++ packages/gym-bandits/gym_bandits-0.0.1.tar.gz/gym_bandits-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='gym_bandits',
+      version='0.0.1',
+      author='Per Mattsson',
+      author_email='magni84@gmail.com',
+      description='Implements multi-armed bandits',
+      url='https://github.com/magni84/gym_bandits',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+      install_requires=['gym', 'numpy']
+)
