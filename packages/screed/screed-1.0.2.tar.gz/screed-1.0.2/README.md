93333716b23e730cb546618b9d87dae6009ee257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package screed/1.0.2/screed-1.0.2.tar.gz

---
+++ packages/screed/screed-1.0.2.tar.gz/screed-1.0.2/README.md
@@ -0,0 +1,11 @@
+screed -- short read sequence utils in Python.
+
+The official central repository for screed is:
+
+   https://github.com/dib-lab/screed
+
+See http://readthedocs.org/docs/screed/en/latest/ for docs.
+
+Issues are tracked at https://github.com/dib-lab/khmer/issues.
+
+[![Build Status](https://travis-ci.org/dib-lab/screed.svg?branch=master)](https://travis-ci.org/dib-lab/screed)
---
+++ packages/screed/screed-1.0.2.tar.gz/screed-1.0.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright (c) 2016, The Regents of the University of California.
+from __future__ import print_function
+try:
+    from setuptools import setup
+except ImportError:
+    print('(WARNING: importing distutils, not setuptools!)')
+    from distutils.core import setup
+
+import versioneer
+
+
+setup(name='screed',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description_file="README.md",
+      long_description_content_type="text/markdown",
+      author='Luiz Irber, Peter Cock, Michael R. Crusoe, Jacob Fenton, '
+             'Thomas Fenzl, Sarah Guermond, Tim Head, Mark Mandel, '
+             'Kevin D. Murray, Alexander Nolley, Camille Scott, '
+             'Daniel Standage, Benjamin R. Taylor, Michael Wright, en zyme, '
+             'C. Titus Brown',
+      author_email='ctbrown@ucdavis.edu',
+      url='http://github.com/dib-lab/screed/',
+      zip_safe=False,
+      include_package_data=True,
+      packages=['screed', 'screed.tests'],
+      package_data={
+          'screed.tests': ['test.*', 'test-whitespace.fa', 'empty.fa']},
+      license='BSD',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest >= 3.0', 'pytest-cov'],
+      install_requires=['bz2file'],
+      entry_points={'console_scripts': [
+          'screed = screed.__main__:main'
+          ]
+      })
