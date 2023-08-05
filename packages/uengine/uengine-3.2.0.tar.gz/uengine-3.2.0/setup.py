651a49fb1df8b7bde372865ee190b2ca24ce8eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package uengine/3.2.0/uengine-3.2.0.tar.gz

---
+++ packages/uengine/uengine-3.2.0.tar.gz/uengine-3.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="uengine",
+    version="3.2.0",
+    description="a micro webframework based on flask and pymongo",
+    url="https://github.com/viert/uengine",
+    author="Pavel Vorobyov",
+    author_email="aquavitale@yandex.ru",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "uengine": ["*.txt", "*,py"]
+    },
+    install_requires=[
+        "jinja2",
+        "flask",
+        "pymongo",
+        "lazy_object_proxy",
+        "ipython",
+        "pylint",
+        "mongomock",
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "uengine=uengine.__main__:main",
+        ]
+    }
+)
