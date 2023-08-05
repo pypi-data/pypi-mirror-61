6f97941b583ae095dccf64e6788370955f9aab45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:20 +0000

    Add package mambaex/0.0.1a1/mambaex-0.0.1a1-py2.py3-none-any.whl

---
+++ packages/mambaex/mambaex-0.0.1a1-py2.py3-none-any.whl/mambaex/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'mambaex'
+__description__ = 'Python HTTP Server for Humans.'
+__url__ = 'https://mambaex.readthedocs.io'
+__version__ = '0.0.1-alpha.1'
+__build__ = 0x022200
+__author__ = 'Prashant Farkya'
+__author_email__ = 'prashant.farkya@gmail.com'
+__license__ = 'MIT Licence'
+__copyright__ = 'Copyright 2020 Prashant Farkya'
+__cake__ = u'\u2728 \U0001f370 \u2728'
---
+++ packages/mambaex/mambaex-0.0.1a1-py2.py3-none-any.whl/mambaex/api.py
@@ -0,0 +1,12 @@
+def getServer(name):
+    """
+    Returns a server intance
+
+    :param string name: Name of the server
+    :return: server instance of it.
+
+    :example:
+        >>> xyzServer = getServer('XYZ')
+
+    """
+    return name
