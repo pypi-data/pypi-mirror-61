e331470b71cd648dbeede3e25218a84d04584a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package pyvpc/0.1.3/pyvpc-0.1.3.tar.gz

---
+++ packages/pyvpc/pyvpc-0.1.3.tar.gz/pyvpc-0.1.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+keywords = ['Networking', 'IPv4', 'IP', 'CIDR', 'VPC', 'AWS', 'Subnet', 'Range', 'Network Suggestion']
+
+setuptools.setup(
+    name="pyvpc",
+    version="0.1.3",
+    author="Arie Lev",
+    author_email="levinsonarie@gmail.com",
+    description="Python AWS VPC CIDR available range finder with sub networks",
+    keywords=keywords,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArieLevs/PyVPC",
+    license='Apache License 2.0',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'argparse==1.4.0',
+        'boto3==1.11.9'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={
+        'console_scripts': [
+            'pyvpc = pyvpc.pyvpc:main'
+        ],
+    },
+)
