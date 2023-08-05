686ecfab4aa05b795308b6a90f26aed3decbe44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:12 +0000

    Add package thoth-messaging/0.3.1/thoth_messaging-0.3.1-py3-none-any.whl

---
+++ packages/thoth-messaging/thoth_messaging-0.3.1-py3-none-any.whl/thoth/messaging/package_releases.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# thoth-messaging
+# Copyright(C) 2019, 2020 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+"""This is Thoth Messaging module."""
+
+
+import faust
+
+
+THOTH_PACKAGE_RELEASES_TOPIC_NAME = "thoth_package_releases"
+
+
+class PackageRelease(faust.Record, serializer="json"):
+    """Class used for Package Release events on Kafka topic."""
+
+    index_url: str
+    package_name: str
+    package_version: str
