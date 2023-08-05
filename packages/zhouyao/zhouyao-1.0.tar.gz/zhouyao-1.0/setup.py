19af21fe214609b06acd5c867a4018cbbf0f5af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:00 +0000

    Add package zhouyao/1.0/zhouyao-1.0.tar.gz

---
+++ packages/zhouyao/zhouyao-1.0.tar.gz/zhouyao-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='zhouyao', # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，测试哦', #描述
+    author='zhouyao', # 作者
+    author_email='734800275@qq.com',
+    py_modules=['zhou.aa'] # 要发布的模块
+)
---
+++ packages/zhouyao/zhouyao-1.0.tar.gz/zhouyao-1.0/zhou/aa.py
@@ -0,0 +1,11 @@
+import turtle
+
+def aa_func():
+    t = turtle.Pen()
+    # explain
+    ''' explain more '''
+    for x in range(360):
+        t.forward(x)
+        t.left(59)
+        if x == 20:
+            break
