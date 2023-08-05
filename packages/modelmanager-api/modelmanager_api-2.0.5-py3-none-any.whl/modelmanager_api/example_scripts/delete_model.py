0396aaf67ed775b682faa2237a86ba413e84958b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:41 +0000

    Add package modelmanager-api/2.0.5/modelmanager_api-2.0.5-py3-none-any.whl

---
+++ packages/modelmanager-api/modelmanager_api-2.0.5-py3-none-any.whl/modelmanager_api/example_scripts/delete_model.py
@@ -0,0 +1,12 @@
+from modelmanager_api.modelmanager_api import Model
+
+secret_key = "Secret-Key"
+
+url = 'URL' #enter url
+model_id = 'Model_id' #use model id
+
+try:
+    Model(secret_key,url).delete_model(model_id)
+except ConnectionResetError as e:
+    print(str(e))
+
