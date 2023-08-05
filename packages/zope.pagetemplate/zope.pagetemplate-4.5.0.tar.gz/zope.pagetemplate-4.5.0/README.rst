e44392c6e0255c4bc7b5725ad17d766cf471b626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package zope.pagetemplate/4.5.0/zope.pagetemplate-4.5.0.tar.gz

---
+++ packages/zope.pagetemplate/zope.pagetemplate-4.5.0.tar.gz/zope.pagetemplate-4.5.0/README.rst
@@ -0,0 +1,36 @@
+===================
+ zope.pagetemplate
+===================
+
+.. image:: https://img.shields.io/pypi/v/zope.pagetemplate.svg
+        :target: https://pypi.python.org/pypi/zope.pagetemplate/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.pagetemplate.svg
+        :target: https://pypi.org/project/zope.pagetemplate/
+        :alt: Supported Python versions
+
+.. image:: https://travis-ci.org/zopefoundation/zope.pagetemplate.svg?branch=master
+        :target: https://travis-ci.org/zopefoundation/zope.pagetemplate
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.pagetemplate/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.pagetemplate?branch=master
+
+.. image:: https://readthedocs.org/projects/zopepagetemplate/badge/?version=latest
+        :target: https://zopepagetemplate.readthedocs.org/en/latest/
+        :alt: Documentation Status
+
+
+Page Templates provide an elegant templating mechanism that achieves a
+clean separation of presentation and application logic while allowing
+for designers to work with templates in their visual editing tools
+(FrontPage, Dreamweaver, GoLive, etc.).
+
+Page Templates are based on `a Template Attribute Language
+<https://pypi.python.org/pypi/zope.tal>`_ with expressions provided by
+`TALES <https://pypi.python.org/pypi/zope.tales>`_. For a description
+of their syntax, see `the reference documentation
+<https://pagetemplates.readthedocs.io/en/latest/>`_.
+
+For detailed documentation on the usage of this package, see
+https://zopepagetemplate.readthedocs.io
---
+++ packages/zope.pagetemplate/zope.pagetemplate-4.5.0.tar.gz/zope.pagetemplate-4.5.0/setup.py
@@ -0,0 +1,102 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.pagetemplate package
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+TESTS_REQUIRE = [
+    'zope.proxy',
+    'zope.security',
+    'zope.testing',
+    'zope.testrunner',
+]
+
+
+setup(name='zope.pagetemplate',
+      version='4.5.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Zope Page Templates',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')),
+      keywords="zope3 page template",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='https://github.com/zopefoundation/zope.pagetemplate',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      extras_require={
+          'test': TESTS_REQUIRE,
+          'test:python_version == "2.7"': [
+              'zope.untrustedpython',
+          ],
+          'untrusted': [
+          ],
+          'untrusted:python_version == "2.7"': [
+              'zope.untrustedpython',
+          ],
+          'docs': [
+              'Sphinx',
+              'repoze.sphinx.autointerface',
+          ],
+      },
+      install_requires=[
+          'setuptools',
+          'six',
+          'zope.interface',
+          'zope.component',
+          'zope.tales',
+          'zope.tal >= 4.2.0',
+          'zope.i18n >= 4.0.1',
+          'zope.i18nmessageid',
+          'zope.traversing',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=TESTS_REQUIRE,
+)
