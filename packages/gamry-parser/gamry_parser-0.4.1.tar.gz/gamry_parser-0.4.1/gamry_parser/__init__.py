10ca8bb5ba2ac853ac9fb52ef6308e9004dd6fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package gamry-parser/0.4.1/gamry_parser-0.4.1.tar.gz

---
+++ packages/gamry-parser/gamry_parser-0.4.1.tar.gz/gamry_parser-0.4.1/gamry_parser/__init__.py
@@ -0,0 +1,6 @@
+from .version import __version__
+from .gamryparser import GamryParser
+from .chronoa import ChronoAmperometry
+from .cv import CyclicVoltammetry
+from .eispot import Impedance
+from .ocp import OpenCircuitPotential
