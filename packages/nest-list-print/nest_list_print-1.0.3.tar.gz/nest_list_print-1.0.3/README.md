69936408523c38559a6f29a3490ee9d7a8d3efd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:44 +0000

    Add package nest-list-print/1.0.3/nest_list_print-1.0.3.tar.gz

---
+++ packages/nest-list-print/nest_list_print-1.0.3.tar.gz/nest_list_print-1.0.3/README.md
@@ -0,0 +1,15 @@
+# Print Nesting or Normal Lists # 
+
+This module provides a function named "print_lol" to print out each independent
+list item within a nesting or normal *Python* list.
+
+## Revision History
+
+|  Revision | Description                                 |
+| :-------: | :------------------------------------------ |
+| 1.0.0     | Initial Release                             |
+| 1.0.1     | Add README.md                               |
+| 1.0.2     | Add Markdown Support                        |
+| 1.0.3     | Fix SetupTools Markdown Support Bug         |
+
+> **NOTE**: This is a non-commercial package for just learning purpose.
---
+++ packages/nest-list-print/nest_list_print-1.0.3.tar.gz/nest_list_print-1.0.3/setup.py
@@ -0,0 +1,30 @@
+from os import path as os_path
+from setuptools import setup
+
+this_directory = os_path.abspath(os_path.dirname(__file__))
+
+
+def read_file(filename):
+    """
+        Read File Contents
+        Args:
+            filename - Specified File to be read.
+    """
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    name='nest_list_print',
+    version='1.0.3',
+    py_modules=['nest_list_print'],
+    author='Wancheng Zhou',
+    author_email='wancheng2012@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    long_description=read_file('README.md'),
+    long_description_content_type="text/markdown",  # Project README
+    data_files=['./README.md'],                     # Data file specification
+    license="MIT",
+)
