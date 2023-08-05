5bd452e236be154cbaa999772e2315354d838172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:29 +0000

    Add package ypackage/2.6.5/ypackage-2.6.5.tar.gz

---
+++ packages/ypackage/ypackage-2.6.5.tar.gz/ypackage-2.6.5/ypackage/regex.py
@@ -0,0 +1,8 @@
+def words_regex(*words):
+    """Kelime arayan regex döndürme
+    """
+    pattern = []
+    for word in words:
+        pattern.append(r"\b" + word)
+
+    return "|".join(pattern)
