5cb386822aa0830775245a6dd70e55b955f83ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:28 +0000

    Add package nfem/1.7.2/nfem-1.7.2.tar.gz

---
+++ packages/nfem/nfem-1.7.2.tar.gz/nfem-1.7.2/nfem/visualization/__init__.py
@@ -0,0 +1,3 @@
+from .interactive import interact #needs to be imported first, because it sets the matplotlib backend
+from .plot import show_load_displacement_curve, show_animation, show_deformation_plot
+from .plot import Plot2D, Animation3D, DeformationPlot3D
