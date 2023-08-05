68713885214df68dbca86af6e9e93de02bee46f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:30 +0000

    Add package pept/0.1.6/pept-0.1.6-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/pept/pept-0.1.6-cp37-cp37m-macosx_10_14_x86_64.whl/pept/scanners/parallel_screens/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# File   : __init__.py
+# License: License: GNU v3.0
+# Author : Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
+# Date   : 20.08.2019
+
+
+from    .parallel_screens   import  ParallelScreens
+from    .parallel_screens   import  ParallelScreensArray
+
+__all__ = [
+    'ParallelScreens',
+    'ParallelScreensArray'
+]
+
+
+__author__ =        "Andrei Leonard Nicusan"
+__credits__ =       ["Andrei Leonard Nicusan", "Kit Windows-Yule", "Sam Manger"]
+__license__ =       "GNU v3.0"
+__maintainer__ =    "Andrei Leonard Nicusan"
+__email__ =         "a.l.nicusan@bham.ac.uk"
+__status__ =        "Development"
+
+
