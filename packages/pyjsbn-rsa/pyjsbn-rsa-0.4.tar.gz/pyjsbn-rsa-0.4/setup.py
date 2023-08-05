75e0bf1a478e96ce0ae6b23f47b939b33dba9358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:05 +0000

    Add package pyjsbn-rsa/0.4/pyjsbn-rsa-0.4.tar.gz

---
+++ packages/pyjsbn-rsa/pyjsbn-rsa-0.4.tar.gz/pyjsbn-rsa-0.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='pyjsbn-rsa',
+    version='0.4',
+    description='Python RSA module compatible with jsbn.js',
+    author='Wonsup Yoon',
+    author_email='pusnow@yonsei.ac.kr',
+    license='BSD',
+    url='https://github.com/Pusnow/pyjsbn-rsa',
+    classifiers=[
+        'Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
+        'Topic :: Security :: Cryptography',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=['jsbn'],
+    keywords='jsbn rsa RSAKey',
+    install_requires=['rsa'])
