b348a02499ae5ee95fcf2df27a3719b147adddee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:51 +0000

    Add package scalyr-agent-2/2.0.59/scalyr-agent-2-2.0.59.tar.gz

---
+++ packages/scalyr-agent-2/scalyr-agent-2-2.0.59.tar.gz/scalyr-agent-2-2.0.59/scalyr_agent/json_lib/serializer.py
@@ -0,0 +1,42 @@
+# Copyright 2014 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------
+#
+# author: Steven Czerwinski <czerwin@scalyr.com>
+
+__author__ = "czerwin@scalyr.com"
+
+import struct
+
+from cStringIO import StringIO
+
+
+def serialize_as_length_prefixed_string(value, output_buffer):
+    """Serializes the str or unicode value using the length-prefixed format special to Scalyr.
+
+    This is a bit more efficient since the value does not need to be blackslash or quote escaped.
+
+    @param value: The string value to serialize.
+    @param output_buffer: The buffer to serialize the string to.
+
+    @type value: str or unicode
+    @type output_buffer: StringIO
+    """
+    output_buffer.write("`s")
+    if type(value) is unicode:
+        to_serialize = value.encode("utf-8")
+    else:
+        to_serialize = value
+    output_buffer.write(struct.pack(">i", len(to_serialize)))
+    output_buffer.write(to_serialize)
---
+++ packages/scalyr-agent-2/scalyr-agent-2-2.0.59.tar.gz/scalyr-agent-2-2.0.59/scalyr_agent/tests/redis_monitor_test.py
@@ -0,0 +1,82 @@
+# Copyright 2015 Scalyr Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------
+#
+# author: Imron Alston <imron@scalyr.com>
+
+__author__ = "imron@scalyr.com"
+
+import time
+import unittest
+from struct import pack
+
+from scalyr_agent.builtin_monitors.redis_monitor import RedisMonitor
+from scalyr_agent.builtin_monitors.redis_monitor import RedisHost
+
+
+class DummyLogger(object):
+    def __init__(self):
+        self.command = ""
+        self.warning = ""
+
+    def emit_value(self, key, value, extra_fields):
+        self.command = extra_fields["command"]
+
+    def warn(self, message, limit_once_per_x_secs=0, limit_key=""):
+        self.warning = message
+
+
+class RedisHostTestCase(unittest.TestCase):
+    def setUp(self):
+        config = {"module": "scalyr_agent.builtin_monitors.syslog_monitor"}
+        self.logger = DummyLogger()
+        self.host = RedisHost("localhost", 6379, "", 1000)
+
+        self.entry = {
+            "command": "",
+            "start_time": time.time(),
+            "duration": 100,
+            "id": 1,
+        }
+
+    def test_invalild_utf8_message(self):
+        expected = u"abc\ufffddef"
+
+        self.entry["command"] = pack("3sB13s", "abc", 0xCE, "def").rstrip("\0")
+
+        self.host.utf8_warning_interval = 1
+        self.host.log_entry(self.logger, self.entry)
+        self.assertEquals(expected, self.logger.command)
+        self.assertTrue(
+            self.logger.warning.startswith("Redis command contains invalid utf8")
+        )
+
+    def test_truncated_utf8_message(self):
+        expected = "abc... (4 more bytes)"
+
+        self.entry["command"] = pack(
+            "3sB18s", "abc", 0xCE, "... (4 more bytes)"
+        ).rstrip("\0")
+
+        self.host.log_entry(self.logger, self.entry)
+        self.assertEquals(expected, self.logger.command)
+
+    def test_non_truncated_utf8_message(self):
+
+        expected = "abc... (4 more bytes)"
+        self.entry["command"] = "abc... (4 more bytes)"
+
+        self.host.log_entry(self.logger, self.entry)
+
+        self.assertEquals(expected, self.logger.command)
