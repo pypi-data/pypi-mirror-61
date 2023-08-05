07c71313a1d3fcbdc9443e9658a1ce6efdbf98d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:46 +0000

    Add package snnw/0.1.12/SNNW-0.1.12.tar.gz

---
+++ packages/snnw/SNNW-0.1.12.tar.gz/SNNW-0.1.12/SNNW/evaluate.py
@@ -0,0 +1,17 @@
+import os
+
+import numpy as np
+
+from SNNW.nn.model import Model
+from SNNW.nn.config.load_config import load_config
+
+
+def evaluate(model_dir, config_path, image_path, label_path):
+
+    model_config = load_config(os.path.realpath(config_path))
+
+    test_images = np.load(os.path.realpath(image_path))
+    test_labels = np.load(os.path.realpath(label_path))
+
+    test_model = Model(x_input=test_images, y_true=test_labels, model_config=model_config)
+    test_model.evaluate(model_dir=model_dir)
