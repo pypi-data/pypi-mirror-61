e2493f109ef0990f96939116ad324e3e4b4b5a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:27:58 +0000

    Add package outlier-aniket/0.5/outlier_aniket-0.5.tar.gz

---
+++ packages/outlier-aniket/outlier_aniket-0.5.tar.gz/outlier_aniket-0.5/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'outlier_aniket',
+  packages = ['outlier_aniket'],
+  version = '0.5',
+  license='MIT',
+  description = 'Outlier Removal using Z-score or IQR',
+  author = 'Aniket Gupta',
+  author_email = 'aniketgupta1495@gmail.com',
+  url = 'https://github.com/aniket1402/outlier_aniket.git',
+  download_url = 'https://github.com/aniket1402/outlier_aniket/archive/v_05.tar.gz',
+  keywords = ['Outlier Removal', 'Row Removal'],
+  install_requires=[
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
