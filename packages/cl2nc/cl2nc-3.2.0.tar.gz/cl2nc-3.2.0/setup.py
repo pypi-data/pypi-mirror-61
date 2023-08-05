6376989f841f31a480391efff879fdf6cda4adcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package cl2nc/3.2.0/cl2nc-3.2.0.tar.gz

---
+++ packages/cl2nc/cl2nc-3.2.0.tar.gz/cl2nc-3.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cl2nc',
+    version='3.2.0',
+    description='Convert Vaisala CL51 and CL31 dat files to NetCDF',
+    author='Peter Kuma',
+    author_email='peter.kuma@fastmail.com',
+    license='MIT',
+    scripts=['cl2nc'],
+    install_requires=['netCDF4>=1.2.9'],
+    keywords=['vaisala', 'ceilometer', 'cl51', 'cl31', 'netcdf', 'lidar'],
+    url='https://github.com/peterkuma/cl2nc',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+    ]
+)
