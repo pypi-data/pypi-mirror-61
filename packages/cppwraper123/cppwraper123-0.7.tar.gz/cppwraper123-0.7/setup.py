478c81a1d9ae4cbf765609cf94e5e41d395c823c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package cppwraper123/0.7/cppwraper123-0.7.tar.gz

---
+++ packages/cppwraper123/cppwraper123-0.7.tar.gz/cppwraper123-0.7/setup.py
@@ -0,0 +1,18 @@
+"""Setup script for realpython-reader"""
+
+import os.path
+from setuptools import setup
+
+setup(
+    name="cppwraper123",
+    version="0.7",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["cppwraper123"],
+    include_package_data=True
+)
