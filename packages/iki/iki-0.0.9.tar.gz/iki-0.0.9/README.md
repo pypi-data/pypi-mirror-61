97cafa7256951664f7c79e1e2bef03fd620f5117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:35 +0000

    Add package iki/0.0.9/iki-0.0.9.tar.gz

---
+++ packages/iki/iki-0.0.9.tar.gz/iki-0.0.9/README.md
@@ -0,0 +1,2 @@
+# iki
+`import this` versi Jawa
---
+++ packages/iki/iki-0.0.9.tar.gz/iki-0.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='iki',
+    version='0.0.9',
+    description='Javanesse version of import this.',
+    url='http://github.com/ismailsunni/iki',
+    author='Ismail Sunni',
+    author_email='imajimatika@gmail.com',
+    license='MIT',
+    packages=['iki'],
+    zip_safe=False)
