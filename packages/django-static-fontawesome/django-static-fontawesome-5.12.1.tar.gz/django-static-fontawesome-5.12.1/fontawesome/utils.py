eb84c76ce152ab6c5dfe2fe8c27f7bc061c6aae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:13 +0000

    Add package django-static-fontawesome/5.12.1/django-static-fontawesome-5.12.1.tar.gz

---
+++ packages/django-static-fontawesome/django-static-fontawesome-5.12.1.tar.gz/django-static-fontawesome-5.12.1/fontawesome/utils.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+app_root = os.path.dirname(__file__)
+brand_icons_folder = os.path.abspath(os.path.join(app_root, "./static/fontawesome/svgs/brands/"))
+regular_icons_folder = os.path.abspath(os.path.join(app_root, "./static/fontawesome/svgs/regular/"))
+solid_icons_folder = os.path.abspath(os.path.join(app_root, "./static/fontawesome/svgs/solid/"))
+
+print(brand_icons_folder)
+print(regular_icons_folder)
+print(solid_icons_folder)
+
+
+def get_solid_icons():
+    return sorted(["fa-" + os.path.splitext(os.path.basename(x))[0] for x in glob.glob(solid_icons_folder+"/*.svg")])
+
+def get_brand_icons():
+    return sorted(["fa-" + os.path.splitext(os.path.basename(x))[0] for x in glob.glob(brand_icons_folder+"/*.svg")])
+
+def get_regular_icons():
+    return sorted(["fa-" + os.path.splitext(os.path.basename(x))[0] for x in glob.glob(regular_icons_folder+"/*.svg")])
+
