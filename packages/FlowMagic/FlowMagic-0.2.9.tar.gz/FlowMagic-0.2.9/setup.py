718d8997e117e44355e54b09e9777aa77f54a691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:13 +0000

    Add package FlowMagic/0.2.9/FlowMagic-0.2.9.tar.gz

---
+++ packages/FlowMagic/FlowMagic-0.2.9.tar.gz/FlowMagic-0.2.9/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name='FlowMagic',  
+     version='0.2.9',
+     author="Abhishek Rana",
+     author_email="abhishek.rana@mantralabsglobal.com",
+     description="FlowMagic SDK",
+     packages=['FlowMagic']
+
+ )
