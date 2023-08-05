9aa5b21adab27ce99f9f0619db09b527dde52575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package enjoyml/0.2.2/enjoyml-0.2.2.tar.gz

---
+++ packages/enjoyml/enjoyml-0.2.2.tar.gz/enjoyml-0.2.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='enjoyml',
+      version='0.2.2',
+      description='',
+      url='https://github.com/ByMyTry/enjoyml.git',
+      author='anton-taleckij',
+      author_email='anton.taleckij.job@gmail.com',
+      license='MIT',
+      packages=['enjoyml'],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'scikit-learn',
+      ],
+      zip_safe=False)
