46805449e13b59eacaf9ddb1b3c221f02a4b37e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:27 +0000

    Add package OASYS1-WONDER/1.0.16/OASYS1-WONDER-1.0.16.tar.gz

---
+++ packages/OASYS1-WONDER/OASYS1-WONDER-1.0.16.tar.gz/OASYS1-WONDER-1.0.16/orangecontrib/wonder/fit/parameters/instrument/instrumental_parameters.py
@@ -0,0 +1,69 @@
+from orangecontrib.wonder.fit.parameters.fit_parameter import ParametersList
+
+class Caglioti(ParametersList):
+    U = None
+    V = None
+    W = None
+    a = None
+    b = None
+    c = None
+
+    @classmethod
+    def get_parameters_prefix(cls):
+        return "caglioti_"
+
+    def __init__(self, U, V, W, a, b, c):
+        super(Caglioti, self).__init__()
+
+        self.U = U
+        self.V = V
+        self.W = W
+        self.a = a
+        self.b = b
+        self.c = c
+
+class Lab6TanCorrection(ParametersList):
+    ax = None
+    bx = None
+    cx = None
+    dx = None
+    ex = None
+
+    @classmethod
+    def get_parameters_prefix(cls):
+        return "lab6_tancorrection_"
+
+    def __init__(self, ax, bx, cx, dx, ex):
+        super(Lab6TanCorrection, self).__init__()
+
+        self.ax = ax
+        self.bx = bx
+        self.cx = cx
+        self.dx = dx
+        self.ex = ex
+
+class ZeroError(ParametersList):
+    shift = None
+
+    @classmethod
+    def get_parameters_prefix(cls):
+        return "zero_error_"
+
+    def __init__(self, shift):
+        super(ZeroError, self).__init__()
+
+        self.shift = shift
+
+class SpecimenDisplacement(ParametersList):
+    goniometer_radius = 1.0
+    displacement = None
+
+    @classmethod
+    def get_parameters_prefix(cls):
+        return "sd_"
+
+    def __init__(self, goniometer_radius=1.0, displacement=None):
+        super(SpecimenDisplacement, self).__init__()
+
+        self.goniometer_radius = goniometer_radius
+        self.displacement = displacement
---
+++ packages/OASYS1-WONDER/OASYS1-WONDER-1.0.16.tar.gz/OASYS1-WONDER-1.0.16/orangecontrib/wonder/util/fit_utilities.py
@@ -0,0 +1,94 @@
+import operator
+import itertools
+import numpy
+
+class Symmetry:
+    FCC = "fcc"
+    BCC = "bcc"
+    SIMPLE_CUBIC = "sc"
+
+    @classmethod
+    def tuple(cls):
+        return [cls.SIMPLE_CUBIC, cls.BCC, cls.FCC]
+
+class Utilities:
+    @classmethod
+    def s_hkl(cls, a, h, k, l):
+        return numpy.sqrt(h**2 + k**2 + l**2) / a
+
+    @classmethod
+    def theta(cls, s, wavelength):
+        return numpy.arcsin (s * wavelength / 2)
+
+    @classmethod
+    def s(cls, theta, wavelength):
+        return 2*numpy.sin(theta)/wavelength
+
+    @classmethod
+    def theta_hkl (cls, a, h, k, l , wavelength):
+        return cls.theta(cls.s_hkl(a, h, k, l), wavelength)
+
+def is_even(a):
+    return a % 2 == 0
+
+def is_odd(a):
+    return a % 2 == 1
+
+def is_fcc(h, k, l):
+    if (is_even(h) and is_even(k) and is_even(l)):
+        return True
+    elif (is_odd(h) and is_odd(k) and is_odd(l)):
+        return True
+    else:
+        return False
+
+def is_bcc(h, k, l):
+    if is_even(h+k+l):
+        return True
+    else:
+        return False
+
+def list_of_s_bragg(lattice_param, symmetry=Symmetry.FCC, n_peaks=numpy.inf, s_max=numpy.inf):
+
+    s_list = []
+    s_hkl_max = 0.0
+    possible_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for h, k, l in itertools.combinations_with_replacement(possible_indexes, 3):
+        if (symmetry == Symmetry.FCC and is_fcc(h, k, l)) or \
+           (symmetry == Symmetry.BCC and is_bcc(h, k, l)) or \
+            symmetry == Symmetry.SIMPLE_CUBIC:
+            s_hkl = Utilities.s_hkl(lattice_param, h, k, l)
+
+            s_hkl_max = s_hkl if s_hkl > s_hkl_max else s_hkl_max
+
+            s_list.append([[h, k, l], s_hkl])
+
+    s_list = sorted(s_list, key=operator.itemgetter(1))
+
+    if not len(s_list) <= n_peaks:
+        s_list = s_list[:n_peaks+1]
+
+    if not s_max > s_hkl_max:
+        last_index = 0
+        for item in s_list:
+            if item[1] > s_max: break
+            last_index +=1
+
+        if last_index == 0:
+            s_list = []
+        else:
+            s_list = s_list[:(last_index)]
+
+    if not s_list == []:
+        for temp_list in s_list:
+            temp_list[0] = sorted(temp_list[0], reverse=True)
+
+        s_list.pop(0)
+
+    return s_list
+
+if __name__=="__main__":
+    list = list_of_s_bragg(0.2873, Symmetry.FCC)
+
+    for item in list:
+        print(item)
