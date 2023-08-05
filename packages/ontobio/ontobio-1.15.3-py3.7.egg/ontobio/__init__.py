d22af2689808b20831be9bce5fa53a6493b0d3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package ontobio/1.15.3/ontobio-1.15.3-py3.7.egg

---
+++ packages/ontobio/ontobio-1.15.3-py3.7.egg/ontobio/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+__version__ = '1.15.3'
+
+from .ontol_factory import OntologyFactory
+from .ontol import Ontology, Synonym, TextDefinition
+from .assoc_factory import AssociationSetFactory
+from .io.ontol_renderers import GraphRenderer
