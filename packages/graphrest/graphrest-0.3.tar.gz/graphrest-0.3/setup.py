a0fa886615c6579ccb20c231698dc0938d995502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:45 +0000

    Add package graphrest/0.3/graphrest-0.3.tar.gz

---
+++ packages/graphrest/graphrest-0.3.tar.gz/graphrest-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='graphrest',
+      version='0.3',
+      license='MIT',
+      author='Adriano Michereff Oliveira',
+      author_email='adriano.michereff@processor.com',
+      packages=find_packages(),
+      zip_safe=False)
