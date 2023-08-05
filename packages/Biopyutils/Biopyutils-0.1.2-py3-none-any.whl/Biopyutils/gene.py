fda048e34fe98d7aaf05b2fcdbf48f140cb352a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:05 +0000

    Add package Biopyutils/0.1.2/Biopyutils-0.1.2-py3-none-any.whl

---
+++ packages/Biopyutils/Biopyutils-0.1.2-py3-none-any.whl/Biopyutils/gene.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# License           : GPL3
+# Author            : Jingxin Fu <jingxinfu.tj@gmail.com>
+# Date              : 07/02/2020
+# Last Modified Date: 07/02/2020
+# Last Modified By  : Jingxin Fu <jingxinfu.tj@gmail.com>
+import os
+from Biopyutils import getGeneRefPath, getSpeciesRefPath
+
+#def keepDtype():
+#    def wrapper(**kwargs):
+#
+
+def specieConvert(df,from_species,to_species):
+    ref = getSpeciesRefPath()
+
+
+
+def idConvert(species,map_id):
+    pass
+
