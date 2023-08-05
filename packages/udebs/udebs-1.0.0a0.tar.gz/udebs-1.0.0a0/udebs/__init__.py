a220a3334d56a76654ce35c9332b5329868875bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:36 +0000

    Add package udebs/1.0.0a0/udebs-1.0.0a0.tar.gz

---
+++ packages/udebs/udebs-1.0.0a0.tar.gz/udebs-1.0.0a0/udebs/__init__.py
@@ -0,0 +1,5 @@
+from .loadxml import battleStart, battleWrite
+from .interpret import importModule, importFunction, interpret, UdebsExecutionError
+from .utilities import *
+from .board import UndefinedMetricError
+from .instance import UndefinedSelectorError
