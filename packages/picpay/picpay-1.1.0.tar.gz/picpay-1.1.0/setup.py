de5f4ba2f6b21da9bf5b5578a85e3991dbded6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:08 +0000

    Add package picpay/1.1.0/picpay-1.1.0.tar.gz

---
+++ packages/picpay/picpay-1.1.0.tar.gz/picpay-1.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['picpay']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.22.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'picpay',
+    'version': '1.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Marcus Pereira',
+    'author_email': 'contato@spacedevs.com.br',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MarcusMann/picpay',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
