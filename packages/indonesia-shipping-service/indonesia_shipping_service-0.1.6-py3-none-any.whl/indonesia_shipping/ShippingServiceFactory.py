e26f2fd7dd8b1d6c240e0f824afc279e040f803c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package indonesia-shipping-service/0.1.6/indonesia_shipping_service-0.1.6-py3-none-any.whl

---
+++ packages/indonesia-shipping-service/indonesia_shipping_service-0.1.6-py3-none-any.whl/indonesia_shipping/ShippingServiceFactory.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+from indonesia_shipping.SicepatService import SicepatService
+
+
+class CourierType(Enum):
+    SICEPAT = 1
+    SAP = 2
+
+    @classmethod
+    def from_string(cls, name):
+        if name.lower() == 'sicepat':
+            return cls.SICEPAT
+        elif name.lower() == 'sap express':
+            return cls.SAP
+
+
+class InvalidCourierException(Exception):
+    pass
+
+
+class NotImplementedCourier(NotImplemented):
+    pass
+
+
+class CourierFactory:
+
+    @classmethod
+    def create(cls, courier_type, config):
+        if courier_type == CourierType.SICEPAT:
+            return SicepatService(**config)
+        elif courier_type == CourierType.SAP:
+            raise NotImplementedCourier("Courer {} Not Implemented".format(CourierType.SAP.value))
+        else:
+            raise InvalidCourierException("No Courier Available")
