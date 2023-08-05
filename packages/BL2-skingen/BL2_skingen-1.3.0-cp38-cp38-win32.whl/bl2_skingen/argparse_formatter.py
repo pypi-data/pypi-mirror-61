af76f0be0f751122ede7ecd0149fdb46470961fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package BL2-skingen/1.3.0/BL2_skingen-1.3.0-cp38-cp38-win32.whl

---
+++ packages/BL2-skingen/BL2_skingen-1.3.0-cp38-cp38-win32.whl/bl2_skingen/argparse_formatter.py
@@ -0,0 +1,63 @@
+import argparse
+import re
+
+WORDSEP_RE = re.compile(r'''
+	( # a newline char
+		\n
+	| # any whitespace
+		{ws}+
+	| # em-dash between words
+		(?<={wp}) -{{2,}} (?=\w)
+	| # word, possibly hyphenated
+		{nws}+? (?:
+		# hyphenated word
+			-(?: (?<={lt}{{2}}-) | (?<={lt}-{lt}-))
+			(?= {lt} -? {lt})
+		| # end of word
+			(?={ws}|\Z)
+		| # em-dash
+			(?<={wp}) (?=-{{2,}}\w)
+		)
+	)'''.format(
+		wp = r'[\w!"\'&.,?]', lt = r'[^\d\W]',
+		ws = '[\t\n\x0b\x0c\r ]',
+		nws = '[^\t\n\x0b\x0c\r ]'),
+	re.VERBOSE) # Borrowed from textwrap.TextWrapper.wordsep_re
+
+class SkingenArgparseFormatter(argparse.HelpFormatter):
+	"""Argparse formatter to override the lacking ability of the default
+	formatter to display linebreaks. If '\\n' is encountered, a linebreak
+	will be forced, whitespace at the start of a line will be preserved.
+	"""
+	def _split_lines(self, text, width):
+		chks = [c for c in WORDSEP_RE.split(text) if c]
+
+		out = []
+		idx_start = 0
+		idx = 0
+		chklist_ln = len(chks)
+		not_first_word = False
+		while idx < chklist_ln:
+			curlnwidth = 0
+			line_empty = True
+			while True:
+				if idx > chklist_ln - 1: # Out of chunks
+					break
+				if chks[idx] == "\n": # Newline
+					idx += 1
+					break
+				curlnwidth += len(chks[idx])
+				idx += 1 # Append next chunk, add to current line length
+				if curlnwidth > (width - 1): # -1 due to the space
+					idx -= 1
+					if line_empty:
+						idx += 1
+						break # We are done with this line. Width now ruined.
+					else:
+						break # Whitespace removed in outer loop layer
+				line_empty = False
+			out.append("".join(chks[idx_start:idx]).rstrip())
+			idx_start = idx
+			not_first_word = True
+
+		return out
---
+++ packages/BL2-skingen/BL2_skingen-1.3.0-cp38-cp38-win32.whl/bl2_skingen/flags.py
@@ -0,0 +1,10 @@
+"""Contains nothing but the class "FLAGS"."""
+
+class FLAGS:
+	NO_ASK = 1
+	DEBUG = 2
+	DUMP_PALETTE = 4
+	EXCLUDE_HEAD = 8
+	EXCLUDE_BODY = 16
+	KEEP_WHITE = 32
+	NO_DECAL = 64
