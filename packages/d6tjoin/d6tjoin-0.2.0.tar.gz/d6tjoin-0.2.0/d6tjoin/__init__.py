645d583225e977f72116b41650996b113cd544d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:30 +0000

    Add package d6tjoin/0.2.0/d6tjoin-0.2.0.tar.gz

---
+++ packages/d6tjoin/d6tjoin-0.2.0.tar.gz/d6tjoin-0.2.0/d6tjoin/__init__.py
@@ -0,0 +1,5 @@
+# import d6tjoin.top1
+import d6tjoin.utils
+
+from d6tjoin.pre import Prejoin
+pd = Prejoin
---
+++ packages/d6tjoin/d6tjoin-0.2.0.tar.gz/d6tjoin-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='d6tjoin',
+    version='0.2.0',
+    packages=['d6tjoin'],
+    url='https://github.com/d6t/d6tjoin',
+    license='MIT',
+    author='DataBolt Team',
+    author_email='support@databolt.tech',
+    description='Easily join python pandas dataframes',
+    long_description='Easily join python pandas dataframes'
+        'See https://github.com/d6t/d6tjoin for details',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'jellyfish',
+        'joblib'
+    ],
+    include_package_data=True,
+    python_requires='>=3.6'
+)
