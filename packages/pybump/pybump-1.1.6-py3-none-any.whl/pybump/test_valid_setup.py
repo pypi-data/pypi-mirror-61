391a9485db5862c86ca9f63fc3955df301d1223c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:19 +0000

    Add package pybump/1.1.6/pybump-1.1.6-py3-none-any.whl

---
+++ packages/pybump/pybump-1.1.6-py3-none-any.whl/pybump/test_valid_setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="pybump",
+    version="2.2.0-alpha+metadata.is.useful",
+    author="Arie Lev",
+    author_email="levinsonarie@gmail.com",
+    description="Python version bumper",
+    long_description="README.rst",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArieLevs/PyBump",
+    license='Apache License 2.0',
+    packages=setuptools.find_packages(),
+)
