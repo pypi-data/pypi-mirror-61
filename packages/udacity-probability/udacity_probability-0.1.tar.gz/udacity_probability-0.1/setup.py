27c29e815476b95cd609250f9b48848699ca4ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:01 +0000

    Add package udacity-probability/0.1/udacity_probability-0.1.tar.gz

---
+++ packages/udacity-probability/udacity_probability-0.1.tar.gz/udacity_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_probability'],
+      author = 'Najla Alharbi',
+      author_email = 'najlachai@gmail.com',
+      zip_safe=False)
