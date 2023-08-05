0785e943d98529fd01dca65771a879df86e1abcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:41 +0000

    Add package eheapq/0.0.0/eheapq-0.0.0.tar.gz

---
+++ packages/eheapq/eheapq-0.0.0.tar.gz/eheapq-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="eheapq",
+    version="0.0.0",
+    description="An extension to standard Python's heapq",
+    long_description="An extension to standard Python's heapq for performance applications.",
+    author="Fridolin Pokorny",
+    author_email="fridex.devel@gmail.com",
+    license="GPLv3+",
+    packages=["eheapq"],
+)
