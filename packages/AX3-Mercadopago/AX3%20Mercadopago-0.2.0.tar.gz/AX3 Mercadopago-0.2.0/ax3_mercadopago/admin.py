e6e4c897e657b2f043ac4ba20502cf13f9803b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:06 +0000

    Add package AX3-Mercadopago/0.2.0/AX3%20Mercadopago-0.2.0.tar.gz

---
+++ packages/AX3-Mercadopago/AX3%20Mercadopago-0.2.0.tar.gz/AX3 Mercadopago-0.2.0/ax3_mercadopago/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+from .models import MercadopagoAccessToken
+
+
+@admin.register(MercadopagoAccessToken)
+class MercadopagoAccessTokenAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user_id',
+        'access_token',
+        'public_key',
+        'refresh_token',
+        'created_at',
+        'expires_in',
+    ]
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'user_id',
+                'access_token',
+                'public_key',
+                'refresh_token',
+                'token_type',
+                'expires_in',
+                'response_json',
+            ),
+        }),
+    )
+
+    list_filter = ['created_at', 'expires_in']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
