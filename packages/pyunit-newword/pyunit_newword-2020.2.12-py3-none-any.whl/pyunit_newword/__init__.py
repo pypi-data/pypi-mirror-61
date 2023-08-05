46f914a13e9ff69b689479a269e471de38fedb7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package pyunit-newword/2020.2.12/pyunit_newword-2020.2.12-py3-none-any.whl

---
+++ packages/pyunit-newword/pyunit_newword-2020.2.12-py3-none-any.whl/pyunit_newword/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2018/2/28 10:58
+# @Author: Jtyoui@qq.com
+from .words import NewWords
+
+__version__ = '2020.2.12'
+__author__ = 'Jtyoui'
+__description__ = '新词发现算法'
+__email__ = 'jtyoui@qq.com'
+__names__ = 'pyUnit_newWord'
+__url__ = 'https://github.com/PyUnit/pyunit-newword'
