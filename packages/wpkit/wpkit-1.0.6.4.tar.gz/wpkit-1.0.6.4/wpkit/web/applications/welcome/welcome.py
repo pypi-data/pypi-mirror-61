5ff3d761557a1d11621953b499c96be1d546fbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:24 +0000

    Add package wpkit/1.0.6.4/wpkit-1.0.6.4.tar.gz

---
+++ packages/wpkit/wpkit-1.0.6.4.tar.gz/wpkit-1.0.6.4/wpkit/web/applications/welcome/welcome.py
@@ -0,0 +1,12 @@
+from wpkit.web.base import MyBlueprint
+from wpkit.web import utils,resources
+
+class BlueWelcomePage(MyBlueprint):
+    def __init__(self,import_name=None,name='welcome',url_prefix='/sitemap',**kwargs):
+        super().__init__(name=name,import_name=import_name,url_prefix=url_prefix,add_to_sitemap=False,**kwargs)
+        @self.route('/')
+        def do_root():
+            return resources.get_template_by_name('welcome.tem').render(links=self.app.sitemap)
+    def register(self, app, *args,**kwargs):
+        self.app=app
+        super().register(app,*args,**kwargs)
