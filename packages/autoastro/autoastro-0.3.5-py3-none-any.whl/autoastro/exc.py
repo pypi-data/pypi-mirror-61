990625478096f4bb360e0de8bbac56f296c71411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:07 +0000

    Add package autoastro/0.3.5/autoastro-0.3.5-py3-none-any.whl

---
+++ packages/autoastro/autoastro-0.3.5-py3-none-any.whl/autoastro/exc.py
@@ -0,0 +1,21 @@
+import autofit as af
+
+
+class CosmologyException(Exception):
+    pass
+
+
+class GalaxyException(Exception):
+    pass
+
+
+class PlottingException(Exception):
+    pass
+
+
+class UnitsException(Exception):
+    pass
+
+
+class PriorException(Exception):
+    pass
