0c0b7ce675befe05353d119f406da808e74b6b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:06 +0000

    Add package revenge/0.17/revenge-0.17.tar.gz

---
+++ packages/revenge/revenge-0.17.tar.gz/revenge-0.17/revenge/techniques/native_timeless_tracer/timeless_trace_item.py
@@ -0,0 +1,74 @@
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class NativeTimelessTraceItem(object):
+    
+    def __init__(self, process, context=None, depth=None, previous=None):
+        """Class describing a single step of NativeTimelessTracing
+
+        Args:
+            process (revenge.Process): Process object
+            context (dict): Dictionary describing this step's context
+            depth (int): Current call depth
+            previous (NativeTimelessTraceItem, optional): Previous timeless
+                trace item to use for differential generation
+        """
+        self._process = process
+        self._previous = previous
+        self.context = context
+        self.depth = depth
+
+    def __repr__(self):
+        attrs = ["NativeTimelessTraceItem"]
+        attrs.append(str(self.context.pc.next.thing))
+
+        return "<{}>".format(' '.join(attrs))
+
+    @classmethod
+    def from_snapshot(klass, process, snapshot, previous=None):
+        """Creates a NativeTimelessTraceItem from a snapshot returned by timeless_snapshot()
+        
+        Args:
+            process (revenge.Process): Process object
+            snapshot (dict): Timeless snapshot dictionary
+            previous (NativeTimelessTraceItem, optional): Previous timeless
+                trace item to use for differential generation
+        """
+
+        if not isinstance(snapshot, dict):
+            raise RevengeInvalidArgumentType("Invalid type for from_snapshot of {}. Expecting dict.".format(type(snapshot)))
+
+        if "is_timeless_snapshot" not in snapshot or not snapshot["is_timeless_snapshot"]:
+            raise RevengeInvalidArgumentType("from_snapshot does not appear to be timeless_snapshot dictionary.")
+
+        context = snapshot["context"]
+        depth = snapshot["depth"]
+        return klass(process, context=context, depth=depth, previous=previous)
+
+    @property
+    def instruction(self):
+        """Returns the assembly instruction object for this item."""
+        return self.context.pc.next.thing
+
+    @property
+    def context(self):
+        return self.__context
+
+    @context.setter
+    def context(self, context):
+        diff = self._previous.context if self._previous is not None else None
+
+        # TODO: This is an assumption...
+        if isinstance(context, dict):
+            self.__context = CPUContext(self._process, diff=diff, **context)
+
+        elif context is None:
+            self.__context = None
+
+        else:
+            raise RevengeInvalidArgumentType("Unhandled context type of {}".format(type(context)))
+
+from ...exceptions import *
+from ...cpu import CPUContext
