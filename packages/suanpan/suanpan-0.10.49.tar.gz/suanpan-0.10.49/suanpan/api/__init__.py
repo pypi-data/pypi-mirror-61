d590adf6245a72f22990f0fb07796cb60e54787b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:27 +0000

    Add package suanpan/0.10.49/suanpan-0.10.49.tar.gz

---
+++ packages/suanpan/suanpan-0.10.49.tar.gz/suanpan-0.10.49/suanpan/api/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.api import app, component, oss
+from suanpan.api.requests import affinity, host
+
+get = host.get
+post = host.post
+put = host.put
+delete = host.delete
+call = host.call
