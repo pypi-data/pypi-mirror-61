6129f348460b35d4f1aa5a7bf5f5d30fc1372899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:50 +0000

    Add package pyeddl/0.3.0/pyeddl-0.3.0.tar.gz

---
+++ packages/pyeddl/pyeddl-0.3.0.tar.gz/pyeddl-0.3.0/examples/Tensor/array_tensor_save.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2019-2020 CRS4
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import numpy as np
+import pyeddl._core.eddlT as eddlT
+
+a = np.arange(6).reshape([2, 3]).astype(np.float32)
+print(a)
+t = eddlT.create(a)
+eddlT.save(t, "./a.bin", "bin")
+t1 = eddlT.load("a.bin", "bin")
+a1 = eddlT.getdata(t1)
+print()
+print(a1)
