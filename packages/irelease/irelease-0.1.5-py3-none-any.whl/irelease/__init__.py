f2ceea069bc4f3d42cfeb1032f24a97320fcd601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:22 +0000

    Add package irelease/0.1.5/irelease-0.1.5-py3-none-any.whl

---
+++ packages/irelease/irelease-0.1.5-py3-none-any.whl/irelease/__init__.py
@@ -0,0 +1,29 @@
+from irelease.irelease import(
+    get_script,
+    main)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.5'
+
+# module level doc-string
+__doc__ = """
+irelease - irelease is an Python package to release your package
+=====================================================================
+
+**irelease**
+
+Example
+-------
+
+# Go to directory path of the package you want to release.
+
+# Start Python:
+>>> import irelease
+>>> irelease.get_script()
+
+# In your terminal:
+$ python release.py
+$ release.sh
+
+"""
