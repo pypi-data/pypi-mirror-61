a1f74ce81bba48d6262bc7bb42be0d54cfbdaf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package nomad-connectors-cli/0.1.0/nomad_connectors_cli-0.1.0.tar.gz

---
+++ packages/nomad-connectors-cli/nomad_connectors_cli-0.1.0.tar.gz/nomad_connectors_cli-0.1.0/nomad_connectors_cli/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from nomad_connectors_cli.api.customs_callback_api import CustomsCallbackApi
+from nomad_connectors_cli.api.customs_order_api import CustomsOrderApi
+from nomad_connectors_cli.api.customs_payment_api import CustomsPaymentApi
+from nomad_connectors_cli.api.customs_product_api import CustomsProductApi
+from nomad_connectors_cli.api.order_api import OrderApi
+from nomad_connectors_cli.api.product_api import ProductApi
+from nomad_connectors_cli.api.ship_api import ShipApi
