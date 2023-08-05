c7bbf74c9ca88608ebfeaabb0dfb2ed9815fe16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:44 +0000

    Add package unofficial-allennlp-nightly/0.9.2/unofficial_allennlp_nightly-0.9.2-py3-none-any.whl

---
+++ packages/unofficial-allennlp-nightly/unofficial_allennlp_nightly-0.9.2-py3-none-any.whl/allennlp/modules/seq2vec_encoders/__init__.py
@@ -0,0 +1,40 @@
+"""
+Modules that transform a sequence of input vectors
+into a single output vector.
+Some are just basic wrappers around existing PyTorch modules,
+others are AllenNLP modules.
+
+The available Seq2Vec encoders are
+
+* `"gru"` https://pytorch.org/docs/master/nn.html#torch.nn.GRU
+* `"lstm"` https://pytorch.org/docs/master/nn.html#torch.nn.LSTM
+* `"rnn"` https://pytorch.org/docs/master/nn.html#torch.nn.RNN
+* `"cnn"` allennlp.modules.seq2vec_encoders.cnn_encoder.CnnEncoder
+* `"augmented_lstm"` allennlp.modules.augmented_lstm.AugmentedLstm
+* `"alternating_lstm"` allennlp.modules.stacked_alternating_lstm.StackedAlternatingLstm
+* `"stacked_bidirectional_lstm"` allennlp.modules.stacked_bidirectional_lstm.StackedBidirectionalLstm
+"""
+
+from typing import Type
+
+import torch
+
+from allennlp.common import Params
+from allennlp.common.checks import ConfigurationError
+from allennlp.modules.seq2vec_encoders.cnn_encoder import CnnEncoder
+from allennlp.modules.seq2vec_encoders.cnn_highway_encoder import CnnHighwayEncoder
+from allennlp.modules.seq2vec_encoders.bert_pooler import BertPooler
+from allennlp.modules.seq2vec_encoders.boe_encoder import BagOfEmbeddingsEncoder
+from allennlp.modules.seq2vec_encoders.pytorch_seq2vec_wrapper import (
+    AugmentedLstmSeq2VecEncoder,
+    GruSeq2VecEncoder,
+    LstmSeq2VecEncoder,
+    PytorchSeq2VecWrapper,
+    RnnSeq2VecEncoder,
+    StackedAlternatingLstmSeq2VecEncoder,
+    StackedBidirectionalLstmSeq2VecEncoder,
+)
+from allennlp.modules.seq2vec_encoders.seq2vec_encoder import Seq2VecEncoder
+from allennlp.modules.augmented_lstm import AugmentedLstm
+from allennlp.modules.stacked_alternating_lstm import StackedAlternatingLstm
+from allennlp.modules.stacked_bidirectional_lstm import StackedBidirectionalLstm
