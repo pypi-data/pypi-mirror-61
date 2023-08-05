0800b8b8fe342314bfd46c40ba590596fdd3561a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package pyldapi/2.1.4/pyldapi-2.1.4-py3-none-any.whl

---
+++ packages/pyldapi/pyldapi-2.1.4-py3-none-any.whl/pyldapi/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: latin-1 -*-
+
+from pyldapi.exceptions import ViewsFormatsException, PagingError
+from pyldapi.renderer import Renderer
+from pyldapi.register_renderer import RegisterRenderer,\
+    RegisterOfRegistersRenderer
+from pyldapi.view import View
+from pyldapi.helpers import setup
+
+__version__ = '2.1.4'
+
+__all__ = ['Renderer', 'RegisterRenderer', 'RegisterOfRegistersRenderer',
+           'View', 'ViewsFormatsException', 'PagingError', 'setup',
+           '__version__']
---
+++ packages/pyldapi/pyldapi-2.1.4-py3-none-any.whl/pyldapi/exceptions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class ViewsFormatsException(ValueError):
+    """
+    TODO: Ashley add docstring for documentation
+    """
+    pass
+
+
+class RegOfRegTtlError(RuntimeError):
+    """
+    TODO: Ashley add docstring for documentation
+    """
+    pass
+
+
+class PagingError(ValueError):
+    """
+    TODO: Ashley add docstring for documentation
+    """
+    pass
+
