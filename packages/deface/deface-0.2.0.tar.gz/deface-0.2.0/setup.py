64267a6d0995dd377f46a76162b1eca60c35f119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:17 +0000

    Add package deface/0.2.0/deface-0.2.0.tar.gz

---
+++ packages/deface/deface-0.2.0.tar.gz/deface-0.2.0/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='deface',
+    version='0.2.0',
+    author='Martin Drawitsch',
+    author_email='martin.drawitsch@gmail.com',
+    description='Video anonymization by face detection',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mdraw/deface',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': [
+        'deface = deface.deface:main',
+    ]},
+    package_data={'deface': ['centerface.onnx']},
+    include_package_data=True,
+    install_requires=[
+        'imageio',
+        'imageio-ffmpeg',
+        'numpy',
+        'tqdm',
+        'scikit-image',
+        'opencv-python',
+    ],
+    extras_require={
+        'gpu':  ['onnxruntime-gpu'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
