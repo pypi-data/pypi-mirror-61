e059d654a5117f6df3e9822dde9a9028d9837436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:16 +0000

    Add package dynamof/2.1.8/dynamof-2.1.8.tar.gz

---
+++ packages/dynamof/dynamof-2.1.8.tar.gz/dynamof-2.1.8/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+VERSION = '2.1.8'
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dynamof',
+    version=VERSION,
+    author='Ray Epps',
+    author_email='rayharryepps@gmail.com',
+    description='A small interface for more easily making calls to dynamo using boto.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rayepps/dynamof',
+    packages=[
+        'dynamof',
+        'dynamof.core',
+        'dynamof.operations'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3',
+    install_requires=[
+        'boto3',
+        'botocore'
+    ]
+)
