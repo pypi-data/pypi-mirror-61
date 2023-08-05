a318f51cd5a3d7114a04085298103736866384fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:49 +0000

    Add package frvcpy/0.1.0rc2/frvcpy-0.1.0rc2.tar.gz

---
+++ packages/frvcpy/frvcpy-0.1.0rc2.tar.gz/frvcpy-0.1.0rc2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='frvcpy',
+      version='0.1.0c2',
+      author='Nicholas Kullman',
+      author_email='nicholas.kullman@etu.univ-tours.fr',
+      description='A solver for fixed route vehicle charging problems',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/e-VRO/frvcpy',
+      packages=setuptools.find_packages(),
+      install_requires=['xmltodict'],
+      license='Apache',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      entry_points={
+            'console_scripts': [
+                  'frvcpy=frvcpy.solver:main',
+                  'frvcpy-translate=frvcpy.translator:main',
+            ],
+      }
+)
