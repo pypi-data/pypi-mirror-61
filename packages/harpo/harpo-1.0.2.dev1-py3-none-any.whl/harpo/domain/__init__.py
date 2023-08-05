0427ca8993df3e9ce4f8d66d5a883841105b18c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package harpo/1.0.2.dev1/harpo-1.0.2.dev1-py3-none-any.whl

---
+++ packages/harpo/harpo-1.0.2.dev1-py3-none-any.whl/harpo/domain/__init__.py
@@ -0,0 +1,2 @@
+from .manager import DomainManager
+from .domain import Domain
---
+++ packages/harpo/harpo-1.0.2.dev1-py3-none-any.whl/harpo/log.py
@@ -0,0 +1,17 @@
+import logging
+
+import colorlog
+
+
+def configure_logging(level=logging.INFO):
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter(
+        '%(log_color)s%(levelname)s%(reset)s | %(message)s'))
+    logger = colorlog.getLogger(__name__)
+    logger.addHandler(handler)
+    logger.setLevel(level)
+
+
+def get_logger():
+    logger = colorlog.getLogger(__name__)
+    return logger
