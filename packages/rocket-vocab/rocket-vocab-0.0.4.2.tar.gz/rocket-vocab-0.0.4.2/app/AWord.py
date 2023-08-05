acfd7c972e1d8422de5351a6d7cbf28c12208405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:56 +0000

    Add package rocket-vocab/0.0.4.2/rocket-vocab-0.0.4.2.tar.gz

---
+++ packages/rocket-vocab/rocket-vocab-0.0.4.2.tar.gz/rocket-vocab-0.0.4.2/app/AWord.py
@@ -0,0 +1,8 @@
+class Aword:
+  # Class Attribute
+  # Initializer / Instance Attributes
+  def __init__(self, word, know_point=0, example="", noted=""):
+    self.word = word
+    self.know_point = know_point
+    self.example = example
+    self.noted = noted
---
+++ packages/rocket-vocab/rocket-vocab-0.0.4.2.tar.gz/rocket-vocab-0.0.4.2/app/utils.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+def get_dir_root():
+    return  Path(__file__).parent.parent
