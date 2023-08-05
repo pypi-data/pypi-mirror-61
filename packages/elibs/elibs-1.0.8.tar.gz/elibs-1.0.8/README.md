7b885814086511036495ae3e2b9a187a57d3dc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:03 +0000

    Add package elibs/1.0.8/elibs-1.0.8.tar.gz

---
+++ packages/elibs/elibs-1.0.8.tar.gz/elibs-1.0.8/README.md
@@ -0,0 +1,28 @@
+# ELibs
+
+ELibs is a Python library for dealing with tomticket api.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install elibs.
+
+```bash
+pip install elibs
+```
+
+## Usage
+
+```python
+import elibs
+
+tt = elibs.TomTicket(API_TOKEN)
+
+print(tt.get_clientes())
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/elibs/elibs-1.0.8.tar.gz/elibs-1.0.8/setup.py
@@ -0,0 +1,45 @@
+"""
+To install AttrDict:
+    python setup.py install
+"""
+import setuptools
+from setuptools import setup
+
+DESCRIPTION = "A collection of useful libraries"
+
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    LONG_DESCRIPTION = DESCRIPTION
+
+setup(
+    name="elibs",
+    version="1.0.8",
+    author="Osni Pezzini Junior",
+    author_email="osni.pezzini@gmail.com",
+    packages=setuptools.find_packages(),
+    url="https://github.com/ellitedev/elibs",
+    license="MIT License",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ),
+    install_requires=(
+        'requests',
+        'wxpython',
+    ),
+    tests_require=(
+        'nose>=1.0',
+        'coverage',
+    ),
+    zip_safe=True,
+)
