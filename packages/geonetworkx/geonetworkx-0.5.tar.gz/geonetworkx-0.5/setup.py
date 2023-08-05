2162c750d2200c104abf5b1732be13e9a2636f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:07 +0000

    Add package geonetworkx/0.5/geonetworkx-0.5.tar.gz

---
+++ packages/geonetworkx/geonetworkx-0.5.tar.gz/geonetworkx-0.5/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fr:
+    install_requires = fr.read().splitlines()
+
+setuptools.setup(
+    name="geonetworkx",
+    version="0.5",
+    author="Hugo Chareyre - Artelys",
+    author_email="hugo.chareyre@artelys.com",
+    description="Python tools for geographic graphs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ])
