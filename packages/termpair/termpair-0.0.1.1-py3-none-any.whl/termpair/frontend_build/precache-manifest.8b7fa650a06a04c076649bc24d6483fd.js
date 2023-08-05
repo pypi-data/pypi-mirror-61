31a17098036b154134fe1da3f1a8aa20f1845f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:55 +0000

    Add package termpair/0.0.1.1/termpair-0.0.1.1-py3-none-any.whl

---
+++ packages/termpair/termpair-0.0.1.1-py3-none-any.whl/termpair/frontend_build/precache-manifest.8b7fa650a06a04c076649bc24d6483fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74cdedb8050c1198d3c477e62d8bd849",
+    "url": "./static/media/logo.74cdedb8.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4be3ca55556d33330c95",
+    "url": "./static/js/main.afd21f7a.chunk.js"
+  },
+  {
+    "revision": "12ea26df2b64301b9a24",
+    "url": "./static/js/2.4aeaad65.chunk.js"
+  },
+  {
+    "revision": "4be3ca55556d33330c95",
+    "url": "./static/css/main.95ffe78b.chunk.css"
+  },
+  {
+    "revision": "6dee7adefa5f5b120507bc0c1383792c",
+    "url": "./index.html"
+  }
+];
