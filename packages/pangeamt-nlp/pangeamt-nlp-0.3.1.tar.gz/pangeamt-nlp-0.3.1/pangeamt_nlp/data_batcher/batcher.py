31ddc35f5c4036c9f1d375cb24596f3403ac2103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:00 +0000

    Add package pangeamt-nlp/0.3.1/pangeamt-nlp-0.3.1.tar.gz

---
+++ packages/pangeamt-nlp/pangeamt-nlp-0.3.1.tar.gz/pangeamt-nlp-0.3.1/pangeamt_nlp/data_batcher/batcher.py
@@ -0,0 +1,9 @@
+import os
+import argparse
+from onmt.bin.preprocess import preprocess, _get_parser
+
+
+def batch(stage_dir: str, *args, **kwargs):
+    parser = _get_parser()
+    opt = parser.parse_args(list(args))
+    preprocess(opt)
