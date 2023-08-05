88a86155ecd60b8e33ff1f0c36fbf64b166d9792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:26 +0000

    Add package cloud-run-compose/1.0.6/cloud-run-compose-1.0.6.tar.gz

---
+++ packages/cloud-run-compose/cloud-run-compose-1.0.6.tar.gz/cloud-run-compose-1.0.6/cloud_run_compose/generate_terraform.py
@@ -0,0 +1,213 @@
+import fire
+import shutil
+import json
+from funcy import drop, pluck
+import random
+from populate import populate_string
+import os.path
+from .support import (
+    load,
+    temporary_write,
+    subprocess_call,
+    ProcessException,
+    dump_env_file,
+    get_stdout,
+    printred,
+    printblue,
+    SERVICE_URL_POSTFIX,
+    valid_name,
+)
+import yaml
+
+
+REMOTE_STATE = r"""
+terraform {
+  backend "gcs" {
+    bucket      = "${{ bucket }}"
+    prefix      = "terraform_states/${{stack_name}}"
+    credentials = "${{ credentials }}"
+  }
+}
+
+"""
+
+
+CREDENTIALS = r"""
+
+provider "google-beta" {
+  credentials = file("${{ credentials }}")
+  project     = "${{projectId}}"
+  region      = "${{region}}"
+}
+
+provider "google" {
+  credentials = file("${{ credentials }}")
+  project     = "${{projectId}}"
+  region      = "${{region}}"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+"""
+
+
+SERVICE_PLAN = r"""
+resource "google_cloud_run_service" "${{ service_name }}" {
+  provider = google-beta
+  name     = "${{ service_name }}"
+  location = "${{ region }}"
+  metadata {
+    namespace = "${{ projectId }}"
+  }
+
+  template {
+    metadata {
+        annotations = {
+            "autoscaling.knative.dev/maxScale" = "1000"
+        }
+        labels      = {}
+    }
+    spec {
+      container_concurrency = 80 
+      containers {
+        image = "${{ image }}"
+        resources {
+            limits   = {
+                cpu    = "1000m"
+                memory = "256M"
+            }
+            requests = {}
+        }
+        ${{ f'command = {json.dumps(command)}' if command else '' }}
+        ${{ f'args = {json.dumps(args)}' if args else '' }}
+
+        ${{
+            indent_to('        ', '\n'.join(['env {\n    name = "' + name + '"\n    value = "' + value + '"\n}\n' for name, value in environment.items()]))
+        }}
+      }
+    }
+  }
+}
+
+
+output "${{output_url}}" {
+  value = "${google_cloud_run_service.${{service_name}}.status[0].url}"
+}
+"""
+
+PUBLIC_SERVICE = r"""
+resource "google_cloud_run_service_iam_policy" "${{service_name}}_noauth" {
+  location    = google_cloud_run_service.${{service_name}}.location
+  project     = google_cloud_run_service.${{service_name}}.project
+  service     = google_cloud_run_service.${{service_name}}.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+"""
+
+
+# plan = load(os.path.join(here, "main.tf"))
+
+
+def get_environment(config):
+    result = {}
+    env_files = config.get("env_file")
+    if isinstance(env_files, list):
+        for path in env_files:
+            result.update(dump_env_file(path))
+    elif isinstance(env_files, str):
+        result.update(dump_env_file(env_files))
+
+    env = config.get("environment")
+    if isinstance(env, dict):
+        result.update(env)
+    if isinstance(env, list):
+        for line in env:
+            k, _, v = line.partition("=")
+            result[k] = v
+    return result
+
+
+def parse_command(command):
+    if isinstance(command, list):
+        return command
+    if isinstance(command, str):
+        return ["sh", "-c", command]
+    raise Exception(f"cannot transform command `{command}` to list")
+
+
+def generate_terraform(file, project, region, credentials, build, bucket, stack_name):
+    assert project
+    credentials = os.path.abspath(credentials)
+    file = os.path.abspath(file)
+    try:
+        data = get_stdout(f"docker-compose -f {file} config")
+    except ProcessException as e:
+        printred(e.message)
+        return
+    config = yaml.safe_load(data)
+    if not stack_name:
+        stack_name = os.path.basename(
+            os.path.normpath(os.path.abspath(os.path.dirname(file)))
+        )
+    if not valid_name(stack_name):
+        raise Exception(
+            "Service name may only start with a letter and contain up to 63 lowercase letters, numbers or hyphens"
+        )
+    if bucket:  # TODO use other bucket provider than gcp
+        plan = populate_string(
+            REMOTE_STATE,
+            dict(credentials=credentials, bucket=bucket, stack_name=stack_name),
+        )
+    plan += populate_string(
+        CREDENTIALS, dict(region=region, projectId=project, credentials=credentials)
+    )
+    url_mappings = {}
+    for service_name, service in config.get("services", {}).items():
+        if not valid_name(service_name):
+            raise Exception(
+                "Service name may only start with a letter and contain up to 63 lowercase letters, numbers or hyphens"
+            )
+        if not service.get("image"):
+            printred("all services need an image to be deployed to Cloud Run")
+            return
+        if build and service.get("build"):
+            code, _, _ = subprocess_call(
+                f"docker-compose -f {file} build {service_name}"
+            )
+            if code:
+                printred("failed building")
+                return
+            print("pushing to registry")
+            code, _, _ = subprocess_call(
+                f"docker-compose -f {file} push {service_name}"
+            )
+            if code:
+                printred("cannot push image")
+                return
+        namespaced_service_name = stack_name + "-" + service_name
+        output_url = namespaced_service_name + SERVICE_URL_POSTFIX
+        url_mappings[output_url] = service_name
+        variables = dict(
+            environment=get_environment(service),
+            service_name=namespaced_service_name,
+            image=service.get("image", ""),
+            command=parse_command(service.get("entrypoint", [])),
+            args=parse_command(service.get("command", [])),
+            region=region,
+            projectId=project,
+            output_url=output_url,
+        )
+        populated_service = populate_string(SERVICE_PLAN, variables)
+        plan += "\n" + populated_service
+        plan += populate_string(
+            PUBLIC_SERVICE, dict(service_name=namespaced_service_name)
+        )
+    return plan, url_mappings
+
---
+++ packages/cloud-run-compose/cloud-run-compose-1.0.6.tar.gz/cloud-run-compose-1.0.6/cloud_run_compose/up.py
@@ -0,0 +1,44 @@
+import os.path
+from .generate_terraform import generate_terraform
+import random
+from .support import subprocess_call, SERVICE_URL_POSTFIX, here, terraform_space
+import json
+
+
+def up(
+    file="docker-compose.yml",
+    project="",
+    region="us-central1",
+    credentials="credentials.json",
+    build=False,
+    bucket=None,
+    stack_name="",
+):
+    try:
+        plan, urls_map = generate_terraform(
+            file=file,
+            project=project,
+            region=region,
+            credentials=credentials,
+            build=build,
+            bucket=bucket,
+            stack_name=stack_name,
+        )
+        with terraform_space(plan):
+            out, _, _ = subprocess_call("terraform plan -compact-warnings")
+            assert not out
+            out, _, _ = subprocess_call("terraform apply -auto-approve")
+            assert not out
+            out, json_out, _ = subprocess_call("terraform output -json")
+            assert not out
+            outputs = json.loads(json_out)
+            urls = {
+                urls_map[k]: v["value"]
+                for k, v in outputs.items()
+                if k in urls_map.keys()
+            }
+            # printblue("run `terraform apply` to execute the plan")
+            return urls
+    except Exception as e:
+        print("error", e)
+
