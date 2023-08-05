b968fdcd3abf1903c04f975a0b81fa4cfb34ffae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:24 +0000

    Add package trains-agent/0.13.2rc2/trains_agent-0.13.2rc2-py3-none-any.whl

---
+++ packages/trains-agent/trains_agent-0.13.2rc2-py3-none-any.whl/trains_agent/backend_api/services/__init__.py
@@ -0,0 +1,15 @@
+from .v2_4 import auth
+from .v2_4 import debug
+from .v2_4 import queues
+from .v2_4 import tasks
+from .v2_4 import workers
+from .v2_4 import events
+
+__all__ = [
+    'auth',
+    'debug',
+    'queues',
+    'tasks',
+    'workers',
+    'events',
+]
