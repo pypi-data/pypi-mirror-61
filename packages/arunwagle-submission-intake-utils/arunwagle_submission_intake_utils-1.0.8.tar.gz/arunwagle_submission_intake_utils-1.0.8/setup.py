77b9389d6509daa65f1f0f5405ad9a86e0911a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:42 +0000

    Add package arunwagle-submission-intake-utils/1.0.8/arunwagle_submission_intake_utils-1.0.8.tar.gz

---
+++ packages/arunwagle-submission-intake-utils/arunwagle_submission_intake_utils-1.0.8.tar.gz/arunwagle_submission_intake_utils-1.0.8/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arunwagle_submission_intake_utils", # Replace with your own username
+    version ="1.0.8",
+    author="Arun Wagle",
+    author_email="arun.wagle@ibm.com",
+    description="Utils Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['submission.utils'],
+    python_requires='>=3.6',
+)
