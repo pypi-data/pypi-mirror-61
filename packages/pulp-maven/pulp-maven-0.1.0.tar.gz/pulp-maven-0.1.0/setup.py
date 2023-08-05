315bd2c353e84189c597e7a4492a6b3d527a0aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:25 +0000

    Add package pulp-maven/0.1.0/pulp-maven-0.1.0.tar.gz

---
+++ packages/pulp-maven/pulp-maven-0.1.0.tar.gz/pulp-maven-0.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+requirements = ["pulpcore>=3.1"]
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="pulp-maven",
+    version="0.1.0",
+    description="pulp-maven plugin for the Pulp Project",
+    long_description=long_description,
+    license="GPLv2+",
+    author="Pulp Project Developers",
+    author_email="pulp-dev@redhat.com",
+    url="http://www.pulpproject.org/",
+    python_requires=">=3.6",
+    install_requires=requirements,
+    include_package_data=True,
+    packages=find_packages(exclude=["tests"]),
+    classifiers=(
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ),
+    entry_points={"pulpcore.plugin": ["pulp_maven = pulp_maven:default_app_config"]},
+)
