c96c9ea30faa7a765442c7ac37ba49ac72a6b514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:17 +0000

    Add package student-mixture/0.1.11/student_mixture-0.1.11.tar.gz

---
+++ packages/student-mixture/student_mixture-0.1.11.tar.gz/student_mixture-0.1.11/student_mixture/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Student's t-mixture model."""
+
+__author__ = """Omri Tomer"""
+__email__ = 'omritomer1@mail.tau.ac.il'
+__version__ = '0.1.11'
+
+import sys
+
+if (sys.version_info < (3, 3)):
+    from student_mixture import StudentMixture
+    from multivariate_t import multivariate_t as multivariate_t
+    from mutlivariate_t_fit import MultivariateTFit
+
+else:
+    from student_mixture.student_mixture import StudentMixture
+    from student_mixture.multivariate_t import multivariate_t  as multivariate_t
+    from student_mixture.mutlivariate_t_fit import MultivariateTFit
+
+__all__ = ['StudentMixture',
+           'multivariate_t',
+           'MultivariateTFit']
