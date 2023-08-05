925dc0979e9cd152be8354741e493280bbe18d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:06 +0000

    Add package FaceReco/0.0.1/FaceReco-0.0.1.tar.gz

---
+++ packages/FaceReco/FaceReco-0.0.1.tar.gz/FaceReco-0.0.1/FaceReco/exceptions.py
@@ -0,0 +1,9 @@
+
+
+class ObjectNotFound(Exception):
+    """Exception raised when an expected object is not found"""
+
+class InvalidOperation(Exception):
+    """Exeception raised when an operation doesnt make sense"""
+
+
---
+++ packages/FaceReco/FaceReco-0.0.1.tar.gz/FaceReco-0.0.1/FaceReco/recognize.py
@@ -0,0 +1,9 @@
+# import dlib, cv2, pickle,os
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from imutils.face_utils import FaceAligner
+
+
+
+
+
