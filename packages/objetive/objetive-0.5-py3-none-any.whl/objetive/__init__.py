253bb42a266b8c527fb6383248752456a5bb0e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:58 +0000

    Add package objetive/0.5/objetive-0.5-py3-none-any.whl

---
+++ packages/objetive/objetive-0.5-py3-none-any.whl/objetive/__init__.py
@@ -0,0 +1,37 @@
+import sys
+import mechanicalsoup
+
+def text(url):
+    # Connect to target
+    browser = mechanicalsoup.StatefulBrowser()
+
+    try:
+        status = browser.open(url)
+        if 'text/' in status.headers['Content-Type']:
+            pass
+        else:
+            pass
+
+    except:
+        pass
+
+    # All functions for get values
+    def all():
+        value = ""
+        for p in browser.get_current_page().select('p'):
+            value = value + p.text + ' '
+
+        for h1 in browser.get_current_page().select('h1'):
+            value = value + h1.text + ' '
+
+        for link in browser.get_current_page().select('a'):
+            value = value + link.text + ' '
+
+        return value
+    try:
+        return all()
+    except:
+        return "Not any texts :("
+
+def listed(url):
+    return list(set(text(url).translate(str.maketrans('', '', '\n\t')).replace('—','').replace(',','').replace('|','').split(" ")))
