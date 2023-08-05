de6bd48d7010ee7a5ffc54872f03b96f9103bdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:19 +0000

    Add package sopel-modules.chanlogs/0.2.0/sopel_modules.chanlogs-0.2.0.tar.gz

---
+++ packages/sopel-modules.chanlogs/sopel_modules.chanlogs-0.2.0.tar.gz/sopel_modules.chanlogs-0.2.0/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    print('Sopel does not correctly load plugins installed with setup.py '
+          'directly. Please use "pip install .", or add {}/sopel_modules to '
+          'core.extra in your config.'.format(
+              os.path.dirname(os.path.abspath(__file__))),
+          file=sys.stderr)
+
+
+setup(
+    name='sopel_modules.chanlogs',
+    version='0.2.0',
+    description='A channel logging plugin for Sopel',
+    author='dgw',
+    author_email='dgw@technobabbl.es',
+    url='https://github.com/sopel-irc/sopel-chanlogs',
+    packages=find_packages('.'),
+    install_requires=['sopel>=7.0,<8'],
+    namespace_packages=['sopel_modules'],
+    include_package_data=True,
+    )
