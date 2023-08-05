0bd21702129a154bcb6a9a772c039d7d38783bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:18 +0000

    Add package pipupgrade/1.6.8/pipupgrade-1.6.8.tar.gz

---
+++ packages/pipupgrade/pipupgrade-1.6.8.tar.gz/pipupgrade-1.6.8/src/pipupgrade/__init__.py
@@ -0,0 +1,10 @@
+# imports - module imports
+from pipupgrade.__attr__    import (
+    __name__,
+    __version__,
+    __author__
+)
+from pipupgrade.__main__    import main
+from pipupgrade.config      import Settings
+
+settings = Settings()
