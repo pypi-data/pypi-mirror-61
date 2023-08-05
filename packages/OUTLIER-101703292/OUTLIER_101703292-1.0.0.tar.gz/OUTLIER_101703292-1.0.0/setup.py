a2e4bd8afbb5cf4d2f40c651f460ac448ae6bb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:02 +0000

    Add package OUTLIER-101703292/1.0.0/OUTLIER_101703292-1.0.0.tar.gz

---
+++ packages/OUTLIER-101703292/OUTLIER_101703292-1.0.0.tar.gz/OUTLIER_101703292-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="OUTLIER_101703292",
+    version="1.0.0",
+    description="A Python package to remove outliers from a dataset",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Kriti Pandey",
+    author_email="kritip105@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["Outlier_3292"],
+    include_package_data=True,
+    install_requires=["numpy","pandas"],
+    entry_points={
+        "console_scripts": [
+            "Outlier-3292=Outlier_3292.3292_outliers:main",
+        ]
+    },
+)
