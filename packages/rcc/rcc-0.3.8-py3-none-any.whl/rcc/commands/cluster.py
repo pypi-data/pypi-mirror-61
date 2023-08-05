d47e47c60e967df2b83a9b8de7ad9aa2117f0a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package rcc/0.3.8/rcc-0.3.8-py3-none-any.whl

---
+++ packages/rcc/rcc-0.3.8-py3-none-any.whl/rcc/commands/cluster.py
@@ -0,0 +1,53 @@
+'''Cluster commands
+
+Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
+'''
+
+import collections
+import hiredis
+
+
+ClusterNode = collections.namedtuple(
+    'ClusterNode', ['node_id', 'address', 'ip', 'port', 'role', 'slots']
+)
+
+
+class ClusterCommandsMixin:
+    async def cluster_nodes(self):
+        async with self.lock:
+            response = await self.send('CLUSTER', None, 'NODES')
+
+            # ERR This instance has cluster support disabled'
+            responseType = type(response)
+            if responseType == hiredis.ReplyError:
+                return []
+
+            response = response.decode()
+
+            nodes = []
+            for line in response.splitlines():
+                tokens = line.split()
+                node_id = tokens[0]
+
+                address = tokens[1]
+                url, _, _ = address.partition('@')
+                ip, _, port = url.partition(':')
+
+                role = tokens[2]
+                role = role.replace('myself,', '')
+                role = role.replace('myself,', '')
+
+                slots = []
+                for token in tokens[8:]:
+                    if '-' in token:
+                        start, _, end = token.partition('-')
+                        start = int(start)
+                        end = int(end)
+                        for i in range(start, end + 1):
+                            slots.append(i)
+                    else:
+                        slots.append(token)
+
+                nodes.append(ClusterNode(node_id, address, ip, port, role, slots))
+
+            return nodes
