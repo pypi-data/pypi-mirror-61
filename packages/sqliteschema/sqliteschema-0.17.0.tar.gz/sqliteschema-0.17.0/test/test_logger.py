a3ea0fa2e00e72a7eb6437a08941a2be5d225bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:38 +0000

    Add package sqliteschema/0.17.0/sqliteschema-0.17.0.tar.gz

---
+++ packages/sqliteschema/sqliteschema-0.17.0.tar.gz/sqliteschema-0.17.0/test/test_logger.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import print_function, unicode_literals
+
+import pytest
+
+from sqliteschema import set_logger
+
+
+class Test_set_logger(object):
+    @pytest.mark.parametrize(["value"], [[True], [False]])
+    def test_smoke(self, value):
+        set_logger(value)
