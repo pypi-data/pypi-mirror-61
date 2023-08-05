5619f40d6806a741bbc1a1016367c8a4ab80bbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:58 +0000

    Add package Pi20/0.1.6/Pi20-0.1.6.tar.gz

---
+++ packages/Pi20/Pi20-0.1.6.tar.gz/Pi20-0.1.6/Pi20/__init__.py
@@ -0,0 +1,5 @@
+from .KumandaKontrol import Kumanda
+from .MotorKontrol import MotorKontrol
+from .ServoKontrol import ServoKontrol
+from .UltrasonikSensor import UltrasonikSensor
+from .PiKamera20 import HizlandirilmisPiKamera
