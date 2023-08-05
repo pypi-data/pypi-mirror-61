cbefb87cdf9a6f07ae2980592815937711fd7270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:53 +0000

    Add package aio-daemon/0.1/aio_daemon-0.1.tar.gz

---
+++ packages/aio-daemon/aio_daemon-0.1.tar.gz/aio_daemon-0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aio_daemon",
+    version="0.1",
+    author="Trog",
+    author_email="trog@swmud.net",
+    description="Helper module for running async daemons.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Trogious/aio-daemon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires='>=3.7',
+)
