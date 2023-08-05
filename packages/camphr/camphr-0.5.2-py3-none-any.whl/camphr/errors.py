884b628507cefd3731b7df7d8926b3727bb97923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package camphr/0.5.2/camphr-0.5.2-py3-none-any.whl

---
+++ packages/camphr/camphr-0.5.2-py3-none-any.whl/camphr/errors.py
@@ -0,0 +1,17 @@
+import warnings
+from typing import Type
+
+
+class W:
+    def __init__(self, msg: str, warning_cls: Type[Warning]):
+        self.msg = msg
+        self.warning_cls = warning_cls
+
+    def __call__(self, *args, **kwargs):
+        warnings.warn(self.msg.format(*args, **kwargs), self.warning_cls, stacklevel=3)
+
+
+class Warnings:
+    _W_FOR_TEST = W("Foo {} wow {bar}!", RuntimeWarning)
+    W0 = W("{} has been deprecated. Please use {} instead.", DeprecationWarning)
+    W1 = W("{} has been deprecated.", DeprecationWarning)
