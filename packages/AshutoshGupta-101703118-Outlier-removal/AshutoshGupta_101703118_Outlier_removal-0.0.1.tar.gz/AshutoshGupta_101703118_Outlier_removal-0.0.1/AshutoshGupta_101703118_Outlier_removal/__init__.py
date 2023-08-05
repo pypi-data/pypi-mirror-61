bcfeddedaa2ea02eb8e380150208baf722034f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:50 +0000

    Add package AshutoshGupta-101703118-Outlier-removal/0.0.1/AshutoshGupta_101703118_Outlier_removal-0.0.1.tar.gz

---
+++ packages/AshutoshGupta-101703118-Outlier-removal/AshutoshGupta_101703118_Outlier_removal-0.0.1.tar.gz/AshutoshGupta_101703118_Outlier_removal-0.0.1/AshutoshGupta_101703118_Outlier_removal/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 
+
+@author: ashu
+"""
+#Made by Ashutosh Gupta
+from AshutoshGupta_101703118_Outlier_removal import outlier_removal
---
+++ packages/AshutoshGupta-101703118-Outlier-removal/AshutoshGupta_101703118_Outlier_removal-0.0.1.tar.gz/AshutoshGupta_101703118_Outlier_removal-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 
+
+@author: ashu
+"""
+#Made by Ashutosh Gupta 101703118
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AshutoshGupta_101703118_Outlier_removal",
+    version="0.0.1",
+    author="Ashutosh Gupta",
+    author_email="agupta12_be17@thapar.edu",
+    description="A small example package for removal of outliers in the given dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashutosh2539/Outliers_removal",
+    download_url="https://github.com/ashutosh2539/Outliers_removal.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
