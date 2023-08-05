021fcbcd498c1b5a53e16c980f29128909f1b567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:04 +0000

    Add package CionAttack-Probability/0.1.0/CionAttack-Probability-0.1.0.tar.gz

---
+++ packages/CionAttack-Probability/CionAttack-Probability-0.1.0.tar.gz/CionAttack-Probability-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="CionAttack-Probability",
+    version="0.1.0",
+    description="A package of rare tools for analyzing random numbers in a flexible and entertaining way quick start roulette slots game",
+    long_description="""
+A set of rare tools to analyze random numbers in a flexible and entertaining way with a graphical interface that makes slots game clear and easy to understand with a quick start roulette slots game
+MyName   : o_OFast
+Youtube  : https://www.youtube.com/channel/UCIHqSjX9NsNLzoW2FY42SdA
+Facebook : https://www.facebook.com/Dashboard-Probability-103791477828191
+""",
+    long_description_content_type="text/markdown",
+    author="io_O",
+    author_email="fastorder9@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=['CionAttack'],
+    include_package_data=True
+)
+
+
