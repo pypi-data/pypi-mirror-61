5a54ea5f5d8df5d8f6efc0b1b9818e976dfd63b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package dataops-salesforce-bulk/0.0.4/dataops-salesforce-bulk-0.0.4.tar.gz

---
+++ packages/dataops-salesforce-bulk/dataops-salesforce-bulk-0.0.4.tar.gz/dataops-salesforce-bulk-0.0.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+packages = [
+    'dataops-salesforce-bulk',
+]
+
+requires = [
+    'six',
+    'requests>=2.2.1',
+    'unicodecsv>=0.14.1',
+    'simple-salesforce>=0.69',
+
+]
+
+with open('README.txt', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='dataops-salesforce-bulk',
+    version='0.0.4',
+    description='Python interface to the Salesforce.com Bulk API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Scott Persinger',
+    author_email='scottp@heroku.com',
+    url='https://github.com/puppetlabs/salesforce-bulk',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT',
+    zip_safe=False,
+)
