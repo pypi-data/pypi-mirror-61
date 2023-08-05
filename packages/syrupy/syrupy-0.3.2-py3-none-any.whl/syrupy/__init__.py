2e5a689c87e0b9aff57e4fd900738f3e9548779c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:02 +0000

    Add package syrupy/0.3.2/syrupy-0.3.2-py3-none-any.whl

---
+++ packages/syrupy/syrupy-0.3.2-py3-none-any.whl/syrupy/__init__.py
@@ -0,0 +1,118 @@
+import glob
+from typing import (
+    Any,
+    List,
+    Optional,
+)
+
+import pytest
+
+from .assertion import SnapshotAssertion
+from .extensions import DEFAULT_EXTENSION
+from .location import TestLocation
+from .session import SnapshotSession
+from .terminal import (
+    green,
+    red,
+    reset,
+)
+
+
+def pytest_addoption(parser: Any) -> None:
+    """
+    Exposes snapshot plugin configuration to pytest.
+    https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_addoption
+    """
+    group = parser.getgroup("syrupy")
+    group.addoption(
+        "--snapshot-update",
+        action="store_true",
+        default=False,
+        dest="update_snapshots",
+        help="Update snapshots",
+    )
+    group.addoption(
+        "--snapshot-warn-unused",
+        action="store_true",
+        default=False,
+        dest="warn_unused_snapshots",
+        help="Do not fail on unused snapshots",
+    )
+
+
+def pytest_assertrepr_compare(op: str, left: Any, right: Any) -> Optional[List[str]]:
+    """
+    Return explanation for comparisons in failing assert expressions.
+    https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_assertrepr_compare
+    """
+    if isinstance(left, SnapshotAssertion):
+        assert_msg = reset(f"{green(left.name)} {op} {red('received')}")
+        return [assert_msg] + left.get_assert_diff(right)
+    elif isinstance(right, SnapshotAssertion):
+        assert_msg = reset(f"{red('received')} {op} {green(right.name)}")
+        return [assert_msg] + right.get_assert_diff(left)
+    return None
+
+
+def __is_testpath(arg: str) -> bool:
+    return not arg.startswith("-") and bool(glob.glob(arg.split("::")[0]))
+
+
+def __is_testmodule(arg: str) -> bool:
+    return arg == "--pyargs"
+
+
+def pytest_sessionstart(session: Any) -> None:
+    """
+    Initialize snapshot session before tests are collected and ran.
+    https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_sessionstart
+    """
+    config = session.config
+    session._syrupy = SnapshotSession(
+        warn_unused_snapshots=config.option.warn_unused_snapshots,
+        update_snapshots=config.option.update_snapshots,
+        base_dir=config.rootdir,
+        is_providing_paths=any(
+            __is_testpath(arg) or __is_testmodule(arg)
+            for arg in config.invocation_params.args
+        ),
+    )
+    session._syrupy.start()
+
+
+def pytest_collection_modifyitems(session: Any, config: Any, items: List[Any]) -> None:
+    """
+    After tests are collected and before any modification is performed.
+    https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_collection_modifyitems
+    """
+    session._syrupy._all_items.update(items)
+
+
+def pytest_collection_finish(session: Any) -> None:
+    """
+    After collection has been performed and modified.
+    https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_collection_finish
+    """
+    session._syrupy._ran_items.update(session.items)
+
+
+def pytest_sessionfinish(session: Any, exitstatus: int) -> None:
+    """
+    Add syrupy report to pytest after whole test run finished, before exiting.
+    https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_sessionfinish
+    """
+    reporter = session.config.pluginmanager.get_plugin("terminalreporter")
+    syrupy_exitstatus = session._syrupy.finish()
+    for line in session._syrupy.report.lines:
+        reporter.write_line(line)
+    session.exitstatus |= syrupy_exitstatus
+
+
+@pytest.fixture
+def snapshot(request: Any) -> "SnapshotAssertion":
+    return SnapshotAssertion(
+        update_snapshots=request.config.option.update_snapshots,
+        extension_class=DEFAULT_EXTENSION,
+        test_location=TestLocation(request.node),
+        session=request.session._syrupy,
+    )
---
+++ packages/syrupy/syrupy-0.3.2-py3-none-any.whl/syrupy/session.py
@@ -0,0 +1,94 @@
+import os
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    List,
+    Optional,
+    Set,
+)
+
+from .constants import EXIT_STATUS_FAIL_UNUSED
+from .data import SnapshotFossils
+from .report import SnapshotReport
+
+
+if TYPE_CHECKING:
+    from .assertion import SnapshotAssertion
+    from .extensions.base import AbstractSyrupyExtension  # noqa: F401
+
+
+class SnapshotSession:
+    def __init__(
+        self,
+        *,
+        warn_unused_snapshots: bool,
+        update_snapshots: bool,
+        base_dir: str,
+        is_providing_paths: bool,
+    ):
+        self.warn_unused_snapshots = warn_unused_snapshots
+        self.update_snapshots = update_snapshots
+        self.base_dir = base_dir
+        self.is_providing_paths: bool = is_providing_paths
+        self.report: Optional["SnapshotReport"] = None
+        self._all_items: Set[Any] = set()
+        self._ran_items: Set[Any] = set()
+        self._assertions: List["SnapshotAssertion"] = []
+        self._extensions: Dict[str, "AbstractSyrupyExtension"] = {}
+
+    def start(self) -> None:
+        self.report = None
+        self._all_items = set()
+        self._ran_items = set()
+        self._assertions = []
+        self._extensions = {}
+
+    def finish(self) -> int:
+        exitstatus = 0
+        self.report = SnapshotReport(
+            base_dir=self.base_dir,
+            all_items=self._all_items,
+            ran_items=self._ran_items,
+            assertions=self._assertions,
+            update_snapshots=self.update_snapshots,
+            warn_unused_snapshots=self.warn_unused_snapshots,
+            is_providing_paths=self.is_providing_paths,
+        )
+        if self.report.num_unused:
+            if self.update_snapshots:
+                self.remove_unused_snapshots(
+                    unused_snapshot_fossils=self.report.unused,
+                    used_snapshot_fossils=self.report.used,
+                )
+            elif not self.warn_unused_snapshots:
+                exitstatus |= EXIT_STATUS_FAIL_UNUSED
+        return exitstatus
+
+    def register_request(self, assertion: "SnapshotAssertion") -> None:
+        self._assertions.append(assertion)
+        discovered_extensions = {
+            discovered.location: assertion.extension
+            for discovered in assertion.extension.discover_snapshots()
+            if discovered.has_snapshots
+        }
+        self._extensions.update(discovered_extensions)
+
+    def remove_unused_snapshots(
+        self,
+        unused_snapshot_fossils: "SnapshotFossils",
+        used_snapshot_fossils: "SnapshotFossils",
+    ) -> None:
+        for unused_snapshot_fossil in unused_snapshot_fossils:
+            snapshot_location = unused_snapshot_fossil.location
+
+            extension = self._extensions.get(snapshot_location)
+            if extension:
+                extension.delete_snapshots(
+                    snapshot_location=snapshot_location,
+                    snapshot_names={
+                        snapshot.name for snapshot in unused_snapshot_fossil
+                    },
+                )
+            elif snapshot_location not in used_snapshot_fossils:
+                os.remove(snapshot_location)
