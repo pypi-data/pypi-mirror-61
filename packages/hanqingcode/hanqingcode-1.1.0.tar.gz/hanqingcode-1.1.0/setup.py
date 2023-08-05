7a7274f8cbe88cfd156681e6183a4f0e09e726a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:51 +0000

    Add package hanqingcode/1.1.0/hanqingcode-1.1.0.tar.gz

---
+++ packages/hanqingcode/hanqingcode-1.1.0.tar.gz/hanqingcode-1.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+NAME = "hanqingcode"
+VERSION = "1.1.0"
+URL = "https://pypi.org/project/hangqincode"
+AUTHOR = "hdy"
+AUTHOR_EMAIL = "1015296415@qq.com"
+LICENSE = "MIT"
+MODULES = ["hanqingcode"]
+DESC = "this is a gift for someone!"
+
+setup(
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    name=NAME,
+    version=VERSION,
+    license=LICENSE,
+    url=URL,
+    py_modules=MODULES,
+    description=DESC,
+    entry_points={"console_scripts": ["hanqingcode=index:main"]},
+)
