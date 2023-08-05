a68a23aa84fd7cd57b3093293030a1eb2cd20caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:19 +0000

    Add package nathan-test/0.1.0/nathan-test-0.1.0.tar.gz

---
+++ packages/nathan-test/nathan-test-0.1.0.tar.gz/nathan-test-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='nathan-test',
+      version='0.1.0',
+      description='Haze generation',
+      author='Nitin Ware',
+      author_email='nitinware@gmail.com',
+      packages=['amaze', 'amaze.demo']
+    )
+
+__author__= 'Nitin Ware'
+
