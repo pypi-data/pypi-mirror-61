73051e4ea8ae3b74427c5114cce58d50e074aaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:21 +0000

    Add package pysindy/0.12.0/pysindy-0.12.0-py3-none-any.whl

---
+++ packages/pysindy/pysindy-0.12.0-py3-none-any.whl/pysindy/optimizers/stlsq.py
@@ -0,0 +1,194 @@
+import warnings
+
+import numpy as np
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.linear_model import ridge_regression
+from sklearn.utils.validation import check_is_fitted
+
+from pysindy.optimizers import BaseOptimizer
+
+
+class STLSQ(BaseOptimizer):
+    """
+    Sequentially thresholded least squares algorithm.
+
+    Attempts to minimize the objective function
+    :math:`\\|y - Xw\\|^2_2 + alpha \\times \\|w\\|^2_2`
+    by iteratively performing least squares and masking out
+    elements of the weight that are below a given threshold.
+
+    Parameters
+    ----------
+    threshold : float, optional (default 0.1)
+        Minimum magnitude for a coefficient in the weight vector.
+        Coefficients with magnitude below the threshold are set
+        to zero.
+
+    alpha : float, optional (default 0)
+        Optional L2 (ridge) regularization on the weight vector.
+
+    max_iter : int, optional (default 20)
+        Maximum iterations of the optimization algorithm.
+
+    ridge_kw : dict, optional
+        Optional keyword arguments to pass to the ridge regression.
+
+    fit_intercept : boolean, optional (default False)
+        Whether to calculate the intercept for this model. If set to false, no
+        intercept will be used in calculations.
+
+    normalize : boolean, optional (default False)
+        This parameter is ignored when fit_intercept is set to False. If True,
+        the regressors X will be normalized before regression by subtracting
+        the mean and dividing by the l2-norm.
+
+    copy_X : boolean, optional (default True)
+        If True, X will be copied; else, it may be overwritten.
+
+    unbias : boolean, optional (default True)
+        Whether to perform an extra step of unregularized linear regression to unbias
+        the coefficients for the identified support.
+        For example, if `STLSQ(alpha=0.1)` is used then the learned coefficients will
+        be biased toward 0 due to the L2 regularization.
+        Setting `unbias=True` will trigger an additional step wherein the nonzero
+        coefficients learned by the `STLSQ` object will be updated using an
+        unregularized least-squares fit.
+
+    Attributes
+    ----------
+    coef_ : array, shape (n_features,) or (n_targets, n_features)
+        Weight vector(s)
+
+    ind_ : array, shape (n_features,) or (n_targets, n_features)
+        Array of 0s and 1s indicating which coefficients of the
+        weight vector have not been masked out.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from scipy.integrate import odeint
+    >>> from pysindy import SINDy
+    >>> from pysindy.optimizers import STLSQ
+    >>> lorenz = lambda z,t : [10*(z[1] - z[0]),
+    >>>                        z[0]*(28 - z[2]) - z[1],
+    >>>                        z[0]*z[1] - 8/3*z[2]]
+    >>> t = np.arange(0,2,.002)
+    >>> x = odeint(lorenz, [-8,8,27], t)
+    >>> opt = STLSQ(threshold=.1, alpha=.5)
+    >>> model = SINDy(optimizer=opt)
+    >>> model.fit(x, t=t[1]-t[0])
+    >>> model.print()
+    x0' = -9.999 1 + 9.999 x0
+    x1' = 27.984 1 + -0.996 x0 + -1.000 1 x1
+    x2' = -2.666 x1 + 1.000 1 x0
+    """
+
+    def __init__(
+        self,
+        threshold=0.1,
+        alpha=0.0,
+        max_iter=20,
+        ridge_kw=None,
+        normalize=False,
+        fit_intercept=False,
+        copy_X=True,
+        unbias=True,
+    ):
+        super(STLSQ, self).__init__(
+            max_iter=max_iter,
+            normalize=normalize,
+            fit_intercept=fit_intercept,
+            copy_X=copy_X,
+            unbias=unbias,
+        )
+
+        if threshold < 0:
+            raise ValueError("threshold cannot be negative")
+        if alpha < 0:
+            raise ValueError("alpha cannot be negative")
+        if max_iter <= 0:
+            raise ValueError("max_iter must be positive")
+
+        self.threshold = threshold
+        self.alpha = alpha
+        self.ridge_kw = ridge_kw
+
+    def _sparse_coefficients(self, dim, ind, coef, threshold):
+        """Perform thresholding of the weight vector(s)
+        """
+        c = np.zeros(dim)
+        c[ind] = coef
+        big_ind = np.abs(c) >= threshold
+        c[~big_ind] = 0
+        self.history_.append(c)
+        return c, big_ind
+
+    def _regress(self, x, y):
+        """Perform the ridge regression
+        """
+        kw = self.ridge_kw or {}
+        coef = ridge_regression(x, y, self.alpha, **kw)
+        self.iters += 1
+        return coef
+
+    def _no_change(self):
+        """Check if the coefficient mask has changed after thresholding
+        """
+        this_coef = self.history_[-1]
+        if len(self.history_) > 1:
+            last_coef = self.history_[-2]
+        else:
+            last_coef = np.zeros_like(this_coef)
+        return all(bool(i) == bool(j) for i, j in zip(this_coef, last_coef))
+
+    def _reduce(self, x, y):
+        """
+        Iterates the thresholding. Assumes an initial guess is saved in
+        self.coef_ and self.ind_
+        """
+        ind = self.ind_
+        n_samples, n_features = x.shape
+        n_features_selected = sum(ind)
+
+        for _ in range(self.max_iter):
+            if np.count_nonzero(ind) == 0:
+                warnings.warn(
+                    """Sparsity parameter is too big ({}) and eliminated all
+                    coeficients""".format(
+                        self.threshold
+                    )
+                )
+                coef = np.zeros_like(ind, dtype=float)
+                break
+
+            coef = self._regress(x[:, ind], y)
+            coef, ind = self._sparse_coefficients(n_features, ind, coef, self.threshold)
+
+            if sum(ind) == n_features_selected or self._no_change():
+                # could not (further) select important features
+                break
+        else:
+            warnings.warn(
+                "STLSQ._reduce did not converge after {} iterations.".format(
+                    self.max_iter
+                ),
+                ConvergenceWarning,
+            )
+            try:
+                coef
+            except NameError:
+                coef = self.coef_
+                warnings.warn(
+                    "STLSQ._reduce has no iterations left to determine coef",
+                    ConvergenceWarning,
+                )
+        self.coef_ = coef
+        self.ind_ = ind
+
+    @property
+    def complexity(self):
+        check_is_fitted(self)
+
+        return np.count_nonzero(self.coef_) + np.count_nonzero(
+            [abs(self.intercept_) >= self.threshold]
+        )
