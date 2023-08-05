1a5511b72ba9e27918c36094e7156f5e8348073d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:18 +0000

    Add package zzo-bibtex-parser/1.0.7/zzo-bibtex-parser-1.0.7.tar.gz

---
+++ packages/zzo-bibtex-parser/zzo-bibtex-parser-1.0.7.tar.gz/zzo-bibtex-parser-1.0.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = ['bibtexparser==1.1.0']
+
+setuptools.setup(
+    name="zzo-bibtex-parser",
+    version="1.0.7",
+    license="WTFPL",
+    author="zzossig",
+    author_email="zzossig@gmail.com",
+    description="bibtex importer for hugo zzo theme",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zzossig/zzo-bibtex-parser",
+    packages=setuptools.find_packages(),
+    entry_points ={
+        'console_scripts': [ 
+            'zzo = zzo.parser:main'
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=install_requires,
+)
+
+# python setup.py sdist
+# pip install twine
+# twine upload dist/*
