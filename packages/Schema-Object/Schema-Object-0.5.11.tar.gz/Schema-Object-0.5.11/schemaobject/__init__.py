b07c21e717c58eedb50544f625b5ebb4c1d4903e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package Schema-Object/0.5.11/Schema-Object-0.5.11.tar.gz

---
+++ packages/Schema-Object/Schema-Object-0.5.11.tar.gz/Schema-Object-0.5.11/schemaobject/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding:utf-8
+__author__ = 'Mitch Matuson, Mustafa Ozgur'
+__copyright__ = """
+Copyright 2009-2016 Mitch Matuson
+Copyright 2016 Mustafa Ozgur
+"""
+__version__ = "0.5.11"
+__license__ = "Apache 2.0"
+
+# shortcut to SchemaObject()
+from schemaobject.schema import SchemaObject
---
+++ packages/Schema-Object/Schema-Object-0.5.11.tar.gz/Schema-Object-0.5.11/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='Schema-Object',
+    packages=['schemaobject'],
+    version='0.5.11',
+    description="Iterate over a MySQL database schema as a Python object.",
+    author="Mitch Matuson, Mustafa Ozgur",
+    author_email="code@matuson.com, root@mit.sh",
+    url="https://github.com/hhyo/SchemaObject",
+    keywords=["MySQL", "database", "schema"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+    ],
+    long_description="""\
+        SchemaObject provides a simple, easy to use Python object interface to a MySQL database schema.
+        You can effortlessly write tools to test, validate, sync, migrate, or manage your schema as well as generate
+        the SQL necessary to make changes to it.
+        """,
+    install_requires=[
+        "PyMySQL >= 0.6.2",
+    ],
+)
