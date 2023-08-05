dda70568e6b524f9b6cae12a45fb7f99bada793e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package google-play-scraper/0.0.1.8/google-play-scraper-0.0.1.8.tar.gz

---
+++ packages/google-play-scraper/google-play-scraper-0.0.1.8.tar.gz/google-play-scraper-0.0.1.8/google_play_scraper/__init__.py
@@ -0,0 +1,71 @@
+import json
+
+try:
+    from urllib.error import HTTPError
+except ImportError:
+    from urllib2 import HTTPError
+
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
+from .constants.element import ElementSpecs
+from .constants.regex import Regex
+from .constants.url import URLFormats
+from .exceptions import (
+    ContentNotFoundException,
+    InvalidURLError,
+    NotFoundError,
+    ExtraHTTPError,
+)
+
+
+__version__ = "0.0.1.8"
+
+
+def _request(url):
+    # type: (str) -> str
+
+    try:
+        resp = urlopen(url)
+    except HTTPError as e:
+        if e.code == 404:
+            raise NotFoundError("App not found(404).")
+        else:
+            raise ExtraHTTPError(
+                "App not found. Status code {} returned.".format(e.code)
+            )
+
+    return resp.read().decode("UTF-8")
+
+
+def app(app_id, lang="en", country="us"):
+    url = URLFormats.Detail.build_url({"id": app_id, "hl": lang, "gl": country})
+
+    dom = _request(url)
+
+    matches = Regex.SCRIPT.findall(dom)
+
+    res = {}
+
+    for match in matches:
+        key_match = Regex.KEY.findall(match)
+        value_match = Regex.VALUE.findall(match)
+
+        if key_match and value_match:
+            key = key_match[0]
+            value = json.loads(value_match[0])
+
+            res[key] = value
+
+    result = {}
+
+    for k, spec in ElementSpecs.Detail.items():
+        content = spec.extract_content(res)
+
+        result[k] = content
+        result["appId"] = app_id
+        result["url"] = url
+
+    return result
