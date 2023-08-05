aec8aa128c67bb8766c79715a22771d23efc289a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:28:03 +0000

    Add package pyshuttlis/0.1.45/pyshuttlis-0.1.45.tar.gz

---
+++ packages/pyshuttlis/pyshuttlis-0.1.45.tar.gz/pyshuttlis-0.1.45/shuttlis/money.py
@@ -0,0 +1,16 @@
+# Deprecated, soon will be removed
+def paisa_to_inr(amount_in_paisa: int):
+    return round(amount_in_paisa / 100, ndigits=2)
+
+
+# Deprecated, soon will be removed
+def inr_to_paisa(amount_in_inr: int):
+    return int(amount_in_inr * 100)
+
+
+def to_base_denomination(amount: int):
+    return int(amount * 100)
+
+
+def from_base_denomination(amount: int):
+    return round(amount / 100, ndigits=2)
