5f8170a8166b2c46962332b13621c962918a3ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package ineedyou/0.1.0/ineedyou-0.1.0.tar.gz

---
+++ packages/ineedyou/ineedyou-0.1.0.tar.gz/ineedyou-0.1.0/ineedyou/__init__.py
@@ -0,0 +1,18 @@
+'''
+ineedyou
+
+I need you, but you need me too.
+'''
+
+__title__ = 'ineedyou'
+__version__ = '0.1.0'
+__all__ = ()
+__author__ = 'Johan Nestaas <johannestaas@gmail.com'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright 2020 Johan Nestaas'
+
+from youneedme import main as other_main
+
+
+def main():
+    pass
---
+++ packages/ineedyou/ineedyou-0.1.0.tar.gz/ineedyou-0.1.0/setup.py
@@ -0,0 +1,57 @@
+import os
+from setuptools import setup
+
+# ineedyou
+# I need you, but you need me too.
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="ineedyou",
+    version="0.1.0",
+    description="I need you, but you need me too.",
+    author="Johan Nestaas",
+    author_email="johannestaas@gmail.com",
+    license="GPLv3",
+    keywords="circular",
+    url="https://bitbucket.org/johannestaas/ineedyou",
+    packages=['ineedyou'],
+    package_dir={'ineedyou': 'ineedyou'},
+    long_description=read('README.rst'),
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: Qt',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+    ],
+    install_requires=[
+        'youneedme',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ineedyou=ineedyou:main',
+        ],
+    },
+    # If you get errors running setup.py install:
+    # zip_safe=False,
+    #
+    # For including non-python files:
+    # package_data={
+    #     'ineedyou': ['templates/*.html'],
+    # },
+    # include_package_data=True,
+)
