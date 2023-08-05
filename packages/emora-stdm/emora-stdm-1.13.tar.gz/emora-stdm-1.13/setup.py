3946e516cc4d1fc3a185271f7b6d063ee20e73cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package emora-stdm/1.13/emora-stdm-1.13.tar.gz

---
+++ packages/emora-stdm/emora-stdm-1.13.tar.gz/emora-stdm-1.13/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='emora-stdm',
+     version='1.13',
+     scripts=[],
+     author="James Finch",
+     author_email="jdfinch@emory.edu",
+     description="Library for creating state-machine-based chatbots.",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/emora-chat/EmoraSTDM.git",
+     packages=setuptools.find_packages(),
+     install_requires=[
+         "structpy==0.2",
+         "lark-parser",
+         "regex",
+         "nltk"
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    package_data={
+        'emora_stdm': ['emora_stdm/state_transition_dialogue_manager/data/*']
+    }
+ )
