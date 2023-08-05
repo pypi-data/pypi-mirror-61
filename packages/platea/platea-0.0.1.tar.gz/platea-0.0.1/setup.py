a1b8a1beabc5be4c2526b5f39bfcf4fa313da51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package platea/0.0.1/platea-0.0.1.tar.gz

---
+++ packages/platea/platea-0.0.1.tar.gz/platea-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from numpy.distutils.core import setup, Extension
+
+# python3 -m pip install --user --upgrade setuptools wheel
+# rm -rf build && rm -rf dist && rm -rf *.info
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+################################################################################
+
+def setup_package():
+    """
+    """
+
+    setup(
+        name="platea",
+        version="0.0.1",
+        author="James Montgomery",
+        author_email="jamesoneillmontgomery@gmail.com",
+        packages=setuptools.find_packages()
+    )
+
+if __name__ == "__main__":
+    setup_package()
