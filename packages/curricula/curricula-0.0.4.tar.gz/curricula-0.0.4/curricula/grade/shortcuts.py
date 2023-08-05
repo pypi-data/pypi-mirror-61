6e51fea5f70f2ab9de78667fb09668da85674020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:10 +0000

    Add package curricula/0.0.4/curricula-0.0.4.tar.gz

---
+++ packages/curricula/curricula-0.0.4.tar.gz/curricula-0.0.4/curricula/grade/shortcuts.py
@@ -0,0 +1,8 @@
+from .grader import Grader
+from .setup.build import BuildResult
+from .setup.check import CheckResult
+from .test.correctness import CorrectnessResult
+from .test.complexity import ComplexityResult
+from .test.memory import MemoryResult
+from .teardown.cleanup import CleanupResult
+from .resource import Context, File, Executable, ExecutableFile
