a1929bda2f4f3447a170129d78969ce9f42cafed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:59 +0000

    Add package babble/2020.1.6/babble-2020.1.6.tar.gz

---
+++ packages/babble/babble-2020.1.6.tar.gz/babble-2020.1.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('./README.md') as f:
+        return f.read()
+
+
+setup(
+    name='babble',
+    version='2020.1.6',
+    license='MIT',
+
+    author='Idin',
+    author_email='py@idin.ca',
+    url='https://github.com/idin/babble',
+
+    keywords='PDF',
+    description='Python library for parsing PDFs',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+
+    packages=find_packages(exclude=("jupyter_tests", ".idea", ".git")),
+    install_requires=[],
+    python_requires='~=3.6',
+    zip_safe=True,
+    test_suite='nose.collector',
+    tests_require=['nose', 'coverage']
+)
