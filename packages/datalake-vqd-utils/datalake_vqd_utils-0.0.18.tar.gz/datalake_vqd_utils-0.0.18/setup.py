b2c1a5fcf7ccc790adcb82bbf6db2de7316d9ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:17 +0000

    Add package datalake-vqd-utils/0.0.18/datalake_vqd_utils-0.0.18.tar.gz

---
+++ packages/datalake-vqd-utils/datalake_vqd_utils-0.0.18.tar.gz/datalake_vqd_utils-0.0.18/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datalake_vqd_utils", # Replace with your own username
+    version="0.0.18",
+    author="Casper van Houten",
+    author_email="casper.vanhouten@viqtordavis.com",
+    description="Package containing tools for cloud-based data lakes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
