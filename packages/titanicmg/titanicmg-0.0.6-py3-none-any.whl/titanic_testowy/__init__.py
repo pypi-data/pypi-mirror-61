68f0c8648c8caadfed9b461e73b0ec81fd7ce261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package titanicmg/0.0.6/titanicmg-0.0.6-py3-none-any.whl

---
+++ packages/titanicmg/titanicmg-0.0.6-py3-none-any.whl/titanic_testowy/__init__.py
@@ -0,0 +1,2 @@
+__version__= '0.0.1'
+from titanic_testowy.titanic import TitanicTestowy
---
+++ packages/titanicmg/titanicmg-0.0.6-py3-none-any.whl/titanic_testowy/titanic.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+class TitanicTestowy():
+    def __init__(self, file_or_buffer):
+        self.file_or_buffer = file_or_buffer
+        self.predictions = np.zeros(1)
+
+    def preprocessing(self):
+        df = pd.read_csv(self.file_or_buffer, sep='\t')
+        df['Cabin'] = df['Cabin'].fillna('empty')
+        df['Age'] = df['Age'].fillna(df['Age'].mean())
+        df['CabinLevel'] = np.where(df['Cabin'] != 'empty', df['Cabin'].str[0], 'empty')
+        df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
+        df = pd.get_dummies(df)
+        return df
+
+    def get_predictions(self):
+        df = self.preprocessing()
+        self.y = df['Survived']
+        self.X = df.drop(['Survived'], axis=1)
+        model = LogisticRegression(solver='lbfgs', max_iter=500)
+        model.fit(self.X, self.y)
+        return model.predict(self.X)
+
+    def get_score_recall(self):
+        self.predictions = self.get_predictions()
+        return accuracy_score(self.y, self.predictions)
+
+
+if __name__ == '__main__':
+    test = TitanicTestowy(r'c:/Users/gromim01/titanic.csv')
+    # print(test.get_score_recall())
+    # print(test.preprocessing())
