0c08faca40aa6c11c7e1263c283a5778821a0f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package treedensitycalculator/1.4/treedensitycalculator-1.4.tar.gz

---
+++ packages/treedensitycalculator/treedensitycalculator-1.4.tar.gz/treedensitycalculator-1.4/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-.
+from setuptools import setup, find_packages
+
+setup(
+    name='treedensitycalculator',
+    version='1.4',
+    description='The Tree Density Calculator is a QGIS plugin and CLI package designed to calculate tree densities.',
+    author='Ann Crabbé (KU Leuven); Tinne Cahy (Geo Solutions)',
+    author_email='ann.crabbe@kuleuven.be',
+    packages=find_packages(where="localmaxfilter"),
+    package_dir={
+        '': 'localmaxfilter',
+    },
+    license='This program is free software; you can redistribute it and/or modify it under the terms of the GNU '
+            'General Public License as published by the Free Software Foundation; either version 3 of the License, or '
+            'any later version.',
+    url='https://bitbucket.org/kul-reseco/localmaxfilter',
+    entry_points={
+        'console_scripts':
+            [
+                'treedensity = localmaxfilter.cli.local_max_filter_cli:main'
+            ],
+    }
+)
