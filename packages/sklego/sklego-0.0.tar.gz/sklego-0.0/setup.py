630369f84ad2ab1f590a114fc36b9c3af4095b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:50 +0000

    Add package sklego/0.0/sklego-0.0.tar.gz

---
+++ packages/sklego/sklego-0.0.tar.gz/sklego-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    name="sklego",
+    version="0.0",
+    description="a collection of lego bricks for scikit-learn pipelines",
+    author="Vincent D. Warmerdam & Matthijs Brouns",
+    url="https://scikit-lego.readthedocs.io/en/latest/",
+    long_description='''Use `scikit-lego <https://pypi.python.org/pypi/scikit-lego/>`_ instead.''',
+    install_requires=["scikit-lego"],
+)
+
+
