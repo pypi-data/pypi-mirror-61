ed02a784317b620971a7eaec74c4e40ca1949aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package ineedyou/0.0.1/ineedyou-0.0.1.tar.gz

---
+++ packages/ineedyou/ineedyou-0.0.1.tar.gz/ineedyou-0.0.1/ineedyou/__init__.py
@@ -0,0 +1,12 @@
+'''
+ineedyou
+
+I need you, but you need me too.
+'''
+
+__title__ = 'ineedyou'
+__version__ = '0.0.1'
+__all__ = ()
+__author__ = 'Johan Nestaas <johannestaas@gmail.com'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright 2020 Johan Nestaas'
