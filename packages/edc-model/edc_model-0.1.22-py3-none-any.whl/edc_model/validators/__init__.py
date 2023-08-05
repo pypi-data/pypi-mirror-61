fd6922b6f5755f1cfe7c432e682427dcbef59391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:08 +0000

    Add package edc-model/0.1.22/edc_model-0.1.22-py3-none-any.whl

---
+++ packages/edc-model/edc_model-0.1.22-py3-none-any.whl/edc_model/validators/__init__.py
@@ -0,0 +1,5 @@
+from .bp import bp_validator
+from .date import datetime_is_future, date_is_future
+from .date import datetime_not_future, date_not_future
+from .duration import hm_validator, ym_validator
+from .phone import CellNumber, TelephoneNumber
