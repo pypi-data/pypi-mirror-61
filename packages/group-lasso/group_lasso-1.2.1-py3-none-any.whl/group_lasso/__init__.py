851f30c685c64913c49ecc3beac3a75ea39148e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:09 +0000

    Add package group-lasso/1.2.1/group_lasso-1.2.1-py3-none-any.whl

---
+++ packages/group-lasso/group_lasso-1.2.1-py3-none-any.whl/group_lasso/__init__.py
@@ -0,0 +1,15 @@
+"""Group lasso regularised linear models.
+"""
+
+
+__version__ = "1.2.1"
+__author__ = "Yngve Mardal Moe"
+__email__ = "yngve.m.moe@gmail.com"
+
+
+from group_lasso._group_lasso import (
+    GroupLasso,
+    LogisticGroupLasso,
+    MultinomialGroupLasso,
+    BaseGroupLasso,
+)
