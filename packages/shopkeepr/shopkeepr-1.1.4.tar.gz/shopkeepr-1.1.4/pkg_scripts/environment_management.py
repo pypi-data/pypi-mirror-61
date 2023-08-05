279f6dcded6c3ff4124c7c1f8c9402455750a66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package shopkeepr/1.1.4/shopkeepr-1.1.4.tar.gz

---
+++ packages/shopkeepr/shopkeepr-1.1.4.tar.gz/shopkeepr-1.1.4/pkg_scripts/environment_management.py
@@ -0,0 +1,32 @@
+import subprocess
+
+from pkg_scripts.misc_functions import check_if_exists, open_database, install, is_in_venv
+
+
+def check_virtualenv_installation():
+    try:
+        subprocess.run(["python3", "-m", "venv", "env"])
+    except Exception as e:
+        print(
+            "We were not able to create your environment. The issue might be due to a missing 'python3-venv' package. "
+            "To install the package, run the following command as root:\n\nsudo apt-get install python3-venv\n\nThen "
+            "try again.")
+        print("The error message we received is as follows:\n\n")
+        print(e)
+
+
+def activate_env(db, engine):
+    check_virtualenv_installation()
+    subprocess.run(["source", "env/bin/activate"])
+    print("Environment Activated.")
+
+
+def deactivate_env(db, engine):
+    status = is_in_venv()
+    if status:
+        print("Deactivating environment...")
+        subprocess.run(["deactivate"])
+        print("Environment Deactivated")
+    else:
+        print("No active environments")
+        exit()
