3732bb692d4fecd4f9380a5dd78b50e834a09c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package magic-timer/0.0.11/magic_timer-0.0.11-py3-none-any.whl

---
+++ packages/magic-timer/magic_timer-0.0.11-py3-none-any.whl/magic_timer/magic_timer.py
@@ -0,0 +1,24 @@
+from functools import wraps
+from magic_timer.MagicTimer import MagicTimer
+
+
+def magic_timer(func):
+    """
+    Use this decorator to turn a function into a timed function.
+    When the decorated function has finished running,
+    its runtime will be printed.
+    """
+
+    @wraps(func)
+    def wrapped_function(*args, **kwargs):
+        
+        t = MagicTimer()
+
+        y = func(*args, **kwargs)
+
+        s = "'{}' -".format(func.__name__)
+        print(s, t)
+
+        return y
+
+    return wrapped_function
