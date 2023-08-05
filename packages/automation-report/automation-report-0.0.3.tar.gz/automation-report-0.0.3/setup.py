a2ac9651f1e47488f8353eaeacef706325e41028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package automation-report/0.0.3/automation-report-0.0.3.tar.gz

---
+++ packages/automation-report/automation-report-0.0.3.tar.gz/automation-report-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+  name = 'automation-report',
+  packages = ['automation-report'],
+  version = '0.0.3',
+  license='MIT',
+  description = 'Simple report for your test automation including various cases/steps specifying its valid status.',
+  author = 'Bibek Adhikari',
+  author_email = 'bbekad94@gmail.com',
+  url = 'https://github.com/bibekad123/automation-report',
+  download_url = 'https://github.com/bibekad123/automation-report/archive/0.0.3.tar.gz',
+  keywords = ['automation report', 'report', 'automation'],  
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+  include_package_data=True,
+)
