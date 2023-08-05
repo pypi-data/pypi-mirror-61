078c0972253ac580f36195fc56ed5eae4bad2ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package dicom-factory/0.0.2/dicom-factory-0.0.2.tar.gz

---
+++ packages/dicom-factory/dicom-factory-0.0.2.tar.gz/dicom-factory-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', mode='r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dicom-factory',
+    version='0.0.2',
+    author='Josep de Cid',
+    author_email='josep.de.cid@gmail.com',
+    description='DICOM data generator for (mainly) testing purposes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jdecid/DICOM-Factory',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
