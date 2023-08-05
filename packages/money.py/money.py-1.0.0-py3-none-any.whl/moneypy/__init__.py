f0e3937e22df3fc5e1295a4174e78f765b6d5433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package money.py/1.0.0/money.py-1.0.0-py3-none-any.whl

---
+++ packages/money.py/money.py-1.0.0-py3-none-any.whl/moneypy/__init__.py
@@ -0,0 +1,11 @@
+"""
+A library to handle money.
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Edgar Luque'
+__license__ = 'MIT'
+
+from .currency import Currency
+from .money import Money
+from .currencies import *
