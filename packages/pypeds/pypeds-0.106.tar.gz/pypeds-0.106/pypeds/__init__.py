4dde6a5f532d3b9388e6f38fec9db6428c99fc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package pypeds/0.106/pypeds-0.106.tar.gz

---
+++ packages/pypeds/pypeds-0.106.tar.gz/pypeds-0.106/pypeds/__init__.py
@@ -0,0 +1,9 @@
+"""A python package to help facilitate the collection and analysis of education-related datasets. """
+__version__ = '0.106'
+
+from pypeds.ipeds import *
+from pypeds.datasets import *
+from pypeds.views import *
+
+
+
