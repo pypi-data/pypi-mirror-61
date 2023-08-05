6e80362c4715712e133169bc77fbc9af1a34ee45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:14 +0000

    Add package wilfred/0.4.0/wilfred-0.4.0.tar.gz

---
+++ packages/wilfred/wilfred-0.4.0.tar.gz/wilfred-0.4.0/wilfred/keyboard.py
@@ -0,0 +1,25 @@
+#################################################################
+#                                                               #
+# Wilfred                                                       #
+# Copyright (C) 2020, Vilhelm Prytz, <vilhelm@prytznet.se>      #
+#                                                               #
+# Licensed under the terms of the MIT license, see LICENSE.     #
+# https://github.com/wilfred-dev/wilfred                        #
+#                                                               #
+#################################################################
+
+import threading
+
+
+class KeyboardThread(threading.Thread):
+    def __init__(self, input_callback, params):
+        self.input_callback = input_callback
+        self.params = params
+
+        super(KeyboardThread, self).__init__(name="wilfred-console-input", daemon=True)
+
+        self.start()
+
+    def run(self):
+        while True:
+            self.input_callback(input(), self.params)
---
+++ packages/wilfred/wilfred-0.4.0.tar.gz/wilfred-0.4.0/wilfred/version.py
@@ -0,0 +1,16 @@
+#################################################################
+#                                                               #
+# Wilfred                                                       #
+# Copyright (C) 2020, Vilhelm Prytz, <vilhelm@prytznet.se>      #
+#                                                               #
+# Licensed under the terms of the MIT license, see LICENSE.     #
+# https://github.com/wilfred-dev/wilfred                        #
+#                                                               #
+#################################################################
+
+# use "0.0.0.dev0" to indicate incremental commit build
+# specify version "0.3.0" to indicate standard release
+# "commit_date" should be replaced upon building
+version = "0.4.0"
+commit_hash = "development"
+commit_date = "YYYY-MM-DD"
