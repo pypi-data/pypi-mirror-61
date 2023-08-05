d4fc9b55e0913503af590d5464d77b407b212dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:15 +0000

    Add package asteroid/0.0.1/asteroid-0.0.1.tar.gz

---
+++ packages/asteroid/asteroid-0.0.1.tar.gz/asteroid-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import asteroid
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='asteroid',
+    version=asteroid.__version__,
+    author='Manuel Pariente',
+    author_email='manuel.pariente@loria.fr',
+    url="https://github.com/mpariente/AsSteroid",
+    description='PyTorch-based audio source separation toolkit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=['numpy',
+                      'scipy',
+                      'pandas',
+                      'pyyaml',
+                      'soundfile',
+                      'torch',
+                      'torchvision',
+                      'pytorch-lightning'],
+    extras_require={
+        'visualize': ['seaborn>=0.9.0'],
+        'tests': ['pytest']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
