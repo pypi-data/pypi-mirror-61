8ab13c45d078c3a6167bea996d8beb6fae38e9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:38 +0000

    Add package generativepoetry/0.2.2/generativepoetry-0.2.2.tar.gz

---
+++ packages/generativepoetry/generativepoetry-0.2.2.tar.gz/generativepoetry-0.2.2/CHANGELOG.md
@@ -0,0 +1,36 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [0.2.2] 2020-02-12
+- Added missing dependencies to setup.py
+
+## [0.2.1] 2020-02-12
+- Added console-menu for ease of use
+- Added filters for hate words
+- Corrected a vs an agreement in poems generated via markov method
+
+## [0.2.0] - 2020-01-13
+- Added frequently following word(s) functions
+- similar_meaning_words, similar_sounding_words, etc. (plural functions) now also accept list of strings as argument
+- Added markov chain algorithm for constructing poem lines
+- Added PDF generation methods
+- Added more cases to 'too similar'
+
+## [0.1.3] - 2019-08-18
+- Added related_rare_word(s) functions.
+- Fixed issue where first and second words of poem could be too similar
+
+## [0.1.2] - 2019-07-29
+- Fixed readme so example image will show on Pypi
+
+## [0.1.1] - 2019-07-29
+### Added
+- Added example image to readme.
+- Added datamuse_api_max option to phonetically_related_words function.
+
+### [0.1.0]
+- Renamed frequently_intratextually_coappearing_word(s) functions to contextually_linked_word(s).
+- Defined public api with __all___.
+- Revised readme's installation instructions.
+- Updated module docstrings, adding further detail and fixing errors.
+- Fixed bug where wrong results were being returned when datamuse_api_max set to None.
---
+++ packages/generativepoetry/generativepoetry-0.2.2.tar.gz/generativepoetry-0.2.2/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Corey Bobco'
+__email__ = 'corey.bobco@gmail.com'
+__version__ = '0.2.2'
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'console-menu==0.6.0',
+    'hunspell>=0.5.5',
+    'nltk==3.4.5',
+    'pdf2image==1.11.0',
+    'pronouncing>=0.2.0',
+    'python-datamuse==1.2.1',
+    'reportlab>=3.5.26',
+    'unittest2==1.1.0',
+    'wordfreq>=2.2.1',
+]
+
+setup(
+    name='generativepoetry',
+    version='0.2.2',
+    description='A library primarily for procedurally generating visual poems',
+    long_description=readme,
+    author="Corey Bobco",
+    author_email='corey.bobco@gmail.com',
+    url='https://github.com/coreybobco/generativepoetry-py',
+    packages=[
+        'generativepoetry',
+    ],
+    package_dir={'generativepoetry':
+                 'generativepoetry'},
+    package_data={'generativepoetry': ['data/hate_words.txt', 'data/abbreviations_etc.txt']},
+    install_requires=requirements,
+    scripts=['bin/generative-poetry-cli'],
+    license="MIT",
+    zip_safe=True,
+    keywords='poetry',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Artistic Software",
+    ],
+    test_suite='tests',
+)
