5aa5a991e59d4dce488418fcd3f89977aab62e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:23 +0000

    Add package zxcv/2.0/zxcv-2.0.tar.gz

---
+++ packages/zxcv/zxcv-2.0.tar.gz/zxcv-2.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="zxcv",
+    version="2.0",
+    description="A Python package to play tic tac toe",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tirth-2001/ttt.git",
+    author="Tirth Patel",
+    author_email="tirthgpatel.27@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["zxcv"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "zxcv=lkj.call:source",
+        ]
+   
+    },
+)
