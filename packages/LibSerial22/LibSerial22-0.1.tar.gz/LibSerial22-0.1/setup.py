f928faf6b652b44e6a6873cf5e5930c8e0d5ee7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:18 +0000

    Add package LibSerial22/0.1/LibSerial22-0.1.tar.gz

---
+++ packages/LibSerial22/LibSerial22-0.1.tar.gz/LibSerial22-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'LibSerial22',       
+  packages = ['LibSerial22'],   
+  version = '0.1',      
+  license='MIT',       
+  description = 'Biblioteca LibSerial22 ',   
+  author = 'Ederson SantAna',                  
+  author_email = 'edersansil@gmail.com',     
+  url = 'https://github.com/EdersonSantAna/LibSerial22',  
+  download_url = 'https://github.com/EdersonSantAna/LibSerial22/archive/0.1.tar.gz',    
+  keywords = ['Serial', 'Senai', 'ComPort'],  
+  install_requires=[           
+          'datetime',
+          'random',         
+      ],
+  classifiers=[
+    #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"   
+    'Development Status :: 5 - Production/Stable',   
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
