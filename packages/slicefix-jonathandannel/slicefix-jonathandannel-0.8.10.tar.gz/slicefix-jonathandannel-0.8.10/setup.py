acab851b5ea38ffd226acd0f4b014942115de914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:28 +0000

    Add package slicefix-jonathandannel/0.8.10/slicefix-jonathandannel-0.8.10.tar.gz

---
+++ packages/slicefix-jonathandannel/slicefix-jonathandannel-0.8.10.tar.gz/slicefix-jonathandannel-0.8.10/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slicefix-jonathandannel", # Replace with your own username
+    version="0.8.10",
+    author="Jonathan Dannel",
+    author_email="usixjad@gmail.com",
+    description="A simple script for formatting Photoshop HTML generated from image slicing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonathandannel/slicefixx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+    'bs4',
+    'datetime'
+    ],
+    entry_points={
+        "console_scripts": [
+            "slicefix = slicefix:fix_and_save",
+        ]
+    } 
+)
