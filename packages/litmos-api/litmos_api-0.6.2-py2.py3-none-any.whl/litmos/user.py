686c2114bee53c6dbf92e3af938dfd1453c088ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:59 +0000

    Add package litmos-api/0.6.2/litmos_api-0.6.2-py2.py3-none-any.whl

---
+++ packages/litmos-api/litmos_api-0.6.2-py2.py3-none-any.whl/litmos/user.py
@@ -0,0 +1,66 @@
+from collections import OrderedDict
+
+from litmos.api import API
+from litmos.litmos import LitmosType
+
+
+class User(LitmosType):
+    SCHEMA = OrderedDict([
+        ('Id', ''),
+        ('UserName', ''),
+        ('FirstName', ''),
+        ('LastName', ''),
+        ('FullName', ''),
+        ('Email', ''),
+        ('AccessLevel', 'Learner'),
+        ('DisableMessages', False),
+        ('Active', True),
+        ('Skype', ''),
+        ('PhoneWork', ''),
+        ('PhoneMobile', ''),
+        ('LastLogin', ''),
+        ('LoginKey', ''),
+        ('IsCustomUsername', False),
+        ('Password', ''),
+        ('SkipFirstLogin', True),
+        ('TimeZone', 'UTC'),
+        ('Street1', ''),
+        ('Street2', ''),
+        ('City', ''),
+        ('State', ''),
+        ('PostalCode', ''),
+        ('Country', ''),
+        ('CompanyName', ''),
+        ('JobTitle', ''),
+        ('CustomField1', ''),
+        ('CustomField2', ''),
+        ('CustomField4', ''),
+        ('CustomField5', ''),
+        ('CustomField6', ''),
+        ('CustomField7', ''),
+        ('CustomField8', ''),
+        ('CustomField9', ''),
+        ('CustomField10', ''),
+        ('Culture', ''),
+    ])
+
+    def deactivate(self):
+        self.Active = False
+        return self.save()
+
+    def remove_teams(self):
+        return API.remove_sub_resource(
+            self.__class__.name(),
+            self.Id,
+            'teams',
+            None
+        )
+
+    @classmethod
+    def all(cls, full_details=False):
+        users = super().all()
+
+        if not full_details:
+            return users
+
+        return [cls.find(user.Id) for user in users]
