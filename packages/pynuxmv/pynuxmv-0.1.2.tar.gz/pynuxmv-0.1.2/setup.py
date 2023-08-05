dfb82b89d9ddb036fb1a0b15d9c6dd3792f427aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:56 +0000

    Add package pynuxmv/0.1.2/pynuxmv-0.1.2.tar.gz

---
+++ packages/pynuxmv/pynuxmv-0.1.2.tar.gz/pynuxmv-0.1.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_desc = f.read()
+    
+setuptools.setup(
+    name="pynuxmv",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    version="0.1.2",
+    author="Matteo Cavada",
+    author_email="cvd00@insicuri.net",
+    description="Transpile Python to nuXmv source code",
+    url="https://github.com/mattyonweb/pynuxmv",
+    packages=["pynuxmv"],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'pynuXmv = pynuxmv.cli:clio',
+        ],
+    },
+    extras_require={
+        "Pretty printing":  ["astpretty"],
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Testing"
+    ],
+)
