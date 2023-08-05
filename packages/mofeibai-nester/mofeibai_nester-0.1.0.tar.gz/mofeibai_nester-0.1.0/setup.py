5ba6a44f95e6906b70c9f71439ff811a0d41d77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:30 +0000

    Add package mofeibai-nester/0.1.0/mofeibai_nester-0.1.0.tar.gz

---
+++ packages/mofeibai-nester/mofeibai_nester-0.1.0.tar.gz/mofeibai_nester-0.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="mofeibai_nester",
+    version="0.1.0",
+    author="Mofeibai",
+    description="A simple printer of nested lists.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
