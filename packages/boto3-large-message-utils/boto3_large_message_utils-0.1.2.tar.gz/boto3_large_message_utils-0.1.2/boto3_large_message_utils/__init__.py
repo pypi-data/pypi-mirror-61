73a47d2888d3beb56776179978c4da5a6619d26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:42 +0000

    Add package boto3-large-message-utils/0.1.2/boto3_large_message_utils-0.1.2.tar.gz

---
+++ packages/boto3-large-message-utils/boto3_large_message_utils-0.1.2.tar.gz/boto3_large_message_utils-0.1.2/boto3_large_message_utils/__init__.py
@@ -0,0 +1,6 @@
+from boto3_large_message_utils.builder import LargeMessageBuilder
+from boto3_large_message_utils.parser import LargeMessageParser
+
+__all__ = ["LargeMessageBuilder", "LargeMessageParser"]
+
+__version__ = "0.1.2"
---
+++ packages/boto3-large-message-utils/boto3_large_message_utils-0.1.2.tar.gz/boto3_large_message_utils-0.1.2/boto3_large_message_utils/parser.py
@@ -0,0 +1,64 @@
+import json
+from json import JSONDecodeError
+
+import boto3
+
+from boto3_large_message_utils.utils.compression import (
+    decode_and_decompress_string,
+    decompress_string,
+)
+from boto3_large_message_utils.exceptions import DecompressionError
+
+
+class LargeMessageParser:
+    def __init__(self, session=None):
+        if session:
+            self.s3 = session.client("s3")
+        self.s3 = boto3.client("s3")
+
+    def parse_json(self, json_message):
+        if not isinstance(json_message, dict):
+            raise ValueError('"message" argument expects type "dict"')
+        message = self._parse_contents(json_message)
+        return json.loads(message)
+
+    def _parse_contents(self, json_message):
+        try:
+            if json_message.get("compressedMessage"):
+                return decode_and_decompress_string(
+                    json_message.get("compressedMessage")
+                )
+            if json_message.get("bucket"):
+                return self._retrieve_message_from_s3(
+                    bucket=json_message["bucket"],
+                    key=json_message["key"],
+                    compressed=json_message["compressed"],
+                )
+            return json_message
+        except (KeyError, JSONDecodeError):
+            return json_message
+        except DecompressionError:
+            raise DecompressionError('"message" could not be decompressed')
+
+    def parse(self, message):
+        if not isinstance(message, str):
+            raise ValueError('"message" argument expects type "str"')
+        try:
+            json_message = json.loads(message)
+            return self._parse_contents(json_message)
+        except (KeyError, JSONDecodeError):
+            return message
+        except DecompressionError:
+            raise DecompressionError('"message" could not be decompressed')
+
+    def _retrieve_message_from_s3(self, bucket, key, compressed=False):
+        try:
+            response = self.s3.get_object(Bucket=bucket, Key=key)
+            body = response["Body"].read()
+            if compressed:
+                return decompress_string(body)
+
+            return body.decode("utf-8")
+        except Exception as e:
+            print("Error retrieving message from S3")
+            raise e
