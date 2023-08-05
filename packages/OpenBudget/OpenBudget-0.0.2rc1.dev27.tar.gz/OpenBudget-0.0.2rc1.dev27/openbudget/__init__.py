c04f88bc04b4f63f7d585ca863c2d7bb580b5b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package OpenBudget/0.0.2rc1.dev27/OpenBudget-0.0.2rc1.dev27.tar.gz

---
+++ packages/OpenBudget/OpenBudget-0.0.2rc1.dev27.tar.gz/OpenBudget-0.0.2rc1.dev27/openbudget/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Simple budget management planner
+# Copyright (C) 2019 Yapbreak
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+Top-level package for OpenBudget.
+"""
+import configparser
+
+
+__author__ = "Yapbreak"
+__email__ = 'yapbreak@yapbreak.fr'
+__version__ = '0.0.2.rc1.dev27'
+
+__config__ = configparser.ConfigParser()
