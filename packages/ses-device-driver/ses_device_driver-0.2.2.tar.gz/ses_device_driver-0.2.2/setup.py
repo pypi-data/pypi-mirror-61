d5fd05a90742590f4d789facc8a0ebae8f6ddad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:39 +0000

    Add package ses-device-driver/0.2.2/ses_device_driver-0.2.2.tar.gz

---
+++ packages/ses-device-driver/ses_device_driver-0.2.2.tar.gz/ses_device_driver-0.2.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ses_device_driver', 'ses_device_driver.filesystem', 'ses_device_driver.ur10']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyyaml>=5.3,<6.0']
+
+setup_kwargs = {
+    'name': 'ses-device-driver',
+    'version': '0.2.2',
+    'description': 'Device drivers/helper classes for each device in the ses of Werk150',
+    'long_description': None,
+    'author': 'Vinu ESB',
+    'author_email': 'vinu.vijayakumaran_nair@reutlingen-university.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
