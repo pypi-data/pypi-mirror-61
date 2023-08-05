64400bd7e25982472534ec5b043ebe902206c94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:05 +0000

    Add package sbanken-api/0.2.1/sbanken-api-0.2.1.tar.gz

---
+++ packages/sbanken-api/sbanken-api-0.2.1.tar.gz/sbanken-api-0.2.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="sbanken-api",
+    version="0.2.1",
+    description="Easily communicate with Sbanken API in python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Rolf Klausen",
+    author_email="redacted@mail.com",
+    url="https://github.com/abstrakct/sbanken-python",
+    download_url="https://github.com/abstrakct/sbanken-python/archive/0.2.tar.gz",
+    packages=["sbanken"],
+    install_requires=[
+        "certifi",
+        "chardet",
+        "idna",
+        "oauthlib",
+        "requests",
+        "requests-oauthlib",
+        "urllib3",
+    ],
+    classifiers=[  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Office/Business :: Financial",
+    ],
+)
