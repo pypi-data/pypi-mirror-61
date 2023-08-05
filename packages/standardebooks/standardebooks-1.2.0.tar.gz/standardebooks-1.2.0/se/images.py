dddfc7676ca1ed2f0a3ff17f9c830d048e73c4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:09 +0000

    Add package standardebooks/1.2.0/standardebooks-1.2.0.tar.gz

---
+++ packages/standardebooks/standardebooks-1.2.0.tar.gz/standardebooks-1.2.0/se/images.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""
+Defines various functions useful for image processing tasks common to epubs.
+"""
+
+import subprocess
+from pathlib import Path
+import shutil
+import tempfile
+import regex
+import psutil
+import se
+import se.formatting
+
+
+def render_mathml_to_png(mathml: str, output_filename: Path) -> None:
+	"""
+	Render a string of MathML into a transparent PNG file.
+
+	INPUTS
+	mathml: A string of MathML
+	output_filename: A filename to store PNG output to
+
+	OUTPUTS
+	A string of XHTML with soft hyphens inserted in words. The output is not guaranteed to be pretty-printed.
+	"""
+
+	firefox_path = se.get_firefox_path()
+	convert_path = shutil.which("convert")
+
+	if convert_path is None:
+		raise se.MissingDependencyException("Couldn’t locate imagemagick. Is it installed?")
+
+	if "firefox" in (p.name() for p in psutil.process_iter()):
+		raise se.FirefoxRunningException("Firefox is required to render MathML, but it’s currently running. Stop all instances of Firefox and try again.")
+
+	with tempfile.NamedTemporaryFile(mode="w+") as mathml_file:
+		with tempfile.NamedTemporaryFile(mode="w+", suffix=".png") as png_file:
+			mathml_file.write(f"<!doctype html><html><head><meta charset=\"utf-8\"><title>MathML fragment</title></head><body>{mathml}</body></html>")
+			mathml_file.seek(0)
+
+			subprocess.call([firefox_path, "-screenshot", png_file.name, f"file://{mathml_file.name}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+			subprocess.call([convert_path, png_file.name, "-fuzz", "10%", "-transparent", "white", "-trim", output_filename])
+
+def format_inkscape_svg(filename: Path):
+	"""
+	Clean and format SVGs created by Inkscape, which have lots of useless metadata.
+
+	INPUTS
+	filename: A filename of an Inkkscape SVG
+
+	OUTPUTS
+	None.
+	"""
+
+	with open(filename, "r+", encoding="utf-8") as file:
+		svg = file.read()
+
+		# Time to clean up Inkscape's mess
+		svg = regex.sub(r"id=\"[^\"]+?\"", "", svg)
+		svg = regex.sub(r"<metadata[^>]*?>.*?</metadata>", "", svg, flags=regex.DOTALL)
+		svg = regex.sub(r"<defs[^>]*?/>", "", svg)
+		svg = regex.sub(r"xmlns:(dc|cc|rdf)=\"[^\"]*?\"", "", svg)
+
+		# Inkscape includes CSS even though we've removed font information
+		svg = regex.sub(r" style=\".*?\"", "", svg)
+
+		svg = se.formatting.format_xhtml(svg)
+
+		file.seek(0)
+		file.write(svg)
+		file.truncate()
+
+def remove_image_metadata(filename: Path) -> None:
+	"""
+	Remove exif metadata from an image.
+
+	INPUTS
+	filename: A filename of an image
+
+	OUTPUTS
+	None.
+	"""
+
+	which_exiftool = shutil.which("exiftool")
+	if which_exiftool:
+		exiftool_path = Path(which_exiftool)
+	else:
+		raise se.MissingDependencyException("Couldn’t locate exiftool. Is it installed?")
+
+	# Path arguments must be cast to string for Windows compatibility.
+	subprocess.run([str(exiftool_path), "-overwrite_original", "-all=", str(filename)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
---
+++ packages/standardebooks/standardebooks-1.2.0.tar.gz/standardebooks-1.2.0/setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+The setup script used to package the se library and executables.
+
+To build the project, enter the project's root directory and do:
+python3 setup.py bdist_wheel
+
+After the project has been built, you can install it locally:
+pip3 install dist/standardebooks-*.whl
+
+To upload the build to pypi, twine is required:
+pip3 install twine
+"""
+
+import re
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+# Get the long description from the README file
+def _get_file_contents(file_path: Path) -> str:
+    """
+    Helper function to get README contents
+    """
+
+    with open(file_path, encoding="utf-8") as file:
+        return file.read()
+
+def _get_version() -> str:
+    """
+    Helper function to get VERSION from source code
+    """
+
+    source_path = Path("se/__init__.py")
+    contents = _get_file_contents(source_path)
+    match = re.search(r'^VERSION = "([^"]+)"$', contents, flags=re.MULTILINE)
+    if not match:
+        raise RuntimeError(f"VERSION not found in {source_path}")
+    return match.group(1)
+
+setup(
+    version=_get_version(),
+    name="standardebooks",
+    description="The toolset used to produce Standard Ebooks epub ebooks.",
+    long_description=_get_file_contents(Path(__file__).resolve().parent / "README.md"),
+    long_description_content_type="text/markdown",
+    url="https://standardebooks.org",
+    author="Standard Ebooks",
+    author_email="admin@standardebooks.org",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+    ],
+    keywords="ebooks epub",
+    packages=find_packages(),
+    python_requires=">=3.6", # The latest version installed by default on Ubuntu 18.04 is 3.6.9
+    install_requires=[
+        "beautifulsoup4==4.8.0",
+        "cssselect==1.0.3",
+        "ftfy==5.5.1",
+        "gitpython==2.1.5",
+        "importlib_resources==1.0.2",
+        "lxml==4.4.0",
+        "psutil==5.5.0",
+        "pyhyphen==3.0.1",
+        "tinycss2==1.0.2",
+        # "pyopenssl>=19.0.0",  # Required to allows the `requests` package to use https on Mac OSX, but segfaults when installed on Ubuntu
+        "python-magic==0.4.13",
+        "regex==2017.7.26",
+        "requests>=2.20.0",
+        "roman==3.2.0",
+        "smartypants==2.0.1",
+        "titlecase==0.11.0",
+        "termcolor==1.1.0",
+        "terminaltables==3.1.0"
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "se = se.executables:main",
+        ],
+    },
+    project_urls={
+        "Source": "https://github.com/standardebooks/tools/",
+    }
+)
