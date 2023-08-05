105a59efd3f213fb1ca24d783c2ed71f37d95445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:17 +0000

    Add package subprocrunner/1.0.0/subprocrunner-1.0.0.tar.gz

---
+++ packages/subprocrunner/subprocrunner-1.0.0.tar.gz/subprocrunner-1.0.0/test/test_logger.py
@@ -0,0 +1,14 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+
+import pytest
+
+from subprocrunner import set_logger
+
+
+class Test_set_logger:
+    @pytest.mark.parametrize(["value"], [[True], [False]])
+    def test_smoke(self, value):
+        set_logger(value)
