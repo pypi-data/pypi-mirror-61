7fd7f01877e398ac71a0abc5407c0d6c18bf0401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:02 +0000

    Add package mozilla-aws-cli-mozilla/1.3.0/mozilla_aws_cli_mozilla-1.3.0.tar.gz

---
+++ packages/mozilla-aws-cli-mozilla/mozilla_aws_cli_mozilla-1.3.0.tar.gz/mozilla_aws_cli_mozilla-1.3.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="mozilla_aws_cli_mozilla",
+    description="Mozilla specific deployment of the mozilla_aws_cli",
+    author="Mozilla Enterprise Information Security",
+    author_email="iam@discourse.mozilla.org",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires=["mozilla_aws_cli>=1.1.0"],
+    keywords="maws Mozilla AWS CLI config",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=["mozilla_aws_cli_config"],
+    url="https://github.com/mozilla-iam/mozilla-aws-cli-mozilla",
+    version="1.3.0",
+    zip_safe=False,
+)
