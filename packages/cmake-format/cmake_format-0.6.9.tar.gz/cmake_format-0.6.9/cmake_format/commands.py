b54b7cc7b3212223b47ac9202bfe0f0cae1e9acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package cmake-format/0.6.9/cmake_format-0.6.9.tar.gz

---
+++ packages/cmake-format/cmake_format-0.6.9.tar.gz/cmake_format-0.6.9/cmake_format/commands.py
@@ -0,0 +1,502 @@
+"""
+Command specifications for cmake built-in commands.
+"""
+
+from __future__ import unicode_literals
+import sys
+
+from cmake_format.common import UserError
+from cmake_format.parse.util import PositionalSpec
+
+ZERO_OR_MORE = '*'
+ONE_OR_MORE = '+'
+
+if sys.version_info[0] < 3:
+  STRING_TYPES = (str, unicode)
+else:
+  STRING_TYPES = (str,)
+
+CONDITIONAL_FLAGS = [
+    "COMMAND",
+    "DEFINED",
+    "EQUAL",
+    "EXISTS",
+    "GREATER",
+    "LESS",
+    "IS_ABSOLUTE",
+    "IS_DIRECTORY",
+    "IS_NEWER_THAN",
+    "IS_SYMLINK",
+    "MATCHES",
+    "NOT",
+    "POLICY",
+    "STRLESS",
+    "STRGREATER",
+    "STREQUAL",
+    "TARGET",
+    "TEST",
+    "VERSION_EQUAL",
+    "VERSION_GREATER",
+    "VERSION_LESS",
+]
+
+CANONICAL_SPELLINGS = {
+    "FetchContent_Declare",
+    "FetchContent_MakeAvailable",
+    "FetchContent_GetProperties",
+    "FetchContent_Populate",
+    "ExternalProject_Add",
+    "ExternalProject_Add_Step",
+    "ExternalProject_Add_StepDependencies",
+    "ExternalProject_Add_StepTargets",
+    "ExternalProject_Get_Property",
+}
+
+
+def get_default_config():
+  """
+  Return the default per-command configuration database
+  """
+  per_command = {}
+  for spelling in CANONICAL_SPELLINGS:
+    per_command[spelling.lower()] = {
+        "spelling": spelling
+    }
+
+  return per_command
+
+
+def make_conditional_spec(name=None):
+  if name is None:
+    name = '<conditional>'
+  flags = list(CONDITIONAL_FLAGS)
+  spec = CommandSpec(name, pargs='+', flags=list(flags))
+  return CommandSpec(name, pargs='+', flags=list(flags),
+                     kwargs={'AND': spec, 'OR': spec})
+
+
+def parse_pspec(pargs, flags):
+  """
+  Parse a positional argument specification.
+  """
+  out = []
+
+  # Default pargs is "*"
+  if pargs is None:
+    pargs = ZERO_OR_MORE
+
+  # If we only have one scalar specification, return a legacy specification
+  if isinstance(pargs, STRING_TYPES + (int,)):
+    return [PositionalSpec(pargs, flags=flags, legacy=True)]
+
+  if flags:
+    raise UserError(
+        "Illegal use of top-level 'flags' keyword with new-style positional"
+        " argument declaration")
+
+  # If we only have one dictionary specification, then put it in a dictionary
+  # so that we can do the rest consistently
+  if isinstance(pargs, dict):
+    pargs = [pargs]
+
+  for pargdecl in pargs:
+    if isinstance(pargdecl, STRING_TYPES + (int,)):
+      # A scalar declaration is interpreted as npargs
+      out.append(PositionalSpec(pargdecl))
+      continue
+
+    if isinstance(pargdecl, dict):
+        # A dictionary is interpreted as init kwargs
+      if "npargs" not in pargdecl:
+        pargdecl = dict(pargdecl)
+        pargdecl["nargs"] = ZERO_OR_MORE
+      out.append(PositionalSpec(**pargdecl))
+      continue
+
+    if isinstance(pargdecl, (list, tuple)):
+        # A list or tuple is interpreted as (*args, [kwargs])
+      args = list(pargdecl)
+      kwargs = {}
+      if isinstance(args[-1], dict):
+        kwargs = args.pop(-1)
+      out.append(PositionalSpec(*args, **kwargs))
+
+  return out
+
+
+class CommandSpec(object):
+  """
+  A command specification is primarily a dictionary mapping keyword arguments
+  to command specifications. It also includes a command name and number of
+  positional arguments
+  """
+
+  def __init__(self, name, pargs=None, flags=None, kwargs=None):
+    super(CommandSpec, self).__init__()
+    scalar_types = (int,) + STRING_TYPES
+
+    self.name = name
+
+    try:
+      self.pargs = parse_pspec(pargs, flags)
+    except (TypeError, UserError) as ex:
+      message = (
+          "Invalid user-supplied specification for positional arguments of "
+          "{}:\n{}".format(name, ex))
+      raise UserError(message)
+
+    self.kwargs = {}
+
+    if kwargs is not None:
+      if isinstance(kwargs, dict):
+        items = kwargs.items()
+      elif isinstance(kwargs, (list, tuple)):
+        items = list(kwargs)
+      for keyword, spec in items:
+        if isinstance(spec, scalar_types):
+          self.kwargs[keyword] = CommandSpec(name=keyword, pargs=spec)
+        elif isinstance(spec, CommandSpec):
+          self.kwargs[keyword] = spec
+        elif isinstance(spec, dict):
+          self.kwargs[keyword] = CommandSpec(name=keyword, **spec)
+        else:
+          raise ValueError("Unexpected type '{}' for kwargs"
+                           .format(type(kwargs)))
+
+  def is_flag(self, key):
+    return self.kwargs.get(key, None) == 0
+
+  def is_kwarg(self, key):
+    subspec = self.kwargs.get(key, None)
+    if subspec is None:
+      return False
+    if isinstance(subspec, int):
+      return subspec != 0
+    if isinstance(subspec, STRING_TYPES + (CommandSpec,)):
+      return True
+    raise ValueError("Unexpected kwargspec for {}: {}"
+                     .format(key, type(subspec)))
+
+  def add(self, name, pargs=None, flags=None, kwargs=None):
+    self.kwargs[name.lower()] = CommandSpec(name, pargs, flags, kwargs)
+
+  def add_conditional(self, name):
+    self.kwargs[name.lower()] = make_conditional_spec(name)
+
+
+def get_fn_spec():
+  """
+  Return a dictionary mapping cmake function names to a dictionary containing
+  kwarg specifications.
+  """
+
+  fn_spec = CommandSpec('<root>')
+
+  fn_spec.add(
+      "add_test",
+      flags=[],
+      kwargs={
+          "COMMAND": dict(
+              pargs=ONE_OR_MORE,
+              kwargs=[('ARGS', ZERO_OR_MORE)]
+          ),
+          "CONFIGURATIONS": ZERO_OR_MORE,
+          "NAME": ZERO_OR_MORE,
+          "WORKING_DIRECTORY": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "cmake_host_system_information",
+      flags=[],
+      kwargs={
+          "QUERY": ZERO_OR_MORE,
+          "RESULT": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "cmake_minimum_required",
+      flags=["FATAL_ERROR"],
+      kwargs={"VERSION": ZERO_OR_MORE})
+
+  fn_spec.add(
+      "cmake_minimum_required_version",
+      flags=[],
+      kwargs={"VERSION": ZERO_OR_MORE})
+
+  fn_spec.add(
+      "configure_file",
+      flags=["@ONLY", "COPYONLY", "ESCAPE_QUOTES"],
+      kwargs={"NEWLINE_STYLE": ZERO_OR_MORE})
+
+  fn_spec.add(
+      "define_property",
+      flags=["CACHED_VARIABLE", "DIRECTORY", "GLOBAL", "INHERITED",
+             "SOURCE", "TARGET", "TEST", "VARIABLE"],
+      kwargs={
+          "BRIEF_DOCS": ZERO_OR_MORE,
+          "FULL_DOCS": ZERO_OR_MORE,
+          "PROPERTY": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "enable_language",
+      flags=["OPTIONAL"],
+      kwargs={})
+
+  fn_spec.add(
+      "execute_process",
+      flags=["ERROR_QUIET", "ERROR_STRIP_TRAILING_WHITESPACE",
+             "OUTPUT_QUIET", "OUTPUT_STRIP_TRAILING_WHITESPACE"],
+      kwargs={
+          "COMMAND": ZERO_OR_MORE,
+          "ERROR_FILE": ZERO_OR_MORE,
+          "ERROR_VARIABLE": ZERO_OR_MORE,
+          "INPUT_FILE": ZERO_OR_MORE,
+          "OUTPUT_FILE": ZERO_OR_MORE,
+          "OUTPUT_VARIABLE": ZERO_OR_MORE,
+          "RESULT_VARIABLE": ZERO_OR_MORE,
+          "TIMEOUT": ZERO_OR_MORE,
+          "WORKING_DIRECTORY": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "export",
+      flags=["APPEND", "EXPORT_LINK_INTERFACE_LIBRARIES"],
+      kwargs={
+          "ANDROID_MK": 1,
+          "EXPORT": 1,
+          "FILE": 1,
+          "NAMESPACE": 1,
+          "PACKAGE": 1,
+          "TARGETS": ONE_OR_MORE
+      })
+
+  fn_spec.add(
+      "find_file",
+      flags=["CMAKE_FIND_ROOT_PATH_BOTH", "NO_CMAKE_ENVIRONMENT_PATH",
+             "NO_CMAKE_FIND_ROOT_PATH", "NO_CMAKE_PATH",
+             "NO_CMAKE_SYSTEM_PATH", "NO_DEFAULT_PATH",
+             "NO_SYSTEM_ENVIRONMENT_PATH",
+             "ONLY_CMAKE_FIND_ROOT_PATH"],
+      kwargs={
+          "DOC": ZERO_OR_MORE,
+          "HINTS": ZERO_OR_MORE,
+          "NAMES": ZERO_OR_MORE,
+          "PATHS": ZERO_OR_MORE,
+          "PATH_SUFFIXES": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "find_library",
+      flags=["CMAKE_FIND_ROOT_PATH_BOTH", "NO_CMAKE_ENVIRONMENT_PATH",
+             "NO_CMAKE_FIND_ROOT_PATH", "NO_CMAKE_PATH",
+             "NO_CMKE_SYSTEM_PATH", "NO_DEFAULT_PATH",
+             "NO_SYSTEM_ENVIRONMENT_PATH",
+             "ONLY_CMAKE_FIND_ROOT_PATH"],
+      kwargs={
+          "DOC": ZERO_OR_MORE,
+          "HINTS": ZERO_OR_MORE,
+          "NAMES": ZERO_OR_MORE,
+          "PATHS": ZERO_OR_MORE,
+          "PATH_SUFFIXES": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "find_package",
+      pargs=ZERO_OR_MORE,
+      flags=["EXACT", "MODULE", "REQUIRED", "NO_POLICY_SCOPE", "QUIET"],
+      kwargs={
+          "COMPONENTS": ZERO_OR_MORE,
+          "OPTIONAL_COMPONENTS": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "find_path",
+      flags=["CMAKE_FIND_ROOT_PATH_BOTH", "NO_CMAKE_ENVIRONMENT_PATH",
+             "NO_CMAKE_FIND_ROOT_PATH", "NO_CMAKE_PATH",
+             "NO_CMKE_SYSTEM_PATH", "NO_DEFAULT_PATH",
+             "NO_SYSTEM_ENVIRONMENT_PATH",
+             "ONLY_CMAKE_FIND_ROOT_PATH"],
+      kwargs={
+          "DOC": ZERO_OR_MORE,
+          "HINTS": ZERO_OR_MORE,
+          "NAMES": ZERO_OR_MORE,
+          "PATHS": ZERO_OR_MORE,
+          "PATH_SUFFIXES": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "find_program",
+      flags=["CMAKE_FIND_ROOT_PATH_BOTH", "NO_CMAKE_ENVIRONMENT_PATH",
+             "NO_CMAKE_FIND_ROOT_PATH", "NO_CMAKE_PATH",
+             "NO_CMAKE_SYSTEM_PATH", "NO_DEFAULT_PATH",
+             "NO_SYSTEM_ENVIRONMENT_PATH",
+             "ONLY_CMAKE_FIND_ROOT_PATH"],
+      kwargs={
+          "DOC": ZERO_OR_MORE,
+          "HINTS": ZERO_OR_MORE,
+          "NAMES": ZERO_OR_MORE,
+          "PATHS": ZERO_OR_MORE,
+          "PATH_SUFFIXES": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "get_directory_property", flags=[], kwargs={
+          "DIRECTORY": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "get_property",
+      flags=["BRIEF_DOCS", "DEFINED", "FULL_DOCS", "GLOBAL", "SET",
+             "VARIABLE"],
+      kwargs={
+          "CACHE": ZERO_OR_MORE,
+          "DIRECTORY": ZERO_OR_MORE,
+          "PROPERTY": ZERO_OR_MORE,
+          "SOURCE": ZERO_OR_MORE,
+          "TARGET": ZERO_OR_MORE,
+          "TEST": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "include",
+      flags=["NO_POLICY_SCOPE", "OPTIONAL"],
+      kwargs={"RESULT_VARIABLE": ZERO_OR_MORE})
+
+  fn_spec.add(
+      "include_directories",
+      flags=["AFTER", "BEFORE", "SYSTEM"], kwargs={})
+
+  fn_spec.add(
+      "mark_as_advanced", flags=["CLEAR", "FORCE"], kwargs={})
+
+  fn_spec.add(
+      "message", flags=[], kwargs={
+          "AUTHOR_WARNING": ZERO_OR_MORE,
+          "DEPRECATION": ZERO_OR_MORE,
+          "FATAL_ERROR": ZERO_OR_MORE,
+          "SEND_ERROR": ZERO_OR_MORE,
+          "STATUS": ZERO_OR_MORE,
+          "WARNING": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "project", flags=[], kwargs={
+          "DESCRIPTION": 1,
+          "HOMEPAGE_URL": 1,
+          "LANGUAGES": ZERO_OR_MORE,
+          "VERSION": ZERO_OR_MORE,
+      })
+
+  fn_spec.add(
+      "set_directory_properties", flags=[], kwargs={
+          "PROPERTIES": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "set_property",
+      flags=["APPEND", "APPEND_STRING", "GLOBAL"],
+      kwargs={
+          "CACHE": ZERO_OR_MORE,
+          "DIRECTORY": ZERO_OR_MORE,
+          "PROPERTY": ZERO_OR_MORE,
+          "SOURCE": ZERO_OR_MORE,
+          "TARGET": ZERO_OR_MORE,
+          "TEST": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "set_tests_properties", flags=[], kwargs={
+          "PROPERTIES": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "string",
+      flags=["@ONLY", "ESCAPE_QUOTES", "REVERSE", "UTC"],
+      kwargs={
+          "ALPHABET": ZERO_OR_MORE,
+          "ASCII": ZERO_OR_MORE,
+          "COMPARE": ZERO_OR_MORE,
+          "CONCAT": ZERO_OR_MORE,
+          "CONFIGURE": ZERO_OR_MORE,
+          "FIND": ZERO_OR_MORE,
+          "LENGTH": ZERO_OR_MORE,
+          "MAKE_C_IDENTIFIER": ZERO_OR_MORE,
+          "MD5": ZERO_OR_MORE,
+          "RANDOM": ZERO_OR_MORE,
+          "RANDOM_SEED": ZERO_OR_MORE,
+          "REGEX": ZERO_OR_MORE,
+          "REPLACE": ZERO_OR_MORE,
+          "SHA1": ZERO_OR_MORE,
+          "SHA256": ZERO_OR_MORE,
+          "SHA384": ZERO_OR_MORE,
+          "SHA512": ZERO_OR_MORE,
+          "STRIP": ZERO_OR_MORE,
+          "SUBSTRING": ZERO_OR_MORE,
+          "TIMESTAMP": ZERO_OR_MORE,
+          "TOLOWER": ZERO_OR_MORE,
+          "TOUPPER": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "try_compile", flags=[], kwargs={
+          "CMAKE_FLAGS": ZERO_OR_MORE,
+          "COMPILE_DEFINITIONS": ZERO_OR_MORE,
+          "COPY_FILE": ZERO_OR_MORE,
+          "LINK_LIBRARIES": ZERO_OR_MORE,
+          "OUTPUT_VARIABLE": ZERO_OR_MORE,
+          "RESULT_VAR": ZERO_OR_MORE
+      })
+
+  fn_spec.add(
+      "target_compile_options", flags=[], kwargs={
+          "PRIVATE": ONE_OR_MORE,
+          "PUBLIC": ONE_OR_MORE,
+          "INTERFACE": ONE_OR_MORE
+      })
+
+  fn_spec.add(
+      "target_include_directories", flags=[], kwargs={
+          "PRIVATE": ONE_OR_MORE,
+          "PUBLIC": ONE_OR_MORE,
+          "INTERFACE": ONE_OR_MORE
+      })
+
+  fn_spec.add(
+      "target_link_libraries", flags=[], kwargs={
+          "PRIVATE": ONE_OR_MORE,
+          "PUBLIC": ONE_OR_MORE,
+          "INTERFACE": ONE_OR_MORE
+      })
+
+  fn_spec.add(
+      "try_run", flags=[], kwargs={
+          "ARGS": ZERO_OR_MORE,
+          "CMAKE_FLAGS": ZERO_OR_MORE,
+          "COMPILE_DEFINITIONS": ZERO_OR_MORE,
+          "COMPILE_OUTPUT_VARIABLE": ZERO_OR_MORE,
+          "OUTPUT_VARIABLE": ZERO_OR_MORE,
+          "RUN_OUTPUT_VARIABLE": ZERO_OR_MORE
+      })
+
+  # fn_spec.add_conditional("if")
+  # fn_spec.add_conditional("elseif")
+  # fn_spec.add_conditional("while")
+
+  # TODO(josh): should this be user-enabled? Maybe some kind of plugin/extension
+  # system?
+  add_standard_nonbuiltins(fn_spec)
+
+  return fn_spec
+
+
+def add_standard_nonbuiltins(fn_spec):
+  """
+  Add commands provided by "standard" listfiles
+  """
+
+  fn_spec.add(
+      "write_basic_package_version_file", flags=[], kwargs={
+          "COMPATIBILITY": 1,
+          "VERSION": 1
+      })
---
+++ packages/cmake-format/cmake_format-0.6.9.tar.gz/cmake_format-0.6.9/cmake_lint/lint_util.py
@@ -0,0 +1,136 @@
+from __future__ import unicode_literals
+import collections
+import logging
+
+from cmake_lint import lintdb
+
+logger = logging.getLogger(__name__)
+
+
+class LintRecord(object):
+  """Records an instance of lint at a particular location
+  """
+
+  def __init__(self, spec, location, msg):
+    self.spec = spec
+    self.location = location
+    self.msg = msg
+
+  def __repr__(self):
+    if self.location is None:
+      return " [{:s}] {:s}".format(self.spec.idstr, self.msg)
+
+    if isinstance(self.location, tuple):
+      return "{:s}: [{:s}] {:s}".format(
+          ",".join("{:02d}".format(val) for val in self.location[:2]),
+          self.spec.idstr, self.msg)
+
+    raise ValueError(
+        "Unexpected type {} for location".format(type(self.location)))
+
+
+class FileContext(object):
+  def __init__(self, global_ctx, infile_path):
+    self.global_ctx = global_ctx
+    self.infile_path = infile_path
+    self.config = None
+    self._lint = []
+    self._supressions = set()
+    self._supressed_count = collections.defaultdict(int)
+
+  def is_idstr(self, idstr):
+    return idstr in self.global_ctx.lintdb
+
+  def supress(self, idlist):
+    """
+    Given a list of lint ids, enable a supression for each one which is not
+    already supressed. Return the list of new supressions
+    """
+    new_supressions = []
+    for idstr in idlist:
+      if idstr in self._supressions:
+        continue
+      self._supressions.add(idstr)
+      new_supressions.append(idstr)
+
+    return new_supressions
+
+  def unsupress(self, idlist):
+    for idstr in idlist:
+      if idstr not in self._supressions:
+        logger.warning(
+            "Unsupressing %s which is not currently surpressed", idstr)
+      self._supressions.discard(idstr)
+
+  def record_lint(self, idstr, *args, **kwargs):
+    if idstr in self.config.lint.disabled_codes:
+      self._supressed_count[idstr] += 1
+      return
+
+    if idstr in self._supressions:
+      self._supressed_count[idstr] += 1
+      return
+
+    spec = self.global_ctx.lintdb[idstr]
+    location = kwargs.pop("location", ())
+    msg = spec.msgfmt.format(*args, **kwargs)
+    record = LintRecord(spec, location, msg)
+    self._lint.append(record)
+
+  def get_lint(self):
+    """Return lint records in sorted order"""
+    return [
+        record for _, _, _, record in sorted(
+            (record.location, record.spec.idstr, idx, record)
+            for idx, record in enumerate(self._lint))]
+
+  def writeout(self, outfile):
+    for record in self.get_lint():
+      outfile.write("{:s}:{}\n".format(self.infile_path, record))
+
+  def has_lint(self):
+    return bool(self._lint)
+
+
+class GlobalContext(object):
+  category_names = {
+      "C": "Convention",
+      "E": "Error",
+      "R": "Refactor",
+      "W": "Warning",
+  }
+
+  def __init__(self, outfile):
+    self.outfile = outfile
+    self.lintdb = lintdb.get_database()
+    self.file_ctxs = {}
+
+  def get_file_ctx(self, infile_path, config):
+    if infile_path not in self.file_ctxs:
+      self.file_ctxs[infile_path] = FileContext(self, infile_path)
+    ctx = self.file_ctxs[infile_path]
+    ctx.config = config
+    return ctx
+
+  def get_category_counts(self):
+    lint_counts = {}
+    for _, file_ctx in sorted(self.file_ctxs.items()):
+      for record in file_ctx.get_lint():
+        category_char = record.spec.idstr[0]
+        if category_char not in lint_counts:
+          lint_counts[category_char] = 0
+        lint_counts[category_char] += 1
+    return lint_counts
+
+  def write_summary(self, outfile):
+    outfile.write("Summary\n=======\n")
+    outfile.write("files scanned: {:d}\n".format(len(self.file_ctxs)))
+    outfile.write("found lint:\n")
+
+    lint_counts = self.get_category_counts()
+    fieldwidth = max(len(name) for _, name in self.category_names.items())
+    fmtstr = "  {:>" + str(fieldwidth) + "s}: {:d}\n"
+    for (category_char, count) in sorted(lint_counts.items()):
+      category_name = self.category_names[category_char]
+      outfile.write(fmtstr.format(category_name, count))
+    outfile.write("\n")
