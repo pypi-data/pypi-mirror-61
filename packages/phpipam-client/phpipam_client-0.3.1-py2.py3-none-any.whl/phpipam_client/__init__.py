423c73ca4edb7f4a3071670d00044e265450b560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:30 +0000

    Add package phpipam-client/0.3.1/phpipam_client-0.3.1-py2.py3-none-any.whl

---
+++ packages/phpipam-client/phpipam_client-0.3.1-py2.py3-none-any.whl/phpipam_client/__init__.py
@@ -0,0 +1,17 @@
+from phpipam_client.client import (
+    PhpIpamClient,
+    PhpIpamException,
+    GET,
+    POST,
+    PATCH,
+    DELETE,
+)
+
+__all__ = [
+    'PhpIpamClient',
+    'PhpIpamException',
+    'GET',
+    'POST',
+    'PATCH',
+    'DELETE',
+]
