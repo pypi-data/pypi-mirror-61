48c553d4226492a3ba534baf62ebf2ab353e7d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:23 +0000

    Add package pysal/2.2.0/pysal-2.2.0.tar.gz

---
+++ packages/pysal/pysal-2.2.0.tar.gz/pysal-2.2.0/pysal/__init__.py
@@ -0,0 +1,8 @@
+__version__='2.2.0'
+from . import lib
+from . import explore
+from . import viz
+from . import model
+
+from .base import memberships, federation_hierarchy, versions
+
---
+++ packages/pysal/pysal-2.2.0.tar.gz/pysal-2.2.0/pysal/model/__init__.py
@@ -0,0 +1,6 @@
+import mgwr
+import spglm
+import spint
+import spreg
+import spvcm
+import tobler
