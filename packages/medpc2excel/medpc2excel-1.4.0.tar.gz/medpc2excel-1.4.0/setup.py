2940982cd8394c966939b59d17a0453e51636aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package medpc2excel/1.4.0/medpc2excel-1.4.0.tar.gz

---
+++ packages/medpc2excel/medpc2excel-1.4.0.tar.gz/medpc2excel-1.4.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 13:41:19 2020
+
+@author: ycheng62
+"""
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'medpc2excel',
+    version = '1.4.0',
+    auhtor = 'Yifeng Cheng',
+    auhtor_email = 'cyfhopkins@gmail.com',
+    description = 'A package for batch-convert medpc file to Excels',
+    long_descprition = long_description,
+    long_description_content_type='text/markdown',
+    url = 'https://github.com/cyf203/medpc2excel',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6'
+    )
