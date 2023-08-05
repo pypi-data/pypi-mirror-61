c66630a7911d10e36f728f465d83dfc395dab188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:01 +0000

    Add package torchmeta/1.3.2/torchmeta-1.3.2-py3-none-any.whl

---
+++ packages/torchmeta/torchmeta-1.3.2-py3-none-any.whl/torchmeta/transforms/__init__.py
@@ -0,0 +1,3 @@
+from torchmeta.transforms.categorical import Categorical, FixedCategory
+from torchmeta.transforms.augmentations import Rotation, HorizontalFlip, VerticalFlip
+from torchmeta.transforms.splitters import Splitter, ClassSplitter, WeightedClassSplitter
