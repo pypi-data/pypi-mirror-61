ce200ee32e48b650c8387108322157f9e5ed0703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:14 +0000

    Add package ledgereth/0.1.3/ledgereth-0.1.3-py3-none-any.whl

---
+++ packages/ledgereth/ledgereth-0.1.3-py3-none-any.whl/ledgereth/objects.py
@@ -0,0 +1,125 @@
+import rlp
+from eth_utils import encode_hex
+from rlp.sedes import big_endian_int, binary, Binary
+from eth_utils import to_checksum_address
+
+from ledgereth.utils import is_bytes, is_bip32_path, parse_bip32_path
+
+
+class ISO7816Command:
+    def __init__(self, CLA: bytes, INS: bytes, P1: bytes, P2: bytes, Lc: bytes = None,
+                 Le: bytes = None, data: bytes = None):
+        try:
+            assert is_bytes(CLA)
+            assert is_bytes(INS)
+            assert is_bytes(P1)
+            assert is_bytes(P2)
+            assert is_bytes(Lc) or Lc is None
+            assert is_bytes(Le) or Le is None
+            assert is_bytes(data) or data is None
+        except AssertionError:
+            raise ValueError("Command parts must be type bytes")
+
+        self.CLA = CLA
+        self.INS = INS
+        self.P1 = P1
+        self.P2 = P2
+        self.Lc = Lc or b'\x00'
+        self.Le = Le
+        self.data = data
+
+    def set_data(self, data: bytes, Lc: bytes = None) -> None:
+        self.data = data
+        if len(self.data) > 255:
+            # TODO: Warning?
+            return
+        if Lc is None:
+            self.Lc = len(self.data).to_bytes(1, 'big')
+        else:
+            self.Lc = Lc
+
+    def encode(self) -> bytes:
+        encoded = self.CLA + self.INS + self.P1 + self.P2
+        if self.data is not None:
+            if self.Lc is None:
+                self.Lc = (len(self.data)).to_bytes(1, 'big')
+            encoded += self.Lc
+            encoded += self.data
+        else:
+            encoded += self.Lc
+        if self.Le is not None:
+            encoded += self.Le
+        return encoded
+
+    def encode_hex(self) -> str:
+        return self.encode().hex()
+
+
+class LedgerAccount:
+    def __init__(self, path, address):
+        if not is_bip32_path(path):
+            raise ValueError('Invalid BIP32 Ethereum path')
+
+        self.path = path
+        self.path_encoded = parse_bip32_path(path)
+        self.address = to_checksum_address(address)
+
+
+class Transaction(rlp.Serializable):
+    fields = [
+        ('nonce', big_endian_int),
+        ('gasprice', big_endian_int),
+        ('startgas', big_endian_int),
+        ('to', Binary.fixed_length(20, allow_empty=True)),
+        ('value', big_endian_int),
+        ('data', binary),
+    ]
+
+    def __init__(self, nonce: int, gasprice: int, startgas: int, to: bytes, value: int, data: str):
+        super(RLPTx, self).__init__(nonce, gasprice, startgas, to, value, data)
+
+    def sender(self, value: str) -> None:
+        self._sender = value
+
+    def to_dict(self) -> dict:
+        d = {}
+        for name, _ in self.__class__._meta.fields:
+            d[name] = getattr(self, name)
+        return d
+
+
+class SignedTransaction(rlp.Serializable):
+    fields = [
+        ('nonce', big_endian_int),
+        ('gasprice', big_endian_int),
+        ('startgas', big_endian_int),
+        ('to', Binary.fixed_length(20, allow_empty=True)),
+        ('value', big_endian_int),
+        ('data', binary),
+        ('v', big_endian_int),
+        ('r', big_endian_int),
+        ('s', big_endian_int),
+    ]
+
+    def __init__(self, nonce: int, gasprice: int, startgas: int, to: bytes, value: int, data: str,
+                 v: int, r: int, s: int):
+        super(SignedTransaction, self).__init__(nonce, gasprice, startgas, to, value, data, v, r, s)
+
+    def sender(self, value: str) -> None:
+        self._sender = value
+
+    def to_dict(self) -> dict:
+        d = {}
+        for name, _ in self.__class__._meta.fields:
+            d[name] = getattr(self, name)
+        return d
+
+    def raw_transaction(self):
+        return encode_hex(rlp.encode(self, SignedTransaction))
+    # Match the API of the web3.py Transaction object
+    rawTransaction = property(raw_transaction)
+
+
+# Compat: Depreciated
+RLPTx = Transaction
+RLPSignedTx = SignedTransaction
---
+++ packages/ledgereth/ledgereth-0.1.3-py3-none-any.whl/ledgereth/web3.py
@@ -0,0 +1,88 @@
+from rlp import encode
+from eth_utils import encode_hex
+from ledgereth.objects import SignedTransaction
+from ledgereth.accounts import find_account, get_accounts
+from ledgereth.transactions import create_transaction
+
+
+"""
+ACCOUNT DERIVATION ISSUES
+
+Derivation path debate is ever ongoing.  Ledger Live app uses opton #3 here.
+The old chrome app used #4.
+
+1) 44'/60'/0'/0/x
+2) 44'/60'/0'/x/0
+3) 44'/60'/x'/0/0
+4) 44'/60'/0'/x
+
+The Ledger Live account enumeration algo appears to be:
+
+1) Try legacy account X (if no balance, GOTO 3)
+2) X+1 and GOTO 1
+3) Try new-derivation Y (if no balance, RETURN)
+4) Y+1 and GOTO 3
+
+Since this library is trying not to resort to JSON-RPC calls, this algorithm is
+not usable, so it's either or, and it currently defaults to the newer
+derivation.
+
+To use legacy derivation, set the environment variable LEDGER_LEGACY_ACCOUNTS
+
+Ref (cannot find an authoritative source):
+https://github.com/ethereum/EIPs/issues/84#issuecomment-292324521
+"""
+
+
+class LedgerSignerMiddleware:
+    def __init__(self, make_request, w3):
+        self.w3 = w3
+        self.make_request = make_request
+
+    def __call__(self, method, params):
+        if method == 'eth_sendTransaction':
+            new_params = []
+            for tx_obj in params:
+                sender_address = tx_obj.get('from')
+                nonce = tx_obj.get('nonce')
+
+                if not sender_address:
+                    # TODO: Should this use a default?
+                    raise ValueError('"from" field not provided')
+
+                sender_account = find_account(sender_address)
+
+                if not sender_account:
+                    raise Exception('Account {} not found'.format(sender_address))
+
+                if nonce is None:
+                    nonce = self.w3.eth.getTransactionCount(sender_address)
+
+                raw_tx = create_transaction(
+                    to=tx_obj.get('to'),
+                    value=tx_obj.get('value'),
+                    gas=tx_obj.get('gas'),
+                    gas_price=tx_obj.get('gasPrice'),
+                    nonce=nonce,
+                    data=tx_obj.get('data', ''),
+                    sender_path=sender_account.path,
+                )
+
+                new_params.append(encode_hex(encode(raw_tx, SignedTransaction)))
+
+            # Change to raw tx call
+            method = 'eth_sendRawTransaction'
+            params = new_params
+
+        elif method == 'eth_accounts':
+            return {
+              "id": 1,
+              "jsonrpc": "2.0",
+              "result": list(map(lambda a: a.address, get_accounts()))
+            }
+
+        elif method == 'eth_sign':
+            raise NotImplementedError('Not yet implemented by LedgerSignerMiddleware')
+
+        response = self.make_request(method, params)
+        return response
