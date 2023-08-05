682316eea4e49ea62acf67dd677e775cb514345d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package outliers-101703384/1.0/outliers_101703384-1.0.tar.gz

---
+++ packages/outliers-101703384/outliers_101703384-1.0.tar.gz/outliers_101703384-1.0/outliers_101703384/remove_outliers.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import sys
+
+def delete_outliers(ds,z_val=3):
+    if ds.ndim>2:
+        out_ds = ds[(np.abs(stats.zscore(ds)) < z_val).all(axis=1)]
+    else:
+        out_ds = ds[(np.abs(stats.zscore(ds)) < z_val)]
+    return (out_ds,len(ds)-len(out_ds))            
+
+
+def main():
+    filename = sys.argv[1]
+    try:
+        data = pd.read_csv(filename)
+    except OSError:
+        print('cannot open', filename)
+        sys.exit(0)
+        
+    (data,count) = delete_outliers(data)
+    
+    try:
+        data.to_csv("out.csv",index=False)
+        print("number of rows deleted: ",count)    
+    except OSError:
+        print('cannot open', filename)
+        sys.exit(0)
+        
+if __name__=="__main__":
+    main()
