feab9e4eb38c44e5e505c460e3906b83a2056522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:05 +0000

    Add package Biopyutils/0.1.2/Biopyutils-0.1.2.tar.gz

---
+++ packages/Biopyutils/Biopyutils-0.1.2.tar.gz/Biopyutils-0.1.2/Biopyutils/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# License           : GPL3
+# Author            : Jingxin Fu <jingxinfu.tj@gmail.com>
+# Date              : 07/02/2020
+# Last Modified Date: 10/02/2020
+# Last Modified By  : Jingxin Fu <jingxinfu.tj@gmail.com>
+__version__ = "0.1.2"
+import os
+import pkg_resources
+
+DATA_DIR = pkg_resources.resource_filename('Biopyutils', 'data/')
+def getGeneRefPath(species,map_id):
+    return os.path.join(DATA_DIR,'geneId_%s_%s.pickle.gz' % (species,map_id))
+
+def getSpeciesRefPath(map_id):
+    return os.path.join(DATA_DIR,'species_hg-mm_%s.pickle.gz' % (map_id))
+
