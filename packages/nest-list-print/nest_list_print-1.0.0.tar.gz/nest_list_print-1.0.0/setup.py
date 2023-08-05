7565ac4cd6481a67e68e5fd65a73a70ab29373fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:48 +0000

    Add package nest-list-print/1.0.0/nest_list_print-1.0.0.tar.gz

---
+++ packages/nest-list-print/nest_list_print-1.0.0.tar.gz/nest_list_print-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nest_list_print',
+    version='1.0.0',
+    py_modules=['nest_list_print'],
+    author='Wancheng Zhou',
+    author_email='wancheng2012@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)
