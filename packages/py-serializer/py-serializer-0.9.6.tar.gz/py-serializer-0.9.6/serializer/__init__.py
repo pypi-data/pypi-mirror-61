6d0ea8192e90128f9514d03e82a8885737d79e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package py-serializer/0.9.6/py-serializer-0.9.6.tar.gz

---
+++ packages/py-serializer/py-serializer-0.9.6.tar.gz/py-serializer-0.9.6/serializer/__init__.py
@@ -0,0 +1,33 @@
+__author__ = 'Hamilakis Nicolas'
+__copyright__ = 'Hamilakis Nicolas 2020'
+__license__ = 'MIT'
+__url__ = ''
+__maintainer__ = 'Hamilakis Nicolas'
+__email__ = 'nick.hamilakis562@gmail.com'
+__status__ = "production"
+__description__ = "Package for serializing python object & dataclasses"
+
+#  The MIT License (MIT)
+#
+#  Copyright (c) 2020 Nicolas Hamilakis
+#
+
+from pathlib import Path
+
+from ._core import _Serializable as SerializableMixin  # noqa: F401
+from ._core import serializable  # noqa: F401
+from ._serializer import serializer  # noqa: F401
+
+__version__ = open(Path(__file__).parents[0] / 'VERSION').read()
+
+__doc__ = f"""
++------======================================-------+
++ Serializer Module  v{__version__} by {__author__}
++ © {__copyright__} under {__license__} licence.
++ Module that allows to mark objects as serializable & 
++ export them into dictionaries. for more info see {__url__}
++------======================================-------+
++ Contact : {__maintainer__} <{__email__}>
++ Status : {__status__}
++------======================================-------+
+"""  # noqa: W291
