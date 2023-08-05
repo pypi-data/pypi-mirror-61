49e295752875698a137722a73a76509906c96d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package Outliers-arjav-101703097/0.1/Outliers-arjav-101703097-0.1.tar.gz

---
+++ packages/Outliers-arjav-101703097/Outliers-arjav-101703097-0.1.tar.gz/Outliers-arjav-101703097-0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+  name = 'Outliers-arjav-101703097',
+  packages = ['Outliers'],
+  version = '0.1',
+  license='MIT',
+  description = 'library to delete outliers using z-index',
+  author = 'Arjav Jain',
+  author_email = 'arjavjain59998@gmail.com',
+  url = 'https://github.com/arjavjain59998',
+  download_url = 'https://github.com/arjavjain59998/Outliers.git',
+  keywords = ['Outliers'],
+  install_requires=[
+        'python',
+        'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
