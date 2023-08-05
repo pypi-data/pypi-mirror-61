0274b568500ba66c308a6983031c1a99a547ae77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:35 +0000

    Add package libgen-dl/0.0.4/libgen_dl-0.0.4.tar.gz

---
+++ packages/libgen-dl/libgen_dl-0.0.4.tar.gz/libgen_dl-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="libgen_dl",  # Replace with your own username
+    version="0.0.4",
+    author="prydt",
+    author_email="pranoydll@gmail.com",
+    description="Download books from Library Genesis from the command line!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.prydt.xyz/pry/libgen_dl",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "libgen_dl=libgen_dl:main",
+        ]
+    },
+)
