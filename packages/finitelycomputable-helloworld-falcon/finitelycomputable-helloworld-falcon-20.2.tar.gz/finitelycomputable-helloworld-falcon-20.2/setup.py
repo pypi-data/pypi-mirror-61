17eba3ba58d87aa3107342b1d7e89d966753da92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:54 +0000

    Add package finitelycomputable-helloworld-falcon/20.2/finitelycomputable-helloworld-falcon-20.2.tar.gz

---
+++ packages/finitelycomputable-helloworld-falcon/finitelycomputable-helloworld-falcon-20.2.tar.gz/finitelycomputable-helloworld-falcon-20.2/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup
+from finitelycomputable_microsites_setup import version, base_setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='finitelycomputable-helloworld-falcon',
+    version=version,
+    py_modules=['finitelycomputable.helloworld_falcon'],
+    entry_points={
+        'console_scripts': [
+            'finitelycomputable-helloworld-falcon = finitelycomputable.helloworld_falcon:run']
+        },
+    description='A hello_world implementation in Falcon',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    scripts=['finitelycomputable_microsites_setup.py'],
+    install_requires=['falcon~=2.0'],
+    extras_require={
+        'bjoern': ['bjoern<4'],
+        'cheroot': ['cheroot<9'],
+        'cherrypy': ['cherrypy<19'],
+        'gunicorn': ['gunicorn<20'],
+        'waitress': ['waitress<1.4'],
+        },
+    url='https://www.finitelycomputable.net/hello_world/',
+    **base_setup
+)
