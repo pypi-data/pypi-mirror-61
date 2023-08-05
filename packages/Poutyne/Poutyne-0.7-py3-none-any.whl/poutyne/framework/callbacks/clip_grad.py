f9d66570a1f9e67bb04bded1aa108d99d4b4a24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:42 +0000

    Add package Poutyne/0.7/Poutyne-0.7-py3-none-any.whl

---
+++ packages/Poutyne/Poutyne-0.7-py3-none-any.whl/poutyne/framework/callbacks/clip_grad.py
@@ -0,0 +1,42 @@
+from torch.nn.utils import clip_grad_norm_, clip_grad_value_
+
+from .callbacks import Callback
+
+
+class ClipNorm(Callback):
+    """
+    Uses PyTorch's :func:`~torch.nn.utils.clip_grad_norm_()`
+    method to clip gradient.
+
+    See:
+        :func:`torch.nn.utils.clip_grad_norm_()`
+
+    """
+
+    def __init__(self, parameters, max_norm, *, norm_type=2):
+        super().__init__()
+        self.parameters = list(parameters)
+        self.max_norm = max_norm
+        self.norm_type = norm_type
+
+    def on_backward_end(self, batch_number):
+        clip_grad_norm_(self.parameters, self.max_norm, norm_type=self.norm_type)
+
+
+class ClipValue(Callback):
+    """
+    Uses PyTorch's :func:`~torch.nn.utils.clip_grad_value_()`
+    method to clip gradient.
+
+    See:
+        :func:`torch.nn.utils.clip_grad_value_()`
+
+    """
+
+    def __init__(self, parameters, clip_value):
+        super().__init__()
+        self.parameters = list(parameters)
+        self.clip_value = clip_value
+
+    def on_backward_end(self, batch_number):
+        clip_grad_value_(self.parameters, self.clip_value)
