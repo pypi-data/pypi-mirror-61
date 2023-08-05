8548d2c7013202ad4d53209b61a036c72adeef93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:00 +0000

    Add package certreq/0.1.0/certreq-0.1.0.tar.gz

---
+++ packages/certreq/certreq-0.1.0.tar.gz/certreq-0.1.0/certreq/__version__.py
@@ -0,0 +1,10 @@
+VERSION = (0, 0, 1)
+
+__version__ = '.'.join(map(str, VERSION))
+__title__ = 'certreq'
+__description__ = 'python implementation of certreq'
+__url__ = 'https://github.com/steschuser/certreq'
+__author__ = 'Steffen Schwebel'
+__author_email__ = 'steffen@schwebel.online'
+__license__ = 'MIT'
+__cake__ = u'\u2728 \U0001f370 \u2728'
