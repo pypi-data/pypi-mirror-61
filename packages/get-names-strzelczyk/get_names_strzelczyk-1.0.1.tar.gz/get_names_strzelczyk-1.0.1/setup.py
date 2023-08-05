f12f4d770701b13328f71077590285423dd9df44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package get-names-strzelczyk/1.0.1/get_names_strzelczyk-1.0.1.tar.gz

---
+++ packages/get-names-strzelczyk/get_names_strzelczyk-1.0.1.tar.gz/get_names_strzelczyk-1.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='get_names_strzelczyk',
+    version='1.0.1',
+    author='SomeCompany PLC',
+    author_email='dave@example.com',
+    packages=find_packages(),
+    url="https://github.com",
+    scripts=['gen_names_strzelczyk/gen_names_strzelczyk.py', 'bin/gen_names_strzelczyk.bat'],
+    include_package_data=True,
+    description='Super useful library',
+    install_requires=['names']
+)
