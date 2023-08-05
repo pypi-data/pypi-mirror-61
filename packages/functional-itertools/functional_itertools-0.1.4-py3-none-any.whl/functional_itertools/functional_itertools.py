7df16c0d4ce8912c858880778cb167059ad1eeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package functional-itertools/0.1.4/functional_itertools-0.1.4-py3-none-any.whl

---
+++ packages/functional-itertools/functional_itertools-0.1.4-py3-none-any.whl/functional_itertools/functional_itertools.py
@@ -0,0 +1,13 @@
+"""The functional-itertools package."""
+
+
+def add(x: int) -> int:
+    """Increment an integer.
+
+    Args:
+        x: the integer to increment
+
+    Returns:
+        The incremented integer
+    """
+    return x + 1
