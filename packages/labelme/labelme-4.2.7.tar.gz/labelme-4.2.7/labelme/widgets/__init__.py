2be1ff960786a47cf0b0417c6e3bb1bc06e88d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:12 +0000

    Add package labelme/4.2.7/labelme-4.2.7.tar.gz

---
+++ packages/labelme/labelme-4.2.7.tar.gz/labelme-4.2.7/labelme/widgets/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+from .canvas import Canvas
+
+from .color_dialog import ColorDialog
+
+from .label_dialog import LabelDialog
+from .label_dialog import LabelQLineEdit
+
+from .label_qlist_widget import LabelQListWidget
+
+from .tool_bar import ToolBar
+
+from .unique_label_qlist_widget import UniqueLabelQListWidget
+
+from .zoom_widget import ZoomWidget
