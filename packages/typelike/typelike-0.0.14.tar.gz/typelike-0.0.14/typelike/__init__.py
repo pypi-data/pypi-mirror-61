7fce5b848186a862c44c3006e9f0e52c1266aaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:16 +0000

    Add package typelike/0.0.14/typelike-0.0.14.tar.gz

---
+++ packages/typelike/typelike-0.0.14.tar.gz/typelike-0.0.14/typelike/__init__.py
@@ -0,0 +1,17 @@
+
+from .core import *
+from .version import __version__
+
+# __all__ = [
+#     'core',
+#     '__version__'
+# ]
+
+__all__ = [
+    'AnyLike',
+    'Anything',
+    'ListLike',
+    'NumberLike',
+    'typelike',
+    '__version__'
+]
