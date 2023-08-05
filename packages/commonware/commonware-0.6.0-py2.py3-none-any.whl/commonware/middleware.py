191eb8e5574a4df6be04178227652e85ff45df24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package commonware/0.6.0/commonware-0.6.0-py2.py3-none-any.whl

---
+++ packages/commonware/commonware-0.6.0-py2.py3-none-any.whl/commonware/middleware.py
@@ -0,0 +1,8 @@
+from commonware.exceptions.middleware import ScrubRequestOnException
+from commonware.log.middleware import ThreadRequestMiddleware
+from commonware.request.middleware import SetRemoteAddrFromForwardedFor
+from commonware.response.middleware import (FrameOptionsHeader,
+                                            ContentTypeOptionsHeader,
+                                            RobotsTagHeader,
+                                            XSSProtectionHeader)
+from commonware.session.middleware import NoVarySessionMiddleware
---
+++ packages/commonware/commonware-0.6.0-py2.py3-none-any.whl/commonware/response/middleware.py
@@ -0,0 +1,66 @@
+from django.conf import settings
+from django.utils.deprecation import MiddlewareMixin
+
+
+class FrameOptionsHeader(MiddlewareMixin):
+    """
+    Set an X-Frame-Options header. Default to DENY. Set
+    response['X-Frame-Options'] = 'SAMEORIGIN'
+    to override.
+    """
+
+    def process_response(self, request, response):
+        if hasattr(response, 'no_frame_options'):
+            return response
+
+        if 'X-Frame-Options' not in response:
+            response['X-Frame-Options'] = 'DENY'
+
+        return response
+
+
+class RobotsTagHeader(MiddlewareMixin):
+    """Set an X-Robots-Tag header.
+
+    Default to noodp to avoid using directories for page titles. Set
+    a value of response['X-Robots-Tag'] or use the relevant decorators
+    to override.
+
+    Change the default in settings by setting X_ROBOTS_DEFAULT = ''.
+    """
+
+    def process_response(self, request, response):
+        if getattr(response, 'no_robots_tag', False):
+            return response
+
+        if 'X-Robots-Tag' not in response:
+            default = getattr(settings, 'X_ROBOTS_DEFAULT', 'noodp')
+            response['X-Robots-Tag'] = default
+
+        return response
+
+
+class XSSProtectionHeader(MiddlewareMixin):
+    """
+    Set the X-XSS-Protection header on responses. Defaults to
+    '1; mode=block'. Set response['X-XSS-Protection'] = '0' (disable)
+    or '1' (rewrite mode) to override.
+    """
+
+    def process_response(self, request, response):
+        if 'X-XSS-Protection' not in response:
+            response['X-XSS-Protection'] = '1; mode=block'
+        return response
+
+
+class ContentTypeOptionsHeader(MiddlewareMixin):
+    """
+    Set the X-Content-Type-Options header on responses. Defaults
+    to 'nosniff'. Set response['X-Content-Type-Options'] = ''
+    to override.
+    """
+
+    def process_response(self, request, response):
+        if 'X-Content-Type-Options' not in response:
+            response['X-Content-Type-Options'] = 'nosniff'
+        return response
