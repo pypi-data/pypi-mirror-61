69703f04fdd08f857b545f57fb38bd56c7c480cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package nestinho/1.3.1/nestinho-1.3.1.tar.gz

---
+++ packages/nestinho/nestinho-1.3.1.tar.gz/nestinho-1.3.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nestinho',
+    version = '1.3.1',
+    py_modules = ['nestinho'],
+    author = 'naerith',
+    author_email = 'busraasan2@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
