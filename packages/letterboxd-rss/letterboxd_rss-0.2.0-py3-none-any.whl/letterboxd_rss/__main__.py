18289f86a50308a564e8f82f06d4e14012435f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:34 +0000

    Add package letterboxd-rss/0.2.0/letterboxd_rss-0.2.0-py3-none-any.whl

---
+++ packages/letterboxd-rss/letterboxd_rss-0.2.0-py3-none-any.whl/letterboxd_rss/__main__.py
@@ -0,0 +1,32 @@
+import sys
+import argparse
+from letterboxd_rss import process
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "letterboxd_url",
+        metavar="LETTERBOXD_PROFILE",
+        help="URL of your letterboxd profile",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="FEED_FILENAME",
+        default="./feed.xml",
+        help="Destination of the generated RSS feed (defaults to ./feed.xml)",
+    )
+    parser.add_argument(
+        "-l",
+        "--max-length",
+        metavar="FEED_LENGTH",
+        default=20,
+        type=int,
+        help="Maximum number of watchlist items to keep in the feed",
+    )
+    args = parser.parse_args(argv)
+    process(args)
+
+
+main(sys.argv[1:])
