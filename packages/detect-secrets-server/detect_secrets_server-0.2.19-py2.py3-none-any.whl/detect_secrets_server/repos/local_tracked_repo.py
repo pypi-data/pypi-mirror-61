daaf5194cd23324c5aafb43c71d8acc1a68dc80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:33 +0000

    Add package detect-secrets-server/0.2.19/detect_secrets_server-0.2.19-py2.py3-none-any.whl

---
+++ packages/detect-secrets-server/detect_secrets_server-0.2.19-py2.py3-none-any.whl/detect_secrets_server/repos/local_tracked_repo.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from .base_tracked_repo import BaseTrackedRepo
+from detect_secrets_server.storage.file import FileStorageWithLocalGit
+
+
+class LocalTrackedRepo(BaseTrackedRepo):
+
+    STORAGE_CLASS = FileStorageWithLocalGit
+
+    @property
+    def name(self):
+        return self.repo
