482253f8ff4a7bc78eba24047459644939f6a0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:16 +0000

    Add package okboomer/0.0.6/okboomer-0.0.6-py3-none-any.whl

---
+++ packages/okboomer/okboomer-0.0.6-py3-none-any.whl/okboomer/__init__.py
@@ -0,0 +1,11 @@
+import sys, random
+
+old_stdout = sys.stdout
+class Stdout:
+	def write(string):
+		boomer = random.choice(('jackson', 'redox', 'lea'))
+		string = string.replace('\n', f' {boomer} is a boomer\n')
+		old_stdout.write(string)
+sys.stdout = Stdout
+
+print('')
