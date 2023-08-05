79b49192bad06980015d2633ae1c83fc99380349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:04 +0000

    Add package andystring/1.0/andystring-1.0-py3-none-any.whl

---
+++ packages/andystring/andystring-1.0-py3-none-any.whl/andystring/StringUtil.py
@@ -0,0 +1,9 @@
+#
+# this function concatenates  str2 to str1 and this is for testing
+#
+def appendString(str1, str2):
+    return str1 + str2
+
+if __name__ == "__main__":
+    print(appendString("kim", "park"))
+    
