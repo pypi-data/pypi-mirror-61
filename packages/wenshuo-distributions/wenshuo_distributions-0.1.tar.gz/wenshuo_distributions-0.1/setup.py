0968f87ade3f4f7fd7c96c3e743c52a64afab13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:36 +0000

    Add package wenshuo-distributions/0.1/wenshuo_distributions-0.1.tar.gz

---
+++ packages/wenshuo-distributions/wenshuo_distributions-0.1.tar.gz/wenshuo_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='wenshuo_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['wenshuo_distributions'],
+      zip_safe=False)
