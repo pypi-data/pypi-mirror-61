0da371fde43218d04eb859e85a65b03a22aead39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:04 +0000

    Add package nfem/1.7.1/nfem-1.7.1-py3-none-any.whl

---
+++ packages/nfem/nfem-1.7.1-py3-none-any.whl/nfem/__init__.py
@@ -0,0 +1,53 @@
+"""
+NFEM teaching tool
+
+A module for the non linear static analysis of 3D truss problems.
+A light weight object oriented FEM code and some usefull postprocessing tools.
+"""
+
+import sys
+
+__version__ = '1.7.1'
+
+print(f"""
+--------------------------------------------------------------------------------
+
+                       _   ________________  ___
+                      / | / / ____/ ____/  |/  /
+                     /  |/ / /_  / __/ / /|_/ /
+                    / /|  / __/ / /___/ /  / /
+                   /_/ |_/_/   /_____/_/  /_/  Teaching Tool
+
+  Authors:   Armin Geiser, Aditya Ghantasala, Thomas Oberbichler, Klaus Sautter
+             Mahmoud Zidan
+  Copyright: © 2018-2020 TUM Statik
+  Version:   {__version__}
+
+  This is a teaching tool! All results without warranty.
+
+--------------------------------------------------------------------------------
+""")
+
+if sys.version_info < (3, 5):
+    raise RuntimeError("The nfem module requires at least Python 3.5!")
+
+from .model import Model
+from .node import Node
+from .truss import Truss
+from .assembler import Assembler
+
+from .newton_raphson import newton_raphson_solve
+from .path_following_method import LoadControl
+from .path_following_method import DisplacementControl
+from .path_following_method import ArcLengthControl
+
+from .visualization import interact
+from .visualization import show_load_displacement_curve, show_animation, show_deformation_plot
+from .visualization import Plot2D, Animation3D, DeformationPlot3D
+
+from .bracketing import bracketing
+
+def test():
+    import unittest
+    suite = unittest.TestLoader().discover('.', pattern='test*')
+    unittest.TextTestRunner(verbosity=2).run(suite)
