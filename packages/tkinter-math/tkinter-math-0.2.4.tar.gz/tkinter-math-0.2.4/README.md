1241d1a41eddd57ef18b096f386e656f9820c198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:04 +0000

    Add package tkinter-math/0.2.4/tkinter-math-0.2.4.tar.gz

---
+++ packages/tkinter-math/tkinter-math-0.2.4.tar.gz/tkinter-math-0.2.4/README.md
@@ -0,0 +1,8 @@
+tkinter-math
+=====
+
+Render math on Tkinter canvas
+
+- Free software: MIT license
+
+tkinter-math is a library to render math on a Tkinter canvas. It is made for docal but making it work with at least with MathML is straightforward and is planned to be implemented.
---
+++ packages/tkinter-math/tkinter-math-0.2.4.tar.gz/tkinter-math-0.2.4/setup.py
@@ -0,0 +1,41 @@
+# -{del dist\* | python %f sdist bdist_wheel}
+# -{pip install -e .}
+# -{twine upload dist/*}
+"""
+:copyright: (c) 2019 by K1DV5
+:license: MIT, see LICENSE for more details.
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = '0.2.4'
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='tkinter-math',
+    author="Kidus Adugna",
+    author_email='kidusadugna@gmail.com',
+    classifiers=[
+    #     'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="render math on tkinter canvas",
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='docal, tkinter, math',
+    packages=['tkinter_math'],
+    url='https://github.com/K1DV5/tkinter-math',
+    version=VERSION,
+    # zip_safe=False,
+)
+
