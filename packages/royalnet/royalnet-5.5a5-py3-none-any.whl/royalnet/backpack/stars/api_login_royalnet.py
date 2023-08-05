2a75fcba36673645dd19695587523ab5ebf2294f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:54 +0000

    Add package royalnet/5.5a5/royalnet-5.5a5-py3-none-any.whl

---
+++ packages/royalnet/royalnet-5.5a5-py3-none-any.whl/royalnet/backpack/stars/api_login_royalnet.py
@@ -0,0 +1,34 @@
+import datetime
+import royalnet.utils as ru
+from royalnet.constellation.api import *
+from ..tables.users import User
+from ..tables.aliases import Alias
+from ..tables.tokens import Token
+
+
+class ApiLoginRoyalnetStar(ApiStar):
+    path = "/api/login/royalnet/v1"
+
+    methods = ["POST"]
+
+    async def api(self, data: ApiData) -> ru.JSON:
+        TokenT = self.alchemy.get(Token)
+        UserT = self.alchemy.get(User)
+        AliasT = self.alchemy.get(Alias)
+
+        username = data["username"]
+        password = data["password"]
+
+        async with self.session_acm() as session:
+            user: User = await ru.asyncify(session.query(UserT).filter_by(username=username).one_or_none)
+            if user is None:
+                raise NotFoundError("User not found")
+            pswd_check = user.test_password(password)
+            if not pswd_check:
+                raise ApiError("Invalid password")
+            token: Token = TokenT.generate(user=user, expiration_delta=datetime.timedelta(days=7))
+            session.add(token)
+            await ru.asyncify(session.commit)
+            response = token.json()
+
+        return response
