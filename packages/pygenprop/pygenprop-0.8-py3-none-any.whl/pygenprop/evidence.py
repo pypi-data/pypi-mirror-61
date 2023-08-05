9f955a867e319a531684026d82d8476b6b725d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:35 +0000

    Add package pygenprop/0.8/pygenprop-0.8-py3-none-any.whl

---
+++ packages/pygenprop/pygenprop-0.8-py3-none-any.whl/pygenprop/evidence.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+"""
+Created by: Lee Bergstrand (2017)
+
+Description: The evidence class.
+"""
+from pygenprop.functional_element import FunctionalElement
+
+
+class Evidence(object):
+    """A piece of evidence (ex. InterPro HMM hit or GenProp) that supports the existence of a functional element."""
+
+    def __init__(self, evidence_identifiers=None, gene_ontology_terms=None,
+                 sufficient=False, parent: FunctionalElement=None):
+        """
+        Creates a new Evidence object.
+
+        :param evidence_identifiers: A list of identifiers of proteins or processes supporting the existence of a
+                                     FunctionalElement (i.e a list of Intro Consortium IDs or GenProp Accessions).
+        :param gene_ontology_terms: The ids for gene ontology (GO) terms defining the protein or process supporting
+                                    the existence of a FunctionalElement.
+        :param sufficient: Can this evidence alone prove the existence of FunctionalElement?
+        """
+
+        if evidence_identifiers is None:
+            evidence_identifiers = []
+        if gene_ontology_terms is None:
+            gene_ontology_terms = []
+        if sufficient is None:
+            sufficient = False
+
+        self.evidence_identifiers = evidence_identifiers
+        self.gene_ontology_terms = gene_ontology_terms
+        self.sufficient = sufficient
+        self.parent = parent
+
+    def __repr__(self):
+        repr_data = ['Evidence_IDs: ' + str(self.evidence_identifiers),
+                     'GO Terms: ' + str(self.gene_ontology_terms),
+                     'Sufficient: ' + str(self.sufficient)]
+        return '(' + ', '.join(repr_data) + ')'
+
+    @property
+    def has_genome_property(self):
+        """
+        Is the evidence a genome property?
+
+        :return: Return True if evidence is a genome property.
+        """
+        genome_property = False
+        for identifier in self.evidence_identifiers:
+            if "genprop" in identifier.lower():
+                genome_property = True
+                break
+        return genome_property
+
+    @property
+    def property_identifiers(self):
+        """
+        Gets the genome properties identifiers representing a piece of evidence.
+
+        :return: A list of genome property identifiers.
+        """
+        genome_property_identifiers = []
+        for identifier in self.evidence_identifiers:
+            if "genprop" in identifier.lower():
+                genome_property_identifiers.append(identifier)
+
+        return genome_property_identifiers
+
+    @property
+    def interpro_identifiers(self):
+        """
+        Gets the InterPro (IPRXXXXXX) identifiers representing a piece of evidence.
+
+        :return: A list of genome property identifiers.
+        """
+        genome_property_identifiers = []
+        for identifier in self.evidence_identifiers:
+            if "ipr" in identifier.lower():
+                genome_property_identifiers.append(identifier)
+
+        return genome_property_identifiers
+
+    @property
+    def consortium_identifiers(self):
+        """
+         Gets the InterPro consortium signature identifiers (PFAM, TIGRFAM, etc.) representing a piece of evidence.
+
+        :return: A set of genome property identifiers.
+        """
+        genome_property_identifiers = []
+        for identifier in self.evidence_identifiers:
+            lowercase_identifier = identifier.lower()
+            if "genprop" not in lowercase_identifier and "ipr" not in lowercase_identifier:
+                genome_property_identifiers.append(identifier)
+
+        return genome_property_identifiers
+
+    @property
+    def genome_properties(self):
+        """
+        Get genome properties that are used by this evidence.
+
+        :return: A list of genome properties.
+        """
+        parent_genome_property = self.parent.parent.parent
+        return [genome_property for genome_property in parent_genome_property.children if
+                genome_property.id in self.property_identifiers]
---
+++ packages/pygenprop/pygenprop-0.8-py3-none-any.whl/pygenprop/step.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+"""
+Created by: Lee Bergstrand (2017)
+
+Description: The step class.
+"""
+
+from pygenprop.genome_property import GenomeProperty
+
+
+class Step(object):
+    """A class representing a step that supports the existence of a genome property."""
+
+    def __init__(self, number, functional_elements: list=None, parent: GenomeProperty=None):
+        """
+        Creates a new Step object.
+
+        :param number: The position of the step in the step list.
+        :param functional_elements: A list of FunctionalElements supporting this step.
+        """
+
+        if functional_elements is None:
+            functional_elements = []
+        else:
+            # Double link functional_elements back to the parent step.
+            for element in functional_elements:
+                element.parent = self
+
+        self.number = int(number)
+        self.functional_elements = functional_elements
+        self.parent = parent
+
+    def __repr__(self):
+        repr_data = ['Step ' + str(self.number),
+                     'Functional_Elements: ' + str(self.functional_elements)]
+        return ', '.join(repr_data)
+
+    @property
+    def name(self):
+        """
+        Get the name for a step based on combine the names of its functional elements.
+
+        :return: The name of the step.
+        """
+        return " ".join(element.name for element in self.functional_elements)
+
+    @property
+    def required(self):
+        """
+        Checks if the step is required by checking if any of the functional elements are required.
+
+        :return: True if the step is required.
+        """
+        required_step = False
+
+        for element in self.functional_elements:
+            if element.required:
+                required_step = True
+                break
+
+        return required_step
+
+    @property
+    def property_identifiers(self):
+        """
+        Collects all the genome properties identifiers supporting a step.
+
+        :return: A list of the steps child genome property identifiers.
+        """
+        genome_properties_identifiers = []
+        for element in self.functional_elements:
+            for evidence in element.evidence:
+                if evidence.has_genome_property:
+                    genome_properties_identifiers.extend(evidence.property_identifiers)
+
+        return genome_properties_identifiers
+
+    @property
+    def consortium_identifiers(self):
+        """
+        All InterPro consortium signature identifiers (PFAM, TIGRFAM, etc.) used by the step.
+
+        :return: A set of all unique consortium identifiers used used by the step.
+        """
+        return self._get_evidence_identifiers(consortium=True)
+
+    @property
+    def interpro_identifiers(self):
+        """
+        All global InterPro identifiers (IPRXXXX, etc.) used by the step.
+
+        :return: A set of all unique InterPro identifiers used used by the step.
+        """
+        return self._get_evidence_identifiers()
+
+    def _get_evidence_identifiers(self, consortium=False):
+        """
+        Gets evidence identifiers from all evidences of the step.
+
+        :param consortium: If true, list the consortium signature identifiers (PFAM, TIGRFAM)
+        :return: A set of all unique evidence identifiers used by the step.
+        """
+        global_identifiers = []
+        for functional_element in self.functional_elements:
+            for evidence in functional_element.evidence:
+                if consortium:
+                    current_identifiers = evidence.consortium_identifiers
+                else:
+                    current_identifiers = evidence.interpro_identifiers
+
+                if current_identifiers:
+                    global_identifiers.extend(current_identifiers)
+        return global_identifiers
+
+    @property
+    def genome_properties(self):
+        """
+        Collects all the child genome properties supporting a step.
+
+        :return: A list of child genome properties for a step.
+        """
+        child_identifiers = self.property_identifiers
+        return [child_property for child_property in self.parent.children if child_property.id in child_identifiers]
