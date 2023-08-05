a720e8d579759cf4235c951cc3c4a088656d2ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:18 +0000

    Add package outlierpack-NG/0.0.9/outlierpack-NG-0.0.9.tar.gz

---
+++ packages/outlierpack-NG/outlierpack-NG-0.0.9.tar.gz/outlierpack-NG-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="outlierpack-NG",
+    version="0.0.9",
+    author="Nikhil Gupta",
+    author_email="ngupta_be17@thapar.edu",
+    description="Removing outliers from a pandas dataframe",
+	url='https://github.com/CachingNik/OutlierPack',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	entry_points = {
+        'console_scripts': ['Outcli=outlib.outcli:main'],
+    },
+	keywords = ['CLI', 'OUTLIER', 'Data', 'outlier removal'], 
+	python_requires='>=2.7', 
+)
