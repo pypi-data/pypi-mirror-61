a4093dd9fafd42f0bfb1c2178a1210eedc1faeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package spacy-ann-linker/0.0.2/spacy_ann_linker-0.0.2-py3-none-any.whl

---
+++ packages/spacy-ann-linker/spacy_ann_linker-0.0.2-py3-none-any.whl/spacy_ann/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+"""spaCy ANN Linker, a pipeline component for generating spaCy KnowledgeBase Alias Candidates for Entity Linking."""
+
+
+__version__ = '0.0.2'
+
+from .ann_linker import ApproxNearestNeighborsLinker
