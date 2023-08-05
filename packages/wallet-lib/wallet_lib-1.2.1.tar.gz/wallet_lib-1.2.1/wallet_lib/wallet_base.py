bf29f6c9f7a30615063a6342249afc7f0e6d7b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:37 +0000

    Add package wallet-lib/1.2.1/wallet_lib-1.2.1.tar.gz

---
+++ packages/wallet-lib/wallet_lib-1.2.1.tar.gz/wallet_lib-1.2.1/wallet_lib/wallet_base.py
@@ -0,0 +1,39 @@
+import json
+
+from abc import ABCMeta, abstractmethod
+from .adapters import WalletAdapterBase
+from .wallet_exceptions import WalletException
+from .zmq_notifier import ZMQNotifier
+
+class WalletBase(metaclass=ABCMeta):
+
+    def load_json(self, val: str, raw: bool):
+        if raw: return val
+        return json.loads(val)
+
+    @abstractmethod
+    def __init__(self, *args): pass
+
+    @abstractmethod
+    def create_address(self, *args): pass
+
+    @abstractmethod
+    def get_balance(self, *args): pass
+
+    @abstractmethod
+    def get_transaction(self, *args): pass
+
+    @abstractmethod
+    def get_transactions(self, *args): pass
+
+    @abstractmethod
+    def send(self, *args): pass
+
+    @abstractmethod
+    def get_transactions_since(self, *args): pass
+
+    @abstractmethod
+    def run(self, *args): pass
+
+    def get_zmq_notifier(self, **kwargs):
+        return ZMQNotifier(**kwargs)
