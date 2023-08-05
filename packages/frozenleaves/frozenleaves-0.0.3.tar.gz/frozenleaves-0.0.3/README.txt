380dab51fc771b449c8550ce71cf7f419b958b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package frozenleaves/0.0.3/frozenleaves-0.0.3.tar.gz

---
+++ packages/frozenleaves/frozenleaves-0.0.3.tar.gz/frozenleaves-0.0.3/README.txt
@@ -0,0 +1,1 @@
+This is a test package, made by 幽若
---
+++ packages/frozenleaves/frozenleaves-0.0.3.tar.gz/frozenleaves-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="frozenleaves",
+    version="0.0.3",
+    py_modules=['demo', 'hello'],
+    packages = ['frozenleaves'],
+    author="frozenleaves",
+    author_email="914814442@qq.com",
+    description="A test package",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="http://frozenleaves.cn",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
