886ca813333a934c5554c6bd199b9bb92fd638e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:30 +0000

    Add package tap-bing-ads/2.0.9/tap-bing-ads-2.0.9.tar.gz

---
+++ packages/tap-bing-ads/tap-bing-ads-2.0.9.tar.gz/tap-bing-ads-2.0.9/tap_bing_ads/exclusions.py
@@ -0,0 +1,103 @@
+EXCLUSIONS = {
+    'AccountPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'ClickSharePercent',
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'AdGroupPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'ClickSharePercent',
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'CampaignPerformanceReport': {
+        'Attributes': [
+            'BidMatchType',
+            'DeviceOS',
+            'TopVsOther',
+            'BudgetAssociationStatus',
+            'BudgetName',
+            'BudgetStatus'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'ClickSharePercent',
+            'ExactMatchImpressionSharePercent',
+            'ImpressionLostToAdRelevancePercent',
+            'ImpressionLostToBidPercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToExpectedCtrPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'ProductDimensionPerformanceReport': {
+        'Attributes': [
+            'AdDistribution',
+            'AdId',
+            'AdStatus',
+            'ClickType',
+            'Language',
+            'LocalStoreCode',
+            'Network',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'BenchmarkBid',
+            'BenchmarkCtr',
+            'ClickSharePercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    },
+    'ProductPartitionPerformanceReport': {
+        'Attributes': [
+            'AdDistribution',
+            'AdId',
+            'AdStatus',
+            'BidMatchType',
+            'ClickType',
+            'DeliveredMatchType',
+            'Language',
+            'LocalStoreCode',
+            'Network',
+            'TopVsOther'
+        ],
+        'ImpressionSharePerformanceStatistics': [
+            'AbsoluteTopImpressionSharePercent',
+            'BenchmarkBid',
+            'BenchmarkCtr',
+            'ClickSharePercent',
+            'ImpressionLostToBudgetPercent',
+            'ImpressionLostToRankPercent',
+            'ImpressionSharePercent'
+        ]
+    }
+}
