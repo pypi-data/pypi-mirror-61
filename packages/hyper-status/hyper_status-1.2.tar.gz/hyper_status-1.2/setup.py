0948f4a0ce12430f4e2febfaf7c668b2da327b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:00 +0000

    Add package hyper-status/1.2/hyper_status-1.2.tar.gz

---
+++ packages/hyper-status/hyper_status-1.2.tar.gz/hyper_status-1.2/setup.py
@@ -0,0 +1,31 @@
+'''
+> hyper_status
+> Copyright (c) 2020 Xithrius
+> MIT license, Refer to LICENSE for more info
+'''
+
+
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="hyper_status",
+    version="1.2",
+    author="Xithrius",
+    author_email="xithrius@example.com",
+    description="A status indicator that you can modify.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Xithrius/hyper-status",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
