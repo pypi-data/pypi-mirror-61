67a679d469d1975186e8a5948e9bf1949edd298a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package surfgen/0.0.3/surfgen-0.0.3.tar.gz

---
+++ packages/surfgen/surfgen-0.0.3.tar.gz/surfgen-0.0.3/README.md
@@ -0,0 +1,8 @@
+# surfgen package - a useful tool for generating slab structures
+
+This package is created for creating slab models in computational simulations of heterogeneous catalysis and electrocatalysis systems.
+
+This package will need:
+* ase
+* pymatgen
+* copy
---
+++ packages/surfgen/surfgen-0.0.3.tar.gz/surfgen-0.0.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from setuptools import setup
+setup(name='surfgen',
+version='0.0.3',
+description='Tools for creating slab models',
+author='Zheng-Da He',
+author_email='z.he@fz-juelich.de, jameshzd@mail.ustc.edu.cn, zhengdahe.electrocatalysis@gmail.com',
+license='MIT',
+packages=setuptools.find_packages(),
+zip_safe=False)
