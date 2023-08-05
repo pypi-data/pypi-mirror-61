b5ccb069da84d15456afa9c333db849094a796e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:20 +0000

    Add package Outliers-Python-101703101/0.2/Outliers_Python_101703101-0.2.tar.gz

---
+++ packages/Outliers-Python-101703101/Outliers_Python_101703101-0.2.tar.gz/Outliers_Python_101703101-0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+  name = 'Outliers_Python_101703101',
+  packages = ['Outliers_Python_101703101'],
+  version = '0.2',
+  license='MIT',
+  description = 'Library to Delete Outliers using Z-Index',
+  author = 'Arpit Singla',
+  author_email = 'arpitsingla1999@gmail.com',
+  url = 'https://github.com/ArpitSingla/Outliers_Python_101703101',
+  download_url = 'https://github.com/ArpitSingla/Outliers_Python_101703101.git',
+  keywords = ['Outliers_Python_101703101'],
+  install_requires=[
+        'python',
+        'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
