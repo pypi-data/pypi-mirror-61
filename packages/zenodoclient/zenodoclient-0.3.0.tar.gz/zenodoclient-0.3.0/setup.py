de991110c26d5f92c68b0cb935d8dff891990fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:54 +0000

    Add package zenodoclient/0.3.0/zenodoclient-0.3.0.tar.gz

---
+++ packages/zenodoclient/zenodoclient-0.3.0.tar.gz/zenodoclient-0.3.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zenodoclient',
+    version='0.3.0',
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    description='programmatic access to ZENODO',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='',
+    license='Apache 2.0',
+    url='https://github.com/shh-dlce/zenodoclient',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={},
+    platforms='any',
+    python_requires='>=3.5',
+    install_requires=[
+        'requests',
+        'attrs>=18.2',
+        'bs4',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'mock',
+            'pytest>=4.3',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
