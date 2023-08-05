ab7d37d2c68fb7029b7c3fd46a98e213ace45630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package meiteimayek-mapping/0.0.1b2/meiteimayek-mapping-0.0.1b2.tar.gz

---
+++ packages/meiteimayek-mapping/meiteimayek-mapping-0.0.1b2.tar.gz/meiteimayek-mapping-0.0.1b2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name='meiteimayek-mapping',
+  packages=['mayek'],
+  include_package_data=True,
+  version='0.0.1b2',
+  license='Apache-2.0',
+  description='Utility Functions for managing Meitei Mayek Characters and mapping to E-pao Font.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author='Gyanendro Kh',
+  author_email='khgyanendro77@gmail.com',
+  url='https://github.com/GyanendroKh/MayekMapping/tree/master/python',
+  keywords=['meiteimayek', 'meitei-mayek', 'mapping', 'utility', 'manipuri'],
+  classifiers=[        
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent'
+  ],
+)
