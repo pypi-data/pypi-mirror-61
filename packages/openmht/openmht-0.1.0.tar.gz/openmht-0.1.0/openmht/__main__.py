5087a357a2b1f1e533d5115dcb6c92077c86ce61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:20 +0000

    Add package openmht/0.1.0/openmht-0.1.0.tar.gz

---
+++ packages/openmht/openmht-0.1.0.tar.gz/openmht-0.1.0/openmht/__main__.py
@@ -0,0 +1,14 @@
+from openmht import cli
+
+
+__author__ = "Jon Perdomo"
+__license__ = "GPL-3.0"
+__version__ = "0.0.2"
+
+
+def main():
+    cli.read_cli_parameters()
+
+
+if __name__ == '__main__':
+    main()
