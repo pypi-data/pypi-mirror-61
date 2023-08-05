c1539c4ff9c61609b78ddbe49b188f711f7fe063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package arxiv-base/0.16.6rc7/arxiv-base-0.16.6rc7.tar.gz

---
+++ packages/arxiv-base/arxiv-base-0.16.6rc7.tar.gz/arxiv-base-0.16.6rc7/arxiv/base/version.py
@@ -0,0 +1,3 @@
+#  Created by tag_check.write_version 
+__when__='12/02/2020 14:47:27'
+__version__='0.16.6-rc7'
---
+++ packages/arxiv-base/arxiv-base-0.16.6rc7.tar.gz/arxiv-base-0.16.6rc7/setup.py
@@ -0,0 +1,23 @@
+"""Install arXiv-base as an importable package."""
+
+from setuptools import setup, find_packages
+from arxiv.release.tag_check import get_version
+
+setup(
+    name='arxiv-base',
+    version=get_version('arxiv-base'),
+    packages=[f'arxiv.{package}' for package
+              in find_packages('arxiv', exclude=['*test*'])],
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'jsonschema',
+        'pytz',
+        'uwsgi',
+        'boto3',
+        'bleach==3.1.0',
+        'backports-datetime-fromisoformat==1.0.0',
+        'typing-extensions'
+    ],
+    include_package_data=True
+)
