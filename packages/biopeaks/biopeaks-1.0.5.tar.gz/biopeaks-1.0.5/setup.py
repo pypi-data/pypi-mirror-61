977ca93ca20ab822ef0ba3b8215df1bce201fb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:24 +0000

    Add package biopeaks/1.0.5/biopeaks-1.0.5.tar.gz

---
+++ packages/biopeaks/biopeaks-1.0.5.tar.gz/biopeaks-1.0.5/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="biopeaks",
+    version="1.0.5",
+    description="A graphical user interface for the analysis of OpenSignals ECG - and breathing biosignals",
+    url="https://github.com/JohnDoenut/biopeaks",
+    author="Jan C. Brammer",
+    author_email="j.brammer@psych.ru.nl",
+    keywords="ECG Breathing Biosignals Bitalino OpenSignals GUI",
+    packages=find_namespace_packages(exclude=["misc"]),
+    python_requires=">=3.7",
+    license="GPL-3.0",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "biopeaks=biopeaks.__main__:main",
+        ],
+    }
+)
