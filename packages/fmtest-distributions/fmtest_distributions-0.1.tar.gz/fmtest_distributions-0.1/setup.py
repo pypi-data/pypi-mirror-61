cb84069c26ee24e0be0793f6026d62834d6a19d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:06 +0000

    Add package fmtest-distributions/0.1/fmtest_distributions-0.1.tar.gz

---
+++ packages/fmtest-distributions/fmtest_distributions-0.1.tar.gz/fmtest_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fmtest_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['fmtest_distributions'],
+      author='Felix Mak',
+      author_email='fhw.mak@gmail.com',
+      zip_safe=False)
