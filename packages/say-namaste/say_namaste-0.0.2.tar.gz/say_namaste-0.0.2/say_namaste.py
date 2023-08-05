e6e1a3aff754c6d39ba46f2792ee00f189ea8c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:02 +0000

    Add package say-namaste/0.0.2/say_namaste-0.0.2.tar.gz

---
+++ packages/say-namaste/say_namaste-0.0.2.tar.gz/say_namaste-0.0.2/say_namaste.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 17:42:05 2020
+
+@author: kbansal_be17
+"""
+
+def hello(name=None):
+    if name is None:
+        print("Hello World!")
+    else:
+         print("Hello",name,"!")   
