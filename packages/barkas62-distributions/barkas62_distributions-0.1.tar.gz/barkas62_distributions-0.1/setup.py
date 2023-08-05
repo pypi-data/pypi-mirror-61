c400394830271e23100023802c0415aaf4d695f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:54 +0000

    Add package barkas62-distributions/0.1/barkas62_distributions-0.1.tar.gz

---
+++ packages/barkas62-distributions/barkas62_distributions-0.1.tar.gz/barkas62_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='barkas62_distributions',
+      version='0.1',
+      description='Gaussian ans Binomial distributions',
+      packages=['barkas62_distributions'],
+      author = 'Boris Gorbatov',
+      zip_safe=False)
