90f872cd198c8cefb4b6c29c70a622d925dd865d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:37 +0000

    Add package enjoyml/0.2.0/enjoyml-0.2.0.tar.gz

---
+++ packages/enjoyml/enjoyml-0.2.0.tar.gz/enjoyml-0.2.0/enjoyml/__init__.py
@@ -0,0 +1,19 @@
+"""
+Content:
+    - multiclass
+        - multiclass_cross_val_results
+        - calc_class_weights
+    - keras
+        - layers
+            - FixedPooling2D
+        - metrics
+            - recall
+            - precision
+            - f1_score
+            - iou
+        -application
+            - U_net
+"""
+from enjoyml import multiclass
+
+__all__ = ['multiclass', 'keras']
