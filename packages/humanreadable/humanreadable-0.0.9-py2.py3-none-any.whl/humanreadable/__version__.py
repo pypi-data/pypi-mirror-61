38d479e6a5c532a8508c7b79455dbe1ea9054db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:24 +0000

    Add package humanreadable/0.0.9/humanreadable-0.0.9-py2.py3-none-any.whl

---
+++ packages/humanreadable/humanreadable-0.0.9-py2.py3-none-any.whl/humanreadable/__version__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+
+__author__ = "Tsuyoshi Hombashi"
+__copyright__ = "Copyright 2019, {}".format(__author__)
+__license__ = "MIT License"
+__version__ = "0.0.9"
+__maintainer__ = __author__
+__email__ = "tsuyoshi.hombashi@gmail.com"
