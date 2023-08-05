a6bd4256cdb0bc1a612c174a5b6ba4ca6aa39e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:41 +0000

    Add package latextomd/0.1.4/latextomd-0.1.4-py3-none-any.whl

---
+++ packages/latextomd/latextomd-0.1.4-py3-none-any.whl/latextomd/postpandoc.py
@@ -0,0 +1,17 @@
+import re
+from latextomd import config
+
+
+class Postpandoc(object):
+    def __init__(self, markdown):
+        self.content = markdown
+
+    def process(self):
+        self._replace()
+
+        return self.content
+
+    def _replace(self):
+        for item in config.postpandoc:
+            p = re.compile(item[0])
+            self.content = p.sub(item[1], self.content)
---
+++ packages/latextomd/latextomd-0.1.4-py3-none-any.whl/latextomd/prepandoc.py
@@ -0,0 +1,16 @@
+import codecs
+import os
+import re
+
+
+class Prepandoc(object):
+    def __init__(self, latex_string="", export_file=""):
+        self.content = latex_string
+        self.export_file = export_file
+
+    def process(self):
+        if "\\begin{document}" in self.content:
+            self.preamble, self.content = self.content.split("\\begin{document}")
+        else:
+            self.preamble = "\\documentclass{article}"
+            self.content = self.content + "\n\\end{document}"
