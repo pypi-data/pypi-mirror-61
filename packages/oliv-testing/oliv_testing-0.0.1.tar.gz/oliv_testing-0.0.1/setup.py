69d9b39a7f00bbd87d653858b64b56cc71cbbd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package oliv-testing/0.0.1/oliv_testing-0.0.1.tar.gz

---
+++ packages/oliv-testing/oliv_testing-0.0.1.tar.gz/oliv_testing-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+                 name='oliv_testing',
+                 version='0.0.1',
+                 description='a python wrapper for an AIS Database API',
+                 install_requires=[
+                     'requests',
+                     'python-dotenv'
+                 ],
+                 url='https://github.com/GeoBigData/AISDatabase',
+                 author='Elizabeth Golden, Rachel Wegener, Melissa Dozier',
+                 author_email='elizabeth.golden@digitalglobe.com',
+                 license='',
+                 packages=setuptools.find_packages(),
+                 python_requires='>=3')
+
+
+# up the version
+# python3 setup.py sdist bdist_wheel
+# delete old dist files
+# python3 -m twine upload dist/*
+# @pypi
+# conda info --envs
+# conda remove --name olive --all
+# conda create -n olive python=3.5
+# conda activate olive
+# pip install olive
