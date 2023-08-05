8db98b50912bbdcb8bf85f877b5014b6b51f210f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:05 +0000

    Add package pygomas/0.3.0/pygomas-0.3.0-py2.py3-none-any.whl

---
+++ packages/pygomas/pygomas-0.3.0-py2.py3-none-any.whl/pygomas/__init__.py
@@ -0,0 +1,26 @@
+import pygomas.vector
+import pygomas.sight
+import pygomas.mobile
+import pygomas.service
+import pygomas.threshold
+import pygomas.config
+import pygomas.map
+import pygomas.agent
+import pygomas.server
+import pygomas.pack
+import pygomas.ammopack
+import pygomas.medicpack
+import pygomas.objpack
+
+__version__ = "__version__ = '0.3.0'"
+
+MIN_POWER = 0
+MAX_POWER = 100
+POWER_UNIT = 25
+MIN_STAMINA = 0
+MAX_STAMINA = 100
+STAMINA_UNIT = 5
+MIN_AMMO = 0
+MAX_AMMO = 100
+MIN_HEALTH = 0
+MAX_HEALTH = 100
