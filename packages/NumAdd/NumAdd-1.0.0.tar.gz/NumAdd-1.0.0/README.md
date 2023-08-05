39f2878f24bdc371e61cd1536174d6784a8bf502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:35 +0000

    Add package NumAdd/1.0.0/NumAdd-1.0.0.tar.gz

---
+++ packages/NumAdd/NumAdd-1.0.0.tar.gz/NumAdd-1.0.0/README.md
@@ -0,0 +1,6 @@
+# Number Functions
+
+A Python Package of various Number(Integer) Functions for computations
+
+## Various Functions and their Description
+You can call directly these functions by importing this package
---
+++ packages/NumAdd/NumAdd-1.0.0.tar.gz/NumAdd-1.0.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    #Here is the module name.
+    name="NumAdd",
+ 
+    #version of the module
+    version="1.0.0",
+ 
+    #Name of Author
+    author="Roop Sai Pavan Tej Pendyala",
+ 
+    #your Email address
+    author_email="roopsai84@gmail.com",
+ 
+    #Small Description about module
+    description="Computations on Numbers",
+ 
+    long_description=long_description,
+ 
+    #Specifying that we are using markdown file for description
+    long_description_content_type="text/markdown",
+ 
+    #Any link to reach this module, if you have any webpage or github profile
+    url="https://github.com/RoopSai-PavanTej/NumberFunctions",
+    packages=setuptools.find_packages(),
+ 
+    #classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
