9971546fbc666e289f528045446a24e686230665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:21 +0000

    Add package textworld/1.2.0/textworld-1.2.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/textworld/textworld-1.2.0-cp37-cp37m-manylinux1_x86_64.whl/textworld/challenges/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+from textworld.challenges.registration import register, CHALLENGES
+from textworld.challenges import coin_collector
+from textworld.challenges import treasure_hunter
+from textworld.challenges import simple
