b28285dcdec4135f0a913e1f26241bb1ebc7c561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:08 +0000

    Add package McStasScript/0.0.2/McStasScript-0.0.2.tar.gz

---
+++ packages/McStasScript/McStasScript-0.0.2.tar.gz/McStasScript-0.0.2/mcstasscript/interface/functions.py
@@ -0,0 +1,93 @@
+from mcstasscript.data.data import McStasData
+from mcstasscript.helper.managed_mcrun import ManagedMcrun
+
+
+def name_search(name, data_list):
+    """"
+    name_search returns McStasData instance with specific name if it is
+    in the given data_list. If no match is found, a search for the data
+    filename is performed. If several matches are found, a list of
+    McStasData objects are returned.
+
+    The index of certain datasets in the data_list can change if
+    additional monitors are added so it is more convinient to access
+    the data files using their names.
+
+    Parameters
+    ----------
+    name : string
+        Name of the dataset to be retrived (component_name)
+
+    data_list : List of McStasData instances
+        List of datasets to search
+    """
+    if type(data_list) is not list:
+        raise InputError(
+            "name_search function needs list of McStasData as input")
+
+    if not type(data_list[0]) == McStasData:
+        raise InputError(
+            "name_search function needs objects of type McStasData as input.")
+
+    # Search by component name
+    list_result = []
+    for check in data_list:
+        if check.name == name:
+            list_result.append(check)
+
+    if len(list_result) == 0:
+        # Search by filename
+        for check in data_list:
+            if check.metadata.filename == name:
+                list_result.append(check)
+    
+    if len(list_result) == 0:
+        raise NameError("No dataset with name: \""
+                        + name
+                        + "\" found.")
+
+    if len(list_result) == 1:
+        return list_result[0]
+    else:
+        return list_result
+
+
+def name_plot_options(name, data_list, **kwargs):
+    """"
+    name_plot_options passes keyword arguments to dataset with certain
+    name in given data list
+
+    Function for quickly setting plotting options on a certain dataset
+    n a larger list of datasets
+
+    Parameters
+    ----------
+    name : string
+        Name of the dataset to be modified (component_name)
+
+    data_list : List of McStasData instances
+        List of datasets to search
+
+    kwargs : keyword arguments
+        Keyword arguments passed to set_plot_options in
+        McStasPlotOptions
+    """
+    object_to_modify = name_search(name, data_list)
+    if type(object_to_modify) is not list:                     
+        object_to_modify.set_plot_options(**kwargs)
+    else:
+        for data_object in object_to_modify:
+            data_object.set_plot_options(**kwargs)
+
+
+def load_data(foldername):
+    """
+    Loads data from a McStas data folder including mccode.sim
+
+    Parameters
+    ----------
+        foldername : string
+            Name of the folder from which to load data
+    """
+    managed_mcrun = ManagedMcrun("dummy", foldername=foldername)
+    return managed_mcrun.load_results()
