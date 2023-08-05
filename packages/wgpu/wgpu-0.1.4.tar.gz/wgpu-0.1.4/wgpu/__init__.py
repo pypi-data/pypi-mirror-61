2c1e3566e547cd7c6c1ab7ad58a41a08d9f72100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:55 +0000

    Add package wgpu/0.1.4/wgpu-0.1.4.tar.gz

---
+++ packages/wgpu/wgpu-0.1.4.tar.gz/wgpu-0.1.4/wgpu/__init__.py
@@ -0,0 +1,25 @@
+from .flags import *  # noqa: F401,F403
+from .enums import *  # noqa: F401,F403
+from .base import *  # noqa: F401,F403
+from ._coreutils import help  # noqa: F401
+from . import base
+
+
+__version__ = "0.1.4"
+
+
+def _register_backend(func, func_async):
+    if not (callable(func) and func.__name__ == "requestAdapter"):
+        raise RuntimeError(
+            "WGPU backend must be registered with function called requestAdapterSync."
+        )
+    if not (callable(func_async) and func_async.__name__ == "requestAdapterAsync"):
+        raise RuntimeError(
+            "WGPU backend must be registered with function called requestAdapterAsync."
+        )
+    if globals()["requestAdapter"] is not base.requestAdapter:
+        raise RuntimeError("WGPU backend can only be set once.")
+    globals()["requestAdapter"] = func
+    globals()["requestAdapterAsync"] = func_async
+
+    # todo: auto-select upon using requestAdapter?
