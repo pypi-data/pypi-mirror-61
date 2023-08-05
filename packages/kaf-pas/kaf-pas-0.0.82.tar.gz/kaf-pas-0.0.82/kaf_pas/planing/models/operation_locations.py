22e2b9b6614f3afa24c5333103b596857d52c8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:08 +0000

    Add package kaf-pas/0.0.82/kaf-pas-0.0.82.tar.gz

---
+++ packages/kaf-pas/kaf-pas-0.0.82.tar.gz/kaf-pas-0.0.82/kaf_pas/planing/models/operation_locations.py
@@ -0,0 +1,55 @@
+import logging
+
+from isc_common.fields.related import ForeignKeyProtect, ForeignKeyCascade
+from isc_common.models.audit import AuditModel, AuditManager, AuditQuerySet
+from kaf_pas.ckk.models.locations import Locations
+from kaf_pas.planing.models.operations import Operations
+
+logger = logging.getLogger(__name__)
+
+
+class Operation_locationsQuerySet(AuditQuerySet):
+    def delete(self):
+        return super().delete()
+
+    def create(self, **kwargs):
+        return super().create(**kwargs)
+
+    def filter(self, *args, **kwargs):
+        return super().filter(*args, **kwargs)
+
+
+class Operation_locationsManager(AuditManager):
+
+    @staticmethod
+    def getRecord(record):
+        res = {
+            'id': record.id,
+            'editing': record.editing,
+            'deliting': record.deliting,
+            'location_id': record.location.id,
+            'location__code': record.location.code,
+            'location__name': record.location.name,
+            'location__full_name': record.location.full_name,
+        }
+        return res
+
+    def get_queryset(self):
+        return Operation_locationsQuerySet(self.model, using=self._db)
+
+
+class Operation_locations(AuditModel):
+    operation = ForeignKeyCascade(Operations, related_name='planing_operation_loc')
+    location = ForeignKeyProtect(Locations, related_name='planing_resource_loc')
+
+    objects = Operation_locationsManager()
+
+    def __str__(self):
+        return f"ID:{self.id}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    class Meta:
+        verbose_name = 'Кросс таблица с место-положением'
+        unique_together = (('operation', 'location'),)
