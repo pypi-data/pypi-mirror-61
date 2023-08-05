1b7491eb8459cac231ecd8d1b9ace12dd4c8a615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:04 +0000

    Add package thingsprompt/0.3.0/thingsprompt-0.3.0.tar.gz

---
+++ packages/thingsprompt/thingsprompt-0.3.0.tar.gz/thingsprompt-0.3.0/README.md
@@ -0,0 +1,58 @@
+# ThingsPrompt
+
+Shell client for ThingsDB
+
+
+## Installation
+
+Using pip:
+
+```shell
+pip install thingsprompt
+```
+
+Or, clone this project and use the setup
+
+```shell
+python setup.py install
+```
+
+## Example usage
+
+```shell
+things-prompt -n localhost -u admin -p pass
+
+
+127.0.0.1:9200 (@thingsdb)> new_collection('my_collection');
+127.0.0.1:9200 (@thingsdb)> @:my_collection
+127.0.0.1:9200 (@:my_collection)> .greet = "Hello world!";
+
+"Hello world!"
+```
+
+## Arguments
+
+```
+  -h, --help            show help message and exit
+  --node NODE, -n NODE  node address
+  --port PORT           TCP port where the node is listening on for API calls
+  --user USER, -u USER  user name
+  --password PASSWORD, -p PASSWORD
+                        password, will be prompted if not given
+  --token TOKEN, -t TOKEN
+                        token key
+  --scope SCOPE, -s SCOPE
+                        set the initial scope
+  --timeout TIMEOUT     connect and query timeout in second
+  --ssl                 enable secure connection (SSL/TLS)
+  --version             print version and exit
+```
+
+## Special commands
+
+command        | description
+---------------|----------------------
+`?`            | Show help.
+`@scope`       | Switch to another scope, for example: `@:stuff`
+`@scope query` | Run a single query in a given scope, for example `@n node_info();`
+`CTRL + n`     | Insert a new line
---
+++ packages/thingsprompt/thingsprompt-0.3.0.tar.gz/thingsprompt-0.3.0/setup.py
@@ -0,0 +1,53 @@
+"""
+Upload to PyPI
+
+python3 setup.py sdist
+twine upload --repository pypitest dist/thingsprompt-X.X.X.tar.gz
+twine upload --repository pypi dist/thingsprompt-X.X.X.tar.gz
+"""
+from setuptools import setup, find_packages
+
+try:
+    with open('README.md', 'r') as f:
+        long_description = f.read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='thingsprompt',
+    version='0.3.0',  # Update version in thingsprompt as well
+    description='ThingsDB Prompt',
+    url='https://github.com/thingsdb/ThingsPrompt',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    scripts=['bin/things-prompt'],
+    license='GPLv3',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Database :: Front-Ends ',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+        'setproctitle',
+        'python-thingsdb',
+        'prompt-toolkit'
+    ],
+    keywords='database connector orm',
+)
