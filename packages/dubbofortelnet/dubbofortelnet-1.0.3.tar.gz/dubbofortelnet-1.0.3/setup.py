e3a425b5bb5c7b99d8a268cb2d3503966689313c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package dubbofortelnet/1.0.3/dubbofortelnet-1.0.3.tar.gz

---
+++ packages/dubbofortelnet/dubbofortelnet-1.0.3.tar.gz/dubbofortelnet-1.0.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     setup
+   Description :
+   Author :       zhangfujun
+   date：          2020/2/12
+-------------------------------------------------
+   Change Activity:
+                   2020/2/12:
+-------------------------------------------------
+"""
+__author__ = 'zhangfujun'
+
+
+from setuptools import setup,find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(filename):
+    with open(os.path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if not line.startswith('#')]
+
+setup(name='dubbofortelnet',
+      version='1.0.3',
+      description='test dubbo use telnet',
+      author='fujun.zhang',
+      author_email='yirandexin@163.com',
+      long_description=read_file('README.txt'),
+      packages=['dubbofortelnet'],
+      url='http://github.com'
+      )
