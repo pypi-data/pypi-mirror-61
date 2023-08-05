82ce2cadfb175c61a06240f786f76a16811f423b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:37 +0000

    Add package pythomata/0.3.0/pythomata-0.3.0.tar.gz

---
+++ packages/pythomata/pythomata-0.3.0.tar.gz/pythomata-0.3.0/pythomata/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Metadata of the package."""
+
+__title__ = "pythomata"
+__description__ = "A Python package implementing automata."
+__url__ = "https://github.com/whitemech/pythomata.git"
+__version__ = "0.3.0"
+__author__ = "Marco Favorito, Francesco Fuggitti"
+__author_email__ = "marco.favorito@gmail.com, francesco.fuggitti@gmail.com"
+__license__ = "Apache License"
+__copyright__ = "2018-2020 WhiteMech"
