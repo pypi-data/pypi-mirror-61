f2094e55e5d4fca28094493927204c720a70af42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:15 +0000

    Add package weaviate-client/0.3.2/weaviate_client-0.3.2-py3-none-any.whl

---
+++ packages/weaviate-client/weaviate_client-0.3.2-py3-none-any.whl/weaviate/__init__.py
@@ -0,0 +1,10 @@
+from .client import Client
+from .batch import ReferenceBatchRequest
+from .batch import ThingsBatchRequest
+from .exceptions import *
+from .util import generate_local_things_beacon
+from .classify import SOURCE_WHERE_FILTER, TRAINING_SET_WHERE_FILTER, TARGET_WHERE_FILTER
+from .auth import AuthClientCredentials, AuthClientPassword
+
+name = "weaviate"
+
