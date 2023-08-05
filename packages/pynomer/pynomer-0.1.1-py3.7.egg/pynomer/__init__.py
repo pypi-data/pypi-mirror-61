05856d5d6cec0d03ffa42bb423004b70a17eefa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:54 +0000

    Add package pynomer/0.1.1/pynomer-0.1.1-py3.7.egg

---
+++ packages/pynomer/pynomer-0.1.1-py3.7.egg/pynomer/__init__.py
@@ -0,0 +1,7 @@
+"""pynomer - A python wrapper for nomer"""
+
+from .pynomer import NomerClient
+
+__version__ = "0.1.1"
+__author__ = "nleguillarme <nicolas.leguillarme@univ-grenoble-alpes.fr>"
+__all__ = ["NomerClient"]
