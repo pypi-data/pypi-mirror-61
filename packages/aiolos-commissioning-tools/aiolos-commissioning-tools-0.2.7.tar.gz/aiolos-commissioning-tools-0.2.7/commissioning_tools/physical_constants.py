1feb8b0779bf5eae064f1cc88dff828e291eddba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:48 +0000

    Add package aiolos-commissioning-tools/0.2.7/aiolos-commissioning-tools-0.2.7.tar.gz

---
+++ packages/aiolos-commissioning-tools/aiolos-commissioning-tools-0.2.7.tar.gz/aiolos-commissioning-tools-0.2.7/commissioning_tools/physical_constants.py
@@ -0,0 +1,24 @@
+"""
+This .py file contains commonly used physical constants used 
+in fluid dynamics calculations. 
+"""
+
+
+# air
+GAMMA = 1.4
+R_AIR = 286.7       # J/kgK
+
+# water
+R_H2O = 461.52      # J/kgK
+
+PASCALS = 1.0           # Pa
+BAR = 1.0e5         # Pa
+PSI = 6894.76       # Pa
+INCHES_H2O = 248.84    # Pa
+
+# Velocity: used to convert values to base units of m/s
+KPH = 2.77778e-1    # m/s
+MPH = 4.44704e-1    # m/s
+
+# length:
+INCHES  = 2.54e-1       # m
