48ab53f235110675cbc40eb0afa339958e30b357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:17 +0000

    Add package Topsis-101703291-thapar/1.0.1/Topsis_101703291_thapar-1.0.1.tar.gz

---
+++ packages/Topsis-101703291-thapar/Topsis_101703291_thapar-1.0.1.tar.gz/Topsis_101703291_thapar-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+setup(
+    name='Topsis_101703291_thapar',
+    version='1.0.1',
+    description='A topsis Package Project of UCS633',
+    author='Krishang Dubey',
+    author_email='kdubey1999@gmail.com',
+    long_description="This package has been created based on Project 1 of course UCS633."
+    "Krishang Dubey COE13 101703291",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Python Software Foundation License",
+    ],
+    packages=["topsis_101703291"],
+    include_package_data=True,
+    entry_points = {
+        "console_scripts": [
+            "Topsis_101703291_thapar= topsis_101703291.Topsis:main",
+        ]
+    },
+    )
