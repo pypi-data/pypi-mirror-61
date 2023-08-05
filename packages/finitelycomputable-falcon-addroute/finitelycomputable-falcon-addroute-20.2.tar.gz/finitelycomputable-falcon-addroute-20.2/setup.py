bdfa5e54d516f61030a0c948742d942461133802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:54 +0000

    Add package finitelycomputable-falcon-addroute/20.2/finitelycomputable-falcon-addroute-20.2.tar.gz

---
+++ packages/finitelycomputable-falcon-addroute/finitelycomputable-falcon-addroute-20.2.tar.gz/finitelycomputable-falcon-addroute-20.2/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup
+from finitelycomputable_microsites_setup import version, base_setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='finitelycomputable-falcon-addroute',
+    version=version,
+    py_modules=['finitelycomputable.falcon_addroute'],
+    entry_points={
+        'console_scripts': [
+            'finitelycomputable-falcon-addroute = finitelycomputable.falcon_addroute:run']
+        },
+    description
+      ='The Falcon-based wsgi app for the microsites of finitelycomputable.net',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    scripts=['finitelycomputable_microsites_setup.py'],
+    install_requires=['falcon~=2.0'],
+    extras_require={
+        'bjoern': ['bjoern<4'],
+        'cheroot': ['cheroot<9'],
+        'cherrypy': ['cherrypy<19'],
+        'gunicorn': ['gunicorn<20'],
+        'waitress': ['waitress<1.4'],
+        },
+    url='https://www.finitelycomputable.net/',
+    **base_setup
+)
