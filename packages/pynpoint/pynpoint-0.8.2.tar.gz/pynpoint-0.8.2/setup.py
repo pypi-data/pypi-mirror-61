358c76520ea488a05d576d7764e56182d1c8e9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:46 +0000

    Add package pynpoint/0.8.2/pynpoint-0.8.2.tar.gz

---
+++ packages/pynpoint/pynpoint-0.8.2.tar.gz/pynpoint-0.8.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+reqs = parse_requirements('requirements.txt', session='hack')
+reqs = [str(ir.req) for ir in reqs]
+
+setup(
+    name='pynpoint',
+    version='0.8.2',
+    description='Pipeline for processing and analysis of high-contrast imaging data',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    author='Tomas Stolker & Markus Bonse',
+    author_email='tomas.stolker@phys.ethz.ch',
+    url='http://pynpoint.ethz.ch',
+    packages=['pynpoint',
+              'pynpoint.core',
+              'pynpoint.readwrite',
+              'pynpoint.processing',
+              'pynpoint.util'],
+    include_package_data=True,
+    install_requires=reqs,
+    license='GPLv3',
+    zip_safe=False,
+    keywords='pynpoint',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    tests_require=['pytest'],
+)
