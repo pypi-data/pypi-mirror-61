0adb3984396f8322cfe3c8de4bcbdeeb02facb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:46 +0000

    Add package distributions-kamal/0.1/distributions_kamal-0.1.tar.gz

---
+++ packages/distributions-kamal/distributions_kamal-0.1.tar.gz/distributions_kamal-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_kamal',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_kamal'],
+      author = 'Kamal Jain',
+      zip_safe=False)
