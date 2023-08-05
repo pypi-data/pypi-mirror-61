5f0745f95a93a333e31f434c2d06d3f4803380e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:44 +0000

    Add package cppwraper123/0.5/cppwraper123-0.5.tar.gz

---
+++ packages/cppwraper123/cppwraper123-0.5.tar.gz/cppwraper123-0.5/cppwraper123/wraper.py
@@ -0,0 +1,31 @@
+import ctypes as c
+import os
+import sys
+#C++
+#int sum(int arg1, int arg2)
+#void ref(int arg1, int arg2, int* res)
+#void mult_array(int* arg1, int k, int len, int* res)
+
+
+class dllwraper:
+
+    def __init__(self):
+    	#dir = os.path.dirname(sys.modules["cppdll"].__file__)
+    	#path = os.path.join(dir, 'CppDll.dll')
+    	self.dll = c.CDLL("cppwraper123/CppDll.dll")
+
+
+    def sum(self, x, y):
+        return self.dll.sum(x,y)
+
+    def ref(self, val):
+        v = c.c_int(val)
+        self.dll.ref(c.byref(v))       
+        return v.value
+
+    def mult_array(self, vector, k):
+        l  = len(vector)
+        v = (c.c_int * l)(*vector)
+        res = (c.c_int * l)()
+        self.dll.mult_array(v, k, l, res)       
+        return list(res)
---
+++ packages/cppwraper123/cppwraper123-0.5.tar.gz/cppwraper123-0.5/setup.py
@@ -0,0 +1,27 @@
+"""Setup script for realpython-reader"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="cppwraper123",
+    version="0.5",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["cppwraper123"],
+    include_package_data=True,
+    install_requires=[]
+)
