87fa03290a70a4f7e3fe87f5bba3735c070b23a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:53 +0000

    Add package spyoutlier2/1.0.2/spyoutlier2-1.0.2.tar.gz

---
+++ packages/spyoutlier2/spyoutlier2-1.0.2.tar.gz/spyoutlier2-1.0.2/outlier/outlier.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sys
+
+def check(inp):
+    try:
+        return pd.read_csv(inp)
+    except IOError:
+        raise Exception("not entered \n")
+        
+def main():
+    inp = sys.argv[1]
+  #  output_file = sys.argv[2]
+    df = check(inp)
+    data = pd.DataFrame(df.iloc[:,:-1].values)
+    q1=data.quantile(0.25)
+    q3=data.quantile(0.75)
+    iqr=q3-q1
+    lis = []
+    for col in data.columns:
+        for row in range(len(data.index)):
+            if(((data.iloc[row,col])<(q1[col]-1.5*iqr[col])) | ((data.iloc[row,col])>(q3[col]+1.5*iqr[col]))):
+                lis.append(row)       
+    for i in reversed(lis):
+        df.drop(df.index[i],inplace = True)
+   # df.to_csv(output_file,index=False)
+    print( "rows are deleted ",len(lis),".\n")
+
+main()
---
+++ packages/spyoutlier2/spyoutlier2-1.0.2.tar.gz/spyoutlier2-1.0.2/readme.md
@@ -0,0 +1,9 @@
+
+A Python package to get outlier result
+
+Usage
+Following query on terminal will provide you the topsis detail.
+Usages:
+ python outlier.py
+ 
+Example: python outlier.py myData.csv 
