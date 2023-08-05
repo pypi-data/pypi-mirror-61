baf613ca72b9312a1b595a2ba1beef2d78e76435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:58 +0000

    Add package kili/1.3.1/kili-1.3.1.tar.gz

---
+++ packages/kili/kili-1.3.1.tar.gz/kili-1.3.1/kili/mutations/tool.py
@@ -0,0 +1,40 @@
+from ..helpers import format_result, json_escape
+
+
+def update_tool(client, tool_id: str, project_id: str, tool_type: str, json_settings: dict):
+    result = client.execute('''
+    mutation {
+      updateTool(toolID: "%s",
+        projectID: "%s",
+        toolType: %s,
+        jsonSettings: "%s") {
+          id
+      }
+    }
+    ''' % (tool_id, project_id, tool_type, json_escape(json_settings)))
+    return format_result('updateTool', result)
+
+
+def append_to_tools(client, project_id: str,  tool_type: str, json_settings: dict):
+    result = client.execute('''
+    mutation {
+      appendToTools(
+        projectID: "%s",
+        toolType: %s,
+        jsonSettings: "%s") {
+          id
+      }
+    }
+    ''' % (project_id,  tool_type, json_escape(json_settings)))
+    return format_result('appendToTools', result)
+
+
+def delete_from_tools(client, tool_id: str):
+    result = client.execute('''
+    mutation {
+      deleteFromTools(toolID: "%s") {
+        id
+      }
+    }
+    ''' % (tool_id))
+    return format_result('deleteFromTools', result)
