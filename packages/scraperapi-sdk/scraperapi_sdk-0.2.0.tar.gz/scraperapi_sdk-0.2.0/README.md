aa6aaec6b66895c207a39f9055e87b1f7585507e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package scraperapi-sdk/0.2.0/scraperapi_sdk-0.2.0.tar.gz

---
+++ packages/scraperapi-sdk/scraperapi_sdk-0.2.0.tar.gz/scraperapi_sdk-0.2.0/README.md
@@ -0,0 +1,9 @@
+# Install
+```bash
+pip install scraperapi-sdk
+```
+
+```python
+import scraper_api
+client = ScraperAPIClient("<token>")
+```
---
+++ packages/scraperapi-sdk/scraperapi_sdk-0.2.0.tar.gz/scraperapi_sdk-0.2.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scraperapi_sdk", # Replace with your own username
+    version="0.2.0",
+    author="ScraperAPI",
+    author_email="dan@scraperapi.com",
+    description="Scraper API SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://scraperapi.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    python_requires='>=3.0',
+)
