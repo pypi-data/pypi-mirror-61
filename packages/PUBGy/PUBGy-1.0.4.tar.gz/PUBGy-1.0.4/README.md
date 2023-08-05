2e3f6e2da1747f77218b03d38da484de0bed09a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package PUBGy/1.0.4/PUBGy-1.0.4.tar.gz

---
+++ packages/PUBGy/PUBGy-1.0.4.tar.gz/PUBGy-1.0.4/README.md
@@ -0,0 +1,5 @@
+# pubgy
+
+Python Wrapper for interacting with the PUBG API
+
+Not fully functional, but working with V16.2.0
---
+++ packages/PUBGy/PUBGy-1.0.4.tar.gz/PUBGy-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from pubgy import __version__ as v
+with open("README.md") as info:
+    desc = info.read()
+print(desc)
+setup(
+   name='PUBGy',
+   version=v,
+   description="PUBG API wrapper for Python",
+   long_description=desc,
+   long_description_content_type="text/markdown",
+   url="https://github.com/Discord-ian/pubgy",
+   author='Discordian',
+   author_email='me.discordian@gmail.com',
+   packages=['pubgy'],
+   install_requires=['asyncio', 'aiohttp'],
+   python_requires='~=3.5'
+)
