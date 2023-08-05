a8d79e1b5557995ca268f6ed4e8c05e1bf215a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:20 +0000

    Add package aglink/1.56/aglink-1.56-py2.py3-none-any.whl

---
+++ packages/aglink/aglink-1.56-py2.py3-none-any.whl/aglink/__init__.py
@@ -0,0 +1,9 @@
+
+from . import __version__ as version
+__version__ 	= version.version
+__test__    	= '0.6'
+__build__		= '2.7'
+__platform__	= 'all'
+__email__		= "licface@yahoo.com"
+
+from .agl import autogeneratelink
