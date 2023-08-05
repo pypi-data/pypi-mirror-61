2e22f7e763ce3c14ead869399e0176326e24ac31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:33 +0000

    Add package bwx-adventure/1.1/bwx-adventure-1.1.tar.gz

---
+++ packages/bwx-adventure/bwx-adventure-1.1.tar.gz/bwx-adventure-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='bwx-adventure',
+   version='1.1',
+   description='A module made by Gever on github that I have turned into a PyPi module with no changes to the original files. I DO NOT OWN OR CLAIM THIS, I just have made it installable with pip. His website: http://gevertulley.com',
+   license="MIT",
+   author='Gever',
+   author_email='',
+   url="https://github.com/gever/bwx-adventure",
+   packages=['/Users/Homework/Documents/GitHub/bwx-adventure'],  #same as name
+)
