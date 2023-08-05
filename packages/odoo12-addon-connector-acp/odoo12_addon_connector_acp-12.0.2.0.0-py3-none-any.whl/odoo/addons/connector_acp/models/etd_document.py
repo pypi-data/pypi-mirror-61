a08fa97113c523079294040736048b8b280a6add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:55 +0000

    Add package odoo12-addon-connector-acp/12.0.2.0.0/odoo12_addon_connector_acp-12.0.2.0.0-py3-none-any.whl

---
+++ packages/odoo12-addon-connector-acp/odoo12_addon_connector_acp-12.0.2.0.0-py3-none-any.whl/odoo/addons/connector_acp/models/etd_document.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2019 Konos
+# Copyright (C) 2019 Blanco Martín & Asociados
+# Copyright (C) 2019 CubicERP
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class EtdDocument(models.Model):
+    _name = "etd.document"
+    _description = "Electronic Tax Documents To Sign"
+
+    name = fields.Char(string="Name", required=True)
+    model = fields.Selection([], string="Odoo Model", required=True)
+    invoicing_policy = fields.Selection(
+        [("ticket", "Ticket"), ("invoice", "Invoice"), ("eguide", "Electronic Guide")],
+    )
+    file_ids = fields.One2many("etd.document.file", "document_id", string="Files")
