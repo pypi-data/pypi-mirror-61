fd5f003af61da82504ef8afe85dc275d1122ec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package django-jutil/2.4.2/django-jutil-2.4.2.tar.gz

---
+++ packages/django-jutil/django-jutil-2.4.2.tar.gz/django-jutil-2.4.2/jutil/management/commands/test_email.py
@@ -0,0 +1,23 @@
+from django.core.management.base import CommandParser
+from django.utils.timezone import now
+from jutil.command import SafeCommand
+from jutil.email import send_email
+
+
+class Command(SafeCommand):
+    help = 'Send test email with attachment'
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument('email', type=str)
+        parser.add_argument('--cc', type=str)
+        parser.add_argument('--bcc', type=str)
+        parser.add_argument('--attach', type=str, nargs='*')
+
+    def do(self, *args, **kw):
+        files = kw['attach'] if kw['attach'] else []
+        subject = 'hello ' + now().isoformat()
+        text = 'body text'
+        html = '<h1>html text</h1>'
+        sender = '"Kajala Group Asiakaspalvelu" <asiakaspalvelu@kajala.com>'
+        res = send_email(kw['email'], subject, text, html, sender, files, bcc_recipients=kw['bcc'], cc_recipients=kw['cc'])
+        print('send_email returned', res)
