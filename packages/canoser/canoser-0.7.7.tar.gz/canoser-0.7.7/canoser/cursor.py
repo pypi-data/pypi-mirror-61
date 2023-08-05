23d002f1a116932792116ee15e37474ff76f0fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package canoser/0.7.7/canoser-0.7.7.tar.gz

---
+++ packages/canoser/canoser-0.7.7.tar.gz/canoser-0.7.7/canoser/cursor.py
@@ -0,0 +1,30 @@
+
+class Cursor:
+    def __init__(self, buffer, offset=0):
+        self.buffer = buffer
+        if isinstance(buffer, list):
+            self.buffer = bytes(buffer)
+        self.offset = offset
+        self.buffer_len = len(self.buffer)
+
+    def read_bytes(self, size):
+        end = self.offset + size
+        if end > self.buffer_len:
+            raise IOError("{} exceed buffer size: {}".format(end, self.buffer_len))
+        ret = self.buffer[self.offset:end]
+        self.offset = end
+        return ret
+
+    def read_to_end(self):
+        ret = self.buffer[self.offset:]
+        self.offset = self.buffer_len
+        return ret
+
+    def peek_bytes(self, size):
+        end = self.offset + size
+        if end > self.buffer_len:
+            raise IOError("{} exceed buffer size: {}".format(end, total))
+        return self.buffer[self.offset:end]
+
+    def is_finished(self):
+        return self.offset == self.buffer_len
