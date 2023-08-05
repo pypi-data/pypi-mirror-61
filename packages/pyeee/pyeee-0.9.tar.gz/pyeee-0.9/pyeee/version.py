ef61a46237fd59068d9ec6d7f81e0f2405cec74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:55 +0000

    Add package pyeee/0.9/pyeee-0.9.tar.gz

---
+++ packages/pyeee/pyeee-0.9.tar.gz/pyeee-0.9/pyeee/version.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import division, absolute_import, print_function
+"""
+Provide version number for pyeee library.
+
+This module was written by Matthias Cuntz while at Institut National
+de Recherche en Agriculture, Alimentation et Environnement (INRAE),
+Nancy, France.
+
+Copyright (c) 2019-2020 Matthias Cuntz - mc (at) macu (dot) de
+Released under the MIT License; see LICENSE file for details.
+
+* Written Oct 2019 by Matthias Cuntz (mc (at) macu (dot) de)
+* Make numpy docstring format, Dec 2019, Matthias Cuntz
+* v0.2, initial PyPI commit, Jan 2020, Matthias Cuntz
+* v0.3, cleaning all minor TODOs, Jan 2020, Matthias Cuntz
+* v0.4, removed numpy deprecation warnings, Jan 2020, Matthias Cuntz
+* v0.4.1, first zenodo release, Jan 2020, Matthias Cuntz
+* v0.4.2, second release to trigger zenodo, Jan 2020, Matthias Cuntz
+* v0.5, removed bug in exe wrappers and added number of tests, Feb 2020, Matthias Cuntz
+* v0.6, fix tests on TravisCI and have tests for all modules, Feb 2020, Matthias Cuntz
+* v0.7, systematically logistic_p versions, and keep formatting with substitution functions, Feb 2020, Matthias Cuntz
+* v0.8, Split tests into individual files per module, and small bug fixes in tests and error handling, Feb 2020, Matthias Cuntz
+* v0.9, Renamed morris.py to morris_method.py and adapted args and kwargs to common names in pyeee, Feb 2020, Matthias Cuntz
+
+.. moduleauthor:: Matthias Cuntz
+"""
+__version__ = "0.9"
