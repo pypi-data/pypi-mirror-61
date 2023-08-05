23cd9244883ed499e09c43fd62fa4807eb386cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:37 +0000

    Add package finitelycomputable-helloworld-flask/20.2/finitelycomputable-helloworld-flask-20.2.tar.gz

---
+++ packages/finitelycomputable-helloworld-flask/finitelycomputable-helloworld-flask-20.2.tar.gz/finitelycomputable-helloworld-flask-20.2/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup
+from finitelycomputable_microsites_setup import version, base_setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='finitelycomputable-helloworld-flask',
+    version=version,
+    py_modules=['finitelycomputable.helloworld_flask'],
+    entry_points={
+        'console_scripts': [
+            'finitelycomputable-helloworld-flask = finitelycomputable.helloworld_flask:run']
+        },
+    description='A hello_world implementation in Flask',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    scripts=['finitelycomputable_microsites_setup.py'],
+    install_requires=['Flask~=1.1'],
+    url='https://www.finitelycomputable.net/identification_of_trust',
+    **base_setup
+)
