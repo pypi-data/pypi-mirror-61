1ac8ba107a13482632118c964671ae3a4af29f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:56 +0000

    Add package botwriter/0.1.0/botwriter-0.1.0.tar.gz

---
+++ packages/botwriter/botwriter-0.1.0.tar.gz/botwriter-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['botwriter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.0.1,<2.0.0', 'requests>=2.22.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['start = botwriter:main']}
+
+setup_kwargs = {
+    'name': 'botwriter',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Mattis Abrahamsson',
+    'author_email': 'mattis.abrahamsson@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
