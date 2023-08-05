0c96348e53161454ee98db189cd26d343dcaea4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:29:39 +0000

    Add package heavytailed/1.0.3/heavytailed-1.0.3.tar.gz

---
+++ packages/heavytailed/heavytailed-1.0.3.tar.gz/heavytailed-1.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+with open('README.md') as fp:
+    readme = fp.read()
+
+setup(
+    name='heavytailed',
+    version='1.0.3',
+    description='Perform distribution analysis on heavy-tailed distributed data',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Xiangwen Wang',
+    author_email='wangxiangwen1989@gmail.com',
+    url='https://github.com/XiangwenWang/heavytailed',
+    keywords='heavy-tailed, distribution analysis',
+    packages=['heavytailed'],
+    include_package_data=True,
+    install_requires=['numpy', 'scipy', 'pandas', 'mpmath', 'matplotlib'],
+    license="BSD 2-Clause License",
+    zip_safe=True,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
