24fc3f81ac6c0b5db9fcaa221181924f264c7898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:24 +0000

    Add package humanreadable/0.0.9/humanreadable-0.0.9.tar.gz

---
+++ packages/humanreadable/humanreadable-0.0.9.tar.gz/humanreadable-0.0.9/setup.py
@@ -0,0 +1,83 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+import io
+import os.path
+
+import setuptools
+
+
+MODULE_NAME = "humanreadable"
+REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
+REQUIREMENT_DIR = "requirements"
+ENCODING = "utf8"
+
+pkg_info = {}
+
+
+def get_release_command_class():
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with io.open("README.rst", encoding=ENCODING) as f:
+    LONG_DESCRIPTION = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    INSTALL_REQUIRES = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    TESTS_REQUIRES = [line.strip() for line in f if line.strip()]
+
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=pkg_info["__version__"],
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description=(
+        "humanreadable is a Python library to convert from human-readable values to Python values."
+    ),
+    include_package_data=True,
+    keywords=["human-readable", "converter"],
+    license=pkg_info["__license__"],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(exclude=["test*"]),
+    project_urls={"Source": REPOSITORY_URL, "Tracker": "{:s}/issues".format(REPOSITORY_URL),},
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        "release": ["releasecmd>=0.2.0,<1"],
+        "test": TESTS_REQUIRES,
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+    ],
+    cmdclass=get_release_command_class(),
+)
