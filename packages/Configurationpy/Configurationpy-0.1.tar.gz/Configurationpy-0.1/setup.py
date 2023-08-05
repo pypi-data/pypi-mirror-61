caae9c515a26fae5bba6a0e5a4a052d114630059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:05 +0000

    Add package Configurationpy/0.1/Configurationpy-0.1.tar.gz

---
+++ packages/Configurationpy/Configurationpy-0.1.tar.gz/Configurationpy-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup (
+    name='Configurationpy',
+    version='0.1',
+    description='A cool module for configuration, port of Conf.rb, my other library: https://rubygems.org/gems/confrb',
+    py_modules=['salt_coordinates'],
+    author = "Gurjus Bhasin",
+    packages = setuptools.find_packages(),
+    author_email="gsbhasin84@gmail.com",
+    install_requires = ['pyyaml'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.0',
+    url = 'https://gitlab.com/Gsbhasin84/configurationpy',
+    license = ("MIT"),
+    long_description = "# Conf.py\nConf.py is a small module for configuration. Docs, code examples, etc should be available on https://gitlab.com/Gsbhasin84/configurationpy"
+
+)
