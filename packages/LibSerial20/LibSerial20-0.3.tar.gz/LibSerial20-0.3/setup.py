f32671fc9015a2f39f22c495da2c1a53b0f32b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package LibSerial20/0.3/LibSerial20-0.3.tar.gz

---
+++ packages/LibSerial20/LibSerial20-0.3.tar.gz/LibSerial20-0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import setuptools
+setup(
+  name = 'LibSerial20',       
+  packages = ['LibSerial20'],   
+  version = '0.3',      
+  license='MIT',       
+  description = 'Biblioteca LibSerial20 ',   
+  author = 'Wesley',                  
+  author_email = 'diviwfel@gmail.com',     
+  url = 'https://github.com/DiviKnight/LibSerial20',  
+  download_url = 'https://github.com/DiviKnight/LibSerial20/archive/0.3.tar.gz',    
+  keywords = ['Serial', 'Senai', 'ComPort'],  
+  install_requires=[           
+    'datetime',       
+  ],
+  classifiers=[
+    #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"   
+    'Development Status :: 5 - Production/Stable',   
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
