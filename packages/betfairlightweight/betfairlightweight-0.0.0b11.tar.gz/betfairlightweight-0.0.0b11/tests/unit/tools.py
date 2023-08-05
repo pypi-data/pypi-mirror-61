32244dfec1fa401c9b8a3f8341f366763b0dfcec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package betfairlightweight/0.0.0b11/betfairlightweight-0.0.0b11.tar.gz

---
+++ packages/betfairlightweight/betfairlightweight-0.0.0b11.tar.gz/betfairlightweight-0.0.0b11/tests/unit/tools.py
@@ -0,0 +1,11 @@
+from unittest import mock
+import json
+
+
+def create_mock_json(path, status_code=200):
+    with open(path) as f:
+        resp = mock.Mock()
+        resp.content = resp.text = f.read()
+        resp.json.return_value = json.loads(resp.content)
+        resp.status_code = status_code
+        return resp
