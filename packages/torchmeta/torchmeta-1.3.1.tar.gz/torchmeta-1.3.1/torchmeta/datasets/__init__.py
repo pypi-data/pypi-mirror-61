a84592e3b204b2ec220c4ad72ae09e70cf9604b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:52 +0000

    Add package torchmeta/1.3.1/torchmeta-1.3.1.tar.gz

---
+++ packages/torchmeta/torchmeta-1.3.1.tar.gz/torchmeta-1.3.1/torchmeta/datasets/__init__.py
@@ -0,0 +1,21 @@
+from torchmeta.datasets.doublemnist import DoubleMNIST
+from torchmeta.datasets.cub import CUB
+from torchmeta.datasets.cifar100 import CIFARFS, FC100
+from torchmeta.datasets.miniimagenet import MiniImagenet
+from torchmeta.datasets.omniglot import Omniglot
+from torchmeta.datasets.tieredimagenet import TieredImagenet
+from torchmeta.datasets.tcga import TCGA
+
+from torchmeta.datasets import helpers
+
+__all__ = [
+    'TCGA',
+    'Omniglot',
+    'MiniImagenet',
+    'TieredImagenet',
+    'CIFARFS',
+    'FC100',
+    'CUB',
+    'DoubleMNIST',
+    'helpers'
+]
