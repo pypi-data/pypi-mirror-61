f3c2ed061439ccb73e39dcbef45b23c0064f8ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:09 +0000

    Add package qcSim/0.1/qcSim-0.1-py3-none-any.whl

---
+++ packages/qcSim/qcSim-0.1-py3-none-any.whl/qcSim/__init__.py
@@ -0,0 +1,10 @@
+from qcSim.state import qubit, zeros, ones, bit_array, bell_state
+from qcSim.state import State
+
+from qcSim.operators import identity, hadamard, pauli_x, pauli_y, pauli_z
+from qcSim.operators import rotation_x, rotation_y, rotation_z, swap, cnot
+from qcSim.operators import toffoli, controlled_operator, u_f, two_pulse
+from qcSim.operators import Operator
+
+
+__version__ = '0.1'
