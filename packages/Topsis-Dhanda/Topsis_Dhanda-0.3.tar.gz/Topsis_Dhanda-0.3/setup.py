bba9285b3be32072801bbb77dc4a4d09441b8089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:44 +0000

    Add package Topsis-Dhanda/0.3/Topsis_Dhanda-0.3.tar.gz

---
+++ packages/Topsis-Dhanda/Topsis_Dhanda-0.3.tar.gz/Topsis_Dhanda-0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Topsis_Dhanda", # Replace with your own username
+    version="0.3",
+    author="Nishant Dhanda",
+    author_email="dhandanishant012@gmail.com",
+    description="A small package that showcases Topsis approach",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    scripts=['bin/outcli'],
+    url="https://github.com/NishantDhanda/Topsis-Python.git",
+    download_url="https://github.com/NishantDhanda/Topsis-Python/archive/0.01.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
