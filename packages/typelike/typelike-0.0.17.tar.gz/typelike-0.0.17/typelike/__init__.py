6a2aecac7164f7d445a6baba684300af402e1fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:47 +0000

    Add package typelike/0.0.17/typelike-0.0.17.tar.gz

---
+++ packages/typelike/typelike-0.0.17.tar.gz/typelike-0.0.17/typelike/__init__.py
@@ -0,0 +1,18 @@
+
+from .core import *
+from .version import __version__
+
+# __all__ = [
+#     'core',
+#     '__version__'
+# ]
+
+__all__ = [
+    'AnyLike',
+    'Anything',
+    'ListLike',
+    'NumberLike',
+    'typelike',
+    'Undefined',
+    '__version__'
+]
---
+++ packages/typelike/typelike-0.0.17.tar.gz/typelike-0.0.17/typelike/core/__init__.py
@@ -0,0 +1,16 @@
+
+from .any import *
+from .core import *
+from .list import *
+from .none import *
+from .number import *
+
+__all__ = [
+    'AnyLike',
+    'Anything',
+    'infer_type',
+    'ListLike',
+    'NoneType',
+    'NumberLike',
+    'Undefined'
+]
