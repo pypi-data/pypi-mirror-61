9380d160619ec1cba733a35565af3e3781050212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:03 +0000

    Add package pyoverloader/1.0.1/pyoverloader-1.0.1.tar.gz

---
+++ packages/pyoverloader/pyoverloader-1.0.1.tar.gz/pyoverloader-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyoverloader',
+    version='1.0.1',
+    packages=find_packages(),
+    author='/in/dev/',
+    author_email='bdmitry450@gmail.com',
+    url='https://github.com/Dmitry450/pyover',
+)
