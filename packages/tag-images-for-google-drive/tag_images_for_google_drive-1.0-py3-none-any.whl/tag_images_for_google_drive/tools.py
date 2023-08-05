fedb60ac20fe97ebe2db3583775d859e4f2f4741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:12 +0000

    Add package tag-images-for-google-drive/1.0/tag_images_for_google_drive-1.0-py3-none-any.whl

---
+++ packages/tag-images-for-google-drive/tag_images_for_google_drive-1.0-py3-none-any.whl/tag_images_for_google_drive/tools.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Tools for machine learning pipeline.
+"""
+import glob
+import logging
+import os
+from typing import Optional, List
+from pathlib import Path
+
+import click
+
+
+def init_logger(logger: logging.Logger, level: int) -> None:
+    """ Init logger
+
+    :param logger The logger to initialize
+    :param level The level
+    """
+    # See https://stackoverflow.com/questions/20240464/python-logging-file-is-not-working-when-using-logging-basicconfig
+    logging.getLogger().setLevel(level)
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    # ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    ch.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+    logger.addHandler(ch)
+
+
+class Glob(click.ParamType):
+    """
+    A Click path argument that returns a List[Path] via un glob syntax, not a string.
+    """
+
+    def __init__(self, exists=False, recursive=False, default_suffix="*"):
+        self.exists = exists
+        self.default_suffix = default_suffix
+        self.recursive = recursive
+
+    def convert(
+            self,
+            value: str,
+            param: Optional[click.core.Parameter],
+            ctx: Optional[click.core.Context],
+    ) -> List[Path]:
+        """
+        Return a ``Path`` from the string ``click`` would have created with
+        the given options.
+        """
+        if "*" not in value and value[-1] in ['/', '\\']:
+            value = os.path.join(value, self.default_suffix)
+        return [Path(x) for x in
+                glob.glob(super().convert(value=value, param=param, ctx=ctx), recursive=self.recursive)]
