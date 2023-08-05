7ba821d801cb8891130055b7440b8ecc1f108574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:47 +0000

    Add package pypykatz/0.3.4/pypykatz-0.3.4-py3-none-any.whl

---
+++ packages/pypykatz/pypykatz-0.3.4-py3-none-any.whl/pypykatz/lsadecryptor/lsa_decryptor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# Author:
+#  Tamas Jos (@skelsec)
+#
+from pypykatz.lsadecryptor.lsa_template_nt5 import LsaTemplate_NT5
+from pypykatz.lsadecryptor.lsa_template_nt6 import LsaTemplate_NT6
+from pypykatz.lsadecryptor.lsa_decryptor_nt6 import LsaDecryptor_NT6
+from pypykatz.lsadecryptor.lsa_decryptor_nt5 import LsaDecryptor_NT5
+
+class LsaDecryptor:
+	def __init__(self):
+		pass
+	
+	@staticmethod
+	def choose(reader, decryptor_template, sysinfo):
+		if isinstance(decryptor_template, LsaTemplate_NT5):
+			#raise Exception('Windows NT5 (XP, 2003) is not yet supported!')
+			return LsaDecryptor_NT5(reader, decryptor_template, sysinfo)
+		else:
+			return LsaDecryptor_NT6(reader, decryptor_template, sysinfo)
