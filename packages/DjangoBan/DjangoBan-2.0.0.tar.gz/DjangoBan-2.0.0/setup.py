af0524a5fc08a7b08a70ba572b5a740ebed6e391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package DjangoBan/2.0.0/DjangoBan-2.0.0.tar.gz

---
+++ packages/DjangoBan/DjangoBan-2.0.0.tar.gz/DjangoBan-2.0.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='DjangoBan',
+    version='2.0.0',
+    packages=['ban', 'ban.migrations'],
+    url='https://github.com/AliYmn/DjangoBan',
+    license='MIT',
+    author='aliyaman',
+    author_email='aliymn.db@gmail.com',
+    description='Django User Ban Management ',
+    download_url='https://github.com/AliYmn/DjangoBan',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
