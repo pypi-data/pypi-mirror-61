a6c5be90641ef5aafc315fd4a3fa19eb2dfac78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package gblpyapi/0.1.1/gblpyapi-0.1.1.tar.gz

---
+++ packages/gblpyapi/gblpyapi-0.1.1.tar.gz/gblpyapi-0.1.1/glennbotlist/GBL.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import aiohttp
+
+from . import errors
+
+class GBL:
+    """
+    API client for Glenn Bot List in Python
+    """
+    def __init__(self, token: str, *, base: str = "https://glennbotlist.xyz/api/v2/bot/"):
+        self.token = token
+        self.base = base
+
+    async def post_server_count(self, botid: int, servers: int):
+        """
+
+        :param botid: Bot ID in which you are posting the stats for
+        :param servers: The bots server coint
+        """
+        if not self.token:
+            raise errors.InvalidKey("A token was not supplied")
+
+        async with aiohttp.ClientSession().post(url=self.base + f"{botid}/stats", data={"serverCount": servers}, headers={"authorization": self.token}) as r:
+            resp = await r.json()
