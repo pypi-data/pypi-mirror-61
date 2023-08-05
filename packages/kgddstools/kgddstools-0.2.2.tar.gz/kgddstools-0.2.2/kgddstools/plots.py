16b8fc2cd5187f407c5ff25dec99daa17e0ee710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package kgddstools/0.2.2/kgddstools-0.2.2.tar.gz

---
+++ packages/kgddstools/kgddstools-0.2.2.tar.gz/kgddstools-0.2.2/kgddstools/plots.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import dabl
+
+def test_plot(some_df):
+
+    sns.set()
+
+    print("Hello world")
+
+    df_types = dabl.detect_types(some_df)
+
+    print(type(df_types))
+
+    print(df_types)
+
+    for index, col in enumerate(some_df.columns):
+
+        fig, axs = plt.subplots(len(some_df.columns), 1, figsize = (15, len(some_df)*5))
+
+        axs.plot(plot_cat(column = some_df[col], target = 0))
+
+        plt.show()
+
+def plot_cat(column, target):
+    fig, axs = plt.subplots(1, 3, figsize = (15, 5))
+
+    axs[0, 1].plot(column)
+    axs[0, 2].plot(column)
+    axs[0, 3].plot(column)
+
+    return fig
---
+++ packages/kgddstools/kgddstools-0.2.2.tar.gz/kgddstools-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+     name = 'kgddstools',
+     version = '0.2.2',
+     #scripts = ['kgddstools'] ,
+
+     author = "Kelvin Ducray",
+     author_email = "kelvin.ducray@gmail.com",
+
+     description = "Data science functions for convience",
+     long_description = long_description,
+     long_description_content_type = "text/markdown",
+
+     url = "https://github.com/kelvinducray/kgddstools",
+     packages = find_packages(),#exclude=['docs', 'tests*']),
+
+     classifiers = [
+         "Programming Language :: Python :: 3",
+         "License :: Public Domain",
+         "Operating System :: OS Independent",
+     ],
+ )
