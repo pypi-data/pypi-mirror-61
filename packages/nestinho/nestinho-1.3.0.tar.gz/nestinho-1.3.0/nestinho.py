acdd245b039bd8e750c4ae25b0192732ed334d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package nestinho/1.3.0/nestinho-1.3.0.tar.gz

---
+++ packages/nestinho/nestinho-1.3.0.tar.gz/nestinho-1.3.0/nestinho.py
@@ -0,0 +1,9 @@
+def print_lol(the_list, indent, level=0): #We turned second argument into a optional argument.
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+                print(each_item)
