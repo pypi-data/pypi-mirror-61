42f1d5ae6cd4cdc319f871756d8a9acb3c7ded68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:41 +0000

    Add package AshutoshGupta-101703118-Outlier-removal/0.1.3/AshutoshGupta_101703118_Outlier_removal-0.1.3.tar.gz

---
+++ packages/AshutoshGupta-101703118-Outlier-removal/AshutoshGupta_101703118_Outlier_removal-0.1.3.tar.gz/AshutoshGupta_101703118_Outlier_removal-0.1.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 
+
+@author: ashu
+"""
+#Made by Ashutosh Gupta 101703118
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AshutoshGupta_101703118_Outlier_removal",
+    version="0.1.3",
+    author="Ashutosh Gupta",
+    author_email="agupta12_be17@thapar.edu",
+    description="A small example package for removal of outliers in the given dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = ['command-line', 'Outliers', 'outlier-removal','row-removal'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
