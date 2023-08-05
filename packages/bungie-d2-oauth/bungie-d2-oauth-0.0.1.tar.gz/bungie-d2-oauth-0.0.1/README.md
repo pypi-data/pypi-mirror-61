68b82302340da18c06f7a6ffafe7af083124a57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:14 +0000

    Add package bungie-d2-oauth/0.0.1/bungie-d2-oauth-0.0.1.tar.gz

---
+++ packages/bungie-d2-oauth/bungie-d2-oauth-0.0.1.tar.gz/bungie-d2-oauth-0.0.1/README.md
@@ -0,0 +1,9 @@
+# bungie-d2-oauth
+
+This package will get auth tokens from Bungie for your application.
+
+# Prerequisites
+
+ - Python 3.5+
+ - Flask
+ - requests 2.20.0+
---
+++ packages/bungie-d2-oauth/bungie-d2-oauth-0.0.1.tar.gz/bungie-d2-oauth-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bungie-d2-oauth",
+    version="0.0.1",
+    author="Pavel Movsesian",
+    author_email="movsesyanpv1995@live.com",
+    description="Get your Bungie auth tokens",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/movsesyanpv/bungie-d2-oauth",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'Flask==1.0.2',
+        'setuptools',
+        'requests>=2.20.0'
+      ],
+    python_requires='>=3.6',
+)
