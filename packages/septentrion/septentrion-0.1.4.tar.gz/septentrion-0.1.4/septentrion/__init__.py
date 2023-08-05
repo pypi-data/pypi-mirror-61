9eb57f125ac0ee57291b1f8b0ed22ad9b6152092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package septentrion/0.1.4/septentrion-0.1.4.tar.gz

---
+++ packages/septentrion/septentrion-0.1.4.tar.gz/septentrion-0.1.4/septentrion/__init__.py
@@ -0,0 +1,8 @@
+from septentrion import metadata as _metadata_module
+
+_metadata = _metadata_module.extract_metadata("septentrion")
+__author__ = _metadata["author"]
+__author_email__ = _metadata["email"]
+__license__ = _metadata["license"]
+__url__ = _metadata["url"]
+__version__ = _metadata["version"]
