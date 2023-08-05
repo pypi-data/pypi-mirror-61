98e5aed9f32d26cb4e93c982f45cfbb1471c9147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package Flask-Discord/0.1.7/Flask-Discord-0.1.7.tar.gz

---
+++ packages/Flask-Discord/Flask-Discord-0.1.7.tar.gz/Flask-Discord-0.1.7/flask_discord/configs.py
@@ -0,0 +1,21 @@
+API_BASE_URL = "https://discordapp.com/api"
+
+AUTHORIZATION_BASE_URL = API_BASE_URL + "/oauth2/authorize"
+TOKEN_URL = API_BASE_URL + "/oauth2/token"
+
+
+ALL_SCOPES = [
+    "bot", "connections", "email", "identify", "guilds", "guilds.join",
+    "gdm.join", "messages.read", "rpc", "rpc.api", "rpc.notifications.read", "webhook.incoming",
+]
+
+DEFAULT_SCOPES = [
+    "identify", "email", "guilds", "guilds.join"
+]
+
+
+IMAGE_BASE_URL = "https://cdn.discordapp.com/"
+IMAGE_FORMAT = "png"
+ANIMATED_IMAGE_FORMAT = "gif"
+USER_AVATAR_BASE_URL = IMAGE_BASE_URL + "avatars/{user_id}/{avatar_hash}.{format}"
+GUILD_ICON_BASE_URL = IMAGE_BASE_URL + "icons/{guild_id}/{icon_hash}.png"
---
+++ packages/Flask-Discord/Flask-Discord-0.1.7.tar.gz/Flask-Discord-0.1.7/flask_discord/models/user.py
@@ -0,0 +1,77 @@
+from .base import DiscordModelsBase
+
+from .. import configs
+
+
+class User(DiscordModelsBase):
+    """Class representing Discord User.
+
+    Attributes
+    ----------
+    id : int
+        The discord ID of the user.
+    username : str
+        The discord username of the user.
+    discriminator : str
+        4 length string representing discord tag of the user.
+    avatar_hash : str
+        Hash of users avatar.
+    bot : bool
+        A boolean representing whether the user belongs to an OAuth2 application.
+    mfa_enabled : bool
+        A boolean representing whether the user has two factor enabled on their account.
+    locale : str
+        The user's chosen language option.
+    verified : bool
+        A boolean representing whether the email on this account has been verified.
+    email : str
+        User's email ID.
+    flags : int
+        An integer representing the
+        `user flags <https://discordapp.com/developers/docs/resources/user#user-object-user-flags>`_.
+    premium_type : int
+        An integer representing the
+        `type of nitro subscription <https://discordapp.com/developers/docs/resources/user#user-object-premium-types>`_.
+
+    """
+
+    def __init__(self, payload):
+        self._payload = payload
+        self.id = int(self._payload["id"])
+        self.username = self._payload["username"]
+        self.discriminator = self._payload["discriminator"]
+        self.avatar_hash = self._payload.get("avatar", self.discriminator)
+        self.bot = self._payload.get("bot", False)
+        self.mfa_enabled = self._payload.get("mfa_enabled")
+        self.locale = self._payload.get("locale")
+        self.verified = self._payload.get("verified")
+        self.email = self._payload.get("email")
+        self.flags = self._payload.get("flags")
+        self.premium_type = self._payload.get("premium_type")
+
+    def __str__(self):
+        return f"{self.name}#{self.discriminator}"
+
+    @property
+    def name(self):
+        """An alias to the username attribute."""
+        return self.username
+
+    @property
+    def avatar_url(self):
+        """A property returning direct URL to user's avatar."""
+        image_format = configs.ANIMATED_IMAGE_FORMAT if self.is_avatar_animated else configs.IMAGE_FORMAT
+        return configs.USER_AVATAR_BASE_URL.format(user_id=self.id, avatar_hash=self.avatar_hash, format=image_format)
+
+    @property
+    def is_avatar_animated(self):
+        """A boolean representing if avatar of user is animated. Meaning user has GIF avatar."""
+        return self.avatar_hash.startswith("a_")
+
+
+class Bot(User):
+    """Class representing the client user itself.
+
+    """
+
+    pass
