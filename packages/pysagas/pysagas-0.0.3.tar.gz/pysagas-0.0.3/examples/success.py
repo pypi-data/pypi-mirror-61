e07aefcc0753d05e0c5ed418ea8e017e88eae09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:55 +0000

    Add package pysagas/0.0.3/pysagas-0.0.3.tar.gz

---
+++ packages/pysagas/pysagas-0.0.3.tar.gz/pysagas-0.0.3/examples/success.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from sagas import SagaBuilder
+
+counter = [10, 20]
+amount = 30
+
+
+def add(amount, index):
+    """Increments array index by amount
+
+    Args:
+        amount (int): increment value
+        index (int): index of value to increment
+    """
+    counter[index] += amount
+
+
+def decrement(amount, index):
+    """Decrements array index by amount
+
+    Args:
+        amount (int): decrement value
+        index (int): index of value to decrement
+    """
+    counter[index] -= amount
+    return counter
+
+
+saga_builder = SagaBuilder.create()
+
+saga = (
+    saga_builder.add_step(lambda: add(amount, 0), lambda: decrement(amount, 0))
+    .add_step(lambda: add(amount, 1), lambda: decrement(amount, 1))
+    .build()
+)
+
+if __name__ == "__main__":
+    asyncio.run(saga.run(exceptions=(OSError)))
+    print(counter)  # [40, 50]
