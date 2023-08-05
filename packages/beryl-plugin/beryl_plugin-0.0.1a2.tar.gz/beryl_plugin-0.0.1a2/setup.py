ce9ddefee3ae4cdd844060c19e9641c475fedc4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package beryl-plugin/0.0.1a2/beryl_plugin-0.0.1a2.tar.gz

---
+++ packages/beryl-plugin/beryl_plugin-0.0.1a2.tar.gz/beryl_plugin-0.0.1a2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md") as fh:
+	long_description = fh.read()
+
+setup(name='beryl_plugin',
+	version='0.0.1-alpha.2',
+	description='A helper library for creating plugins for the Beryl Timing System.',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='http://github.com/zPaw/beryl_plugin',
+	author='Brenek Harrison',
+	author_email='brenekharrison@gmail.com',
+	license='MIT',
+	packages=['beryl_plugin'],
+	zip_safe=False,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires=">=3.6")
+
+# Commands to deploy
+# Build step: python setup.py sdist bdist_wheel
+# Release step: python -m twine upload dist/*
