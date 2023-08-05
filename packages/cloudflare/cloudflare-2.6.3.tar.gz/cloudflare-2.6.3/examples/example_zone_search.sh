8377583a607f063b10db462b4fb4fbb4f43d281a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:30 +0000

    Add package cloudflare/2.6.3/cloudflare-2.6.3.tar.gz

---
+++ packages/cloudflare/cloudflare-2.6.3.tar.gz/cloudflare-2.6.3/examples/example_zone_search.sh
@@ -0,0 +1,25 @@
+:
+
+ZONE=${1-example.com}
+EXTRA=${2}
+
+while read search_type
+do
+	echo TRY: "name=${search_type}:${ZONE}"
+	cli4 per_page=50 name="${search_type}:${ZONE}" ${EXTRA} /zones/ | jq -r '.[]|.id,.name' | paste - -
+done <<!!
+equal
+not_equal
+greater_than
+less_than
+starts_with
+ends_with
+contains
+starts_with_case_sensitive
+ends_with_case_sensitive
+contains_case_sensitive
+list_contains
+!!
+
+exit 0
+
