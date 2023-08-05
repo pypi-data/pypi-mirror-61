92b99d9bb786d4697809cb1c91570c20e8e53e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:01 +0000

    Add package hello-word-joao-neto/0.1/hello_word_joao_neto-0.1.tar.gz

---
+++ packages/hello-word-joao-neto/hello_word_joao_neto-0.1.tar.gz/hello_word_joao_neto-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hello_word_joao_neto',
+    packages=['hello_word_joao_neto'],
+    description='Hello world joao neto',
+    version='0.1',
+    url='https://gitlab.com/joaoNeto/alexandria',
+    author='Linode',
+    author_email='joao.neto@madeiramadeira.com.br'
+)
