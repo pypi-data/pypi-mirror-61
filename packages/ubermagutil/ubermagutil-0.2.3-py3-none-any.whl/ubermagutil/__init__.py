836b6a38105f055171a4f4a43558c8cc45f70dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:24 +0000

    Add package ubermagutil/0.2.3/ubermagutil-0.2.3-py3-none-any.whl

---
+++ packages/ubermagutil/ubermagutil-0.2.3-py3-none-any.whl/ubermagutil/__init__.py
@@ -0,0 +1,9 @@
+import pytest
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution(__name__).version
+__dependencies__ = pkg_resources.require(__name__)
+
+
+def test():
+    return pytest.main(["-v", "--pyargs", "ubermagutil"])  # pragma: no cover
