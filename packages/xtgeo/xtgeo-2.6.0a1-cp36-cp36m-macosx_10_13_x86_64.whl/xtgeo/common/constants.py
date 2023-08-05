867b4201bf337d1dca39cacc724f96c9aab520cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:48 +0000

    Add package xtgeo/2.6.0a1/xtgeo-2.6.0a1-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.6.0a1-cp36-cp36m-macosx_10_13_x86_64.whl/xtgeo/common/constants.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Module for basic XTGeo constants"""
+
+import xtgeo.cxtgeo._cxtgeo as _cxtgeo
+
+UNDEF = -999
+UNDEF_LIMIT = -999
+UNDEF_INT = -999
+UNDEF_INT_LIMIT = -999
+VERYLARGEPOSITIVE = -999
+VERYLARGENEGATIVE = -999
+
+
+try:
+    UNDEF = _cxtgeo.UNDEF
+    UNDEF_LIMIT = _cxtgeo.UNDEF_LIMIT
+    UNDEF_INT = _cxtgeo.UNDEF_INT
+    UNDEF_INT_LIMIT = _cxtgeo.UNDEF_INT_LIMIT
+    VERYLARGENEGATIVE = _cxtgeo.VERYLARGENEGATIVE
+    VERYLARGEPOSITIVE = _cxtgeo.VERYLARGEPOSITIVE
+except AttributeError:
+    print("Dummy settings")
