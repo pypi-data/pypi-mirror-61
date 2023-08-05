451f7dd579eb4ef43ac40f84bd655a0dd57c5033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:53 +0000

    Add package aiohttp-basicauth/0.2.0/aiohttp_basicauth-0.2.0.tar.gz

---
+++ packages/aiohttp-basicauth/aiohttp_basicauth-0.2.0.tar.gz/aiohttp_basicauth-0.2.0/README.md
@@ -0,0 +1,75 @@
+## aiohttp-basicauth
+
+[![Build Status](https://travis-ci.org/romis2012/aiohttp-basicauth.svg?branch=master)](https://travis-ci.org/romis2012/aiohttp-basicauth)
+[![Coverage Status](https://coveralls.io/repos/github/romis2012/aiohttp-basicauth/badge.svg?branch=master)](https://coveralls.io/github/romis2012/aiohttp-basicauth?branch=master)
+[![PyPI version](https://badge.fury.io/py/aiohttp-basicauth.svg)](https://badge.fury.io/py/aiohttp-basicauth)
+
+HTTP basic authentication middleware for aiohttp 3.0+. 
+Inspired by [Flask-BasicAuth](https://github.com/jpvanhal/flask-basicauth).
+
+## Requirements
+- Python >= 3.5.3
+- aiohttp >= 3.0
+
+## Installation
+```
+pip install aiohttp_basicauth
+```
+
+## Simple usage
+
+```python
+from aiohttp import web
+from aiohttp_basicauth import BasicAuthMiddleware
+
+
+auth = BasicAuthMiddleware(username='user', password='password')
+app = web.Application(middlewares=[auth])
+
+web.run_app(app, host='127.0.0.1', port=80)
+```
+
+## Protect specific view(s)
+```python
+from aiohttp import web
+from aiohttp_basicauth import BasicAuthMiddleware
+
+auth = BasicAuthMiddleware(username='user', password='password', force=False)
+
+
+async def public_view(request):
+    return web.Response(text='Public view')
+
+
+@auth.required
+async def secret_view(request):
+    return web.Response(text='Secret view')
+
+
+app = web.Application(middlewares=[auth])
+
+app.router.add_route('GET', '/public', public_view)
+app.router.add_route('GET', '/secret', secret_view)
+
+web.run_app(app, host='127.0.0.1', port=80)
+```
+
+## Advanced usage
+
+You can override ```check_credentials``` method to implement specific user verification logic:
+
+```python
+from aiohttp import web
+from aiohttp_basicauth import BasicAuthMiddleware
+
+
+class CustomBasicAuth(BasicAuthMiddleware):
+    async def check_credentials(self, username, password, request):
+        return username == 'user' and password == 'password'
+
+
+auth = CustomBasicAuth()
+app = web.Application(middlewares=[auth])
+
+web.run_app(app, host='127.0.0.1', port=80)
+```
---
+++ packages/aiohttp-basicauth/aiohttp_basicauth-0.2.0.tar.gz/aiohttp_basicauth-0.2.0/aiohttp_basicauth/__init__.py
@@ -0,0 +1,70 @@
+import functools
+from aiohttp import BasicAuth, web, hdrs
+from aiohttp.web import middleware
+
+__version__ = '0.2.0'
+
+
+@middleware
+class BasicAuthMiddleware(object):
+    def __init__(self, username=None, password=None, force=True, realm=''):
+        self.username = username
+        self.password = password
+        self.force = force
+        self.realm = realm
+
+    # noinspection PyMethodMayBeStatic
+    def parse_auth_header(self, request):
+        auth_header = request.headers.get(hdrs.AUTHORIZATION)
+        if not auth_header:
+            return None
+        try:
+            auth = BasicAuth.decode(auth_header=auth_header)
+        except ValueError:  # pragma: no cover
+            auth = None
+        return auth
+
+    async def authenticate(self, request):
+        auth = self.parse_auth_header(request)
+        return (auth is not None
+                and await self.check_credentials(auth.login, auth.password,
+                                                 request))
+
+    async def check_credentials(self, username, password, request):
+        if username is None:
+            raise ValueError('username is None')  # pragma: no cover
+
+        if password is None:
+            raise ValueError('password is None')  # pragma: no cover
+
+        return username == self.username and password == self.password
+
+    def challenge(self):
+        return web.Response(
+            body=b'', status=401, reason='UNAUTHORIZED',
+            headers={
+                hdrs.WWW_AUTHENTICATE: 'Basic realm="%s"' % self.realm,
+                hdrs.CONTENT_TYPE: 'text/html; charset=utf-8',
+                hdrs.CONNECTION: 'keep-alive'
+            }
+        )
+
+    def required(self, handler):
+        @functools.wraps(handler)
+        async def wrapper(*args):
+            request = args[-1]
+            if await self.authenticate(request):
+                return await handler(*args)
+            else:
+                return self.challenge()
+
+        return wrapper
+
+    async def __call__(self, request, handler):
+        if not self.force:
+            return await handler(request)
+        else:
+            if await self.authenticate(request):
+                return await handler(request)
+            else:
+                return self.challenge()
