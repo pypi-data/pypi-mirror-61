abd29fd340a871bfbabb79c4e8dcf3248c42cdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:57 +0000

    Add package autots/0.1.1/AutoTS-0.1.1.tar.gz

---
+++ packages/autots/AutoTS-0.1.1.tar.gz/AutoTS-0.1.1/autots/__init__.py
@@ -0,0 +1,22 @@
+"""
+Automated Time Series Model Selection for Python
+
+https://github.com/winedarksea/AutoTS
+"""
+from autots.datasets import load_toy_hourly
+from autots.datasets import load_toy_daily
+from autots.datasets import load_toy_monthly
+from autots.datasets import load_toy_yearly
+
+from autots.evaluator.auto_ts import AutoTS
+
+__version__ = '0.1.1'
+
+
+__all__ = ['load_toy_daily','load_toy_monthly', 'load_toy_yearly', 'load_toy_hourly',
+           'AutoTS']
+
+# import logging
+# logger = logging.getLogger(__name__)
+# logger.addHandler(logging.StreamHandler())
+# logger.setLevel(logging.INFO)
