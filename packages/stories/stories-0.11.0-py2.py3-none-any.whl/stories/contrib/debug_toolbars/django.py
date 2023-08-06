4e6585f542d2f25a51a6c6262956faf7cf88968f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:45 +0000

    Add package stories/0.11.0/stories-0.11.0-py2.py3-none-any.whl

---
+++ packages/stories/stories-0.11.0-py2.py3-none-any.whl/stories/contrib/debug_toolbars/django.py
@@ -0,0 +1,14 @@
+# type: ignore
+"""
+stories.contrib.debug_toolbars.django
+-------------------------------------
+
+This module contains integration with Django Debug Toolbar.
+
+:copyright: (c) 2018-2020 dry-python team.
+:license: BSD, see LICENSE for more details.
+"""
+from _stories.contrib.debug_toolbars.django.panels import StoriesPanel
+
+
+__all__ = ["StoriesPanel"]
---
+++ packages/stories/stories-0.11.0-py2.py3-none-any.whl/stories/contrib/sentry/__init__.py
@@ -0,0 +1,13 @@
+"""
+stories.contrib.sentry
+----------------------
+
+This module contains integration with Sentry.
+
+:copyright: (c) 2018-2020 dry-python team.
+:license: BSD, see LICENSE for more details.
+"""
+import _stories.contrib.sentry  # noqa
+
+
+__all__ = []
