73df6b6c0f5cdcbf00fc565338620dc2bfcb5a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:52 +0000

    Add package django3-collectionfield/1.0.0/django3-collectionfield-1.0.0.tar.gz

---
+++ packages/django3-collectionfield/django3-collectionfield-1.0.0.tar.gz/django3-collectionfield-1.0.0/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+from distutils.core import Command
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+
+class Test(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            INSTALLED_APPS=('collectionfield.tests',),
+            SECRET_KEY='AAA',
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                }
+            },
+            MIDDLEWARE_CLASSES = ()
+        )
+        from django.core.management import call_command
+        import django
+        django.setup()
+        call_command('test', 'collectionfield')
+
+
+setup(
+    name='django3-collectionfield',
+    version='1.0.0',
+    description='Custom Django model field to store multiple values.',
+    long_description=long_description,
+    url='https://github.com/metalbass/django3-collectionfield',
+    author='Pawel Scierski',
+    author_email='escer@protonmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django :: 3.0',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords='django models fields collections',
+    packages=find_packages(),
+    cmdclass={'test': Test},
+)
