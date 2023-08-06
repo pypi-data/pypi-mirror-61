8ad92efcab50b0ca3dd39a65f41e2009bfe9112f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:10 +0000

    Add package galileo-cli/0.0.2/galileo_cli-0.0.2.tar.gz

---
+++ packages/galileo-cli/galileo_cli-0.0.2.tar.gz/galileo_cli-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import platform
+from setuptools import find_packages, setup
+
+install_requires = (
+    [
+        "requests>=2.21.0",
+        "python-socketio",
+        "termcolor",
+        "pyfiglet",
+        "click",
+        "click-shell",
+        "pandas",
+        "click-shell[readline]",
+        "gnureadline",
+        "galileo-sdk==0.0.13",
+    ],
+)
+
+if platform.system() == "Windows":
+    install_requires.extend(["click-shell[windows]", "pyreadline"])
+
+setup(
+    name="galileo_cli",
+    version="0.0.2",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["galileo-cli = galileo_cli.cli:main",]},
+    python_requires=">=3.6.7",
+    install_requires=install_requires,
+    extras_require={"docs": ["sphinx>=2.2.0", "sphinx-material"]},
+    setup_requires=["pytest-runner", "black", "isort"],
+    tests_require=["pytest", "mock"],
+)
