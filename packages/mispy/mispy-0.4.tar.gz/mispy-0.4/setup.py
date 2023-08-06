37ba2bc4448ffbbe904f186e8e30ff0ba257e8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:51 +0000

    Add package mispy/0.4/mispy-0.4.tar.gz

---
+++ packages/mispy/mispy-0.4.tar.gz/mispy-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mispy',
+    version='0.4',
+    description='A pythonic MISP module',
+    url='https://github.com/nbareil/mispy',
+    download_url = 'https://github.com/nbareil/mispy/archive/0.3.tar.gz',
+    author='Nicolas Bareil',
+    author_email='nico@chdir.org',
+    keywords='misp',
+    install_requires=['requests', 'lxml'],
+    license='Apachev2',
+    packages=['mispy'],
+    zip_safe=False
+)
