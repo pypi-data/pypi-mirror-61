c08f860269cd7d3111adb2f53e47c98ff56aa654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:56 +0000

    Add package wpkit/1.0.9.9/wpkit-1.0.9.9.tar.gz

---
+++ packages/wpkit/wpkit-1.0.9.9.tar.gz/wpkit-1.0.9.9/wpkit/install.py
@@ -0,0 +1,16 @@
+
+dependencies=[
+    'flask',
+    'Flask-Cors',
+    'fire',
+    'dulwich'
+]
+optional_dependencies=[
+    'gitpython'
+]
+def install_requirements():
+    import os
+    for dep in dependencies:
+        os.system('pip3 install %s'%(dep))
+if __name__ == '__main__':
+    install_requirements()
