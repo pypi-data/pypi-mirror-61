047da366d50c3e7781d7ccf4a60daa4f057a7b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:46 +0000

    Add package RobotPy-Limelight/2020.1.2/RobotPy-Limelight-2020.1.2.tar.gz

---
+++ packages/RobotPy-Limelight/RobotPy-Limelight-2020.1.2.tar.gz/RobotPy-Limelight-2020.1.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import os
+
+
+setup(
+    name="RobotPy-Limelight",
+    version="2020.1.2",
+    author="Josh Bacon",
+    author_email="bacon.josh09@gmail.com",
+    packages=["limelight"],
+    url="https://pypi.org/project/robotpy-limelight",
+    license="LICENSE",
+    description="RobotPy Limelight utilities",
+    long_description="RobotPy Limelight utilities to make using Limelight with RobotPy easier.",
+    install_requires=["pynetworktables >= 2019"],
+)
