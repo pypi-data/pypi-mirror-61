110ddd2b5f4b749f2d3f5a87f81428174f157ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:19 +0000

    Add package geo-cache-client/0.1.1/geo-cache-client-0.1.1.tar.gz

---
+++ packages/geo-cache-client/geo-cache-client-0.1.1.tar.gz/geo-cache-client-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geo-cache-client", # Replace package name
+    version="0.1.1",
+    author="Rafael",
+    author_email="timbo.rafa@gmail.com",
+    description="A geo distributed cache",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timbo-rafa/geo-cache",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
