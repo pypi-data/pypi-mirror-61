83c9b5795b481cb03ead8faf44d856091ebbf319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:50 +0000

    Add package pandas-tfrecords/0.1.1/pandas-tfrecords-0.1.1.tar.gz

---
+++ packages/pandas-tfrecords/pandas-tfrecords-0.1.1.tar.gz/pandas-tfrecords-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='pandas-tfrecords',
+    version='0.1.1',
+    description='Converter pandas to tfrecords & tfrecords to pandas',
+    long_description=open('README.rst').read(),
+    url='https://github.com/schipiga/pandas-tfrecords/',
+    author='Sergei Chipiga',
+    author_email='chipiga86@gmail.com',
+    packages=['pandas_tfrecords'],
+    install_requires=[
+        's3fs==0.4.0',
+        'tensorflow==2.1.0',
+        'pandas==1.0.1',
+        'numpy==1.18.1',
+    ],
+)
