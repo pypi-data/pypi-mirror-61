a8dee59b99658c590a91ad42b6ed7aa623e5b2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:34 +0000

    Add package nahaath/1.2/nahaath-1.2.tar.gz

---
+++ packages/nahaath/nahaath-1.2.tar.gz/nahaath-1.2/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(name='nahaath',
+      version='1.2',
+      py_modules=['nahaath'],
+      )
