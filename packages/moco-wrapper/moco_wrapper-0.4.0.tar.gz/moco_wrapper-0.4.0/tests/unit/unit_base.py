82d5f99f2c374896cf182b151411ac85372506fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:27 +0000

    Add package moco-wrapper/0.4.0/moco_wrapper-0.4.0.tar.gz

---
+++ packages/moco-wrapper/moco_wrapper-0.4.0.tar.gz/moco_wrapper-0.4.0/tests/unit/unit_base.py
@@ -0,0 +1,26 @@
+import pytest
+
+from moco_wrapper import moco
+from moco_wrapper.util.requestor import RawRequestor
+from moco_wrapper.util.objector import RawObjector
+
+class UnitTest(object):
+    def setup(self):
+        self.setup_moco()
+
+    def setup_moco(self):
+        """create a moco instance where no requests will be fired against the api"""
+        self._moco = moco.Moco(
+            api_key="[HERE IS THE API KEY]",
+            domain="[DOMAIN]",
+            http=RawRequestor(),
+            objector=RawObjector()
+        )
+
+    @property
+    def moco(self):
+        return self._moco
+
+    @property
+    def requestor(self):
+        return self._moco._requestor
