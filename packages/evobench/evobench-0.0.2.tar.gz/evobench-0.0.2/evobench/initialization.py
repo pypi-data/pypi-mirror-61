d0d1810c784016c089dc4b42436b59d4e3ecf61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:07 +0000

    Add package evobench/0.0.2/evobench-0.0.2.tar.gz

---
+++ packages/evobench/evobench-0.0.2.tar.gz/evobench-0.0.2/evobench/initialization.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+
+from lazy import lazy
+
+from evobench.model import Population
+
+
+class Initialization(ABC):
+
+    """
+    Base class for population initialization.
+    """
+
+    def __init__(self, population_size: int):
+        super(Initialization, self).__init__()
+
+        self.POPULATION_SIZE = int(population_size)
+
+    @abstractmethod
+    def initialize_population(self, genome_size: int) -> Population:
+        pass
+
+    @lazy
+    def as_dict(self) -> Dict:
+        """
+        Initialization description in dictionary format.
+        You can dump it as `json` file to log your research.
+        """
+
+        as_dict = {}
+
+        as_dict['name'] = self.__class__.__name__
+        as_dict['population_size'] = self.POPULATION_SIZE
+
+        return as_dict
