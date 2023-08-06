0389a79463d091ae8122641da3b98af45f800b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:53 +0000

    Add package hardware-communicator/0.1.1582151219/hardware_communicator-0.1.1582151219-py3-none-any.whl

---
+++ packages/hardware-communicator/hardware_communicator-0.1.1582151219-py3-none-any.whl/hardware_communicator/controller/usb.py
@@ -0,0 +1,28 @@
+import logging
+
+from json_dict import JsonDict
+
+from hardware_communicator.usb_commmunicator import SerialCommunicator
+
+
+class SerialDevice(SerialCommunicator):
+    AVAILABLE_QUERIES = {}
+
+    def __init__(self, port=None, auto_port=True,interpreter=None,**kwargs):
+        SerialCommunicator.__init__(self, port=port, auto_port=auto_port,interpreter=interpreter,**kwargs)
+        if not hasattr(self, "logger"):
+            self.logger = logging.getLogger(self.__class__.__name__)
+        if not hasattr(self, "config"):
+            self.config = JsonDict()
+
+    def set_device_property(self, name, value):
+        print(self.config.put("device", "properties", name, value=value))
+
+    def get_device_property(self, name):
+        return self.config.get("device", "properties", name, autosave=False)
+
+    def set_device_status(self, name, value):
+        self.config.put("device", "status", name, value=value)
+
+    def get_device_status(self, name):
+        return self.config.get("device", "status", name, autosave=False)
