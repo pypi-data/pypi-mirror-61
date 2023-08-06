9f86e0c390eafd3210cbb062707eb167b8fe5e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:38 +0000

    Add package JavPy/0.3.5/JavPy-0.3.5-py2.py3-none-any.whl

---
+++ packages/JavPy/JavPy-0.3.5-py2.py3-none-any.whl/JavPy/embed/decode.py
@@ -0,0 +1,13 @@
+from JavPy.embed.fembed import fembed
+from JavPy.embed.avgle import avgle
+from JavPy.embed.smartshare_tv import smartshare_tv
+
+
+embeds = [fembed, avgle, smartshare_tv]
+
+
+def decode(url):
+    for embed in embeds:
+        if embed.pattern(url):
+            return embed.decode(url)
+    return url
