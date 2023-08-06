bf4b6c4da0559e96bed7738ee7d720b739126e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:17 +0000

    Add package drf-toolkit/0.0.6/drf_toolkit-0.0.6.tar.gz

---
+++ packages/drf-toolkit/drf_toolkit-0.0.6.tar.gz/drf_toolkit-0.0.6/drf_toolkit/json_api_views.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.renderers import JSONRenderer
+from rest_framework.views import APIView
+
+from drf_toolkit.json_parser import JsonParser
+
+
+class JsonApiView(APIView):
+    parser_classes = (JsonParser, )
+    renderer_classes = (JSONRenderer,)
+
+
+class AnonymousJsonApiView(JsonApiView):
+    authentication_classes = ()
+    permission_classes = (AllowAny,)
+
+
+class AdminJsonApiView(JsonApiView):
+    permission_classes = (IsAdminUser,)
