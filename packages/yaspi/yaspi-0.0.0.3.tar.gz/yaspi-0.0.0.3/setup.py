9068238f0042f6e5586fa008a95fbc0e8d305fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package yaspi/0.0.0.3/yaspi-0.0.0.3.tar.gz

---
+++ packages/yaspi/yaspi-0.0.0.3.tar.gz/yaspi-0.0.0.3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from pathlib import Path
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+data_files = [
+    ("misc", [str(x) for x in Path("misc").glob("*.py")]),
+    ("templates", [str(x) for x in Path("templates").glob("**/*.sh")]),
+]
+
+
+setuptools.setup(
+    name="yaspi",
+    version="0.0.0.3",
+    entry_points={
+        "console_scripts": [
+            "yaspi=yaspi.yaspi:main",
+        ],
+    },
+    author="Samuel Albanie",
+    description="Yet Another Slurm Python Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albanie/yaspi",
+    packages=["yaspi"],
+    data_files=data_files,
+    python_requires=">=3.7",
+    install_requires=[
+        "watchlogs",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux',
+    ],
+)
