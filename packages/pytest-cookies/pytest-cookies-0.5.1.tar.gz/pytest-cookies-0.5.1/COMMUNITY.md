f6ee352514c1cb6cbe2f4d4382fed78e1d874406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:45 +0000

    Add package pytest-cookies/0.5.1/pytest-cookies-0.5.1.tar.gz

---
+++ packages/pytest-cookies/pytest-cookies-0.5.1.tar.gz/pytest-cookies-0.5.1/COMMUNITY.md
@@ -0,0 +1,25 @@
+# Maintainers
+
+- [@hackebrot]
+- [@michaeljoseph]
+
+[@hackebrot]: https://github.com/hackebrot
+[@michaeljoseph]: https://github.com/michaeljoseph
+
+# Contributors
+
+- [@jakirkham]
+- [@jamesmyatt]
+- [@jsmedmar]
+- [@jyrno42]
+- [@nedbat]
+- [@obestwalter]
+- [@pydanny]
+
+[@jakirkham]: https://github.com/jakirkham
+[@jamesmyatt]: https://github.com/jamesmyatt
+[@jsmedmar]: https://github.com/jsmedmar
+[@jyrno42]: https://github.com/Jyrno42
+[@nedbat]: https://github.com/nedbat
+[@obestwalter]: https://github.com/obestwalter
+[@pydanny]: https://github.com/pydanny
---
+++ packages/pytest-cookies/pytest-cookies-0.5.1.tar.gz/pytest-cookies-0.5.1/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+import setuptools
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+setuptools.setup(
+    name="pytest-cookies",
+    version="0.5.1",
+    author="Raphael Pierzina",
+    author_email="raphael@hackebrot.de",
+    maintainer="Raphael Pierzina",
+    maintainer_email="raphael@hackebrot.de",
+    license="MIT",
+    url="https://github.com/hackebrot/pytest-cookies",
+    project_urls={
+        "Repository": "https://github.com/hackebrot/pytest-cookies",
+        "Issues": "https://github.com/hackebrot/pytest-cookies/issues",
+    },
+    description="The pytest plugin for your Cookiecutter templates. 🍪",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=[
+        "arrow<0.14.0", "cookiecutter>=1.4.0,<1.8.0", "pytest>=3.3.0,<6.0.0"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+        "Framework :: Pytest",
+    ],
+    entry_points={"pytest11": ["cookies = pytest_cookies.plugin"]},
+    keywords=["cookiecutter", "pytest"],
+)
