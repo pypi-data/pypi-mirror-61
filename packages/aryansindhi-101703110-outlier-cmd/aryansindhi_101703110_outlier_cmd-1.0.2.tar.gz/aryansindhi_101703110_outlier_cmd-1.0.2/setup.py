56df051610132c041b563553e0c7530a95250877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:16 +0000

    Add package aryansindhi-101703110-outlier-cmd/1.0.2/aryansindhi_101703110_outlier_cmd-1.0.2.tar.gz

---
+++ packages/aryansindhi-101703110-outlier-cmd/aryansindhi_101703110_outlier_cmd-1.0.2.tar.gz/aryansindhi_101703110_outlier_cmd-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aryansindhi_101703110_outlier_cmd", # Replace with your own username
+    version="1.0.2",
+    author="Aryan Sindhi",
+    author_email="asindhi_be17@thapar.edu",
+    description="A small package that removes outliers from a pandas dataframe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aryansindhi18/Aryan_Sindhi_101703110_outlier_removal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={"console_scripts":["outRem=aryansindhi_outlier.outlier_cmd:main"]},
+)
