106f31cbb9f804e0d1942ff96b0163ae43b71ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:06 +0000

    Add package chariot-base/0.13.0/chariot_base-0.13.0.tar.gz

---
+++ packages/chariot-base/chariot_base-0.13.0.tar.gz/chariot_base-0.13.0/chariot_base/datasource/local.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from influxdb import InfluxDBClient
+
+
+def open_datasource(options):
+    return LocalDataSource(
+        options['host'], options['port'], options['username'], options['password'], options['database']
+    )
+
+
+class LocalDataSource(object):
+
+    def __init__(self, hostname, port, username, password, db_name, duration='4w'):
+        self.db = InfluxDBClient(hostname, port, username, password, db_name)
+        self.db.create_database(db_name)
+        self.db.create_retention_policy('awesome_policy', duration, 3, default=True)
+
+    def publish(self, point):
+        json_body = [
+            {
+                'measurement': point.table,
+                'tags': {
+                    'topic': point.topic,
+                    'sensor_id': point.sensor_id
+                },
+                'time': point.timestamp,
+                'fields': point.message
+            }
+        ]
+        return self.db.write_points(json_body, protocol='json', retention_policy='awesome_policy')
+
+    def publish_dict(self, point_dict):
+        json_body = [
+            {
+                'measurement': point_dict['table'],
+                'tags': point_dict['tags'],
+                'time': point_dict['timestamp'],
+                'fields': point_dict['message']
+            }
+        ]
+        return self.db.write_points(json_body, protocol='json', retention_policy='awesome_policy')
+
+    def query(self, q, db_name):
+        return self.db.query(q, database=db_name)
