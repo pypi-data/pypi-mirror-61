71cb533cfd9e6cdf95b9f7351ba86bb7b870bca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:05 +0000

    Add package kim-property/1.0.1/kim_property-1.0.1.tar.gz

---
+++ packages/kim-property/kim_property-1.0.1.tar.gz/kim_property-1.0.1/tests/test_kim_property/__init__.py
@@ -0,0 +1,17 @@
+import doctest
+import unittest
+
+try:
+    import kim_property
+except:
+    raise Exception('Failed to import property `kim_property` utility module')
+
+
+class PyTest(unittest.TestCase):
+    """Make a basic py test class.
+
+    The basic py test class will be used by the other tests.
+
+    """
+    KIMPropertyError = staticmethod(kim_property.KIMPropertyError)
+    kim_property = staticmethod(kim_property)
