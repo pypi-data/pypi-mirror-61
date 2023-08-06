9076f7258c37faedbcf533093ecd90a3657e20e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:05 +0000

    Add package bacchus/0.1.15/bacchus-0.1.15-py3-none-any.whl

---
+++ packages/bacchus/bacchus-0.1.15-py3-none-any.whl/bacchus/certificates.py
@@ -0,0 +1,28 @@
+from bacchus.base import HomeServerApp
+
+
+class CertManager(HomeServerApp):
+    def setup(self):
+        cmd = [
+            '--dns', 'gandiv5', '-a', '-d', f'private.{self.domain}',
+            '--email', f'{self.meta["nextcloud_username"]}', 'run'
+        ]
+        self.logger.debug('Setting up certificates with lego')
+        self.logger.debug(
+            self.client.containers.run(
+                'goacme/lego',
+                command=cmd,
+                environment={'GANDIV5_API_KEY': self.meta['dns_api_key']},
+                volumes={
+                    self.path.absolute(): {
+                        'bind': '/.lego',
+                        'mode': 'rw'
+                    }
+                },
+                auto_remove=True,
+                detach=False))
+        self.logger.debug('Set up certificates with lego')
+
+    def wait_for_status(self):
+        # Not actually needed...
+        return True
