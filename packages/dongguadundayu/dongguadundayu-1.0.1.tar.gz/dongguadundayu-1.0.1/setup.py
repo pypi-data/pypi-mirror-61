dde7f0eea10a0c1d4bcdf68cd5a0abeffd3de80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:41 +0000

    Add package dongguadundayu/1.0.1/dongguadundayu-1.0.1.tar.gz

---
+++ packages/dongguadundayu/dongguadundayu-1.0.1.tar.gz/dongguadundayu-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dongguadundayu",
+    version="1.0.1",
+    author="dong gua dun da yu",
+    author_email="2548737967@qq.com",
+    description="wo shi zhong guo ren",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
