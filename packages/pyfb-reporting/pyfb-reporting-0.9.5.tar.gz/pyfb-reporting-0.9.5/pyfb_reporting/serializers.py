a54ddfb09764a86cef4352aa1312340ed9873171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:01 +0000

    Add package pyfb-reporting/0.9.5/pyfb-reporting-0.9.5.tar.gz

---
+++ packages/pyfb-reporting/pyfb-reporting-0.9.5.tar.gz/pyfb-reporting-0.9.5/pyfb_reporting/serializers.py
@@ -0,0 +1,138 @@
+from . import models
+
+from rest_framework import serializers
+
+
+class CDRSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.CDR
+        fields = (
+            'pk',
+            'customer_ip',
+            'aleg_uuid',
+            'bleg_uuid',
+            'rtp_uuid',
+            'caller_number',
+            'callee_number',
+            'chan_name',
+            'start_stamp',
+            'answered_stamp',
+            'end_stamp',
+            'duration', 
+            'read_codec',
+            'write_codec',
+            'sip_code',
+            'sip_reason',
+            'cost_rate',
+            'total_sell',
+            'total_cost',
+            'rate',
+            'sip_charge_info',
+            'sip_user_agent',
+            'sip_rtp_rxstat',
+            'sip_rtp_txstat',
+            'kamailio_server',
+            'hangup_disposition',
+            'direction',
+        )
+
+
+class DimDateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimDate
+        fields = (
+            'pk',
+            'date',
+            'day',
+            'day_of_week',
+            'hour',
+            'month',
+            'quarter',
+            'year',
+        )
+
+
+class DimCustomerHangupcauseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimCustomerHangupcause
+        fields = (
+            'pk',
+            'hangupcause',
+            'total_calls',
+            'direction',
+        )
+
+
+class DimCustomerSipHangupcauseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimCustomerSipHangupcause
+        fields = (
+            'pk',
+            'sip_hangupcause',
+            'total_calls',
+            'direction',
+        )
+
+
+class DimProviderHangupcauseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimProviderHangupcause
+        fields = (
+            'pk',
+            'hangupcause',
+            'total_calls',
+            'direction',
+        )
+
+
+class DimProviderSipHangupcauseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimProviderSipHangupcause
+        fields = (
+            'pk',
+            'sip_hangupcause',
+            'total_calls',
+            'direction',
+        )
+
+
+class DimCustomerDestinationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimCustomerDestination
+        fields = (
+            'pk',
+            'total_calls',
+            'success_calls',
+            'total_duration',
+            'avg_duration',
+            'max_duration',
+            'min_duration',
+            'total_sell',
+            'total_cost',
+            'direction',
+        )
+
+
+class DimProviderDestinationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DimProviderDestination
+        fields = (
+            'pk',
+            'total_calls',
+            'success_calls',
+            'total_duration',
+            'avg_duration',
+            'max_duration',
+            'min_duration',
+            'total_sell',
+            'total_cost',
+            'direction',
+        )
---
+++ packages/pyfb-reporting/pyfb-reporting-0.9.5.tar.gz/pyfb-reporting-0.9.5/setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_version(*file_paths):
+    """Retrieves the version from pyfb_reporting/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+version = get_version("pyfb_reporting", "__init__.py")
+
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='pyfb-reporting',
+    version=version,
+    description="""Reporting application for PyFB project""",
+    long_description=readme + '\n\n' + history,
+    author='Mathias WOLFF',
+    author_email='mathias@celea.org',
+    url='https://github.com/mwolff44/pyfb-reporting',
+    packages=[
+        'pyfb_reporting',
+    ],
+    include_package_data=True,
+    install_requires=[
+        "django-model-utils>=2.0",
+        "django-extensions>=2.1.3",
+        "djangorestframework>=3.7.7",
+        "django-crispy-forms>=1.7.0",
+        "pygments>=2.4.2",
+        "pyfb-direction>=0.9.0",
+        "pyfb-company>=0.9.0",
+        "pyfb-endpoint>=0.9.0",
+        "pyfb-rating>=0.9.0",
+        "pyfb-routing>=0.9.0",
+        "pyfb-kamailio>=0.9.0",
+        "pyfb-did>=0.9.0",
+        "django-partial-index>=0.5.2",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='pyfb-reporting',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
