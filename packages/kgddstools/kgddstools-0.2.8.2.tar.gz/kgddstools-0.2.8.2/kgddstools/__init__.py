a2a2de0bac5421dfc0d616854aa27f85fb85545b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:46 +0000

    Add package kgddstools/0.2.8.2/kgddstools-0.2.8.2.tar.gz

---
+++ packages/kgddstools/kgddstools-0.2.8.2.tar.gz/kgddstools-0.2.8.2/kgddstools/__init__.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+def plot_cont(input_df_cont, cont_cols):
+    print(cont_cols)
+
+def plot_cat(input_df_cat, cat_cols):
+    print(cat_cols)
+
+def eda(input_df, target_var, type, cat = [], cont = []):
+
+    if type == 'df_summary':
+        #missingno, etc.
+
+        #Spit out column names
+
+        print('zzzzzz')
+
+    elif type == 'var_plots':
+        if (len(cat) + len(cont)) >= 1:
+            if(len(cat) >= 1):
+                print('cat var')
+
+                plot_cat(input_df_cat = input_df, cat_cols = cat)
+
+            if(len(cont) >= 1):
+                print('cont var')
+
+                plot_cont(input_df_cont = input_df, cont_cols = cont)
+        else
+            raise ValueError("You need to define at least column in a list via either cat or cont arguments.")
+    else:
+        raise ValueError("Inputs for type are 'df_summary' and 'var_plots'.")
