dc5e70eeb7bebbf6bd18eb838c8c962349d49304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package cst-modeling3d/0.1.1/cst_modeling3d-0.1.1.tar.gz

---
+++ packages/cst-modeling3d/cst_modeling3d-0.1.1.tar.gz/cst_modeling3d-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('Readme.md') as f:
+      long_description = f.read()
+
+setup(name='cst_modeling3d',
+      version='0.1.1',
+      description='This is the module of surface/airfoil modeling',
+      long_description=long_description,
+      keywords='CST modeling',
+      download_url='https://github.com/swayli94/cst-modeling3d/',
+      license='MIT',
+      author='Runze LI',
+      author_email='swayli94@gmail.com',
+      packages=find_packages(exclude=['example']),
+      install_requires=['numpy', 'scipy', 'matplotlib'],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ]
+)
+
