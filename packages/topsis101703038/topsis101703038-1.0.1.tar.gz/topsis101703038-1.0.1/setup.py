1f163bcffa591e084621a468c026783e7a98a45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:17 +0000

    Add package topsis101703038/1.0.1/topsis101703038-1.0.1.tar.gz

---
+++ packages/topsis101703038/topsis101703038-1.0.1.tar.gz/topsis101703038-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="topsis101703038",
+    version="1.0.1",
+    description="A Python package for topsis analysis.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kiliumnatta/Topsis",
+    author="Aditya Tomar",
+    author_email="aditya65921@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["TOPSIS101703038"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "TOPSIS101703038=TOPSIS101703038.Topsis:main",
+        ]
+    },
+)
