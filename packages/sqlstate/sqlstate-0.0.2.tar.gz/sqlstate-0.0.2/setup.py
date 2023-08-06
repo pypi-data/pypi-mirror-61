4d861284f864e590fe7016f52494327436f1bb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:24 +0000

    Add package sqlstate/0.0.2/sqlstate-0.0.2.tar.gz

---
+++ packages/sqlstate/sqlstate-0.0.2.tar.gz/sqlstate-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sqlstate",
+    version="0.0.2",
+    author="Andryo Marzuki",
+    author_email="stabbish@gmail.com",
+    description="A lightweight state framework for Python applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marzukia/sqlstate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
