333866c83603e81575a1df09746a16b75379f6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:46 +0000

    Add package jpprint/0.0.9/jpprint-0.0.9.tar.gz

---
+++ packages/jpprint/jpprint-0.0.9.tar.gz/jpprint-0.0.9/setup.py
@@ -0,0 +1,30 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='jpprint',
+    version='0.0.9',
+    description='Basic print and comparison of two dictionaries and/or json files',
+    url='https://github.com/voidnologo/jpprint',
+    author='voidnologo',
+    author_email='void.no.logo@gmail.com',
+    license='None',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    py_modules=["jpprint"],
+)
