3d36325799825b64391b18ec11ee76c11449b454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:21 +0000

    Add package shoobx.immutable/1.4.0/shoobx.immutable-1.4.0.tar.gz

---
+++ packages/shoobx.immutable/shoobx.immutable-1.4.0.tar.gz/shoobx.immutable-1.4.0/src/shoobx/immutable/__init__.py
@@ -0,0 +1,7 @@
+# Re-export.
+# flake8: noqa
+
+from .immutable import ImmutableBase, Immutable, update
+from .immutable import ImmutableList, ImmutableSet, ImmutableDict
+from .revisioned import RevisionedImmutableBase, RevisionedImmutable
+from .revisioned import SimpleRevisionedImmutableManager, RevisionedMapping
