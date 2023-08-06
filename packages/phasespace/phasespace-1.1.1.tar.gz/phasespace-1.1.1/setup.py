cdd37211e616df0822f54453ef18517bbc3e9960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:40 +0000

    Add package phasespace/1.1.1/phasespace-1.1.1.tar.gz

---
+++ packages/phasespace/phasespace-1.1.1.tar.gz/phasespace-1.1.1/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open(os.path.join(here, 'requirements_dev.txt'), encoding='utf-8') as requirements_dev_file:
+    requirements_dev = requirements_dev_file.read().splitlines()
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8') as history_file:
+    history = history_file.read()
+
+# split the developer requirements into setup and test requirements
+if not requirements_dev.count("") == 1 or requirements_dev.index("") == 0:
+    raise SyntaxError("requirements_dev.txt has the wrong format: setup and test "
+                      "requirements have to be separated by one blank line.")
+requirements_dev_split = requirements_dev.index("")
+
+setup_requirements = ["pip>9",
+                      "setuptools_scm",
+                      "setuptools_scm_git_archive"]
+test_requirements = requirements_dev[requirements_dev_split + 1:]  # +1: skip empty line
+
+setup(
+    author="Albert Puig Navarro",
+    author_email='apuignav@gmail.com',
+    maintainer="zfit",
+    maintainer_email='zfit@physik.uzh.ch',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    description="TensorFlow implementation of the Raubold and Lynch method for n-body events",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme.replace(":math:", "") + '\n\n' + history,
+    include_package_data=True,
+    keywords='phasespace',
+    name='phasespace',
+    packages=find_packages(include=['phasespace']),
+    # setup_requires=setup_requirements,
+    python_requires=">=3.6",
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/zfit/phasespace',
+    # use_scm_version=True,
+    version="1.1.1",
+    zip_safe=False,
+)
---
+++ packages/phasespace/phasespace-1.1.1.tar.gz/phasespace-1.1.1/tests/test_generate.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# @file   test_generate.py
+# @author Albert Puig (albert.puig@cern.ch)
+# @date   27.02.2019
+# =============================================================================
+"""Basic dimensionality tests."""
+
+import os
+import sys
+
+import pytest
+
+import phasespace
+
+sys.path.append(os.path.dirname(__file__))
+
+from .helpers import decays
+
+B0_MASS = decays.B0_MASS
+PION_MASS = decays.PION_MASS
+
+
+def test_one_event():
+    """Test B->pi pi pi."""
+    decay = phasespace.nbody_decay(B0_MASS, [PION_MASS, PION_MASS, PION_MASS])
+    norm_weights, particles = decay.generate(n_events=1)
+    assert norm_weights.shape[0] == 1
+    assert all([weight.numpy() < 1 for weight in norm_weights])
+    assert len(particles) == 3
+    assert all([part.shape == (1, 4) for part in particles.values()])
+
+
+def test_one_event_tf():
+    """Test B->pi pi pi."""
+    decay = phasespace.nbody_decay(B0_MASS, [PION_MASS, PION_MASS, PION_MASS])
+    norm_weights, particles = decay.generate(n_events=1)
+
+    assert norm_weights.shape[0] == 1
+    assert all([weight.numpy() < 1 for weight in norm_weights])
+    assert len(particles) == 3
+    assert all([part.shape == (1, 4) for part in particles.values()])
+
+
+@pytest.mark.parametrize("n_events", argvalues=[5])
+def test_n_events(n_events):
+    """Test 5 B->pi pi pi."""
+    decay = phasespace.nbody_decay(B0_MASS, [PION_MASS, PION_MASS, PION_MASS])
+    norm_weights, particles = decay.generate(n_events=n_events)
+    assert norm_weights.shape[0] == 5
+    assert all([weight.numpy() < 1 for weight in norm_weights])
+    assert len(particles) == 3
+    assert all([part.shape == (5, 4) for part in particles.values()])
+
+
+# def test_cache():
+#     from phasespace import GenParticle
+#
+#     mother_particle = GenParticle('mother', 10000)
+#     daughter1 = GenParticle('daughter1', mass=2000)
+#     _ = mother_particle.set_children(daughter1, GenParticle('daughter2', mass=1000))
+#     assert not mother_particle._cache_valid
+#     _ = mother_particle.generate(n_events=8)
+#     tensor1 = mother_particle._cache
+#     _ = mother_particle.generate(n_events=5)
+#     tensor1_too = mother_particle._cache
+#     assert tensor1 is tensor1_too
+#     assert mother_particle._cache is not None
+#     assert mother_particle._cache_valid
+#
+#     daughter1.set_children(GenParticle('daugther21', mass=100),
+#                            GenParticle('daughter22', mass=500))
+#     assert not mother_particle._cache_valid
+#     _ = mother_particle.generate(n_events=3)
+#     tensor2 = mother_particle._cache
+#     assert tensor2 is not tensor1
+#     assert mother_particle._cache_valid
+
+
+if __name__ == "__main__":
+    test_one_event()
+    test_n_events(5)
+
+    # EOF
