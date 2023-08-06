825d501f350c642a048174742dcd57180d3a14d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:07 +0000

    Add package docker-droplet/1.0.1/docker_droplet-1.0.1-py3-none-any.whl

---
+++ packages/docker-droplet/docker_droplet-1.0.1-py3-none-any.whl/docker_droplet/terraform/template.py
@@ -0,0 +1,32 @@
+import pathlib
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def create_config(droplet_name, ssh_key, project, domain) -> str:
+    """
+    Create a terraform configuration file for a single droplet
+    
+    Args:
+        droplet_name ([type]): 
+        ssh_key ([type]):
+        project ([type]):
+        domain ([type]):
+    
+    Returns:
+        str: Configuration file as a string
+    """
+    directory = pathlib.Path(__file__).parent.absolute()
+    templateLoader = FileSystemLoader(searchpath=directory)
+    templateEnv = Environment(loader=templateLoader)
+
+    print(templateEnv)
+
+    providers_template = templateEnv.get_template("providers.jinja2")
+    resources_template = templateEnv.get_template("digitalocean.jinja2")
+
+    providers_config = providers_template.render()
+    resources_config = resources_template.render(
+        droplet_name=droplet_name, ssh_key=ssh_key, project=project, domain=domain
+    )
+    return "\n\n".join([providers_config, resources_config])
