54c4f7ccb8bc6e6652abadeccfb2169548d11d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:05 +0000

    Add package rediz/0.1.2/rediz-0.1.2.tar.gz

---
+++ packages/rediz/rediz-0.1.2.tar.gz/rediz-0.1.2/rediz/rediz_test_config_private.py
@@ -0,0 +1,5 @@
+
+#raise Exception("testing locally")
+REDIZ_TEST_CONFIG = {"password":"5r70RHtd2m0x3BHdLE8mp7APxoIJS1aH",
+                     "host":"redis-19709.c11.us-east-1-3.ec2.cloud.redislabs.com",
+                     "port":"19709"}
