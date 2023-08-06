9733cddfb45545644429aa73322fe71b7992dc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:13 +0000

    Add package poetry-publish/0.3.1/poetry-publish-0.3.1.tar.gz

---
+++ packages/poetry-publish/poetry-publish-0.3.1.tar.gz/poetry-publish-0.3.1/poetry_publish/tests/test_project_setup.py
@@ -0,0 +1,78 @@
+"""
+    :created: 1.2.2020 by Jens Diemer
+    :copyleft: 2020 by the poetry-publish team
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+from creole.setup_utils import assert_rst_readme
+
+import poetry_publish
+
+
+PACKAGE_ROOT = Path(poetry_publish.__file__).parent.parent
+
+
+def assert_file_contains_string(file_path, string):
+    with file_path.open('r') as f:
+        for line in f:
+            if string in line:
+                return
+    raise AssertionError(f'File {file_path} does not contain {string!r} !')
+
+
+def test_version(package_root=None, version=None):
+    if package_root is None:
+        package_root = PACKAGE_ROOT
+
+    if version is None:
+        version = poetry_publish.__version__
+
+    if 'dev' not in version and 'rc' not in version:
+        version_string = f'v{version}'
+
+        assert_file_contains_string(
+            file_path=Path(package_root, 'README.creole'),
+            string=version_string
+        )
+
+        assert_file_contains_string(
+            file_path=Path(package_root, 'README.rst'),
+            string=version_string
+        )
+
+    assert_file_contains_string(
+        file_path=Path(package_root, 'pyproject.toml'),
+        string=f'version = "{version}"'
+    )
+
+
+def test_assert_rst_readme(package_root=None, version=None, filename='README.creole'):
+    if package_root is None:
+        package_root = PACKAGE_ROOT
+
+    if version is None:
+        version = poetry_publish.__version__
+
+    if 'dev' not in version and 'rc' not in version:
+        assert_rst_readme(package_root=package_root, filename=filename)
+
+
+def test_poetry_check(package_root=None):
+    if package_root is None:
+        package_root = PACKAGE_ROOT
+
+    poerty_bin = shutil.which('poetry')
+
+    output = subprocess.check_output(
+        [poerty_bin, 'check'],
+        universal_newlines=True,
+        env=os.environ,
+        stderr=subprocess.STDOUT,
+        cwd=str(package_root),
+    )
+    print(output)
+    assert output == 'All set!\n'
