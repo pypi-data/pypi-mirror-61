8f0b8872c1fc0d177ead50c1004d7448eb1c8c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:55 +0000

    Add package pipelinewise-singer-python/1.1.0/pipelinewise-singer-python-1.1.0.tar.gz

---
+++ packages/pipelinewise-singer-python/pipelinewise-singer-python-1.1.0.tar.gz/pipelinewise-singer-python-1.1.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="pipelinewise-singer-python",
+      version='1.1.0',
+      description="Singer.io utility library - PipelineWise compatible",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="TransferWise",
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      url="https://github.com/transferwise/pipelinewise-singer-python",
+      install_requires=[
+          'pytz==2018.4',
+          'jsonschema==2.6.0',
+          'simplejson==3.11.1',
+          'python-dateutil>=2.6.0',
+          'backoff==1.8.0',
+          'ciso8601',
+      ],
+      extras_require={
+          'dev': [
+              'pylint',
+              'ipython',
+              'ipdb',
+              'nose',
+              'singer-tools'
+          ]
+      },
+      packages=['singer'],
+      package_data={
+          'singer': [
+              'logging.conf'
+          ]
+      },
+      include_package_data=True
+      )
