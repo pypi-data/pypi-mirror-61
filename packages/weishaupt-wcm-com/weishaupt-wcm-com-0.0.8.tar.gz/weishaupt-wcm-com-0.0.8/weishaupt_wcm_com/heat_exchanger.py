2975f52e2b8d3e2ff5ba530918ba30666444949d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:25 +0000

    Add package weishaupt-wcm-com/0.0.8/weishaupt-wcm-com-0.0.8.tar.gz

---
+++ packages/weishaupt-wcm-com/weishaupt-wcm-com-0.0.8.tar.gz/weishaupt-wcm-com-0.0.8/weishaupt_wcm_com/heat_exchanger.py
@@ -0,0 +1,47 @@
+import requests
+import json
+from requests.auth import HTTPDigestAuth
+
+ENDPOINT = "/parameter.json"
+QUERYTELEGRAM = (
+    '{"prot":"coco","telegramm":[[10,0,1,3793,0,0,0,0],[10,0,1,12,0,0,0,0],[10,0,1,14,0,0,0,0],[10,0,1,3101,0,0,0,0],[10,0,1,325,0,0,0,0],[10,0,1,3197,0,0,0,0]]}'
+)
+
+VALUE = 1
+TEMP = 2
+
+# ID, Name, Value/Temp
+QUERIES = [[3793, "Oil Meter", VALUE], [3197, "Load Setting", VALUE], [12, "Outside Temperature", TEMP], [14, "Warm Water Temperature", TEMP], [3101, "Flow Temperature", TEMP], [325, "Flue Gas Temperature", TEMP]]
+
+
+
+def getTemperture(lowByte, highByte):
+    return (lowByte + 265 * highByte) / 10
+
+
+def getValue(lowByte, highByte):
+    return lowByte + 265 * highByte
+
+
+def process_values(server, username, password):
+    try:
+        req = requests.post(
+            "http://" + server + ENDPOINT,
+            auth=HTTPDigestAuth(username, password),
+            data=QUERYTELEGRAM,
+        )
+        telegram = json.loads(req.text)["telegramm"]
+        result = {}
+        for message in telegram:
+            for reading in QUERIES:
+                # print(reading)
+                if message[3] == reading[0]:
+                    if reading[2] == TEMP:
+                        result[reading[1]] = getTemperture(message[6], message[7])
+                    else:
+                        result[reading[1]] = getValue(message[6], message[7])
+        return json.dumps(result)
+    except:
+        print("Error getting readings")
+        return None
+
