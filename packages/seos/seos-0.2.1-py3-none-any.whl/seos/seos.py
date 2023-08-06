761f5ed8e4e41705cd6f21902303f3ece9afcad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:41 +0000

    Add package seos/0.2.1/seos-0.2.1-py3-none-any.whl

---
+++ packages/seos/seos-0.2.1-py3-none-any.whl/seos/seos.py
@@ -0,0 +1,73 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from rainbow import RainbowLogger
+logger = RainbowLogger(__name__)
+
+
+class Seos:
+    def __init__(
+        self,
+        credentials_file,
+        spreadsheet_id=None,
+        token_file="./token.pickle"
+    ):
+        SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+        creds = None
+        if os.path.exists(token_file):
+            with open(token_file, "rb") as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentials_file,
+                    SCOPES
+                )
+                creds = flow.run_console()
+            with open(token_file, "wb") as token:
+                pickle.dump(creds, token)
+
+        self.service = build("sheets", "v4", credentials=creds)
+        self.sheet = self.service.spreadsheets()
+        self.sheet_id = spreadsheet_id
+
+    @property
+    def sheet_name(self):
+        return self._sheet_name
+
+    @sheet_name.setter
+    def sheet_name(self, name):
+        self._sheet_name = name
+
+    @property
+    def scope(self):
+        return self._scope
+
+    @scope.setter
+    def scope(self, scope):
+        self._scope = scope
+
+    def extract(self):
+        if self.sheet_id is None:
+            logger.error("Sheet ID is not set")
+            return
+
+        if not self.sheet_name or not self.scope:
+            logger.warning("Nothing to extract")
+            return
+
+        result = self.sheet.values().get(
+            spreadsheetId=self.sheet_id,
+            range="{}!{}".format(self.sheet_name, self.scope)
+        ).execute()
+
+        values = result.get("values", [])
+
+        if values:
+            return values
+
+        return []
