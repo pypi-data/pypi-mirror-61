c37e08b04d9aa31d1edc9b37f41f952ef791edb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package logicdrop-sparks-openapi/4.3.1/logicdrop-sparks-openapi-4.3.1.tar.gz

---
+++ packages/logicdrop-sparks-openapi/logicdrop-sparks-openapi-4.3.1.tar.gz/logicdrop-sparks-openapi-4.3.1/logicdrop/sparks/openapi/api/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from logicdrop.sparks.openapi.api.admin_services_api import AdminServicesApi
+from logicdrop.sparks.openapi.api.cache_services_api import CacheServicesApi
+from logicdrop.sparks.openapi.api.client_services_api import ClientServicesApi
+from logicdrop.sparks.openapi.api.compute_services_api import ComputeServicesApi
+from logicdrop.sparks.openapi.api.content_services_api import ContentServicesApi
+from logicdrop.sparks.openapi.api.data_services_api import DataServicesApi
+from logicdrop.sparks.openapi.api.email_services_api import EmailServicesApi
+from logicdrop.sparks.openapi.api.kiosk_services_api import KioskServicesApi
+from logicdrop.sparks.openapi.api.office_services_api import OfficeServicesApi
+from logicdrop.sparks.openapi.api.package_services_api import PackageServicesApi
+from logicdrop.sparks.openapi.api.platform_services_api import PlatformServicesApi
+from logicdrop.sparks.openapi.api.project_services_api import ProjectServicesApi
+from logicdrop.sparks.openapi.api.query_services_api import QueryServicesApi
+from logicdrop.sparks.openapi.api.schema_services_api import SchemaServicesApi
+from logicdrop.sparks.openapi.api.security_services_api import SecurityServicesApi
+from logicdrop.sparks.openapi.api.template_services_api import TemplateServicesApi
