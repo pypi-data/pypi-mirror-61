5fa1d307f4f301f6cef9a3708e79612e0a312690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:39 +0000

    Add package filetrace/0.0.3/filetrace-0.0.3.tar.gz

---
+++ packages/filetrace/filetrace-0.0.3.tar.gz/filetrace-0.0.3/filetrace/cli.py
@@ -0,0 +1,29 @@
+import sys
+from optparse import OptionParser
+
+
+def parse_cmd_line():
+    parser = OptionParser()
+    parser.add_option(
+        "-l",
+        "--live",
+        action="store_false",
+        dest="live",
+        default=False,
+        help="print file operations while running",
+    )
+    parser.add_option(
+        "-o",
+        "--output-file",
+        dest="output",
+        help="output to file instead of stderr",
+        default=None,
+    )
+    parser.add_option(
+        "-x", "--exclude", dest="exclude", help="exclude path from lookups"
+    )
+    options, args = parser.parse_args()
+    if len(args) < 1:
+        print("Usage: {} -- command".format(sys.argv[0]))
+        exit(1)
+    return options, args
