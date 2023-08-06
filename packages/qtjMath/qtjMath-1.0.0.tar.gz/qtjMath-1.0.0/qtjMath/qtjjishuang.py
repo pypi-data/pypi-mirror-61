d43441b8eeac0fafa2873ca986ac637563bb69d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:37 +0000

    Add package qtjMath/1.0.0/qtjMath-1.0.0.tar.gz

---
+++ packages/qtjMath/qtjMath-1.0.0.tar.gz/qtjMath-1.0.0/qtjMath/qtjjishuang.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    print('qtjjishuang add')
+    return a+b
+
+def minus(a,b):
+    print('qtjjishuang minus')
+    return a-b
+
+def multiple(a,b):
+    print('qtjjishuang multiple')
+    return a*b
+
+def division(a,b):
+    print('qtjjishuang division')
+    return a/b
---
+++ packages/qtjMath/qtjMath-1.0.0.tar.gz/qtjMath-1.0.0/setup.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from distutils.core import setup
+setup(
+    name='qtjMath',
+    version='1.0.0',
+    description='这是我的第一个对外发布的模块，测试用的。',
+    author='qiantianjiang',
+    author_email='qiantianjiang@163.com',
+    py_modules=['qtjMath.qtjjishuang']
+)
