856164c6525c030b11b06ee25bf545cb38240a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:06 +0000

    Add package config-client/0.5.0/config-client-0.5.0.tar.gz

---
+++ packages/config-client/config-client-0.5.0.tar.gz/config-client-0.5.0/setup.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="config-client",
+    version="0.5.0",
+    author="alexandre menezes",
+    author_email="alexandre.fmenezes@gmail.com",
+    description="config service client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache-2.0',
+    url="https://github.com/amenezes/config-client",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.0',
+    project_urls=OrderedDict((
+        ('Documentation', 'https://github.com/amenezes/config-client'),
+        ('Code', 'https://github.com/amenezes/config-client'),
+        ('Issue tracker', 'https://github.com/amenezes/config-client/issues')
+    )),
+    install_requires=[
+        'attrs>=19.1.0',
+        'glom>=19.2.0',
+        'requests>=2.22.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: AsyncIO",
+        "Framework :: Flask",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Java Libraries",
+    ],
+)
