a9c09bf2fa9fa754e7ea4dbc2cdae8fe6479369a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:43 +0000

    Add package another-name-of-distributions/0.2/another_name_of_distributions-0.2.tar.gz

---
+++ packages/another-name-of-distributions/another_name_of_distributions-0.2.tar.gz/another_name_of_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='another_name_of_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['another_name_of_distributions'],
+      zip_safe=False)
