258f146b34ebe92245020e1dfb4f3404f6b681c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:10 +0000

    Add package measure-horseshoe-bat-calls/0.0.2b0/measure_horseshoe_bat_calls-0.0.2b0.tar.gz

---
+++ packages/measure-horseshoe-bat-calls/measure_horseshoe_bat_calls-0.0.2b0.tar.gz/measure_horseshoe_bat_calls-0.0.2b0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='measure_horseshoe_bat_calls',
+      version='0.0.2b',
+      description='Measure single Cf bat calls, or sounds like it',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/thejasvibr/measure_horseshoe_bat_calls.git',
+      author='Thejasvi Beleyur',
+      author_email='thejasvib@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+     install_requires=['numpy','pandas','soundfile','peakutils',
+        'scipy','matplotlib'],
+      zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Topic :: Multimedia :: Sound/Audio'])
