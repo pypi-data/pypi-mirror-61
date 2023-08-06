8d309b29e4853e17ce195f2cffccafcc39ed23da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:17 +0000

    Add package pyguppyclient/0.0.2/pyguppyclient-0.0.2.tar.gz

---
+++ packages/pyguppyclient/pyguppyclient-0.0.2.tar.gz/pyguppyclient-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from pyguppyclient import __version__
+
+setup(
+    name='pyguppyclient',
+    version=__version__,
+    author="Oxford Nanopore Technologies, Ltd",
+    author_email="support@nanoporetech.com",
+    url="https://github.com/nanoporetech/pyguppyclient"
+)
