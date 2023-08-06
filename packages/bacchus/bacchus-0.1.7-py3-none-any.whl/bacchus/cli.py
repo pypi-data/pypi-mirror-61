7b8a7754023ceed95999be5ffaaeee814b9d5145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:44 +0000

    Add package bacchus/0.1.7/bacchus-0.1.7-py3-none-any.whl

---
+++ packages/bacchus/bacchus-0.1.7-py3-none-any.whl/bacchus/cli.py
@@ -0,0 +1,29 @@
+from cleo import Command
+from cleo import Application
+
+from . import HomeServerSetup
+
+
+class GreetCommand(Command):
+    """
+    Installs bacchus 
+
+    install
+        {domain? : Domain (FQDN) for virtualhosts} 
+        {username? : Nextcloud first user's username}
+        {password? : Nextcloud first user's password}
+        {provider? : (Optional) Set up only one service} 
+    """
+    def handle(self):
+        """Handle command"""
+        setup = HomeServerSetup(domain=self.argument('domain'),
+                                nextcloud_username=self.argument('username'),
+                                nextcloud_password=self.argument('password'))
+
+        setup.configure(self.argument('provider'))
+
+
+def main():
+    application = Application()
+    application.add(GreetCommand())
+    application.run()
