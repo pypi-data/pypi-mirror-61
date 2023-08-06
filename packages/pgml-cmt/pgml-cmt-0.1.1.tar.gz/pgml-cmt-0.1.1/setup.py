f77f891620ec0344564632474519277c168fda77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package pgml-cmt/0.1.1/pgml-cmt-0.1.1.tar.gz

---
+++ packages/pgml-cmt/pgml-cmt-0.1.1.tar.gz/pgml-cmt-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pgml-cmt',
+      version='0.1.1',
+      description='Navigating the Generalization Landscape of Neural Networks using Physics: A Case Study in Quantum Mechanics',
+      url='http://shorturl.at/tCZ05',
+      author='Anonymous Author',
+      author_email='pgml_cmt@yahoo.com',
+      license='MIT',
+      packages=['cmt'],
+      zip_safe=False)
