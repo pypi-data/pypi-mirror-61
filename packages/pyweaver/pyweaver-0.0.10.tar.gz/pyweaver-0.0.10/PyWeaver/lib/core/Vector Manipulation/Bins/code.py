985144322be39f572e441750314dc4cb9463ecfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:13 +0000

    Add package pyweaver/0.0.10/pyweaver-0.0.10.tar.gz

---
+++ packages/pyweaver/pyweaver-0.0.10.tar.gz/pyweaver-0.0.10/PyWeaver/lib/core/Vector Manipulation/Bins/code.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def f(vector, bins=None):
+	
+	if bins is None:
+		min_v = np.min(vector)
+		max_v = np.max(vector)
+		bins = np.array([min_v, max_v])
+	
+	v = np.digitize(vector, bins)
+	v = bins[v]
+	return v
