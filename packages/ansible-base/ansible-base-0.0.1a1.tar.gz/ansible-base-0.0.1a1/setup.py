7fa07d6038592ea192164458fdc8b619c8eef09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:17 +0000

    Add package ansible-base/0.0.1a1/ansible-base-0.0.1a1.tar.gz

---
+++ packages/ansible-base/ansible-base-0.0.1a1.tar.gz/ansible-base-0.0.1a1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="ansible-base",
+	version="0.0.1a1",
+	packages=find_packages("lib"),
+	author="Ansible Project",
+	author_email="mrd+ansible@redhat.com",
+	url="https://github.com/ansible/ansible/"
+)
