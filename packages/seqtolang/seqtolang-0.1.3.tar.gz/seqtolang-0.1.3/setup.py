194755cf2288b96363b12badb6807272f68144a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:50 +0000

    Add package seqtolang/0.1.3/seqtolang-0.1.3.tar.gz

---
+++ packages/seqtolang/seqtolang-0.1.3.tar.gz/seqtolang-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.3'
+
+setup(
+    name='seqtolang',
+    version=VERSION,
+    description="Multi Langauge Documents Langauge identification",
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hiredscorelabs/seqtolang",
+    keywords='Deep Learning Natural Language Processing NLP Machine Learning',
+    license='Apache',
+    packages=find_packages(exclude=['test*']),
+    package_data={'': ['seqtolang/checkpoints']},
+    include_package_data=True,
+    install_requires=[
+        'torch>=1.1.0',
+    ],
+)
