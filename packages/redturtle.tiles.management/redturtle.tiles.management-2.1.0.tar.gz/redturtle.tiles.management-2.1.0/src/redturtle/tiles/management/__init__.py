4402f61cb6e1d68eafa7c2fdbbce6b1cc7623f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:14 +0000

    Add package redturtle.tiles.management/2.1.0/redturtle.tiles.management-2.1.0.tar.gz

---
+++ packages/redturtle.tiles.management/redturtle.tiles.management-2.1.0.tar.gz/redturtle.tiles.management-2.1.0/src/redturtle/tiles/management/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Init and utils."""
+
+from zope.i18nmessageid import MessageFactory
+
+import logging
+
+
+_ = MessageFactory('redturtle.tiles.management')
+logger = logging.getLogger('redturtle.tiles.management')
