06ec44a6458c129575a867aeae3ccfb0ebf216b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:07 +0000

    Add package galileo-sdk/0.0.13/galileo_sdk-0.0.13.tar.gz

---
+++ packages/galileo-sdk/galileo_sdk-0.0.13.tar.gz/galileo_sdk-0.0.13/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+import glob
+
+setup(
+    name="galileo_sdk",
+    version="0.0.13",
+    license="MIT",
+    author="Hypernet Labs",
+    author_email="hypernet@hypernetlabs.io",
+    url="https://hypernetwork.io/",
+    packages=["galileo_sdk"],
+    package_data={"galileo_sdk": ["sdk/**", "business/**/*", "config/**/*", "data/**/*"]},
+    python_requires=">=3.6.7",
+    install_requires=["requests>=2.21.0", "python-socketio"],
+    extras_require={"docs": ["sphinx>=2.2.0", "sphinx-material"]},
+    setup_requires=["pytest-runner", "black", "isort"],
+    tests_require=["pytest", "unittest", "mock"],
+)
