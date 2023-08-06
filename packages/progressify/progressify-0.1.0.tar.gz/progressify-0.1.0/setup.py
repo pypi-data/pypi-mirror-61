f635478cf3e41299348fad2e0051b9243d2c45fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:04 +0000

    Add package progressify/0.1.0/progressify-0.1.0.tar.gz

---
+++ packages/progressify/progressify-0.1.0.tar.gz/progressify-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['progressify']
+setup_kwargs = {
+    'name': 'progressify',
+    'version': '0.1.0',
+    'description': 'Progress bar from an iterable, as a context manager, or decorator',
+    'long_description': None,
+    'author': 'L3viathan',
+    'author_email': 'git@l3vi.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
