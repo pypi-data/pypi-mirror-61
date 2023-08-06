5d1f0da93dbaa47035bd8cf4f92f87d12bf460fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:40 +0000

    Add package python3-seco-range/1.0.0/python3-seco-range-1.0.0.tar.gz

---
+++ packages/python3-seco-range/python3-seco-range-1.0.0.tar.gz/python3-seco-range-1.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='python3-seco-range',
+    version = '1.0.0',
+    author='ytoolshed',
+    description='A Python 3 version of the library to interact with Range from ytoolshed',
+    long_description=long_description,
+    url='https://github.com/xadrnd/range/tree/master/python_seco_range/source',
+    project_urls={
+        'Original Project': 'https://github.com/ytoolshed/range'
+    },
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['requests', 'BeautifulSoup4', 'PyYaml', 'argparse'],
+    python_requires='>=3.6'
+)
+
