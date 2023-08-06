0e507fe8e319e9cf0232656063edd3a92f11d0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package adadamp/0.1.0rc1/adadamp-0.1.0rc1-py3-none-any.whl

---
+++ packages/adadamp/adadamp-0.1.0rc1-py3-none-any.whl/adadamp/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.1.0rc1"
+
+from .damping import (
+    BaseDamper,
+    AdaDamp,
+    PadaDamp,
+    GeoDamp,
+    GeoDampLR,
+    CntsDampLR,
+    GradientDescent,
+    ConvergenceError,
+)
+
+__all__ = [
+    "BaseDamper",
+    "AdaDamp",
+    "PadaDamp",
+    "GeoDamp",
+    "GeoDampLR",
+    "CntsDampLR",
+    "GradientDescent",
+]
