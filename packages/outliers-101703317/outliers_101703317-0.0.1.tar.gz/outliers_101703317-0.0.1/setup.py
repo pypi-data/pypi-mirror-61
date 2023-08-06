30e776ed75bb909604453cac902be0b5ab58dc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package outliers-101703317/0.0.1/outliers_101703317-0.0.1.tar.gz

---
+++ packages/outliers-101703317/outliers_101703317-0.0.1.tar.gz/outliers_101703317-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="outliers_101703317", # Replace with your own username
+    version="0.0.1",
+    author="Manas khatri",
+    author_email="manaskhatri4534@gmail.com",
+    description="A Simple Package to detect and remove outliers using Inter-Quatile Range",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
