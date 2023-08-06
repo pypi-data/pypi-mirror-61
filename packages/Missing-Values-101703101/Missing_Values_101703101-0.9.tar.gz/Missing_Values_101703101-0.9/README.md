ed78d66df75bd23476d4731e4f5ace717feadcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:02 +0000

    Add package Missing-Values-101703101/0.9/Missing_Values_101703101-0.9.tar.gz

---
+++ packages/Missing-Values-101703101/Missing_Values_101703101-0.9.tar.gz/Missing_Values_101703101-0.9/README.md
@@ -0,0 +1,4 @@
+PROJECT 3, UCS633 - Data Analysis and Visualization
+Arpit Singla 
+COE05
+Roll number: 101703101
---
+++ packages/Missing-Values-101703101/Missing_Values_101703101-0.9.tar.gz/Missing_Values_101703101-0.9/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 15:42:57 2020
+
+@author: Arpit Singla
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Missing_Values_101703101", 
+    version="0.09",
+    author="Arpit Singla",
+    author_email="arpitsingla1999@gmail.com",
+    description="Python Package to find missing values in a dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArpitSingla/Missing_Value_Python_101703101.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['Missing_Value=Missing_Values_101703101.Missing_Values_101703101:main'],
+    },
+    python_requires='>=3.6',
+)
