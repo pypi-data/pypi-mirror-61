73be75f43aa278950bfbc08e0e13800e6ed8ca30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:31 +0000

    Add package load-confounds/0.4/load_confounds-0.4.tar.gz

---
+++ packages/load-confounds/load_confounds-0.4.tar.gz/load_confounds-0.4/__init__.py
@@ -0,0 +1,5 @@
+"""
+loading fMRIprep confounds into python
+"""
+from .load_confounds import load_confounds
+__all__ = ['load_confounds']
---
+++ packages/load-confounds/load_confounds-0.4.tar.gz/load_confounds-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='load_confounds',
+   version='0.4',
+   description='load fMRIprep confounds in python',
+   author='Hanad Sharmarke and collaborators',
+   packages=['.'],  #same as name
+   author_email='hanadsharmarke@gmail.com',
+   install_requires=['numpy>=1.17.4', 'pandas>=0.25.3', 'scikit-learn>=0.21.3', 'scipy>=1.3.2'], #external packages as dependencies
+)
