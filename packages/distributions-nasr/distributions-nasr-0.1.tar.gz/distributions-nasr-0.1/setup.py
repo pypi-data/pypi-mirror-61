73d663f34c12df10f44751e021abb55148df8b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:16 +0000

    Add package distributions-nasr/0.1/distributions-nasr-0.1.tar.gz

---
+++ packages/distributions-nasr/distributions-nasr-0.1.tar.gz/distributions-nasr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-nasr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-nasr'],
+      zip_safe=False)
