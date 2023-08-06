7dea908bedd8eb1edff3f20aad0849b225f2de31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:18 +0000

    Add package jai/0.0.7/jai-0.0.7-py3-none-any.whl

---
+++ packages/jai/jai-0.0.7-py3-none-any.whl/jai/__init__.py
@@ -0,0 +1,9 @@
+from .arch import *
+from .trainer import *
+from .dataset import *
+from .improc import *
+from jai.kaggler.kaggle_data import *
+from .utils import *
+from .logger import *
+import jai.kaggler
+
---
+++ packages/jai/jai-0.0.7-py3-none-any.whl/jai/borrowed.py
@@ -0,0 +1,9 @@
+from fastai.basics import *
+
+"""
+An implementation of Mich by 
+"""
+
+
+
+
