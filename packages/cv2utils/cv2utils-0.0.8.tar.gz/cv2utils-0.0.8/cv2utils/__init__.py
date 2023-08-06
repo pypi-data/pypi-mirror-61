c1793ac212b4e2dcce135b0b3ddb8024ec0b7e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:51 +0000

    Add package cv2utils/0.0.8/cv2utils-0.0.8.tar.gz

---
+++ packages/cv2utils/cv2utils-0.0.8.tar.gz/cv2utils-0.0.8/cv2utils/__init__.py
@@ -0,0 +1,5 @@
+from cv2utils.haar_cascade import FaceCascade
+from cv2utils.haar_cascade import EyeCascade
+from cv2utils.opencv_dnn import FaceDnn
+
+__all__ = ["FaceCascade", "EyeCascade", "FaceDnn"]
