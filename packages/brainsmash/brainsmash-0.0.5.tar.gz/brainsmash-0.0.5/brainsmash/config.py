c0ef9f0ad95b103efa2be0708524eb154f401c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:01 +0000

    Add package brainsmash/0.0.5/brainsmash-0.0.5.tar.gz

---
+++ packages/brainsmash/brainsmash-0.0.5.tar.gz/brainsmash-0.0.5/brainsmash/config.py
@@ -0,0 +1,16 @@
+from os.path import join, dirname, abspath
+
+# TODO document this file
+
+# TODO __all__ = []
+
+kernels = ['exp', 'gaussian', 'invdist', 'uniform']
+
+repo_root = dirname(dirname(abspath(__file__)))  # root directory path
+package_root = join(repo_root, "brainsmash")
+data = join(package_root, "data")  # data directory
+surfaces = join(data, "surfaces")  # surface files
+
+parcel_labels_lr = join(
+    data, "CortexSubcortex_ColeAnticevic_NetPartition_"
+          "wSubcorGSR_netassignments_LR.dlabel.nii")
