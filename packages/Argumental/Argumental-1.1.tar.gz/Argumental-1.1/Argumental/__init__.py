75c557c997d14e6d3a68e754c44a9f216d1ae110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:26 +0000

    Add package Argumental/1.1/Argumental-1.1.tar.gz

---
+++ packages/Argumental/Argumental-1.1.tar.gz/Argumental-1.1/Argumental/__init__.py
@@ -0,0 +1,11 @@
+
+from Argue import Argue
+from Argument import Argument
+from Attribute import Attribute
+from Command import Command
+from Getters import Getters
+from NamedShort import NamedShort
+from Operation import Operation
+from Parameter import Parameter
+from Returns import Returns
+from Value import Value
