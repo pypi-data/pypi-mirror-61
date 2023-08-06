95c9254cd4dfc0aef737eac62fdabb1cc20a1bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:01 +0000

    Add package paper2remarkable/0.5.2/paper2remarkable-0.5.2-py3-none-any.whl

---
+++ packages/paper2remarkable/paper2remarkable-0.5.2-py3-none-any.whl/paper2remarkable/providers/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from .acm import ACM
+from .arxiv import Arxiv
+from .citeseerx import CiteSeerX
+from .html import HTML
+from .local import LocalFile
+from .nber import NBER
+from .neurips import NeurIPS
+from .openreview import OpenReview
+from .pdf_url import PdfUrl
+from .pmlr import PMLR
+from .pubmed import PubMed
+from .springer import Springer
+
+# NOTE: Order matters here, PdfUrl and HTML should be last
+providers = [
+    ACM,
+    Arxiv,
+    CiteSeerX,
+    NBER,
+    NeurIPS,
+    OpenReview,
+    PMLR,
+    PubMed,
+    Springer,
+    LocalFile,
+    PdfUrl,
+    HTML,
+]
