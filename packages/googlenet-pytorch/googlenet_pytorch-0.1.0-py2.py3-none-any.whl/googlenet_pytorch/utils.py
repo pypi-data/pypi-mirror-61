a2ca138fb2acc4081fee7a9de7bb6e77626de35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package googlenet-pytorch/0.1.0/googlenet_pytorch-0.1.0-py2.py3-none-any.whl

---
+++ packages/googlenet-pytorch/googlenet_pytorch-0.1.0-py2.py3-none-any.whl/googlenet_pytorch/utils.py
@@ -0,0 +1,84 @@
+# Copyright 2020 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import torch
+
+
+def load_trained_weights(model, model_name):
+    """ Loads custom weights, and train if loading for the first time. """
+    checkpoint = torch.load(model_name)
+    model.load_state_dict(checkpoint['state_dict'])
+
+
+def get_parameter_number(model):
+    total_num = sum(p.numel() for p in model.parameters())
+    trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Total parameters: {total_num / 1000000:.1f}M")
+    print(f"Trainable parameters: {trainable_num / 1000000:.1f}M")
+
+
+def print_state_dict(model):
+    print("----------------------------------------------------------")
+    print("|                    state dict pram                     |")
+    print("----------------------------------------------------------")
+    for param_tensor in model.state_dict():
+        print(param_tensor, '\t', model.state_dict()[param_tensor].size())
+    print("----------------------------------------------------------")
+
+
+#########################################################################
+############## HELPERS FUNCTIONS FOR TRAINING MODEL PARAMS ##############
+#########################################################################
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
