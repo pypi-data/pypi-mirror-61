c5bb8a9dd77f3d1fdb8910bff5ef6b2508e19f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:53 +0000

    Add package NlpToolkit-NamedEntityRecognition/1.0.4/NlpToolkit-NamedEntityRecognition-1.0.4.tar.gz

---
+++ packages/NlpToolkit-NamedEntityRecognition/NlpToolkit-NamedEntityRecognition-1.0.4.tar.gz/NlpToolkit-NamedEntityRecognition-1.0.4/NamedEntityRecognition/Gazetteer.py
@@ -0,0 +1,51 @@
+class Gazetteer:
+
+    __data: set
+    __name: str
+
+    def __init__(self, name: str, fileName: str):
+        """
+        A constructor for a specific gazetteer. The constructor takes name of the gazetteer and file name of the
+        gazetteer as input, reads the gazetteer from the input file.
+
+        PARAMETERS
+        ----------
+        name : str
+            Name of the gazetteer. This name will be used in programming to separate different gazetteers.
+        fileName : str
+            File name of the gazetteer data.
+        """
+        self.__name = name
+        self.__data = set()
+        inputFile = open(fileName, "r", encoding="utf8")
+        lines = inputFile.readlines()
+        for line in lines:
+            self.__data.add(line)
+
+    def getName(self) -> str:
+        """
+        Accessor method for the name of the gazetteer.
+
+        RETURNS
+        -------
+        str
+            Name of the gazetteer.
+        """
+        return self.__name
+
+    def contains(self, word: str) -> bool:
+        """
+        The most important method in Gazetteer class. Checks if the given word exists in the gazetteer. The check
+        is done in lowercase form.
+
+        PARAMETERS
+        ----------
+        word : str
+            Word to be search in Gazetteer.
+
+        RETURNS
+        -------
+        bool
+            True if the word is in the Gazetteer, False otherwise.
+        """
+        return word.lower() in self.__data
