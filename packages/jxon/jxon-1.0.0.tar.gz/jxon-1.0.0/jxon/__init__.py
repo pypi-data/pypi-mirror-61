51c2355fda0f7f88832af67706906e3f3b1d40e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:29 +0000

    Add package jxon/1.0.0/jxon-1.0.0.tar.gz

---
+++ packages/jxon/jxon-1.0.0.tar.gz/jxon-1.0.0/jxon/__init__.py
@@ -0,0 +1,6 @@
+from . import jxsd
+from .jxontype import JXONType
+from .combined import CombinedParser as JXONParser
+from .combined import load, loads, dump, dumps, jxon_equal
+
+__version__ = "1.0.0"
