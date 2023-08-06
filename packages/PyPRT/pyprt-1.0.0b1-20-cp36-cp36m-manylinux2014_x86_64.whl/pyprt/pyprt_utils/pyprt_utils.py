6b7802b11cabc36ae98408609f93f7448bd61c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:38 +0000

    Add package PyPRT/1.0.0b1/pyprt-1.0.0b1-20-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/PyPRT/pyprt-1.0.0b1-20-cp36-cp36m-manylinux2014_x86_64.whl/pyprt/pyprt_utils/pyprt_utils.py
@@ -0,0 +1,45 @@
+def visualize_prt_results(models):
+    print('\nNumber of generated geometries (= nber of initial shapes):')
+    print(len(models))
+
+    for m in models:
+        if m:
+            geometry_vertices = m.get_vertices()
+            rep = m.get_report()
+
+            print()
+            print('Initial Shape Index: ' + str(m.get_initial_shape_index()))
+
+            if len(geometry_vertices) > 0:
+                print()
+                print('Size of the model vertices vector: ' +
+                      str(len(geometry_vertices)))
+                print('Number of model vertices: ' + str(int(len(geometry_vertices)/3)))
+                print('Size of the model faces vector: ' +
+                      str(len(m.get_faces())))
+
+            if len(rep) > 0:
+                print()
+                print('Report of the generated model:')
+                print(rep)
+        else:
+            print('\nError while instanciating the model generator.')
+
+
+def vertices_vector_to_matrix(vertices):
+    vertices_as_matrix = []
+    for count in range(0, int(len(vertices)/3)):
+        vector_per_pt = [vertices[count*3],
+                         vertices[count*3+1], vertices[count*3+2]]
+        vertices_as_matrix.append(vector_per_pt)
+    return vertices_as_matrix
+
+
+def faces_indices_vectors_to_matrix(indices, faces):
+    faces_as_matrix = []
+    offset = 0
+    for f in faces:
+        ind_per_face = indices[offset:offset+f]
+        offset += f
+        faces_as_matrix.append(ind_per_face)
+    return faces_as_matrix
