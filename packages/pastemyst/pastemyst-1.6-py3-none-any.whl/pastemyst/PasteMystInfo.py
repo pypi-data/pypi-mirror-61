bbdcb05285abbc7430aa1ed8db372ffaef0d8c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:16 +0000

    Add package pastemyst/1.6/pastemyst-1.6-py3-none-any.whl

---
+++ packages/pastemyst/pastemyst-1.6-py3-none-any.whl/pastemyst/PasteMystInfo.py
@@ -0,0 +1,30 @@
+import datetime
+import json
+import urllib.parse
+
+class PasteMystInfo (object):
+    def __init__ (self, ID: str, CreatedAt: float, Code: str, ExpiresIn: str, Language: str, BaseURL: str):
+        self.ID = ID
+        self.CreatedAt = datetime.datetime.fromtimestamp (CreatedAt)
+        self.Code = urllib.parse.unquote (Code)
+        self.ExpiresIn = ExpiresIn
+        self.Language = Language
+
+        self.id = ID
+        self.created_at = CreatedAt
+        self.code = Code
+        self.expires_in = ExpiresIn
+        self.language = Language
+
+        self.URL = BaseURL + ID
+        self.url = BaseURL + ID
+
+    def __str__ (self):
+        return json.dumps ({
+            'id': self.ID,
+            'createdAt': self.CreatedAt,
+            'code': self.Code,
+            'expiresIn': self.ExpiresIn,
+            'language': self.language,
+            'url': self.URL
+        })
