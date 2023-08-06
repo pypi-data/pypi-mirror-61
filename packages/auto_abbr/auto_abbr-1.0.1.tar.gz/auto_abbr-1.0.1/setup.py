8377a5de9b163be8075dae5c702eade937c2480e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:07 +0000

    Add package auto_abbr/1.0.1/auto_abbr-1.0.1.tar.gz

---
+++ packages/auto_abbr/auto_abbr-1.0.1.tar.gz/auto_abbr-1.0.1/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name='auto_abbr',
+    version='1.0.1',
+    py_modules=['abbr'],
+    author='dandanlemuria',
+    author_email='18110980003@fudan.edu.cn',
+    url='https://github.com/LemuriaChen/auto_abbr',
+    description='a list of abbreviations for a given string', requires=['requests', 'lxml']
+)
