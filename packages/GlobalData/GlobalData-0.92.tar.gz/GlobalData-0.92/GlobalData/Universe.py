2a58906e3f057b5d2336048385f3f0e102781d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:13 +0000

    Add package GlobalData/0.92/GlobalData-0.92.tar.gz

---
+++ packages/GlobalData/GlobalData-0.92.tar.gz/GlobalData-0.92/GlobalData/Universe.py
@@ -0,0 +1,55 @@
+# Universe.py
+
+# from Globals import *
+#from GlobalData import *
+from GlobalData.Globals import *
+
+import os, time
+
+def Get(gd, x):
+    return gd.GetX(x)
+
+def GetCurrentDir():
+## first file in current dir (with full path)
+    file = os.path.join(os.getcwd(), os.listdir(os.getcwd())[0])
+    return os.path.dirname(file) ## directory of file
+
+class obj(dict):
+    def __init__(self):
+        self.exist = True
+        self.birth = time.time()
+        self.dict = {"Eyes":3, "exist":self.exist, "birth":self.birth}
+        self.gd = GD
+        self.root = GetCurrentDir()
+
+
+
+
+o = obj()
+print()
+print()
+print()
+print()
+print(getAllAtr(o))
+print()
+print(getDict(o))
+
+'''
+### DynamicData
+### Storage
+### Functionality
+### Load and Represent
+### Location
+### Inputs - Mouse, Keyboard, Screen
+### Draw
+###
+###
+###
+###
+###
+###
+###
+###
+###
+### SuperHearing
+'''
