4a42db69a06a1b1144a8a4ccdfe4fc542e26a864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:39 +0000

    Add package apispec-starlette/0.0.1/apispec_starlette-0.0.1-py3-none-any.whl

---
+++ packages/apispec-starlette/apispec_starlette-0.0.1-py3-none-any.whl/apispec_starlette/_starlette.py
@@ -0,0 +1,43 @@
+from apispec import APISpec
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+from apispec_starlette._plugin import StarlettePlugin
+
+
+def add_swagger_json_endpoint(
+    app: Starlette,
+    *,
+    title: str = "My API",
+    version: str = "0.0.1",
+    plugins: list = None,
+    **options
+) -> APISpec:
+    """
+    Create an APISpec instance and add a /swagger.json endpoint to return the OpenAPI definition 2.0 (Swagger).
+
+    Every application endpoint will be documented.
+
+    :param app: Starlette application.
+    :param title: OpenAPI definition title. Default to "My API".
+    :param version: OpenAPI definition version. Default to "0.0.1".
+    :param plugins: APISpec plugins to use in addition to the StarlettePlugin.
+    :param options: APISpec additional options.
+    :return: APISpec instance.
+    """
+    plugin = StarlettePlugin(app)
+    spec = APISpec(
+        title=title,
+        version=version,
+        openapi_version="2.0",
+        plugins=(plugins or []) + [plugin],
+        **options
+    )
+
+    @app.route("/swagger.json", include_in_schema=False)
+    def schema(request):
+        for endpoint in plugin.endpoints():
+            spec.path(path=endpoint.path, endpoint=endpoint)
+        return JSONResponse(spec.to_dict())
+
+    return spec
