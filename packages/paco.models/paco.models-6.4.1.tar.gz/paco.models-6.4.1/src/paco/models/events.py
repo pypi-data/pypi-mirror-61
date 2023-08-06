b6a2f702cb8fa41def2ab7a9ff1eac9a60908e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:51 +0000

    Add package paco.models/6.4.1/paco.models-6.4.1.tar.gz

---
+++ packages/paco.models/paco.models-6.4.1.tar.gz/paco.models-6.4.1/src/paco/models/events.py
@@ -0,0 +1,16 @@
+from paco.models import schemas
+from paco.models.base import Resource
+from zope.interface import implementer
+from zope.schema.fieldproperty import FieldProperty
+
+
+@implementer(schemas.IEventsRule)
+class EventsRule(Resource):
+    title = "CloudWatch Event Rule"
+    description = FieldProperty(schemas.IEventsRule['description'])
+    schedule_expression = FieldProperty(schemas.IEventsRule['schedule_expression'])
+    enabled_state = FieldProperty(schemas.IEventsRule['enabled_state'])
+
+    def __init__(self, name, parent):
+        super().__init__(name, parent)
+        self.targets = []
