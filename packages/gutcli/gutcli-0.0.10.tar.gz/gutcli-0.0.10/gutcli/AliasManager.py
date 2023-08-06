ef6b05049bb7c4333ad0a16e326e11242347fc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:45 +0000

    Add package gutcli/0.0.10/gutcli-0.0.10.tar.gz

---
+++ packages/gutcli/gutcli-0.0.10.tar.gz/gutcli-0.0.10/gutcli/AliasManager.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+GUT_DIRECTORY = ".gut"
+ALIAS_FILE = "aliases"
+ALIAS_KEY_TEMPLATE = "gut.%s"
+
+
+class AliasManager:
+
+    @staticmethod
+    def delete_by_name(alias_name):
+        alias_key = ALIAS_KEY_TEMPLATE % alias_name
+        lines = []
+        alias_file = "%s/%s/%s" % (str(Path.home()), GUT_DIRECTORY, ALIAS_FILE)
+        with open(alias_file, 'r') as file:
+            for line in file.readlines():
+                if not line.split('=')[0].split(' ')[1] == alias_key:
+                    lines.append(line)
+        file.close()
+
+        with open(alias_file, 'w') as file:
+            for line in lines:
+                file.write(line)
+            file.close();
+
+    @staticmethod
+    def add_alias(alias):
+        alias_file = "%s/%s/%s" % (str(Path.home()), GUT_DIRECTORY, ALIAS_FILE)
+
+        with open(alias_file, 'a+') as file:
+            file.write(alias)
+            file.close();
+
+    @staticmethod
+    def craft_alias(name, path):
+        template = ALIAS_KEY_TEMPLATE
+        key = ALIAS_KEY_TEMPLATE % name
+        return 'alias %s="cd %s"\n' % (key, path)
+
+    @staticmethod
+    def get_alias_keys():
+        aliases = []
+        alias_file = "%s/%s/%s" % (str(Path.home()), GUT_DIRECTORY, ALIAS_FILE)
+        with open(alias_file, 'r') as file:
+            for line in file.readlines():
+                aliases.append(line.split('=')[0].split(' ')[1])
+        file.close()
+        return aliases
+
+    @staticmethod
+    def ensure_file_exists():
+        alias_file_path = Path("%s/%s/%s" % (str(Path.home()), GUT_DIRECTORY, ALIAS_FILE))
+        if not alias_file_path.is_file():
+            alias_file_path.touch()
---
+++ packages/gutcli/gutcli-0.0.10.tar.gz/gutcli-0.0.10/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gutcli",
+    version="0.0.10",
+    author="Edgardo Gutierrez Jr",
+    author_email="edgardogutierrezjr@gmail.com",
+    description="Quick project navigator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gutiere/gutcli",
+    packages=setuptools.find_packages(),
+    entry_points ={
+        'console_scripts': [
+            'gut = gutcli.gut:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
