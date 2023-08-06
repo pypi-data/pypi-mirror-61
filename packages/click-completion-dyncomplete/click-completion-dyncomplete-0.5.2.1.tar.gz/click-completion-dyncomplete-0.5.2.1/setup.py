6a49b218b76859b3371a7577f4c33a30e75d20e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:22 +0000

    Add package click-completion-dyncomplete/0.5.2.1/click-completion-dyncomplete-0.5.2.1.tar.gz

---
+++ packages/click-completion-dyncomplete/click-completion-dyncomplete-0.5.2.1.tar.gz/click-completion-dyncomplete-0.5.2.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import ast
+import re
+
+from setuptools import setup, find_packages
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('click_completion_dyncomplete/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='click-completion-dyncomplete',
+    version=version,
+    description='Fish, Bash, Zsh and PowerShell completion for Click',
+    author='Gaëtan Lehmann',
+    author_email='gaetan.lehmann@gmail.com',
+    url='https://github.com/Kotaimen/click-completion',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'': ['*.j2']},
+    install_requires=[
+        'click',
+        'jinja2',
+        'six',
+        'shellingham',
+        'enum34; python_version<"3"',
+    ],
+)
