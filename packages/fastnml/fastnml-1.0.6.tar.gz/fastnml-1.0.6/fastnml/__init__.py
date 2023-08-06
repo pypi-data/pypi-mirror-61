ea65f79fbbc2f2d2c22436e336d425e5a4484125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:26 +0000

    Add package fastnml/1.0.6/fastnml-1.0.6.tar.gz

---
+++ packages/fastnml/fastnml-1.0.6.tar.gz/fastnml-1.0.6/fastnml/__init__.py
@@ -0,0 +1,23 @@
+__appname__ = "fastnml"
+__version__ = '1.0.6'
+__credits__ = ["Jacob Williams", "Randy Eckman"]
+__license__ = "BSD"
+
+import f90nml  # noqa: E402
+assert f90nml
+
+from .reader import (
+    read_namelist,
+)  # noqa: E402
+assert all((
+    read_namelist,
+))
+
+from .writer import (
+    save_namelist,
+    write_namelist_to_stream,
+)  # noqa: E402
+assert all((
+    save_namelist,
+    write_namelist_to_stream,
+))
