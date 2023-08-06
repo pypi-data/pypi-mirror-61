085108e1719f80dc9e9a6d10d4b95d008bf3ed2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package basic-wrangler/0.7.1/basic_wrangler-0.7.1-py3-none-any.whl

---
+++ packages/basic-wrangler/basic_wrangler-0.7.1-py3-none-any.whl/basicwrangler/common/constants.py
@@ -0,0 +1,10 @@
+""" This module contains constants accessed by other modules. """
+
+RE_QUOTES = r'(?=([^"]*"[^"]*")*[^"]*$)'  # this selects things NOT inside quotes
+NO_TOKENIZER = [
+    "generic",
+    "freebasic",
+    "qbasic",
+    "gwbasic",
+]
+CBM_BASIC = ["pet", "vic20", "c64", "plus4", "c128"]
