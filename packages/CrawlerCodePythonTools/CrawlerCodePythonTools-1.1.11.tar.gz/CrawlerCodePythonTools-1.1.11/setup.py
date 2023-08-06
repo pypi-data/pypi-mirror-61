634635f2fe80cb9ff2ef1dfbaeb3409b5ff43c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:33 +0000

    Add package CrawlerCodePythonTools/1.1.11/CrawlerCodePythonTools-1.1.11.tar.gz

---
+++ packages/CrawlerCodePythonTools/CrawlerCodePythonTools-1.1.11.tar.gz/CrawlerCodePythonTools-1.1.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open("README.rst") as f:
+        README = f.read()
+    return README
+
+setup(
+    name='CrawlerCodePythonTools',
+    version='1.1.11',
+    packages=['pythontools.core', 'pythontools.identity', 'pythontools.sockets', 'pythontools.telegrambot', 'pythontools.dev', 'pythontools.webbot', 'pythontools.gui'],
+    url='https://github.com/CrawlerCode',
+    license='',
+    author='CrawlerCode',
+    author_email='',
+    description='',
+    long_description=readme(),
+    long_description_content_type="text/x-rst",
+    include_package_data=True,
+    install_requires=["colorama", "telegram", "python-telegram-bot", "PyQt5", "selenium"]
+)
