96e50c30b3f9a8c98d8d7e57f1b7d1727a5f726c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:53 +0000

    Add package MissingValues-101703292/1.0.3/MissingValues_101703292-1.0.3.tar.gz

---
+++ packages/MissingValues-101703292/MissingValues_101703292-1.0.3.tar.gz/MissingValues_101703292-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="MissingValues_101703292",
+    version="1.0.3",
+    description="A Python package to handle missing values in the dataset",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Kriti Pandey",
+    author_email="kritip105@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["MISSING_3292"],
+    include_package_data=True,
+    install_requires=["numpy","pandas"],
+    entry_points={
+        "console_scripts": [
+            "MISSING-3292=MISSING_3292.missingval_3292:main",
+        ]
+    },
+)
