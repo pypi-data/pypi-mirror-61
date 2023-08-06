4343153bc246953f073fae0454b7f84fe779937e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:01 +0000

    Add package Flask-ACSecure/0.0.2/Flask-ACSecure-0.0.2.tar.gz

---
+++ packages/Flask-ACSecure/Flask-ACSecure-0.0.2.tar.gz/Flask-ACSecure-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+import flask_acsecure as app
+
+with open('README.md', 'r') as f:
+  longDescription = f.read()
+
+with open('requirements.txt', 'r') as f:
+  install_requires = f.read().split('\n')
+
+setup(
+  name='Flask-ACSecure',
+  version=app.__version__,
+  description='Access Secure Flask Middleware',
+  long_description=longDescription,
+  long_description_content_type='text/markdown',
+  license='MIT',
+  platforms=['OS Independent'],
+  keywords='Flask, log, anti-spam',
+  author='Xin Jin',
+  author_email='opposcript@gmail.com',
+  url='https://github.com/TrentaIcedCoffee/access-secure',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  install_requires=install_requires,
+  python_requires='>=3.5',
+)
