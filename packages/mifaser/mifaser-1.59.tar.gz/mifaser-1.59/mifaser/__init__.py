fb28447dedccc8fe66f3c10ebffb23b770563aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:59 +0000

    Add package mifaser/1.59/mifaser-1.59.tar.gz

---
+++ packages/mifaser/mifaser-1.59.tar.gz/mifaser-1.59/mifaser/__init__.py
@@ -0,0 +1,13 @@
+name = "mifaser"
+__author__ = 'mmiller'
+__version__ = '1.59'
+__releasedate__ = '02/20/20'
+__all__ = [
+    'alignment',
+    'annotation',
+    'caller',
+    'cli',
+    'external',
+    'run',
+    'translate'
+]
