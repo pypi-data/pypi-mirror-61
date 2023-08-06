e8bf2595db117261ff82bde586a789b6b108eecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:27 +0000

    Add package from-dict/0.2.0/from-dict-0.2.0.tar.gz

---
+++ packages/from-dict/from-dict-0.2.0.tar.gz/from-dict-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="from-dict",
+    version="0.2.0",
+    packages=find_packages(),
+    author="Wanja Chresta",
+    author_email="wanja.hs@chrummibei.ch",
+    url="https://github.com/wchresta/from-dict",
+    description="Create data structures from dictionaries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    tests_require=["pytest", "attr"],
+)
