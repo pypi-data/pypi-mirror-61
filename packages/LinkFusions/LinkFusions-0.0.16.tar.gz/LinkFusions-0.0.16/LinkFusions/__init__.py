6987da317fe85d0ba80ef70c4eb142cd382d0593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:08 +0000

    Add package LinkFusions/0.0.16/LinkFusions-0.0.16.tar.gz

---
+++ packages/LinkFusions/LinkFusions-0.0.16.tar.gz/LinkFusions-0.0.16/LinkFusions/__init__.py
@@ -0,0 +1,77 @@
+__version__ = '0.0.16'
+
+__dev__ = 'live'
+# This could be 'local', 'dev', 'live'
+
+CLIENT_ID = None
+CLIENT_SECRET = None
+USER = None
+PASSWORD = None
+
+TOKEN = None
+
+
+def get_url(url):
+    if __dev__ == 'local':
+        base_url = 'http://localhost:8000'
+    elif __dev__ == 'dev':
+        base_url = 'https://dev.linkfusions.com'
+    else:
+        base_url = 'https://app.linkfusions.com'
+    return base_url + url
+
+
+def get_credentials():
+    return {
+        'grant_type': 'password',
+        'username': USER,
+        'password': PASSWORD,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }
+
+
+def get_token():
+    return TOKEN
+
+
+class LinkFusions(object):
+    def __init__(self):
+        from .exceptions import BadRequest
+        from .campaigns import EmailChannel, Campaign, MMSChannel, SMSChannel, VoiceChannel
+        from .phone_numbers import Numbers, Redirect
+        from .templates import Template
+        from .contacts import Contact, ContactGroup
+
+        if not get_token():
+            global TOKEN
+            TOKEN = self.__class__.auth().get('access_token')
+        self.EmailChannel = EmailChannel(get_token())
+        self.Campaign = Campaign(get_token())
+        self.Numbers = Numbers(get_token())
+        self.Redirect = Redirect(get_token())
+        self.MMSChannel = MMSChannel(get_token())
+        self.SMSChannel = SMSChannel(get_token())
+        self.VoiceChannel = VoiceChannel(get_token())
+        self.Template = Template(get_token())
+        self.Contact = Contact(get_token())
+        self.ContactGroup = ContactGroup(get_token())
+
+    @classmethod
+    def auth(cls):
+        from .requestor import ApiRequestor
+        from .exceptions import BadRequest, InvalidCredentialsException
+        ep = '/o/token/'
+        data = get_credentials()
+        try:
+            return ApiRequestor(endpoint=ep, data=data).send(as_json=False)
+        except BadRequest as e:
+            raise InvalidCredentialsException(str(e))
+
+
+__all__ = [
+    'LinkFusions',
+    'get_url',
+    '__dev__',
+    '__version__'
+]
---
+++ packages/LinkFusions/LinkFusions-0.0.16.tar.gz/LinkFusions-0.0.16/LinkFusions/test.py
@@ -0,0 +1,18 @@
+# import json
+# import os
+#
+import os
+
+import LinkFusions as lf
+
+lf.CLIENT_SECRET = 'RrAcveMhXdQC7rPMTqqzuLbe05zRtEVIM6HmV3U8DxlXTnJW3QPDQBZAymAh6TBmV9O1ie9U21xxrfhGVbBL5CuPQm5YaSxyImxcJIIBKOkAg28dlQZKWsnR2Mgb5MAb'
+lf.CLIENT_ID = 'GPj7I9FxcybyHCEeZ50UQ84LMmjhTOU0MzlIIhjh'
+lf.USER = 'admin@localhost.com'
+lf.PASSWORD = 'greatness2011'
+
+lf.__dev__ = 'local'
+
+fusion = lf.LinkFusions()
+file = open(os.path.join(os.path.dirname(__file__), 'sample', 'me.png'), 'rb')
+fusion.Campaign.create(name='Damilola Next Another Lib', logo=file, company='Cant say')
+# fusion.Campaign.list()
