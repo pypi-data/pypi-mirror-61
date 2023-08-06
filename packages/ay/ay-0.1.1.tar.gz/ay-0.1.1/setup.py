c0013929ea16283823d419e38505e266882d2c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:07 +0000

    Add package ay/0.1.1/ay-0.1.1.tar.gz

---
+++ packages/ay/ay-0.1.1.tar.gz/ay-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ay",
+    version="0.1.1",
+    author="Ruben Afonso",
+    author_email="rbfrancos@gmail.com",
+    description="Tiny framework for generative art using Python",
+    url="https://github.com/rubenafo/ay",
+    keywords = ["generative art", "cairo", "generative", "python", "design"],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = ['colour']
+)
