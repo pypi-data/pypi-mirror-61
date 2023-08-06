8bdeb3036ddc0c7ee0120c9d648d60e335dc1bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:33 +0000

    Add package nanome-docking/0.2.1/nanome_docking-0.2.1-py2.py3-none-any.whl

---
+++ packages/nanome-docking/nanome_docking-0.2.1-py2.py3-none-any.whl/nanome_docking/_DockingCalculations.py
@@ -0,0 +1,239 @@
+import traceback
+import math
+import re
+import nanome
+import shutil
+import gzip
+import itertools
+import operator
+import os
+import subprocess
+import tempfile
+import stat
+from timeit import default_timer as timer
+from functools import partial
+
+from nanome.util.enums import NotificationTypes
+
+from .ComplexUtils import ComplexUtils
+
+DEBUG = True
+
+SDFOPTIONS = nanome.api.structure.Complex.io.SDFSaveOptions()
+SDFOPTIONS.write_bonds = True
+PDBOPTIONS = nanome.api.structure.Complex.io.PDBSaveOptions()
+PDBOPTIONS.write_bonds = True
+
+SMINA_PATH = os.path.join(os.path.dirname(__file__), 'smina')
+try:
+    os.chmod(SMINA_PATH, stat.S_IXGRP | stat.S_IEXEC)
+except:
+    pass
+
+class DockingCalculations():
+    def __init__(self, plugin):
+        self._plugin = plugin
+        self._request_pending = False
+        self._running = False
+        self._structures_written = False
+        self._started_docking = False
+        self.requires_site = True
+
+
+    def initialize(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self._receptor_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._ligands_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._site_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._docking_output = tempfile.NamedTemporaryFile(delete=False, prefix="output", suffix=".sdf", dir=self.temp_dir.name)
+        self._ligand_output = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._log_file = tempfile.NamedTemporaryFile(delete=False, dir=self.temp_dir.name)
+
+    def start_docking(self, receptor, ligands, site, exhaustiveness, modes, align, replace, scoring, visual_scores, autobox):
+        self._exhaustiveness = exhaustiveness
+        self._modes = modes
+        self._receptor = receptor
+        self._ligands = ligands
+        self._combined_ligands = ComplexUtils.combine_ligands(receptor, ligands)
+        self._site = site
+        self._align = align
+        self._replace = replace
+        self._scoring = scoring
+        self._visual_scores = visual_scores
+        self._autobox = autobox
+
+        # Start docking process
+        self._running = False
+        self._structures_written = False
+        self._started_docking = False
+        self._request_pending = True
+
+    def update(self):
+        if self._request_pending == False:
+            return
+
+        if not self._running and not self._structures_written:
+            self.write_structures_to_file()
+            self._structures_written = True
+        elif not self._started_docking:
+            self._start_docking()
+            self._started_docking = True
+        elif self._check_docking():
+            self._docking_finished()
+
+    def write_structures_to_file(self):
+        self.initialize()
+         # Save all input files
+        self._receptor.io.to_pdb(self._receptor_input.name, PDBOPTIONS)
+        nanome.util.Logs.debug("Saved PDB", self._receptor_input.name)
+        self._combined_ligands.io.to_pdb(self._ligands_input.name, PDBOPTIONS)
+        nanome.util.Logs.debug("Saved PDB", self._ligands_input.name)
+        self._site.io.to_pdb(self._site_input.name, PDBOPTIONS)
+        nanome.util.Logs.debug("Saved PDB", self._site_input.name)
+
+    def _check_conversion(self):
+        poll = self._obabel_process.poll()
+        if poll == None:
+            self._obabel_process.communicate()
+        return poll != None
+
+    def _start_docking(self):
+        exe_path = os.path.join(os.path.dirname(__file__), 'smina')
+
+        if self._scoring:
+            smina_args = [exe_path, '-r', self._receptor_input.name, '-l', self._ligands_input.name, '--autobox_ligand', self._site_input.name, '--score_only', '--out', self._ligand_output.name]
+        else:
+
+            smina_args = [exe_path, '-r', self._receptor_input.name, '-l', self._ligands_input.name, '--autobox_ligand', self._site_input.name, '--out', \
+                self._docking_output.name, '--log', self._log_file.name, '--exhaustiveness', str(self._exhaustiveness), '--num_modes', str(self._modes), '--autobox_add', str(self._autobox), '--atom_term_data']
+
+        nanome.util.Logs.debug("Run SMINA")
+        self._start_timer = timer()
+        try:
+            self._smina_process = subprocess.Popen(smina_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except:
+            nanome.util.Logs.error("Couldn't execute smina, please check if executable is in the plugin folder and has permissions. Path:", SMINA_PATH)
+            self._request_pending = False
+            self._running = False
+            self._plugin.make_plugin_usable()
+            self._plugin.send_notification(NotificationTypes.error, "Docking error, check plugin")
+            return
+
+        self._running = True
+        self._plugin.send_notification(NotificationTypes.message, "Docking started")
+
+    def _check_docking(self):
+        return self._smina_process.poll() != None
+
+    def readd_bonds(self, complex, callback):
+        for residue in complex.residues:
+            for bond in residue.bonds:
+                residue.remove_bond(bond)
+            for atom in residue.atoms:
+                atom._bonds = []
+
+    def _resume_docking_finished(self, docking_results):
+        docking_results = docking_results[0]
+        nanome.util.Logs.debug("Read SDF", self._docking_output.name)
+        for i, molecule in enumerate(docking_results.molecules):
+            nanome.util.Logs.debug("molecule " + str(i))
+            self.set_scores(i, molecule)
+
+        docking_results.set_current_frame(0)
+
+        if self._visual_scores:
+            self.visualize_scores(docking_results)
+
+        if not self._scoring:
+            if len(self._combined_ligands.names) > 1:
+                docking_results.name += "Docking Results"
+            elif len(self._combined_ligands.names) == 1:
+                docking_results.name = self._combined_ligands.names[0] + " (Docked)"
+            docking_results.visible = True
+
+        # # TODO: Check this for readd_bonds param correctness
+        # replace_conformer = partial(self._plugin.replace_conformer, callback=partial(self._plugin.add_result_to_workspace, align=self._align))
+        # self.readd_bonds(docking_results, replace_conformer)
+
+        # self._plugin.add_bonds([complex], callback)
+
+        if self._scoring:
+            nanome.util.Logs.debug("Display scoring result")
+            self._plugin.display_scoring_result(docking_results)
+        else:
+            nanome.util.Logs.debug("Update workspace")
+            self._plugin.add_result_to_workspace([docking_results], self._align)
+
+        shutil.rmtree(self.temp_dir.name)
+
+        self._plugin.send_notification(NotificationTypes.success, "Docking finished")
+
+    def make_ligands_invisible(self):
+        for ligand in self._ligands:
+            ligand.visible = False
+        self._plugin.update_structures_shallow(self._ligands)
+
+    def _docking_finished(self):
+        end = timer()
+        nanome.util.Logs.debug("Docking Finished in", end - self._start_timer, "seconds")
+        self._request_pending = False
+
+        self.make_ligands_invisible()
+
+        docking_results = nanome.structure.Complex.io.from_sdf(path=self._docking_output.name)
+        docking_results.index = -1
+        if not self._scoring:
+            docking_results.visible = True
+            docking_results.locked = True
+            if len(self._combined_ligands.names) > 1:
+                docking_results.name += "Docking Results"
+            elif len(self._combined_ligands.names) == 1:
+                docking_results.name = self._combined_ligands.names[0] + " (Docked)"
+
+        self._plugin.replace_conformer([docking_results], self._resume_docking_finished, existing=False)
+
+    def update_min_max_scores(self, molecule, score):
+        min_score = molecule.min_atom_score
+        max_score = molecule.max_atom_score
+        molecule.min_atom_score = score if score < min_score else min_score
+        molecule.max_atom_score = score if score > max_score else max_score
+
+    def set_scores(self, lig_i, molecule):
+        molecule.min_atom_score = float('inf')
+        molecule.max_atom_score = float('-inf')
+
+        num_rgx = '(-?[\d.]+(?:e[+-]\d+)?)'
+        pattern = re.compile('<{},{},{}> {} {} {} {} {}'.format(*([num_rgx] * 8)), re.U)
+        for associated in molecule.associateds:
+            pose_score = associated['> <minimizedAffinity>']
+            for residue in molecule.residues:
+                residue.label_text = pose_score + " kcal/mol"
+                residue.labeled = True
+            interaction_terms = associated['> <atomic_interaction_terms>']
+            interaction_values = re.findall(pattern, interaction_terms)
+            atom_count = len([atom for atom in molecule.atoms])
+            for i, atom in enumerate(molecule.atoms):
+                if i < len(interaction_values) - 1:
+                    nanome.util.Logs.debug("interaction values for atom " + str(i) + ": "+ str(interaction_values[i]))
+                    atom.score = float(interaction_values[i][5])
+                    self.update_min_max_scores(molecule, atom.score)
+
+
+    def visualize_scores(self, ligand_complex):
+        for molecule in ligand_complex.molecules:
+            for atom in molecule.atoms:
+                if hasattr(atom, "score"):
+                    atom.atom_mode = nanome.api.structure.Atom.AtomRenderingMode.Point
+                    denominator = -molecule.min_atom_score if atom.score < 0 else molecule.max_atom_score
+                    norm_score = atom.score / denominator
+                    atom.atom_scale = norm_score * 1.5 + 0.1
+                    atom.label_text = self.truncate(atom.score, 3)
+                    # atom.labeled = True
+
+    def truncate(self, f, n):
+        '''Truncates/pads a float f to n decimal places without rounding'''
+        s = '{}'.format(f)
+        if 'e' in s or 'E' in s:
+            return '{0:.{1}f}'.format(f, n)
+        i, p, d = s.partition('.')
+        return '.'.join([i, (d+'0'*n)[:n]])
---
+++ packages/nanome-docking/nanome_docking-0.2.1-py2.py3-none-any.whl/nanome_docking/_DockingMenu.py
@@ -0,0 +1,354 @@
+import nanome
+from nanome.api.ui.image import Image as Image
+
+import os
+
+class DockingMenu():
+    def __init__(self, docking_plugin):
+        self._plugin = docking_plugin
+        self._selected_receptor = None
+        self._selected_ligands = []
+        self._selected_site = None
+        self._exhaustiveness = 10
+        self._modes = 5
+        self._autobox = 4
+        self._run_button = None
+        self._run_button = None
+        self._align = True
+        self._replace = False
+        self._scoring = False
+        self._visual_scores = False
+        self._tab = None
+        self._autobox_enabled = True
+    
+    def get_params(self):
+        params = {"exhaustiveness": None, "modes": None, "align": None, "replace": None, "scoring": None, "autobox": None}
+        for key, value in params.items():
+            newvalue = getattr(self, "_"+key)
+            params[key] = newvalue
+        return params
+
+    def get_receptor(self):
+        return self._selected_receptor.complex
+
+    def get_ligands(self):
+        ligands = []
+        for item in self._selected_ligands:
+            ligands.append(item.complex)
+        return ligands
+
+    def get_site(self):
+        if self._selected_site == None:
+            return None
+        return self._selected_site.complex
+
+    def get_params(self):
+        params = {"exhaustiveness": None, "modes": None, "align": None, "replace": None, "scoring": None, "visual_scores": None, "autobox": None}
+        for key, value in params.items():
+            newvalue = getattr(self, "_"+key)
+            params[key] = newvalue
+        return params
+
+    def _run_docking(self):
+        if self._selected_receptor == None or len(self._selected_ligands) == 0:
+            if self._autobox_enabled == True and self._selected_site == None:
+                nanome.util.Logs.warning("Trying to run docking without having one receptor, one site and at least one ligand selected")
+                return
+        ligands = []
+        for item in self._selected_ligands:
+            ligands.append(item.complex)
+        site = None
+        if self._autobox_enabled:
+            site = self._selected_site.complex
+        self._plugin.run_docking(self._selected_receptor.complex, ligands, site, self.get_params())
+
+    def disable_autobox(self):
+        self._site_btn.unusable = True
+        self._score_btn.unusable = True
+        self._txt1.unusable = True # Doesn't do anything for now
+        self._txt2.unusable = True
+        self._txt3.unusable = True
+        self._autobox_enabled = False
+        self._plugin.update_menu(self._menu)
+
+    def make_plugin_usable(self, state=True):
+        self._run_button.unusable = (not state) | self.refresh_run_btn_unusable(False)
+        self._plugin.update_content(self._run_button)
+
+    def receptor_pressed(self, button):
+        lastSelected = self._selected_receptor
+        if lastSelected != None:
+            lastSelected.selected = False
+            self._plugin.update_content(lastSelected)
+        button.selected = True
+        self._selected_receptor = button
+        self._plugin.update_content(button)
+        self._receptor_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'checkmark.png')
+        self._plugin.update_content(self._receptor_checkmark)
+
+        self.refresh_run_btn_unusable()
+
+    def ligand_pressed(self, button):
+        if button.selected == False:
+            button.selected = True
+            self._selected_ligands.append(button)
+        else:
+            button.selected = False
+            self._selected_ligands.remove(button)
+        self._plugin.update_content(button)
+        if len(self._selected_ligands) != 0:
+            self._ligand_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'checkmark.png')
+        else:
+            self._ligand_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'none.png')
+        self._plugin.update_content(self._ligand_checkmark)
+
+        self.refresh_run_btn_unusable()
+
+    def site_pressed(self, button):
+        lastSelected = self._selected_site
+        if lastSelected != None:
+            lastSelected.selected = False
+            self._plugin.update_content(lastSelected)
+        button.selected = True
+        self._selected_site = button
+        self._plugin.update_content(button)
+        self._site_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'checkmark.png')
+        self._plugin.update_content(self._site_checkmark)
+
+        self.refresh_run_btn_unusable()
+
+    def refresh_run_btn_unusable(self, update=True):
+        site_requirement_met = self._selected_site != None or not self._plugin._calculations.requires_site
+        if self._selected_receptor != None and len(self._selected_ligands) > 0 and site_requirement_met:
+            self._run_button.text.value_unusable = "Running..."
+            self._run_button.unusable = False
+        else:
+            self._run_button.text.value_unusable = "Run"
+            self._run_button.unusable = True
+
+        if update:
+            self._plugin.update_content(self._run_button)
+
+        return self._run_button.unusable
+
+    def change_complex_list(self, complex_list):
+        def complex_pressed(button):
+            if self._tab.text.value_idle == "Receptor":
+                self.receptor_pressed(button)
+            elif self._tab.text.value_idle == "Ligand":
+                self.ligand_pressed(button)
+            elif self._tab.text.value_idle == "Site":
+                self.site_pressed(button)
+
+        self.reset(update_menu=False)
+
+        for complex in complex_list:
+            clone = self._complex_item_prefab.clone()
+            ln_btn = clone.get_children()[0]
+            btn = ln_btn.get_content()
+            btn.set_all_text(complex.full_name)
+            btn.complex = complex
+            btn.register_pressed_callback(complex_pressed)
+            self._complex_list.items.append(clone)
+
+        self._plugin.update_menu(self._menu)
+
+    def display_scoring_result(self, result):
+        self.reset()
+
+        for molecule in result.molecules:
+            clone = self._score_item_prefab.clone()
+            ln_lbl = clone.get_children()[0]
+            lbl = ln_lbl.get_content()
+            lbl.text_value = molecule.name + " - " + molecule._associated["> <minimizedAffinity>"]
+            self._score_list.items.append(clone)
+
+    def reset(self, update_menu=True):
+        self._selected_receptor = None
+        self._selected_ligands = []
+        self._selected_site = None
+        self._complex_list.items = []
+        self._receptor_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'none.png')
+        self._ligand_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'none.png')
+        self._site_checkmark.file_path = os.path.join(os.path.dirname(__file__), 'none.png')
+
+        self.make_plugin_usable()
+        self._plugin.update_menu(self._menu)
+
+    def open_menu(self):
+        self._plugin.menu = self._menu
+        self._plugin.menu.enabled = True
+        self._plugin.update_menu(self._plugin.menu)
+
+    def build_menu(self):
+        # defining callbacks
+        def run_button_pressed_callback(button):
+            if self._scoring:
+                self._docking_param_panel.enabled = False
+                self._score_panel.enabled = True
+                self._score_list.items = []
+                self._plugin.update_menu(self._menu)
+            self._run_docking()
+
+        def exhaustiveness_changed(input):
+            try:
+                self._exhaustiveness = int(input.input_text)
+                nanome.util.Logs.debug("Exhaustiveness set to", self._exhaustiveness)
+            except:
+                self._exhaustiveness = 8
+            if self._exhaustiveness <= 0:
+                self._exhaustiveness = 8
+
+        def modes_changed(input):
+            try:
+                self._modes = int(input.input_text)
+                nanome.util.Logs.debug("Modes number set to", self._modes)
+            except:
+                self._modes = 9
+            if self._modes <= 0:
+                self._modes = 9
+
+        def autobox_changed(input):
+            try:
+                self._autobox = int(input.input_text)
+                nanome.util.Logs.debug("Autobox size set to", self._autobox)
+            except:
+                self._autobox = 4
+            if self._autobox <= 0:
+                self._autobox = 4
+
+        def tab_button_pressed_callback(button):
+            if self._tab == button:
+                return
+
+            self._tab.selected = False
+            button.selected = True
+            self._tab = button
+
+            if button.text.value_idle == "Receptor":
+                for item in self._complex_list.items:
+                    btn = item.get_children()[0].get_content()
+                    if btn == self._selected_receptor:
+                        btn.selected = True
+                    else:
+                        btn.selected = False
+            elif button.text.value_idle == "Site":
+                for item in self._complex_list.items:
+                    btn = item.get_children()[0].get_content()
+                    if btn == self._selected_site:
+                        btn.selected = True
+                    else:
+                        btn.selected = False
+            elif button.text.value_idle == "Ligand":
+                for item in self._complex_list.items:
+                    btn = item.get_children()[0].get_content()
+                    if btn in self._selected_ligands:
+                        btn.selected = True
+                    else:
+                        btn.selected = False
+
+            self._plugin.update_menu(self._menu)
+
+        def align_button_pressed_callback(button):
+            self._align = not self._align
+            button.selected = self._align
+            self._plugin.update_content(button)
+
+        def close_score_pressed_callback(button):
+            self._docking_param_panel.enabled = True
+            self._score_panel.enabled = False
+            self._plugin.update_menu(self._menu)
+
+        def scoring_button_pressed_callback(button):
+            self._scoring = not self._scoring
+            button.selected = self._scoring
+            self._plugin.update_content(button)
+
+        def visual_scores_button_pressed_callback(button):
+            self._visual_scores = not self._visual_scores
+            button.selected = self._visual_scores
+            self._plugin.update_content(button)
+
+        # Create a prefab that will be used to populate the lists
+        self._complex_item_prefab = nanome.ui.LayoutNode()
+        self._complex_item_prefab.layout_orientation = nanome.ui.LayoutNode.LayoutTypes.horizontal
+        child = self._complex_item_prefab.create_child_node()
+        child.add_new_button()
+
+        self._score_item_prefab = nanome.ui.LayoutNode()
+        self._score_item_prefab.layout_orientation = nanome.ui.LayoutNode.LayoutTypes.horizontal
+        child = self._score_item_prefab.create_child_node()
+        # child.forward_dist = 0.002
+        child.add_new_label()
+
+        # loading menus
+        menu = nanome.ui.Menu.io.from_json(os.path.join(os.path.dirname(__file__), '_docking_menu.json'))
+        self._plugin.menu = menu
+
+        # registering and saving special nodes
+
+        # panels
+        self._docking_param_panel = menu.root.find_node("LeftSide", True)
+        self._score_panel = menu.root.find_node("LeftSideScore", True)
+
+        # images
+        none_path = os.path.join(os.path.dirname(__file__), 'none.png')
+        self._receptor_checkmark = menu.root.find_node("ReceptorIcon", True).add_new_image(none_path)
+        self._receptor_checkmark.scaling_option = Image.ScalingOptions.fit
+        self._ligand_checkmark = menu.root.find_node("LigandIcon", True).add_new_image(none_path)
+        self._ligand_checkmark.scaling_option = Image.ScalingOptions.fit
+        self._site_checkmark = menu.root.find_node("SiteIcon", True).add_new_image(none_path)
+        self._site_checkmark.scaling_option = Image.ScalingOptions.fit
+
+        # texts
+        self._txt1 = menu.root.find_node("ExhaustivenessInput", True).get_content()
+        self._txt1.register_changed_callback(exhaustiveness_changed)
+
+        self._txt2 = menu.root.find_node("ModesInput", True).get_content()
+        self._txt2.register_changed_callback(modes_changed)
+
+        self._txt3 = menu.root.find_node("AutoboxInput", True).get_content()
+        self._txt3.register_changed_callback(autobox_changed)
+
+        # buttons
+        receptor_btn = menu.root.find_node("ReceptorButton", True).get_content()
+        receptor_btn.register_pressed_callback(tab_button_pressed_callback)
+        receptor_btn.selected = True
+        self._tab = receptor_btn
+
+        ligand_btn = menu.root.find_node("LigandButton", True).get_content()
+        ligand_btn.register_pressed_callback(tab_button_pressed_callback)
+
+        self._site_btn = menu.root.find_node("SiteButton", True).get_content()
+        self._site_btn.register_pressed_callback(tab_button_pressed_callback)
+
+        align_btn = menu.root.find_node("AlignButton", True).get_content()
+        align_btn.register_pressed_callback(align_button_pressed_callback)
+        align_btn.selected = True
+
+        self._score_btn = menu.root.find_node("ScoringButton", True).get_content()
+        self._score_btn.register_pressed_callback(scoring_button_pressed_callback)
+
+        self._display_score_btn = menu.root.find_node("VisualScoresButton", True).get_content()
+        self._display_score_btn.register_pressed_callback(visual_scores_button_pressed_callback)
+
+        close_score_btn = menu.root.find_node("CloseScoreButton", True).get_content()
+        close_score_btn.register_pressed_callback(close_score_pressed_callback)
+
+        run_button = menu.root.find_node("RunButton", True).get_content()
+        run_button.register_pressed_callback(run_button_pressed_callback)
+        self._run_button = run_button
+        self._run_button.enabled = False
+        self.refresh_run_btn_unusable()
+
+        # lists
+        self._complex_list = menu.root.find_node("ComplexList", True).get_content()
+        self._score_list = menu.root.find_node("ScoreList", True).get_content()
+        self._receptor_tab = menu.root.find_node("ReceptorButton", True).get_content()
+        self._ligand_tab = menu.root.find_node("LigandButton", True).get_content()
+        self._site_tab = menu.root.find_node("SiteButton", True).get_content()
+
+        # Update the menu
+        self._menu = menu
+        self._plugin.update_menu(menu)
+        nanome.util.Logs.debug("Constructed plugin menu")
