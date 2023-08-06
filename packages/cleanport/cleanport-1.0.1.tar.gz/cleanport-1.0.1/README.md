f6a841b0e99f65c20c4aa572445daad4c44a8726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:52 +0000

    Add package cleanport/1.0.1/cleanport-1.0.1.tar.gz

---
+++ packages/cleanport/cleanport-1.0.1.tar.gz/cleanport-1.0.1/README.md
@@ -0,0 +1,9 @@
+CleanPort scripts cleans the cleans port that are being used right now as non root user.
+
+### Usage 
+cleanport port_number 
+#### Example :
+##### cleanport 4200
+##### cleanport 8080
+
+
---
+++ packages/cleanport/cleanport-1.0.1.tar.gz/cleanport-1.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here,"README.md"),encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+        name="cleanport",
+        version="1.0.1",
+        author="Shamsher Ahmed",
+        author_email='hi@shamsherahmed.in',
+        description="A package to free port for non root owner only",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        scripts=['src/cleanport'],
+        url="https://github.com/shamsid/cleanport",
+        package_dir={'': 'src'},
+        packages=setuptools.find_packages(where='src'),
+        keywords='port-clean process-kill development',
+        python_requires='>=3.7.5',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            'License :: OSI Approved :: MIT License',
+            "Operating System :: OS Independent",
+        ],
+        project_urls={  # Optional
+                'Bug Reports': 'https://github.com/shamsid/cleanport/issues',
+                'Source': 'https://github.com/shamsid/cleanport',
+        },
+)
+
+
