949c8aa6c19f86ec0f840956907344b0cc9ec38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:06 +0000

    Add package py-trade-signal/1.0.3/py_trade_signal-1.0.3-py3-none-any.whl

---
+++ packages/py-trade-signal/py_trade_signal-1.0.3-py3-none-any.whl/py_trade_signal/psar.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from py_trade_signal.exception import TradeSignalException
+from pandas.errors import EmptyDataError
+from finta import TA
+
+
+class PSAR:
+    """
+    https://www.investorsunderground.com/technical-indicators/parabolic-sar/
+
+    https://www.investopedia.com/trading/introduction-to-parabolic-sar/
+
+    """
+
+    __module__ = "py_trade_signal"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def buy(dataframe):
+        pass
+
+    @staticmethod
+    def sell(dataframe):
+        pass
