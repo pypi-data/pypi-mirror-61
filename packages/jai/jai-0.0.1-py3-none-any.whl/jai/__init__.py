ddc464335aa5572ceb7cf2820ef51770890096e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:31 +0000

    Add package jai/0.0.1/jai-0.0.1-py3-none-any.whl

---
+++ packages/jai/jai-0.0.1-py3-none-any.whl/jai/__init__.py
@@ -0,0 +1,9 @@
+from .arch import *
+from .trainer import *
+from .datahandler import *
+from .improc import *
+from jai.kaggler.kaggler import *
+from .utils import *
+from .logger import *
+import jai.kaggler
+
