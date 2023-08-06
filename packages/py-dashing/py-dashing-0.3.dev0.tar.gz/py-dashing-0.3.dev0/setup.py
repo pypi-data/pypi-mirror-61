1717cd9fa41b4e4d272a40837b420e75e9d9de8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:20 +0000

    Add package py-dashing/0.3.dev0/py-dashing-0.3.dev0.tar.gz

---
+++ packages/py-dashing/py-dashing-0.3.dev0.tar.gz/py-dashing-0.3.dev0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+version = '0.3'
+
+setup(name='py-dashing',
+      version=version,
+      description="High-level terminal-based dashboard - Forked from FedericoCeratto/dashing",
+      long_description="""\
+Easily create dashboards""",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+      ],
+      keywords='dashboard terminal',
+      author='Federico Ceratto',
+      author_email='federico@debian.org',
+      url='https://github.com/hemanthmalla/dashing',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'blessed'
+      ],
+      )
