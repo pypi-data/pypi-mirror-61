fc8f2c58d6c9796cb0b5ec867a280c49826eb2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:18 +0000

    Add package djedefre/1.0.0/djedefre-1.0.0.tar.gz

---
+++ packages/djedefre/djedefre-1.0.0.tar.gz/djedefre-1.0.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+
+def get_packages(package):
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+
+setup(
+    name="djedefre",
+    version="1.0.0",
+    packages=get_packages("djoser"),
+    license="MIT",
+    author="iQueensu",
+    description="REST version of Django authentication system. This is a fork of djoser.",
+    # author_email="fake@fake.email",
+    long_description=readme,
+    install_requires=["django-templated-mail"],
+    setup_requires=["Babel>=2.6.0"],
+    include_package_data=True,
+    url="https://github.com/iqueensu/djedefre",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
