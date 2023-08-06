4a7f9b87ed91fae3151eb3ee07c187aa210fb90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:22 +0000

    Add package toughio/1.1.2/toughio-1.1.2.tar.gz

---
+++ packages/toughio/toughio-1.1.2.tar.gz/toughio-1.1.2/toughio/__init__.py
@@ -0,0 +1,30 @@
+from . import _cli, mesh, meshmaker
+from .__about__ import (
+    __author__,
+    __author_email__,
+    __license__,
+    __version__,
+    __website__,
+)
+from ._io import read_input, read_output, read_save, write_input
+from .mesh import Mesh
+from .mesh import read as read_mesh
+from .mesh import write as write_mesh
+
+__all__ = [
+    "Mesh",
+    "mesh",
+    "meshmaker",
+    "read_input",
+    "write_input",
+    "read_output",
+    "read_save",
+    "read_mesh",
+    "write_mesh",
+    "_cli",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__website__",
+    "__license__",
+]
