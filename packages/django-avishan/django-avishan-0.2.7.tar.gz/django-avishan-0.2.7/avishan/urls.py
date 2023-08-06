80e9c831d89252c855616c1d72b6368e71f2c235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:40 +0000

    Add package django-avishan/0.2.7/django-avishan-0.2.7.tar.gz

---
+++ packages/django-avishan/django-avishan-0.2.7.tar.gz/django-avishan-0.2.7/avishan/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from avishan.views.function_based import avishan_hash_password, avishan_doc
+
+from avishan.views.class_based import AvishanModelApiView, AvishanView
+from avishan.configure import get_avishan_config
+from avishan.views.panel_views import AvishanPanelView, AvishanPanelLoginView
+
+urlpatterns = [
+    path('api/av1/hash_password/<str:password>', avishan_hash_password),
+    path('api/av1/<str:model_plural_name>', AvishanModelApiView.as_view()),
+    path('api/av1/<str:model_plural_name>/<int:model_item_id>', AvishanModelApiView.as_view()),
+    path('api/av1/<str:model_plural_name>/<str:model_function_name>', AvishanModelApiView.as_view()),
+    path(
+        'api/av1/<str:model_plural_name>/<int:model_item_id>/<str:model_function_name>',
+        AvishanModelApiView.as_view()
+    ),
+    path('api/doc', avishan_doc),
+    path('test_cbv', AvishanView.as_view()),
+    path(f'{get_avishan_config().PANEL_ROOT}', AvishanPanelView.as_view()),
+    path(f'{get_avishan_config().PANEL_ROOT}/login', AvishanPanelLoginView.as_view()),
+]
