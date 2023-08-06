30007875649de08098bb768699e71b58fa094fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:09 +0000

    Add package pen/0.1.0a3/pen-0.1.0a3-py3-none-any.whl

---
+++ packages/pen/pen-0.1.0a3-py3-none-any.whl/pen/gitsync.py
@@ -0,0 +1,8 @@
+from .utils import print_err
+
+
+def init() -> None:
+    print_err("Git sync is currently not implemented, but we saved your setting")
+    print_err("and will activate sync once it's ready.")
+    print_err()
+    print_err()
