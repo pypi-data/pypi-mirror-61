b1c4f18bc2fbb73585626c6ef877383ac719703c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:27 +0000

    Add package tisdk/0.0.23/tisdk-0.0.23.tar.gz

---
+++ packages/tisdk/tisdk-0.0.23.tar.gz/tisdk-0.0.23/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+
+try:
+    long_description = open('README.md', encoding='utf8').read()
+except Exception as e:
+    long_description = ''
+
+
+try:
+    install_requires = []
+    for line in open('requirements.txt').readlines():
+        line = line.strip()
+        if line and not line.startswith('#'):
+            install_requires.append(line)
+except Exception as e:
+    pass
+
+
+setup(
+    name='tisdk',
+    version='0.0.23',
+    description='python sdk of taiqiyun [private]',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.22.0',
+        'pycrypto>=2.6.1',
+    ],
+    py_modules=['tisdk', 'sm4'],
+    entry_points={
+        'console_scripts': ['tireq=tisdk:main'],
+    },
+    include_package_data=True,
+)
