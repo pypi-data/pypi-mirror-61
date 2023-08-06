26652e25cd83288d0da0d16a82d27c04aa1f250e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:00 +0000

    Add package datahandler-bhanu/0.2/datahandler_bhanu-0.2.tar.gz

---
+++ packages/datahandler-bhanu/datahandler_bhanu-0.2.tar.gz/datahandler_bhanu-0.2/README.md
@@ -0,0 +1,13 @@
+datahandler(python)
+
+Handling Missing values in a given data set and imputing them in python.
+
+Install this library using pip in cmd/powershell (ADMIN) ----> pip install datahandler-bhanu
+
+Comes with command line support. Use directly from cmd/powershell by typing python filename.py datafilename.csv and wallah!
+Github Link -> https://github.com/eternaldemon/datahandler
+PyPi Link -> https://pypi.org/project/datahandler-bhanu/
+
+Libraries Used --> Pandas,sklearn,sys,numpy
+
+You can import it directly using import datahandler and use the function datahandler as datahandler.datahandler The updated data will be exported to a new file called data_new.csv
---
+++ packages/datahandler-bhanu/datahandler_bhanu-0.2.tar.gz/datahandler_bhanu-0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 19:49:25 2020
+
+@author: eternal_demon
+"""
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="datahandler_bhanu", # Replace with your own username
+    version="0.2",
+    author="Bhanu/eternal_demon",
+    author_email="aggarwal.bhanu02@gmail.com",
+    description="Handling Missing values in a given data set and imputing them.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eternaldemon/datahandler",
+    download_url="https://github.com/eternaldemon/datahandler/archive/0.2.tar.gz",
+    packages=["datahandler_bhanu"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
