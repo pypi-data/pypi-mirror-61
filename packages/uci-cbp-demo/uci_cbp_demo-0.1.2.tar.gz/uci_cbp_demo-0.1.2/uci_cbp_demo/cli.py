7d0540eca82f140f7a36c2a65ce5729914fa09d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package uci-cbp-demo/0.1.2/uci_cbp_demo-0.1.2.tar.gz

---
+++ packages/uci-cbp-demo/uci_cbp_demo-0.1.2.tar.gz/uci_cbp_demo-0.1.2/uci_cbp_demo/cli.py
@@ -0,0 +1,28 @@
+# MIT License
+# Copyright (C) Michael Tao-Yi Lee (taoyil AT UCI EDU)import sys
+import sys
+
+import click
+
+DEBUG = False
+
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    global DEBUG
+    DEBUG = debug
+    click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+
+
+@cli.command()  # @cli, not @click!
+@click.option('-a', default=1, help='Scaling coefficient')
+@click.option('-b', default=0, help='Shifting in Y')
+@click.option('--mock/--no-mock', default=False)
+def gui(a=1, b=0, mock=False):
+    from uci_cbp_demo.gui_cap import start_gui
+    start_gui(a, b, mock=mock)
+
+
+if __name__ == "__main__":
+    sys.exit(cli())
