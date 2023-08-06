0bc394f991e749643aefd7209685e3a7fe7c2e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:01 +0000

    Add package beancount-syspath/0.2.1/beancount_syspath-0.2.1-py3-none-any.whl

---
+++ packages/beancount-syspath/beancount_syspath-0.2.1-py3-none-any.whl/beancount_syspath/append.py
@@ -0,0 +1,31 @@
+# License: The MIT License
+# Copyright (c) 2020 Aetf
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+'''
+Beancount Plugin
+
+Append configured paths to sys.path
+'''
+from .plugin import run_legacy
+
+__plugins__ = ['append']
+
+
+def append(entries, options_map, config):
+    '''Prepend paths to sys.path'''
+    return run_legacy('append', entries, options_map, config)
