0b62f39a8b7d586bd57da5b98ddd6a1e90738b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:19 +0000

    Add package pileups/0.1.10/pileups-0.1.10.tar.gz

---
+++ packages/pileups/pileups-0.1.10.tar.gz/pileups-0.1.10/pileups/pileups_dist.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#===============================================================================
+# pileups_dist.py
+#===============================================================================
+
+"""Plot distribution of a pileup"""
+
+
+
+
+# Imports ======================================================================
+
+import argparse
+import seaborn as sns
+
+from pileups.pileups import generate_counts
+
+
+
+
+# Functions ====================================================================
+
+def ref_frac_dist(pileup_file_path):
+    with open(pileup_file_path, 'r') as f:
+        return tuple(r / c for _, _, c, r in generate_counts(f))
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="plot the distribution of a pileup"
+    )
+    parser.add_argument(
+        'pileup',
+        metavar='<path/to/file.pileup>',
+        help='path to pileup'
+    )
+    parser.add_argument(
+        'output',
+        metavar='<path/to/output.{pdf,png,svg}>',
+        help='path to output file'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    dist = ref_frac_dist(args.pileup)
+    ax = sns.distplot(dist)
+    fig = ax.get_figure()
+    fig.savefig(args.output)
