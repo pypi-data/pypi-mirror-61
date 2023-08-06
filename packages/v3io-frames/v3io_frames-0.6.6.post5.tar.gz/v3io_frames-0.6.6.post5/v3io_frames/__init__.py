014fbbe20d823b793281576617051c5e0a18093b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:09 +0000

    Add package v3io-frames/0.6.6.post5/v3io_frames-0.6.6.post5.tar.gz

---
+++ packages/v3io-frames/v3io_frames-0.6.6.post5.tar.gz/v3io_frames-0.6.6.post5/v3io_frames/__init__.py
@@ -0,0 +1,119 @@
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Stream data from/to Nuclio into pandas DataFrame"""
+
+__version__ = '0.6.6-5'
+
+import json
+from os import environ
+from urllib.parse import urlparse
+
+import pandas as pd
+
+from .errors import *  # noqa
+from .frames_pb2 import TableSchema as Schema, SchemaKey, FAIL, IGNORE, Session  # noqa
+from .grpc import Client as gRPCClient  # noqa
+from .http import Client as HTTPClient  # noqa
+from .pbutils import SchemaField  # noqa
+
+SESSION_ENV_KEY = 'V3IO_SESSION'
+
+_known_protocols = {'grpc', 'http', 'https'}
+
+
+def Client(address='', data_url='', container='', path='', user='',
+           password='', token='', session_id='', frame_factory=pd.DataFrame,
+           concat=pd.concat, persist_connection=False):
+    """Creates a new Frames client object
+
+    Parameters
+    ----------
+    address : str
+        Address of the Frames service (framesd). Use the grpc:// prefix for
+        gRPC (default; recommended) or the http:// prefix for HTTP.
+        Use `framesd:8081` (gRPC; recommended) or `framesd:8080` for local
+        execution on an Iguazio Data Science Platform ("the platform").
+    data_url : str
+        Base URL for accessing the backend data
+    container : str
+        Container name (session info)
+    path : str
+        DEPRECATED
+    user : str
+        The username of a platform user with permissions to access the backend
+        data; can't be used with `token`
+    password : str
+        A platform password for the user configured in the `user` parameter;
+        required when `user` is set; can't be used with `token`
+    token : str
+        A valid platform access key that allows access to the backend data;
+        can't be used with `user` and `password`
+    session_id : str
+        Session ID
+    frame_factory : class
+        DataFrame factory; currently, pandas DataFrame (default)
+    concat : function
+        Function for concatenating DataFrames; default: pandas concat
+    persist_connection: bool
+        Whether the underlying connection should persist between requests.
+        This only effect http client today. grpc client persists channels.
+        Use True where the same client is rarely instantiated but requests
+        are made often. When True is used, due to the nature of the
+        underlying clients, rapid instantiation of the client
+        may cause failures (e.g. HTTP NewConnectionError)
+
+    Return Value
+    ----------
+    A new `Client` object
+    """
+    if not (address.startswith("grpc://") or
+            address.startswith("https://") or
+            address.startswith("http://")):
+        address = "grpc://" + address
+    protocol = urlparse(address).scheme or 'grpc'
+    if protocol not in _known_protocols:
+        raise ValueError('unknown protocol - {}'.format(protocol))
+    if (user != "" or password != "") and token != "":
+        raise ValueError('both basic username-password and '
+                         'access-key authentication were provided')
+
+    env = session_from_env()
+
+    session = Session(
+        url=data_url or env.url,
+        container=container or env.container,
+        path=path or env.path,
+        user=user or env.user or environ.get('V3IO_USERNAME'),
+        password=password or env.password or environ.get('V3IO_PASSWORD'),
+        id=session_id or env.id,
+    )
+
+    if user == "" and password == "":
+        session.token = token or env.token or \
+                        environ.get('V3IO_ACCESS_KEY') or ''
+
+    cls = gRPCClient if protocol == 'grpc' else HTTPClient
+    return cls(address, session, persist_connection,
+               frame_factory=frame_factory, concat=concat)
+
+
+def session_from_env():
+    """Load session from V3IO_SESSION environment variable (JSON encoded)"""
+    data = environ.get(SESSION_ENV_KEY)
+    if data is None:
+        return Session()
+
+    obj = json.loads(data)
+    return Session(**obj)
---
+++ packages/v3io-frames/v3io_frames-0.6.6.post5.tar.gz/v3io_frames-0.6.6.post5/v3io_frames/pbutils.py
@@ -0,0 +1,279 @@
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warnings
+
+import google.protobuf.pyext._message as message
+import numpy as np
+import pandas as pd
+import pytz
+from google.protobuf.message import Message
+from pandas.core.dtypes.dtypes import CategoricalDtype
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
+
+from . import frames_pb2 as fpb
+from .dtypes import dtype_of
+from .errors import MessageError, WriteError
+
+
+pb_list_types = (
+    message.RepeatedCompositeContainer,
+    message.RepeatedScalarContainer,
+)
+
+
+def pb_value(v):
+    """Convert Python type to frames_pb2.Value"""
+    if v is None:
+        return None
+
+    dtype = dtype_of(v)
+    kw = {dtype.val_key: v}
+    return fpb.Value(**kw)
+
+
+def pb_map(d):
+    """Convert map values to frames_pb2.Value"""
+    return None if d is None else {k: pb_value(v) for k, v in d.items()}
+
+
+def SchemaField(name=None, doc=None, default=None, type=None, properties=None):
+    """A schema field"""
+    # We return a frames_pb2.SchemaField from Python types
+    return fpb.SchemaField(
+        name=name,
+        doc=doc,
+        default=pb_value(default),
+        type=type,
+        properties=pb_map(properties),
+    )
+
+
+def pb2py(obj):
+    """Convert protobuf object to Python object"""
+    if isinstance(obj, fpb.Value):
+        return getattr(obj, obj.WhichOneof('value'))
+
+    if isinstance(obj, Message):
+        return {
+            field.name: pb2py(value) for field, value in obj.ListFields()
+        }
+
+    if isinstance(obj, pb_list_types):
+        return [pb2py(v) for v in obj]
+
+    if isinstance(obj, message.MessageMapContainer):
+        return {
+            key: pb2py(value) for key, value in obj.items()
+        }
+
+    return obj
+
+
+def msg2df(frame, frame_factory, columns=None, do_reorder=True):
+    indices = [col2series(idx, None) for idx in frame.indices]
+    if len(indices) == 1:
+        new_index = indices[0]
+    elif len(indices) > 1:
+        new_index = pd.MultiIndex.from_arrays(indices)
+    else:
+        new_index = None
+
+    data = {col.name: col2series(col, new_index) for col in frame.columns}
+
+    df = frame_factory(data, new_index)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        df.labels = pb2py(frame.labels)
+
+    if do_reorder:
+        if columns:
+            df = df.reindex(columns=columns)
+        else:
+            is_range = True
+            indices = [False] * len(df.columns)
+            for name in df.columns:
+                try:
+                    if name.startswith('column_'):
+                        col_index = int(name[len('column_'):])
+                        if col_index < len(indices):
+                            indices[col_index] = True
+                            continue
+                except ValueError:
+                    pass
+                is_range = False
+                break
+
+            if is_range and all(elem for elem in indices):
+                renameDict = {}
+                for i in range(len(df.columns)):
+                    renameDict['column_' + str(i)] = i
+                df.rename(columns=renameDict, inplace=True)
+                new_index = pd.RangeIndex(start=0, step=1,
+                                          stop=len(df.columns))
+                df = df.reindex(columns=new_index)
+            else:
+                df = df.reindex(columns=sorted(df.columns))
+
+    return df
+
+
+def col2series(col, index):
+    current_dtype = ""
+    if col.dtype == fpb.BOOLEAN:
+        data = col.bools
+        current_dtype = "bool"
+    elif col.dtype == fpb.FLOAT:
+        data = col.floats
+        current_dtype = "float"
+    elif col.dtype == fpb.INTEGER:
+        data = col.ints
+        current_dtype = "int"
+    elif col.dtype == fpb.STRING:
+        data = col.strings
+        current_dtype = "object"
+    elif col.dtype == fpb.TIME:
+        data = [pd.Timestamp(t, unit='ns') for t in col.times]
+        current_dtype = "datetime64[ns, UTC]"
+    else:
+        raise MessageError('unknown dtype - {}'.format(col.dtype))
+
+    if col.kind == col.LABEL:
+        data = [data[0]] * col.size
+        if col.dtype == fpb.STRING:
+            data = pd.Series(data, dtype='category',
+                             index=index,
+                             name=col.name)
+    else:
+        data = pd.Series(data, index=index, name=col.name, dtype=current_dtype)
+
+    return data
+
+
+def idx2series(idx):
+    return pd.Series(idx.values, name=idx.name)
+
+
+def df2msg(df, labels=None, index_cols=None):
+    indices = None
+    if index_cols is not None:
+        indices = [series2col(df[name], name) for name in index_cols]
+        cols = [col for col in df.columns if col not in index_cols]
+        df = df[cols]
+    elif should_encode_index(df):
+        if hasattr(df.index, 'levels'):
+            by_name = df.index.get_level_values
+            names = df.index.names
+            serieses = (idx2series(by_name(name)) for name in names)
+        else:
+            serieses = [idx2series(df.index)]
+
+        indices = [series2col(s, s.name) for s in serieses]
+
+    is_range = isinstance(df.columns, pd.RangeIndex)
+    columns = []
+    for name in df.columns:
+        if not is_range and not isinstance(name, str):
+            raise Exception('Column names must be strings')
+        series = df[name]
+        if isinstance(name, int):
+            name = 'column_' + str(name)
+        columns.append(series2col(series, name))
+
+    return fpb.Frame(
+        columns=columns,
+        indices=indices,
+        labels=pb_map(labels),
+    )
+
+
+def series2col(s, name):
+    kw = {
+        'name': name,
+        'kind': fpb.Column.SLICE,
+    }
+
+    if is_int_dtype(s.dtype):
+        kw['dtype'] = fpb.INTEGER
+        kw['ints'] = s
+    elif is_float_dtype(s.dtype):
+        kw['dtype'] = fpb.FLOAT
+        kw['floats'] = s
+    elif s.dtype == np.object:  # Pandas dtype for str is object
+        kw['strings'] = s
+        kw['dtype'] = fpb.STRING
+    elif s.dtype == np.bool:
+        kw['bools'] = s
+        kw['dtype'] = fpb.BOOLEAN
+    elif is_datetime(s.dtype):
+        if s.dt.tz:
+            try:
+                s = s.dt.tz_localize(pytz.UTC)
+            except TypeError:
+                s = s.dt.tz_convert('UTC')
+        kw['times'] = s.astype(np.int64)
+        kw['dtype'] = fpb.TIME
+    elif is_categorical_dtype(s.dtype):
+        # We assume catgorical data is strings
+        kw['strings'] = s.astype(str)
+        kw['dtype'] = fpb.STRING
+    else:
+        raise WriteError('{} - unsupported type - {}'.format(s.name, s.dtype))
+
+    return fpb.Column(**kw)
+
+
+def should_encode_index(df):
+    if df.index.name:
+        return True
+
+    return not isinstance(df.index, pd.RangeIndex)
+
+
+# We can't use a set since hash(np.int64) != hash(pd.Series([1]).dtype)
+def is_int_dtype(dtype):
+    return \
+        dtype == np.int64 or \
+        dtype == np.int32 or \
+        dtype == np.int16 or \
+        dtype == np.int8 or \
+        dtype == np.int
+
+
+def is_float_dtype(dtype):
+    return \
+        dtype == np.float64 or \
+        dtype == np.float32 or \
+        dtype == np.float16 or \
+        dtype == np.float
+
+
+def is_categorical_dtype(dtype):
+    return isinstance(dtype, CategoricalDtype)
+
+
+def _fix_pd():
+    # cudf works with older versions of pandas
+    import pandas.api.types
+    if not hasattr(pandas.api.types, 'is_categorical_dtype'):
+        pandas.api.types.is_categorical_dtype = is_categorical_dtype
+
+    import pandas.core.common
+    if not hasattr(pandas.core.common, 'is_categorical_dtype'):
+        pandas.core.common.is_categorical_dtype = is_categorical_dtype
+
+
+_fix_pd()
+del _fix_pd
