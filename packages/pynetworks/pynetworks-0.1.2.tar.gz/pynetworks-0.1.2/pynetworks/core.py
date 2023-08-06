6c2a0afe8e6b77cb4742de37c50ed8c312296649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:05 +0000

    Add package pynetworks/0.1.2/pynetworks-0.1.2.tar.gz

---
+++ packages/pynetworks/pynetworks-0.1.2.tar.gz/pynetworks-0.1.2/pynetworks/core.py
@@ -0,0 +1,57 @@
+from graphviz import Digraph
+
+
+class Network:
+    # TODO: parse from dotlang
+    '''Contain several interconnected ``Node`` objects.'''
+
+    def __init__(self, name, nodes=None):
+        self.name = name
+        self.nodes = set(nodes) if nodes else set()
+
+    def __str__(self):
+        graph = Digraph(self.name, graph_attr={'concentrate': 'true'})
+        for node in self.nodes:
+            if node.connections:
+                for connection in node.connections:
+                    graph.edge(node.name, connection.name)
+            else:
+                graph.node(node.name)
+        return str(graph)
+
+    def add(self, node):
+        '''Add ``node`` to this ``Network``.'''
+        self.nodes.add(node)
+
+    def remove(self, node):
+        '''Remove ``node`` from this ``Network``.'''
+        self.nodes.remove(node)
+
+
+class Node:
+    '''Hold a graph theory node its connections.'''
+
+    def __init__(self, name, connections=None):
+        self.name = name
+        self.connections = set(connections) if connections else set()
+        for node in self.connections:
+            node.connections.add(self)
+
+    def __str__(self):
+        graph = Digraph()
+        if self.connections:
+            for node in self.connections:
+                graph.edge(self.name, node.name)
+        else:
+            graph.node(self.name)
+        return str(graph)
+
+    def connect(self, other):
+        '''Add a connection between this ``Node`` and ``other``.'''
+        self.connections.add(other)
+        other.connections.add(self)
+
+    def disconnect(self, other):
+        '''Remove the connection between this ``Node`` and ``other``.'''
+        self.connections.remove(other)
+        other.connections.remove(self)
