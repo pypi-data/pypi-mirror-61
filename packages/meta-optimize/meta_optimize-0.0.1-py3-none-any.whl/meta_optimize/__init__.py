8f278b6266d124b29cf423f106ad89c50528f5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:05 +0000

    Add package meta-optimize/0.0.1/meta_optimize-0.0.1-py3-none-any.whl

---
+++ packages/meta-optimize/meta_optimize-0.0.1-py3-none-any.whl/meta_optimize/__init__.py
@@ -0,0 +1,12 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+
+from .meta_optimize_api import MetaOptimize
+
+
+__all__ = ["MetaOptimize"]
---
+++ packages/meta-optimize/meta_optimize-0.0.1-py3-none-any.whl/meta_optimize/meta_optimize_api.py
@@ -0,0 +1,9 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+class MetaOptimize:
+    def __init__(self):
+	pass
+
