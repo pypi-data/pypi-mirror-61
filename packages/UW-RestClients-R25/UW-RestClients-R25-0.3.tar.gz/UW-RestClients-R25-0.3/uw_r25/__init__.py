d4b71d97489276ce88fb7c43fef57314f8efda1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:11 +0000

    Add package UW-RestClients-R25/0.3/UW-RestClients-R25-0.3.tar.gz

---
+++ packages/UW-RestClients-R25/UW-RestClients-R25-0.3.tar.gz/UW-RestClients-R25-0.3/uw_r25/__init__.py
@@ -0,0 +1,33 @@
+from uw_r25.dao import R25_DAO
+from restclients_core.exceptions import DataFailureException
+from lxml import etree
+
+
+nsmap = {"r25": "http://www.collegenet.com/r25",
+         "xhtml": "http://www.w3.org/1999/xhtml"}
+
+
+def get_resource(url):
+    """
+    Issue a GET request to R25 with the given url
+    and return a response as an etree element.
+    """
+
+    instance = R25_DAO().get_service_setting('INSTANCE')
+    if instance is not None:
+        url = "/r25ws/wrd/{}/run/{}".format(instance, url)
+    else:
+        url = "/r25ws/servlet/wrd/run/{}".format(url)
+
+    response = R25_DAO().getURL(url, {"Accept": "text/xml"})
+    if response.status != 200:
+        raise DataFailureException(url, response.status, response.data)
+
+    tree = etree.fromstring(response.data.strip())
+
+    # XHTML response is an error response
+    xhtml = tree.xpath("//xhtml:html", namespaces=nsmap)
+    if len(xhtml):
+        raise DataFailureException(url, 500, response.data)
+
+    return tree
---
+++ packages/UW-RestClients-R25/UW-RestClients-R25-0.3.tar.gz/UW-RestClients-R25-0.3/uw_r25/dao.py
@@ -0,0 +1,18 @@
+import logging
+import os
+import re
+from os.path import abspath, dirname
+from restclients_core.dao import DAO
+
+
+class R25_DAO(DAO):
+    def service_name(self):
+        return 'r25'
+
+    def service_mock_paths(self):
+        return [abspath(os.path.join(dirname(__file__), "resources"))]
+
+    def _custom_headers(self, method, url, headers, body):
+        basic_auth = self.get_service_setting('BASIC_AUTH')
+        if basic_auth is not None:
+            return {"Authorization": "Basic {}".format(basic_auth)}
