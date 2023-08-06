2526eccc987c773a3d17c3a07f2b3d8ca886d5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:26 +0000

    Add package epcpm/2020.2.5/epcpm-2020.2.5-py3-none-any.whl

---
+++ packages/epcpm/epcpm-2020.2.5-py3-none-any.whl/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '30887138'
+build_number = '598'
+build_version = '1.0.598'
+job_id = 'cvgmryob1sx7kkml'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/pm/build/job/cvgmryob1sx7kkml'
