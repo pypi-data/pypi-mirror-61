30160212cf1ab0208f00a82309e3d6f15ae93a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:54 +0000

    Add package toto-logger-nicolasances/1.0.2/toto-logger-nicolasances-1.0.2.tar.gz

---
+++ packages/toto-logger-nicolasances/toto-logger-nicolasances-1.0.2.tar.gz/toto-logger-nicolasances-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="toto-logger-nicolasances", # Replace with your own username
+    version="1.0.2",
+    author="nicolasances",
+    author_email="nicolas.matteazzi@gmail.com",
+    description="A simple logger for Toto",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nicolasances/py-toto-logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
