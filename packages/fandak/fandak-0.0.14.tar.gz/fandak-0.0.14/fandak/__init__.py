f18dec2fc8491354a635d9bc5de2f736ad60181c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:27 +0000

    Add package fandak/0.0.14/fandak-0.0.14.tar.gz

---
+++ packages/fandak/fandak-0.0.14.tar.gz/fandak-0.0.14/fandak/__init__.py
@@ -0,0 +1,10 @@
+from .core.datasets import Dataset
+from .core.evaluators import Evaluator
+from .core.models import Model
+from .core.trainers import Trainer
+
+name = "fandak"
+
+__version__ = "0.0.14"
+
+__all__ = ["__version__", "Dataset", "Model", "Trainer", "Evaluator"]
