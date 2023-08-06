374264b6d208efecb252ef9af3b4e06fee4a69cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:18 +0000

    Add package django-sass/1.0.0/django-sass-1.0.0.tar.gz

---
+++ packages/django-sass/django-sass-1.0.0.tar.gz/django-sass-1.0.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf8") as readme:
+    README = readme.read()
+
+setup(
+    name="django-sass",
+    version="1.0.0",
+    author="CodeRed LLC",
+    author_email="info@coderedcorp.com",
+    url="https://github.com/coderedcorp/django-sass",
+    description=("The absolute simplest way to use Sass with Django. Pure Python, "
+                 "minimal dependencies, and no special configuration required!"),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="BSD license",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        "django",
+        "libsass"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Environment :: Web Environment",
+    ],
+)
