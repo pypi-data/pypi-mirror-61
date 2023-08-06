58c6eccc5ddb9b9c62568be02fae2784df63ed8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:29 +0000

    Add package pudo/1.0.0/pudo-1.0.0-py3-none-any.whl

---
+++ packages/pudo/pudo-1.0.0-py3-none-any.whl/pudo/pudo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import os
+import sys
+import subprocess
+
+def run(cmd):
+	'''
+	Usage:
+		user$ python3 # or python2
+		>>> from . import pudo
+		>>> (ret, out) = pudo.run(('ls', '/root')) # or pudo.run('ls /root')
+		>>> print(ret)
+		>>> 0
+		>>> print(out)
+		>>> b'Desktop\\nDownloads\\nPictures\\nMusic\\n'
+	'''
+	pudoPath = os.path.join(sys.prefix, 'bin', 'pudo')
+	if (not os.access(pudoPath, os.X_OK)):
+		raise FileNotFoundError
+	cmd = '%s %s' %(pudoPath, cmd)
+	process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	(out, _) = process.communicate()
+	ret = process.wait()
+	return(ret, out)
+
+if (__name__ == '__main__'):
+	(ret, out) = run(' '.join(sys.argv[1:]))
+	print(out)
+	sys.exit(ret)
+
