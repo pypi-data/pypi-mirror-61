1fe2e01e8b1bd3492c4d7beaec4f95571c8a192e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:52 +0000

    Add package roboradar/0.1.0/roboradar-0.1.0.tar.gz

---
+++ packages/roboradar/roboradar-0.1.0.tar.gz/roboradar-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="roboradar",
+    version="0.1.0",
+    author="David Johnston",
+    author_email="dwjgame11@gmail.com",
+    description="Robot position display software for FRC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Short-SirKit-6527/RoboRadar",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+print("done")
