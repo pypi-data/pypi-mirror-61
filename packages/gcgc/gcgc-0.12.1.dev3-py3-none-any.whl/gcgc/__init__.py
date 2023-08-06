d54d3b3afdfa26f8db89166d26260be2f3e44428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:53 +0000

    Add package gcgc/0.12.1.dev3/gcgc-0.12.1.dev3-py3-none-any.whl

---
+++ packages/gcgc/gcgc-0.12.1.dev3-py3-none-any.whl/gcgc/__init__.py
@@ -0,0 +1,15 @@
+# (c) Copyright 2018 Trent Hauck
+# All Rights Reserved
+"""Top-level GCGC module."""
+
+import warnings as _warnings
+
+from gcgc.cli import cli
+from gcgc.tokenizer import KmerTokenizer
+from gcgc.tokenizer import KmerTokenizerSettings
+from gcgc.tokenizer import SequenceTokenizer
+from gcgc.tokenizer import SequenceTokenizerSettings
+
+__version__ = "0.12.1-dev.3"
+
+_warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
