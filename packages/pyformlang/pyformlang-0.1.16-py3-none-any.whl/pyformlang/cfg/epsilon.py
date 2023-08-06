c1063afe0030265078373f479dde02941ffdd22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:03 +0000

    Add package pyformlang/0.1.16/pyformlang-0.1.16-py3-none-any.whl

---
+++ packages/pyformlang/pyformlang-0.1.16-py3-none-any.whl/pyformlang/cfg/epsilon.py
@@ -0,0 +1,10 @@
+""" An epsilon terminal """
+
+from .terminal import Terminal
+
+
+class Epsilon(Terminal):
+    """ An epsilon terminal """
+
+    def __init__(self):
+        super().__init__("epsilon")
