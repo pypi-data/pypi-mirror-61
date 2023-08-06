0ef5b1f160502dfef6d66d1272dd7b6153644740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:19 +0000

    Add package udacity-dsnd-distributions/0.1/udacity_dsnd_distributions-0.1.tar.gz

---
+++ packages/udacity-dsnd-distributions/udacity_dsnd_distributions-0.1.tar.gz/udacity_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_dsnd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udacity_dsnd_distributions'],
+      author = 'DJ Dong',
+      author_email = 'ustcdj@hotmail.com',
+      zip_safe=False)
