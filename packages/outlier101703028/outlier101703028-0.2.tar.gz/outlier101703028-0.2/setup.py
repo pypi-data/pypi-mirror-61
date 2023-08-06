956a1105ebb956ddfa2bc5ff586b1068cf280bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:31 +0000

    Add package outlier101703028/0.2/outlier101703028-0.2.tar.gz

---
+++ packages/outlier101703028/outlier101703028-0.2.tar.gz/outlier101703028-0.2/setup.py
@@ -0,0 +1,53 @@
+from distutils.core import setup
+
+setup(
+
+  name = 'outlier101703028',
+
+  packages = ['outlier101703028'],
+
+  version = '0.2',
+
+  license='MIT',
+
+  description = 'Outlier removal method',
+
+  author = 'Abhishek Shivaram',
+
+  author_email = 'ashivaram_be17@thapar.com',
+
+  url = 'https://github.com/shivaram-abhishek/outlier101703028',
+
+  download_url = 'https://github.com/shivaram-abhishek/outlier101703028/archive/0.2.tar.gz',
+
+  keywords = ['Outlier', 'Removal'],
+
+  install_requires=[
+
+          'numpy',
+
+          'pandas'
+
+      ],
+
+  classifiers=[
+
+    'Development Status :: 3 - Alpha',
+
+    'Intended Audience :: Developers',
+
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3',
+
+    'Programming Language :: Python :: 3.4',
+
+    'Programming Language :: Python :: 3.5',
+
+    'Programming Language :: Python :: 3.6',
+
+  ],
+
+)
