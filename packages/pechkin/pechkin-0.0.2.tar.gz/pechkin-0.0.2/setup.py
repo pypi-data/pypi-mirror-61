6ab32b289acec501a6aaf109d1e1cac3ed85811d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:31 +0000

    Add package pechkin/0.0.2/pechkin-0.0.2.tar.gz

---
+++ packages/pechkin/pechkin-0.0.2.tar.gz/pechkin-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pechkin",
+    version="0.0.2",
+    author="Vitalii Nemchenko",
+    author_email="vitalik648@gmail.com",
+    description="Some tools for delivery services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VNemchenko/pechkin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
