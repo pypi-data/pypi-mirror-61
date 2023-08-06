2379dea510daf00b4f2de9a48998defd93247217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:59 +0000

    Add package minty/1.3.0/minty-1.3.0.tar.gz

---
+++ packages/minty/minty-1.3.0.tar.gz/minty-1.3.0/minty/config/parser/base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+from ... import Base
+
+
+class ConfigParserBase(ABC, Base):
+    @abstractmethod
+    def parse(self, content: str) -> str:
+        """Parse configuration content.
+
+        Method is called in child classes `super().parse(content)` to make sure
+        we can implement generic parsing behaviour if necessary.
+
+        :param content: contents of the configuration file to parse
+        :type content: str
+        :return: the "content" parameter, unchanged
+        :rtype: str
+        """
+        return content
