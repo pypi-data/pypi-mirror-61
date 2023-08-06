f69a9807e38fcf34a7b7356e6aa5daab72b4cdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:29 +0000

    Add package statick/0.3.2/statick-0.3.2.tar.gz

---
+++ packages/statick/statick-0.3.2.tar.gz/statick-0.3.2/setup.py
@@ -0,0 +1,51 @@
+"""Setup."""
+
+
+try:
+    from setuptools import setup
+except:  # pylint: disable=bare-except # noqa: E722 # NOLINT
+    from distutils.core import setup  # pylint: disable=wrong-import-order
+
+import statick_tool
+
+with open('README.md') as f:
+    long_description = f.read()  # pylint: disable=invalid-name
+
+TEST_DEPS = [
+    'backports.tempfile',
+    'pytest',
+    'mock',
+]
+EXTRAS = {
+    'test': TEST_DEPS,
+}
+
+setup(
+    author='SSC Pacific',
+    name='statick',
+    description='Making code quality easier.',
+    version=statick_tool.__version__,
+    packages=['statick_tool'],
+    package_data={'statick_tool': ['rsc/*.*', 'rsc/.clang-format',
+                                   'rsc/plugin_mapping/*',
+                                   'plugins/*.py', 'plugins/discovery/*',
+                                   'plugins/tool/*', 'plugins/reporting/*']},
+    scripts=['statick', 'statick_ws'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['bandit', 'cmakelint', 'flawfinder', 'lizard',
+                      'pycodestyle', 'pydocstyle', 'pyflakes', 'pylint',
+                      'PyYAML', 'xmltodict', 'yamllint', 'yapsy'],
+    tests_require=TEST_DEPS,
+    extras_require=EXTRAS,
+    url='https://github.com/sscpac/statick',
+    classifiers=[
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Testing",
+    ],
+)
