f320dd6404eaaa343865ff892cd149fd53319244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:21 +0000

    Add package mesh-rpc/0.1.0/mesh_rpc-0.1.0.tar.gz

---
+++ packages/mesh-rpc/mesh_rpc-0.1.0.tar.gz/mesh_rpc-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mesh_rpc",
+    version="0.1.0",
+    author="Abhishek Upperwal",
+    author_email="mesh@soket.in",
+    description="RPC library for mesh-pravah",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pravahio",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'grpcio'
+    ]
+)
