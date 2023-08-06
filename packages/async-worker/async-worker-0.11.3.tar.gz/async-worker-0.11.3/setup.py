685b1401ef177bd4d2df338a3e99f033af2ea730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:44 +0000

    Add package async-worker/0.11.3/async-worker-0.11.3.tar.gz

---
+++ packages/async-worker/async-worker-0.11.3.tar.gz/async-worker-0.11.3/setup.py
@@ -0,0 +1,31 @@
+# Always prefer setuptools over distutils
+from os import path
+
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="async-worker",
+    version="0.11.3",
+    description="Microframework para escrever workers assíncronos em Python",
+    long_description="Microframework para escrever workers assíncronos em Python",
+    url="https://github.com/B2W-BIT/async-worker",
+    # Author details
+    author="Dalton Barreto",
+    author_email="daltonmatos@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    install_requires=[
+        "aioamqp==0.14.0",
+        "aiologger>=0.4.0-rc1",
+        "pydantic==0.30",
+        "cached-property==1.5.1",
+        "aiohttp==3.4.4",
+    ],
+    entry_points={},
+)
