50c418eca6a3accdff70a21048750369d2cfe90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:59 +0000

    Add package bacchus/0.1.9/bacchus-0.1.9-py3-none-any.whl

---
+++ packages/bacchus/bacchus-0.1.9-py3-none-any.whl/bacchus/jackett.py
@@ -0,0 +1,26 @@
+import json
+import shutil
+from .base import HomeServerApp
+from .base import TEMPLATES
+
+
+class Jackett(HomeServerApp):
+    def setup(self):
+        self.container.stop()
+        self.setup_nginx()
+        self.compose.start()
+        self.copy_indexers()
+
+    def copy_indexers(self):
+        indexers_path = (self.path / 'Jackett' / 'Indexers')
+        indexers_path.mkdir(parents=True, exists_ok=True)
+
+        files = ((TEMPLATES / 'jackett' / 'Indexers').absolute()).glob('*')
+        for file in files:
+            shutil.copyfile(str(file.absolute()), indexers_path)
+
+    def setup_nginx(self):
+        config = self.path / 'Jackett' / 'ServerConfig.json'
+        result = json.load(config.open('r'))
+        result['BasePathOverride'] = '/trackers'
+        json.dump(result, config.open('w'))
