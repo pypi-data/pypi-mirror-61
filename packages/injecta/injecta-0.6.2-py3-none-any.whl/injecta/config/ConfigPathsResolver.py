48772c07b41e0ac0e1713351f01fb02b14abb5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:18 +0000

    Add package injecta/0.6.2/injecta-0.6.2-py3-none-any.whl

---
+++ packages/injecta/injecta-0.6.2-py3-none-any.whl/injecta/config/ConfigPathsResolver.py
@@ -0,0 +1,27 @@
+from injecta.config.ConfigLoader import ConfigLoader
+from injecta.config.ImportDefinitionResolver import ImportDefinitionResolver
+from pathlib import Path
+
+class ConfigPathsResolver:
+
+    def __init__(self):
+        self.__configLoader = ConfigLoader()
+        self.__importDefinitionResolver = ImportDefinitionResolver()
+
+    def resolve(self, configPath: Path, baseDir: Path) -> list:
+        yamlConfig = self.__configLoader.load(configPath)
+        configPaths = [configPath]
+
+        if yamlConfig is None:
+            return []
+
+        if 'imports' in yamlConfig:
+            newConfigPaths = []
+
+            for importDefinition in yamlConfig['imports']:
+                newConfigPaths += self.__importDefinitionResolver.resolve(importDefinition, baseDir)
+
+            for newConfigPath in newConfigPaths:
+                configPaths += self.resolve(newConfigPath, baseDir)
+
+        return configPaths
