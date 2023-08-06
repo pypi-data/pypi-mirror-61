94d7d206a306029d49bcf30f45e26851a8c2aea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:30 +0000

    Add package guillotina-amqp/5.0.17/guillotina_amqp-5.0.17.tar.gz

---
+++ packages/guillotina-amqp/guillotina_amqp-5.0.17.tar.gz/guillotina_amqp-5.0.17/guillotina_amqp/exceptions.py
@@ -0,0 +1,26 @@
+class TaskNotFinishedException(Exception):
+    pass
+
+
+class TaskNotFoundException(Exception):
+    pass
+
+
+class TaskAlreadyAcquired(Exception):
+    pass
+
+
+class TaskAlreadyCanceled(Exception):
+    pass
+
+
+class TaskAccessUnauthorized(Exception):
+    pass
+
+
+class TaskMaxRetriesReached(Exception):
+    pass
+
+
+class ObjectNotFoundException(Exception):
+    pass
