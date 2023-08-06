11435eda8d6f33774b4527c8ce7ad207c7c381b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:42 +0000

    Add package structureboost/0.0.2/structureboost-0.0.2.tar.gz

---
+++ packages/structureboost/structureboost-0.0.2.tar.gz/structureboost-0.0.2/structureboost/__init__.py
@@ -0,0 +1,7 @@
+"""structureboost is a Python package for Gradient Boosting using categorical structure"""
+
+import graphs
+# import structure_dt
+from structure_gb import StructureBoost
+
+__version__ = '0.0.2'
