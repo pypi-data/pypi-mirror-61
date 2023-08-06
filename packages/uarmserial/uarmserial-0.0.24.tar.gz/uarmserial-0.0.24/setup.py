fb0c3c1216dafa1e50c78e22798639a3555c8591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:32 +0000

    Add package uarmserial/0.0.24/uarmserial-0.0.24.tar.gz

---
+++ packages/uarmserial/uarmserial-0.0.24.tar.gz/uarmserial-0.0.24/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uarmserial",
+    version="0.0.24",
+    author="Ryan Shah",
+    author_email="ryan.k.shah@gmail.com",
+    description="Python package for connecting to and communicating with a uARM Swift Pro robot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ryankshah/uarmserial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
