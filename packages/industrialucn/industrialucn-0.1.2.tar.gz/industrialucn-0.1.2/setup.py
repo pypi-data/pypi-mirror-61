d8d178c5e350cf2a0b4b0c043491155a1f34a143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:22 +0000

    Add package industrialucn/0.1.2/industrialucn-0.1.2.tar.gz

---
+++ packages/industrialucn/industrialucn-0.1.2.tar.gz/industrialucn-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='industrialucn',
+    packages=['industrialucn'],
+    version='0.1.2',
+    license='MIT',
+    description='Industrial UCN',
+    author='Hernan Caceres',
+    author_email='industrial@ucn.cl',
+    url='https://industrial.ucn.cl',
+    download_url='https://github.com/hernan-caceres/orlab-code/archive/0.1.2.tar.gz',
+    keywords=['industrial', 'ucn', 'optimization', 'orlab', 'statistics'],
+    install_requires=[
+        'docplex',
+        'numpy',
+        'pandas',
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
