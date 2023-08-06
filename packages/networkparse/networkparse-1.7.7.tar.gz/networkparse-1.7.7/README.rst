500b235f9ffa9f37db70e53801d2fce281e71c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:28 +0000

    Add package networkparse/1.7.7/networkparse-1.7.7.tar.gz

---
+++ packages/networkparse/networkparse-1.7.7.tar.gz/networkparse-1.7.7/README.rst
@@ -0,0 +1,26 @@
+Installing
+==========
+
+.. code-block::
+
+    pip install --user networkparse
+
+Using
+=====
+Docs are available on `Read the Docs`_
+
+.. _`Read the Docs`: https://networkparse.readthedocs.io/en/latest/
+
+
+License
+=======
+This module was developed by and for Xylok_. The code is release under the MIT license.
+
+.. _Xylok: https://www.xylok.io
+
+
+Credits
+=======
+This module was inspired by CiscoConfParse_.
+
+.. _CiscoConfParse: https://github.com/mpenning/ciscoconfparse
---
+++ packages/networkparse/networkparse-1.7.7.tar.gz/networkparse-1.7.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['networkparse']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dataclasses>=0.6.0,<0.7.0']
+
+setup_kwargs = {
+    'name': 'networkparse',
+    'version': '1.7.7',
+    'description': 'Simple read-only parser for Cisco IOS, NX-OS, HP, and other network device running configs',
+    'long_description': 'Installing\n==========\n\n.. code-block::\n\n    pip install --user networkparse\n\nUsing\n=====\nDocs are available on `Read the Docs`_\n\n.. _`Read the Docs`: https://networkparse.readthedocs.io/en/latest/\n\n\nLicense\n=======\nThis module was developed by and for Xylok_. The code is release under the MIT license.\n\n.. _Xylok: https://www.xylok.io\n\n\nCredits\n=======\nThis module was inspired by CiscoConfParse_.\n\n.. _CiscoConfParse: https://github.com/mpenning/ciscoconfparse\n',
+    'author': 'Ryan Morehart',
+    'author_email': 'ryan@xylok.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/xylok/networkparse',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
