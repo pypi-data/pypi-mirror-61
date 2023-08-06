4e89029dc00d08316364116a749c08902174d4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:47 +0000

    Add package networkml/0.4.8/networkml-0.4.8.tar.gz

---
+++ packages/networkml/networkml-0.4.8.tar.gz/networkml-0.4.8/setup.py
@@ -0,0 +1,22 @@
+# WARNING about imp deprecation because of setuptools
+from setuptools import setup
+
+setup(
+    name='networkml',
+    version=open('VERSION', 'r').read().strip(),
+    packages=['networkml', 'networkml.algorithms', 'networkml.algorithms.sos',
+              'networkml.parsers', 'networkml.parsers.netflow',
+              'networkml.parsers.pcap', 'networkml.utils'],
+    package_data={'networkml': ['trained_models/*', 'configs/*']},
+    install_requires=open('requirements.txt', 'r').read().splitlines(),
+    scripts=['bin/networkml'],
+    license='Apache License 2.0',
+    author='cglewis',
+    author_email='clewis@iqt.org',
+    maintainer='cglewis',
+    maintainer_email='clewis@iqt.org',
+    description=(
+        'A utility package for extracting and analyzing features in network traffic.'),
+    keywords='machine learning network analysis utilities',
+    url='https://github.com/CyberReboot/NetworkML',
+)
