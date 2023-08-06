4dca09ee87f35b7f311fab13c22ad623159307d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:58 +0000

    Add package mindbody-api/1.0.4/mindbody_api-1.0.4.tar.gz

---
+++ packages/mindbody-api/mindbody_api-1.0.4.tar.gz/mindbody_api-1.0.4/mindbody_api/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .appointment_api import AppointmentApi
+from .class_api import ClassApi
+from .client_api import ClientApi
+from .enrollment_api import EnrollmentApi
+from .payroll_api import PayrollApi
+from .sale_api import SaleApi
+from .site_api import SiteApi
+from .staff_api import StaffApi
+from .user_token_api import UserTokenApi
