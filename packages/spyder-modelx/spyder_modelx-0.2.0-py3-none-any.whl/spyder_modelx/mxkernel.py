28050f5f990e030b73cabdbce4a49935e3bab48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:35 +0000

    Add package spyder-modelx/0.2.0/spyder_modelx-0.2.0-py3-none-any.whl

---
+++ packages/spyder-modelx/spyder_modelx-0.2.0-py3-none-any.whl/spyder_modelx/mxkernel.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018-2019 Fumito Hamamura <fumito.ham@gmail.com>
+
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation version 3.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# The source code contains parts copied and modified from Spyder project:
+# https://github.com/spyder-ide/spyder
+# See below for the original copyright notice.
+
+#
+# Copyright (c) Spyder Project Contributors
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import json
+import spyder
+
+if spyder.version_info < (3, 3, 0):
+    from spyder.utils.ipython.spyder_kernel import SpyderKernel
+else:
+    from spyder_kernels.console.kernel import SpyderKernel
+
+from spyder_modelx.util import hinted_tuple_hook
+
+
+class ModelxKernel(SpyderKernel):
+
+    def __init__(self, *args, **kwargs):
+        super(ModelxKernel, self).__init__(*args, **kwargs)
+
+        if spyder.version_info > (4,):
+            self.frontend_comm.register_call_handler(
+                'mx_get_modellist',
+                self.mx_get_modellist
+            )
+
+    def get_modelx(self):
+        from modelx.core import mxsys
+        return mxsys
+
+    def mx_get_object(self, msgtype, fullname=None):
+
+        import modelx as mx
+        if fullname is None:
+            obj = mx.cur_model()
+        else:
+            obj = mx.get_object(fullname)
+
+        self.send_mx_msg(msgtype, data=obj._baseattrs if obj else None)
+
+    def mx_get_modellist(self):
+        """Returns a list of model info.
+
+         Returns a list of dicts of basic model attributes.
+         The first element of the list is the current model info.
+         None if not current model is set.
+         """
+
+        import modelx as mx
+        from modelx.core.base import Interface
+
+        data = [Interface._baseattrs.fget(m) for m in mx.get_models().values()]
+
+        if mx.cur_model():
+            cur = Interface._baseattrs.fget(mx.cur_model())
+        else:
+            cur = None
+
+        data.insert(0, cur)
+
+        if spyder.version_info > (4,):
+            return data
+        else:
+            self.send_mx_msg("modellist", data=data)
+
+    def mx_get_codelist(self, fullname):
+        import modelx as mx
+
+        try:
+            obj = mx.get_object(fullname)
+            data = obj._to_attrdict(['formula'])
+        except:
+            data = None
+
+        self.send_mx_msg('codelist', data=data)
+
+    def mx_get_evalresult(self, msgtype, data):
+
+        # The code below is based on SpyderKernel.get_value
+        try:
+            self.send_mx_msg(msgtype, data=data)
+        except:
+            # * There is no need to inform users about
+            #   these errors.
+            # * value = None makes Spyder to ignore
+            #   petitions to display a value
+            self.send_mx_msg(msgtype, data=None)
+
+        self._do_publish_pdb_state = False
+
+    def mx_get_adjacent(self, msgtype, obj: str,
+                        jsonargs: str, adjacency: str):
+
+        import modelx as mx
+        from modelx.core.base import Interface
+
+        args = json.loads(jsonargs, object_hook=hinted_tuple_hook)
+        node = mx.get_object(obj).node(*args)
+        nodes = getattr(node, adjacency)
+        attrs = [node._baseattrs for node in nodes]
+
+        for node in attrs:
+            if isinstance(node["value"], Interface):
+                node["value"] = repr(node["value"])
+
+        content = {'mx_ogj': obj, 'mx_args': args, 'mx_adjacency': adjacency}
+        self.send_mx_msg(msgtype, content=content, data=attrs)
+
+    def send_mx_msg(self, mx_msgtype, content=None, data=None):
+        """
+        Publish custom messages to the Spyder frontend.
+
+        This code is modified from send_spyder_msg in spyder-kernel v0.2.4
+
+        Parameters
+        ----------
+
+        mx_msgtype: str
+            The spyder message type
+        content: dict
+            The (JSONable) content of the message
+        data: any
+            Any object that is serializable by cloudpickle (should be most
+            things). Will arrive as cloudpickled bytes in `.buffers[0]`.
+        """
+        import cloudpickle
+
+        if content is None:
+            content = {}
+        content['mx_msgtype'] = mx_msgtype
+        self.session.send(
+            self.iopub_socket,
+            'modelx_msg',
+            content=content,
+            buffers=[cloudpickle.dumps(data, protocol=2)],
+            parent=self._parent_header)
+
---
+++ packages/spyder-modelx/spyder_modelx-0.2.0-py3-none-any.whl/spyder_modelx/widgets/mxexplorer.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018-2019 Fumito Hamamura <fumito.ham@gmail.com>
+
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation version 3.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# The source code is originated from:
+# https://github.com/spyder-ide/spyder-plugin-cookiecutter
+# See below for the original copyright notice.
+
+#
+# Copyright (c) Spyder Project Contributors
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+"""modelx Widget."""
+import sys
+
+from spyder_modelx.widgets.mxtreemodel import MxTreeModel, ModelItem
+from qtpy.QtCore import Signal, Slot, Qt, QStringListModel
+from qtpy.QtWidgets import (QHBoxLayout, QLabel, QMenu, QMessageBox, QAction,
+                            QToolButton, QVBoxLayout, QWidget, QTreeView,
+                            QSplitter, QComboBox, QSizePolicy)
+import spyder
+from spyder.config.base import _
+from spyder.utils.qthelpers import create_plugin_layout
+from spyder_modelx.widgets.mxcodelist import MxCodeListWidget
+from spyder_modelx.widgets.mxtoolbar import MxToolBarMixin
+
+
+class MxTreeView(QTreeView):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.shell = None
+
+        # Context menu
+        self.contextMenu = QMenu(self)
+        self.action_update_formulas = \
+            self.contextMenu.addAction("Show Formulas")
+
+        self.setAlternatingRowColors(True)
+
+    def contextMenuEvent(self, event):
+        action = self.contextMenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == self.action_update_formulas:
+            index = self.currentIndex()
+            if index.isValid():
+                item = index.internalPointer()
+                if item.getType() == 'Space':
+                    # QMessageBox(text=item.itemData['fullname']).exec()
+                    self.shell.update_codelist(item.itemData['fullname'])
+
+
+class MxExplorer(QWidget):
+    """modelx widget."""
+
+    def __init__(self, parent):
+        QWidget.__init__(self, parent)
+
+        self.setWindowTitle("Mx explorer") # Not visible
+
+        self.treeview = treeview = MxTreeView(self)
+
+        # Main layout
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.treeview)
+        self.setLayout(layout)
+
+    def process_remote_view(self, data):
+        if data:
+            model = self.treeview.model()
+            if model:
+                if model.modelid == data['id']:
+                    if model.rootItem.itemData != data:
+                        model.updateRoot(ModelItem(data))
+                else:
+                    self.treeview.setModel(MxTreeModel(ModelItem(data)))
+            else:
+                self.treeview.setModel(MxTreeModel(ModelItem(data)))
+        else:
+            self.treeview.setModel(None)
+
+
+class MxMainWidget(MxToolBarMixin, QWidget):
+
+    def __init__(self, parent, **kwargs):
+        QWidget.__init__(self, parent)
+
+        self.plugin = parent
+
+        # Create tool bar
+        if "options_button" in kwargs:
+            self.options_button = kwargs["options_button"]
+        else:
+            self.options_button = None
+        self.plugin_actions = []
+        MxToolBarMixin.__init__(
+            self,
+            options_button=self.options_button,
+            plugin_actions=self.plugin_actions
+        )
+
+        # Create widget and add to dockwindow
+        self.explorer = MxExplorer(self)
+
+        # Create code list
+        self.codelist = MxCodeListWidget(self)
+
+        # Create splitter
+        self.splitter = QSplitter(self)
+        self.splitter.setContentsMargins(0, 0, 0, 0)
+        # self.splitter.addWidget(self.widget)
+        # self.splitter.setStretchFactor(0, 5)
+        # self.splitter.setStretchFactor(1, 1)
+
+        # Layout management
+        self.splitter.addWidget(self.explorer)
+        self.splitter.addWidget(self.codelist)
+
+        layout = create_plugin_layout(self.tools_layout, self.splitter)
+
+        self.setFocusPolicy(Qt.ClickFocus)
+        self.setLayout(layout)
+
+    def set_shellwidget(self, shellwidget):
+        """Bind shellwidget instance to namespace browser"""
+        self.shellwidget = shellwidget
+        self.shellwidget.set_mxexplorer(self.explorer, self.model_selector)
+        self.shellwidget.set_mxcodelist(self.codelist)
+
+    # MxToolBarMixin interface method
+    def setup_toolbar(self):
+
+        txt = _("Model")
+        if sys.platform == 'darwin':
+            expr_label = QLabel("  " + txt)
+        else:
+            expr_label = QLabel(txt)
+
+        expr_label.setAlignment(Qt.AlignCenter | Qt.AlignRight)
+
+        if spyder.version_info < (4,):
+            font = self.plugin.get_plugin_font()
+        else:
+            font = self.plugin.get_font()
+
+        self.model_selector = MxModelSelector(self)
+
+        return [expr_label, self.model_selector]
+
+
+class MxModelSelector(QComboBox):
+
+    sig_mxmodelselected = Signal()
+
+    def __init__(self, parent):
+        QComboBox.__init__(self, parent=parent)
+
+        # modellist is a list of dicts that contains basic model attributes.
+        # The first element represents the current model,
+        # and it can be None if no current model is set.
+        self.modellist = []
+
+
+    def get_selected_model(self, modellist=None):
+        """Gets the name of the selected model."""
+
+        if modellist:
+            self.update_modellist(modellist)
+
+        idx = self.currentIndex()
+        if idx < 0:
+            return ""
+        elif idx == 0 and not self.modellist[idx]:
+            return ""
+        else:
+            m = self.modellist[idx]
+            return m["name"]
+
+    def update_modellist(self, modellist):
+        """Update the list of models.
+
+        modellist can be [None, ...]
+
+        if the current model is previously selected, select the current model
+        after updating.
+        if the previously selected model does not exist after updating,
+        the current model is selected.
+        """
+
+        if not self.is_modellist_updated(modellist):
+            return
+
+        idx = self.currentIndex()
+
+        if idx > 0:
+            modelid = self.modellist[idx]["id"]
+
+        textlist = []
+        newidx = 0
+        for i, m in enumerate(modellist):
+            if not i:   # Current model
+                modelname = m["name"] if m else "None"
+                textlist.append("Current Model - %s" % modelname)
+            else:
+                if idx > 0 and m["id"] == modelid:
+                    newidx = i
+                textlist.append(m["name"])
+
+        self.clear()
+        self.addItems(textlist)
+
+        if idx > 0:
+            self.setCurrentIndex(newidx)
+
+        self.modellist = modellist
+
+    def is_modellist_updated(self, modellist):
+
+        if len(self.modellist) != len(modellist):
+            return True
+
+        for cur, oth in zip(self.modellist, modellist):
+            if cur is None:
+                if oth is None:
+                    continue
+                else:
+                    return True
+            if cur["name"] == oth["name"] and cur["id"] == oth["id"]:
+                continue
+            else:
+                return True
+
+        return False
+
