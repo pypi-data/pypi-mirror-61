004f9863315cc858db13ef620bacf1e1997cff82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:42 +0000

    Add package MemePy/1.0.6/MemePy-1.0.6.tar.gz

---
+++ packages/MemePy/MemePy-1.0.6.tar.gz/MemePy-1.0.6/MemePy/MemeGenerator.py
@@ -0,0 +1,24 @@
+from io import BytesIO
+
+from MemePy.MemeFactory import MemeFactory
+
+
+def get_meme_factory(template, args):
+    return MemeFactory.factory_from_template(template, args)
+
+
+def get_meme_image(template, args):
+    return get_meme_factory(template, args).output_image
+
+
+def get_meme_image_bytes(template, args):
+    image_bytes = BytesIO()
+    meme = MemeFactory.factory_from_template(template, args)
+    meme.output_image.save(image_bytes, format="PNG")
+    image_bytes.seek(0)
+    return image_bytes
+
+
+def save_meme_to_disk(template, path, args):
+    MemeFactory.factory_from_template(template, args).output_image.save(path + "\meme.png")
+    return "Image saved to " + path
