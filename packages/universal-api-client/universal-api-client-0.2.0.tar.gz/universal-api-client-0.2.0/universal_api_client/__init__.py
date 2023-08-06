6429f2509090893b3cb4bf927b43e79bbd31e96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:35 +0000

    Add package universal-api-client/0.2.0/universal-api-client-0.2.0.tar.gz

---
+++ packages/universal-api-client/universal-api-client-0.2.0.tar.gz/universal-api-client-0.2.0/universal_api_client/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for universal-api-client."""
+
+__author__ = """George Goranov"""
+__email__ = 'g.p.goranov@gmail.com'
+__version__ = '0.1.0'
+
+from .client import Client
+
+
+__all__ = ['Client']
