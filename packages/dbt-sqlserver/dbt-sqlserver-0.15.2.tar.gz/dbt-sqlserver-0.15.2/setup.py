d7e5b374ff04e2cf9fc1a6e9da8614104e725931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:41 +0000

    Add package dbt-sqlserver/0.15.2/dbt-sqlserver-0.15.2.tar.gz

---
+++ packages/dbt-sqlserver/dbt-sqlserver-0.15.2.tar.gz/dbt-sqlserver-0.15.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from distutils.core import setup
+
+package_name = "dbt-sqlserver"
+package_version = "0.15.2"
+description = """A sqlserver adpter plugin for dbt (data build tool)"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author="Mikael Ene",
+    author_email="mikael.ene@gmail.com",
+    url="https://github.com/mikaelene/dbt-sqlserver",
+    packages=find_packages(),
+    package_data={
+        'dbt': [
+            'include/sqlserver/dbt_project.yml',
+            'include/sqlserver/macros/*.sql',
+            'include/sqlserver/macros/**/*.sql',
+            'include/sqlserver/macros/**/**/*.sql',
+        ]
+    },
+    install_requires=[
+        'dbt-core>=0.15.0',
+        'pyodbc>=4.0.27',
+    ]
+)
