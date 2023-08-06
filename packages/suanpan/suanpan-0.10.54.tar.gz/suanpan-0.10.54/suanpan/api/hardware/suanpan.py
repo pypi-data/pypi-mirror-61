aa37b8e2cedcf89fbf0e2371dfb86a3125c1e75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:48 +0000

    Add package suanpan/0.10.54/suanpan-0.10.54.tar.gz

---
+++ packages/suanpan/suanpan-0.10.54.tar.gz/suanpan-0.10.54/suanpan/api/hardware/suanpan.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan import g
+
+
+class Interface(object):
+    def __init__(self, request):
+        super(Interface, self).__init__()
+        self.request = request
+
+    def init(self, suanpanUrl):
+        return self.request.post(
+            "/api/suanpan/init",
+            json={
+                "suanpanUrl": suanpanUrl,
+                "userId": g.userId,
+                "accessKey": g.accessKey,
+                "accessSecret": g.accessSecret,
+            },
+        )
