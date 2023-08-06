fa813ed0d9c9ad51f7bfc12b6c0c0b6c3d598d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:49 +0000

    Add package simplygopy/2020.2.1/simplygopy-2020.2.1-py3-none-any.whl

---
+++ packages/simplygopy/simplygopy-2020.2.1-py3-none-any.whl/simplygo/Constants.py
@@ -0,0 +1,22 @@
+# From p014sg.transitlink.base.Constants
+
+# Used for HMAC
+ABT_APP_ID = "e62e7c778eed4b5ba220a8d3c512a555"
+ABT_APP_KEY = "hbssWNf65LyAuYomc4HQTzopBw0MPOY/llZ54mTeKNM="
+
+# Used for Device object
+API_APP_VERSION = "6.0.0"
+
+# Fields used for API communication
+ABT_API_URL = 'https://abtapp.transitlink.com.sg/'
+API_ABT_GET_CARD_LIST = 'api/card/get'
+API_ABT_GET_MONTHLY_USAGES = 'api/card/GetUsages'
+API_ABT_GET_RECIPIENT = 'api/account/GetRecipient'
+API_ABT_GET_TRAIN_STATIONS = 'api/claims/GetTrainStations'
+API_ABT_GET_TRANSACTION = 'api/card/Transactions'
+API_ABT_NOTIFICATION = 'api/Notification/Get'
+API_ABT_SIGN_IN = 'api/account/SignIn'
+API_ABT_USER_DETAIL = 'api/account/Get'
+
+# Constants not from the transitlink app
+MYSQL_DATE_FORMAT = "%d-%m-%Y"
