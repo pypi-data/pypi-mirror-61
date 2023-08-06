54bb218aabfa70cd6dcaa0325569a119ac679cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:31 +0000

    Add package mbuild/0.10.5/mbuild-0.10.5.tar.gz

---
+++ packages/mbuild/mbuild-0.10.5.tar.gz/mbuild-0.10.5/mbuild/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.10.5'
+short_version = '0.10.5'
+full_version = '0.10.5'
+git_revision = '5a375d96aa0179497a8f64cfe1be28bca412f8c4'
+release = True
