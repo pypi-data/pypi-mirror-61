c1436090138654db518caa834aa6d9aeb33b0b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:32 +0000

    Add package zenoh/0.4.0/zenoh-0.4.0.tar.gz

---
+++ packages/zenoh/zenoh-0.4.0.tar.gz/zenoh-0.4.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='zenoh',
+    version='0.4.0',
+    packages=['zenoh', 'zenoh.net', 'zenoh.core'],
+    author='kydos',
+    description="Python client API for zenoh",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url='https://github.com/atolab/zenoh-python',
+    install_requires=['hexdump', 'mvar', 'papero==0.2.7'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
+        "Topic :: System :: Networking",
+        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)',
+        "Operating System :: OS Independent"]
+    )
