45d0f5c4ea4a215cec3aefa69c5b1d953a29b19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:26 +0000

    Add package actupac/0.1/actupac-0.1.tar.gz

---
+++ packages/actupac/actupac-0.1.tar.gz/actupac-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='actupac',
+      version='0.1',
+      description='Interpolate and extrapolate interest rates',
+      packages=['actupac'],
+      author = 'Jingde Guo',
+      author_email = 'guojingde17@163.com',
+      zip_safe=False)
