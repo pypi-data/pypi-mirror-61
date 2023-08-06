0553d832a51b68faa9ccc5966ce363c33a5860b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:26 +0000

    Add package pytreebank/0.2.7/pytreebank-0.2.7.tar.gz

---
+++ packages/pytreebank/pytreebank-0.2.7.tar.gz/pytreebank-0.2.7/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+
+def readfile(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pytreebank',
+    version='0.2.7',
+    description='Python package for loading Stanford Sentiment Treebank corpus',
+    long_description=readfile('README.md'),
+    long_description_content_type="text/markdown",
+    ext_modules=[],
+    packages=find_packages(),
+    author='Jonathan Raiman',
+    author_email='jonathanraiman@gmail.com',
+    url='https://github.com/JonathanRaiman/pytreebank',
+    download_url='https://github.com/JonathanRaiman/pytreebank',
+    keywords='Machine Learning, NLP',
+    license='MIT',
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    setup_requires=[],
+    install_requires=[],
+    include_package_data=True,
+)
