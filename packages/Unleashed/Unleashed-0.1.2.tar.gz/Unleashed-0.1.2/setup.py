96a2d81b46e9db1577384b01e4be71bd27b46883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:52 +0000

    Add package Unleashed/0.1.2/Unleashed-0.1.2.tar.gz

---
+++ packages/Unleashed/Unleashed-0.1.2.tar.gz/Unleashed-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from distutils.core import setup
+setup(
+  name = 'Unleashed',
+  packages = ['Unleashed'],
+  version = '0.1.2',
+  license='MIT',
+  description = 'A Python library to interact with the Unleashed API.',
+  author = 'ejedev',
+  author_email = 'ev3098@gmail.com',
+  url = 'https://github.com/ejedev/Unleashed',
+  download_url = 'https://github.com/ejedev/Unleashed/archive/v0.1.1.tar.gz',
+  keywords = ['Unleashed', 'API'],
+  install_requires=[
+    'wheel',
+          ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
