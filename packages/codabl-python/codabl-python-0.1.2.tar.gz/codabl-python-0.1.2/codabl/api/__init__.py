bde48fc95e1f1fb3351debf913aa45028cf7387e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:42 +0000

    Add package codabl-python/0.1.2/codabl-python-0.1.2.tar.gz

---
+++ packages/codabl-python/codabl-python-0.1.2.tar.gz/codabl-python-0.1.2/codabl/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from codabl.api.anomaly_api import AnomalyApi
+from codabl.api.integrations_api import IntegrationsApi
+from codabl.api.language_api import LanguageApi
