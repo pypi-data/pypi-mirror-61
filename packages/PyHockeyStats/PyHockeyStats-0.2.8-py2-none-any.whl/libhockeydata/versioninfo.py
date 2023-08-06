bcd671be8799cd6c65043aafe11b28f7852e3656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:24 +0000

    Add package PyHockeyStats/0.2.8/PyHockeyStats-0.2.8-py2-none-any.whl

---
+++ packages/PyHockeyStats/PyHockeyStats-0.2.8-py2-none-any.whl/libhockeydata/versioninfo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2015-2020 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2015-2020 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: versioninfo.py - Last Update: 2/17/2020 Ver. 0.2.8 RC 1 - Author: cooldude2k $
+'''
+
+import re;
+
+__program_name__ = "PyHockeyStats";
+__project__ = __program_name__;
+__project_url__ = "https://github.com/GameMaker2k/Neo-Hockey-Test";
+__version_info__ = (0, 2, 8, "RC 1", 1);
+__version_date_info__ = (2020, 2, 17, "RC 1", 1);
+__version_date__ = str(__version_date_info__[0])+"."+str(__version_date_info__[1]).zfill(2)+"."+str(__version_date_info__[2]).zfill(2);
+__revision__ = __version_info__[3];
+__revision_id__ = "$Id: 420389155015d678dd586e40e7c5f39b29272a1c $";
+#__revision_id__ = re.findall(r"\$Id: 420389155015d678dd586e40e7c5f39b29272a1c $", __full_revision_id__);
+if(__version_info__[4] is not None):
+ __version_date_plusrc__ = __version_date__+"-"+str(__version_date_info__[4]);
+if(__version_info__[4] is None):
+ __version_date_plusrc__ = __version_date__;
+if(__version_info__[3] is not None):
+ __version__ = str(__version_info__[0])+"."+str(__version_info__[1])+"."+str(__version_info__[2])+" "+str(__version_info__[3]);
+if(__version_info__[3] is None):
+ __version__ = str(__version_info__[0])+"."+str(__version_info__[1])+"."+str(__version_info__[2]);
