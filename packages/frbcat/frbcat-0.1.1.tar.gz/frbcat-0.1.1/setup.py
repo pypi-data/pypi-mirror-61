112ad3436a6155a159aafc31836a0c2bd01bdc6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:45 +0000

    Add package frbcat/0.1.1/frbcat-0.1.1.tar.gz

---
+++ packages/frbcat/frbcat-0.1.1.tar.gz/frbcat-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='frbcat',
+      version='0.1.1',
+      description='Query the FRB catalogue',
+      url='http://github.com/davidgardenier/frbcat',
+      author='David Gardenier',
+      author_email='davidgardenier@gmail.com',
+      license='MIT',
+      packages=['frbcat'],
+      install_requires=['pandas', 'requests', 'numpy'],
+      zip_safe=False)
