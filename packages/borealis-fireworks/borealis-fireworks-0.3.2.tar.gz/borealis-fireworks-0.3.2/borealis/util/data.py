4c41e33866d0582e52be09060c3e274bf1fde56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:32 +0000

    Add package borealis-fireworks/0.3.2/borealis-fireworks-0.3.2.tar.gz

---
+++ packages/borealis-fireworks/borealis-fireworks-0.3.2.tar.gz/borealis-fireworks-0.3.2/borealis/util/data.py
@@ -0,0 +1,25 @@
+"""Data utilities."""
+
+from __future__ import absolute_import, division, print_function
+
+import datetime
+from typing import Any, Dict, Iterable, Mapping, Optional
+
+
+def select_keys(mapping, keys, **kwargs):
+    # type: (Mapping[str, Any], Iterable[str], **Any) -> Dict[str, Any]
+    """Return a dict of the selected keys from mapping (if present) plus the
+    kwargs.
+    """
+    result = {key: mapping[key] for key in keys if key in mapping}
+    result.update(**kwargs)
+    return result
+
+
+def timestamp(dt=None):
+    # type: (Optional[datetime.datetime]) -> str
+    """Construct a datetime timestamp from `dt`, default = `now()`."""
+    if not dt:
+        dt = datetime.datetime.now()
+
+    return dt.strftime('%Y%m%d.%H%M%S')
