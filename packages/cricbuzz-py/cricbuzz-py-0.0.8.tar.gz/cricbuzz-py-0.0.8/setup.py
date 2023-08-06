a583272b4aefe36ecc8dba890107598f089bad71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:07 +0000

    Add package cricbuzz-py/0.0.8/cricbuzz-py-0.0.8.tar.gz

---
+++ packages/cricbuzz-py/cricbuzz-py-0.0.8.tar.gz/cricbuzz-py-0.0.8/setup.py
@@ -0,0 +1,25 @@
+name = "cricbuzz_py"
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cricbuzz-py",
+    version="0.0.8",
+    author="Akshay T",
+    author_email="sup.aks.apps@gmail.com",
+    description="A package to retrive cricket scores",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aksty/cricbuzz-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ],
+    python_requires='>=3.6',
+)
