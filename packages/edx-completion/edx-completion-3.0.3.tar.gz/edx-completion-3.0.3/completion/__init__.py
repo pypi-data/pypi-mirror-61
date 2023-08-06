4a0ec32a6bd22da932a2c42041703c7b383dacb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:19 +0000

    Add package edx-completion/3.0.3/edx-completion-3.0.3.tar.gz

---
+++ packages/edx-completion/edx-completion-3.0.3.tar.gz/edx-completion-3.0.3/completion/__init__.py
@@ -0,0 +1,10 @@
+"""
+Completion App
+"""
+
+from __future__ import unicode_literals
+
+
+__version__ = '3.0.3'
+
+default_app_config = 'completion.apps.CompletionAppConfig'  # pylint: disable=invalid-name
