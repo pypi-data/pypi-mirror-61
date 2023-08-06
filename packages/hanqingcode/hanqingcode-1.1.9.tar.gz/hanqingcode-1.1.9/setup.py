1017e884408815f6ecbe89b042139b802e9ad825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:26 +0000

    Add package hanqingcode/1.1.9/hanqingcode-1.1.9.tar.gz

---
+++ packages/hanqingcode/hanqingcode-1.1.9.tar.gz/hanqingcode-1.1.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+NAME = "hanqingcode"
+PACKAGES = ["pack"]
+VERSION = "1.1.9"
+URL = "https://pypi.org/project/hangqincode"
+AUTHOR = "hdy"
+AUTHOR_EMAIL = "1015296415@qq.com"
+LICENSE = "MIT"
+MODULES = ["hanqingcode"]
+DATAFILE = ["rose.txt"]
+DESC = "this is a gift for someone!"
+
+setup(
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    name=NAME,
+    version=VERSION,
+    license=LICENSE,
+    url=URL,
+    data_files=DATAFILE,
+    py_modules=MODULES,
+    description=DESC,
+    entry_points={"console_scripts": ["hanqingcode = hanqingcode:main"]},
+)
