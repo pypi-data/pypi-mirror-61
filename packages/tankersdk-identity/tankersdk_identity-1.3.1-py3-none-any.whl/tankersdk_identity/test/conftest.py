de4c60a862bdc836e97db73da0bd57e317834f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:14 +0000

    Add package tankersdk-identity/1.3.1/tankersdk_identity-1.3.1-py3-none-any.whl

---
+++ packages/tankersdk-identity/tankersdk_identity-1.3.1-py3-none-any.whl/tankersdk_identity/test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def test_app():
+    """ Note: this was generated with the Javascript SDK. We keep it to check Javascript
+    and Python code are interoperable
+
+    """
+    return {
+        "id": "AzES0aJwDCej9bQVY9AUMZBCLdX0msEc/TJ4DOhZaQs=",
+        "public_key":  "dOeLBpHz2IF37UQkS36sXomqEcEAjSyCsXZ7irn9UQA=",
+        "secret": "cBAq6A00rRNVTHicxNHdDFuq6LNUo6gAz58oKqy9CGd054sGkfPYgXftRCRLfqxeiaoRwQCNLIKxdnuKuf1RAA=="
+    }
