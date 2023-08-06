4bbebc0a96b6cbaa2514503d810b7cb6208438a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:54 +0000

    Add package omelet/0.0.0/omelet-0.0.0.tar.gz

---
+++ packages/omelet/omelet-0.0.0.tar.gz/omelet-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'omelet',
+    packages = ['omelet'],
+    version = '0.0.0',
+    description = 'pre-release',
+    author = 'Eunjong Kim',
+    author_email = 'eunjong@live.com',
+    url = 'https://github.com/yoonslab/omelet',
+    download_url = 'https://github.com/yoonslab/omelet/archive/0.0.0.tar.gz',
+    keywords = ['machine learning', 'auto ml', 'xai'],
+    classifiers = [],
+)
