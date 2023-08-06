43ec5dcf01daebf1eb58e77abde2642cf6b17035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:52 +0000

    Add package moco-wrapper/0.3.0.post2/moco_wrapper-0.3.0.post2-py2.py3-none-any.whl

---
+++ packages/moco-wrapper/moco_wrapper-0.3.0.post2-py2.py3-none-any.whl/moco_wrapper/models/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for moco-wrapper."""
+
+__author__ = """Sommalia"""
+__email__ = 'sommalia@protonmail.com'
+__version__ = '0.1.0'
+
+from .activity import Activity
+from .contact import Contact
+from .company import Company
+from .comment import Comment
+from .unit import Unit
+from .user import User, UserLanguage
+from .schedule import Schedule
+from .deal import Deal
+from .deal_category import DealCategory
+from .invoice import Invoice
+from .invoice_payment import InvoicePayment
+from .offer import Offer
+from .presence import Presence
+from .holiday import Holiday
+from .employment import Employment
+
+
+from .project import Project
+from .project_contract import ProjectContract
+from .project_expense import ProjectExpense
+from .project_task import ProjectTask
+from .project_recurring_expense import ProjectRecurringExpense
---
+++ packages/moco-wrapper/moco_wrapper-0.3.0.post2-py2.py3-none-any.whl/moco_wrapper/models/moco/activity.py
@@ -0,0 +1,3 @@
+class Activity(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
