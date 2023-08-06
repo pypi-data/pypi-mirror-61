683ad6da4e30c363a0d3cff2c152683b1f416a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:44 +0000

    Add package queue-utilities/0.0.6/queue_utilities-0.0.6.tar.gz

---
+++ packages/queue-utilities/queue_utilities-0.0.6.tar.gz/queue_utilities-0.0.6/queue_utilities/__init__.py
@@ -0,0 +1,15 @@
+"""queue_utilities - utility functions for a better queue experience
+  @jaycosaur / https://github.com/jaycosaur/queue-utilities
+"""
+
+name = "queue_utilities"
+
+from .select import Select
+from .multiplex import Multiplex
+from .multicast import Multicast
+from .pipe import Pipe
+from .ticker import Ticker
+from .timer import Timer
+from .as_thread import as_thread
+
+__version__ = "0.0.6"
---
+++ packages/queue-utilities/queue_utilities-0.0.6.tar.gz/queue_utilities-0.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="queue_utilities",
+    version="0.0.6",
+    author="Jacob Richter",
+    author_email="jaycorichter@gmail.com",
+    description="A collection of helpful queue utilities. Pipes, timers, tickers, multiplexors, multicasters and queue select.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaycosaur/queue-utilities",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
