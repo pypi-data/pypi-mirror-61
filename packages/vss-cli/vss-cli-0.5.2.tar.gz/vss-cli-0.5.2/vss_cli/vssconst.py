e327b84d3e0a228b804fb4c425c7e4ac5ca27623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:49 +0000

    Add package vss-cli/0.5.2/vss-cli-0.5.2.tar.gz

---
+++ packages/vss-cli/vss-cli-0.5.2.tar.gz/vss-cli-0.5.2/vss_cli/vssconst.py
@@ -0,0 +1,6 @@
+CONTENT_TYPE_JSON = 'application/json'
+CONTENT_TYPE_MULTIPART = 'multipart/x-mixed-replace; boundary={}'
+CONTENT_TYPE_TEXT_PLAIN = 'text/plain'
+
+STATUS_PAGE_ID = 'ftgqfszqxm8y'
+STATUS_PAGE_SERVICE = 'Virtual Server'
