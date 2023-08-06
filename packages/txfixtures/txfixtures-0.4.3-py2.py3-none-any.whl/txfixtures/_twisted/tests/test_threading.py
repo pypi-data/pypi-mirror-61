3b1e62a1789c6ca1ac2afd163d7ca9701d1c59d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:46 +0000

    Add package txfixtures/0.4.3/txfixtures-0.4.3-py2.py3-none-any.whl

---
+++ packages/txfixtures/txfixtures-0.4.3-py2.py3-none-any.whl/txfixtures/_twisted/tests/test_threading.py
@@ -0,0 +1,50 @@
+from six import b
+
+from testtools import TestCase
+
+from twisted.internet import reactor
+from twisted.internet.defer import (
+    Deferred,
+    succeed,
+    fail,
+)
+
+from txfixtures._twisted.threading import (
+    CallFromThreadTimeout,
+    interruptableCallFromThread,
+)
+
+from txfixtures.reactor import Reactor
+
+
+class InterruptableCallFromThreadTest(TestCase):
+
+    def setUp(self):
+        super(InterruptableCallFromThreadTest, self).setUp()
+        self.useFixture(Reactor(reactor))
+
+    def test_success(self):
+        """
+        If the async call executed in the thread succeeds, the result is
+        returned.
+        """
+        self.assertEqual(
+            "hello",
+            interruptableCallFromThread(reactor, 1, lambda: succeed("hello")))
+
+    def test_fail(self):
+        """
+        If the async call executed in the thread fails, an exception is raised.
+        """
+        self.assertRaises(
+            RuntimeError, interruptableCallFromThread, reactor, 1,
+            lambda: fail(RuntimeError("boom")))
+
+    def test_timeout(self):
+        """
+        If the async call takes more than the given timeout to execuluted, an
+        error is raised.
+        """
+        self.assertRaises(
+            CallFromThreadTimeout, interruptableCallFromThread,
+            reactor, 0.1, lambda: Deferred())
---
+++ packages/txfixtures/txfixtures-0.4.3-py2.py3-none-any.whl/txfixtures/osutils.py
@@ -0,0 +1,132 @@
+# Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
+# GNU General Public License version 3.
+
+
+"""General os utilities useful for txfxtures."""
+
+
+import errno
+import os
+import os.path
+from signal import (
+    SIGKILL,
+    SIGTERM,
+    )
+import socket
+import time
+
+
+def _kill_may_race(pid, signal_number):
+    """Kill a pid accepting that it may not exist."""
+    try:
+        os.kill(pid, signal_number)
+    except OSError as e:
+        if e.errno in (errno.ESRCH, errno.ECHILD):
+            # Process has already been killed.
+            return
+        # Some other issue (e.g. different user owns it)
+        raise
+
+
+def get_pid_from_file(pidfile_path):
+    """Retrieve the PID from the given file, if it exists, None otherwise."""
+    # Get the pid, ignoring failures due to the pidfile not existing.
+    try:
+        with open(pidfile_path, 'r') as fd:
+            pid = fd.read().split()[0]
+    except IOError:
+        return None
+    try:
+        pid = int(pid)
+    except ValueError:
+        # pidfile contains rubbish
+        return None
+    return pid
+
+
+def process_exists(pid):
+    """Return True if the specified process already exists."""
+    try:
+        os.kill(pid, 0)
+    except os.error as err:
+        if err.errno == errno.ESRCH:
+            # All is well - the process doesn't exist.
+            return False
+        else:
+            # We got a strange OSError, which we'll pass upwards.
+            raise
+    return True
+
+
+def two_stage_kill(pid, poll_interval=0.1, num_polls=50):
+    """Kill process 'pid' with SIGTERM. If it doesn't die, SIGKILL it.
+
+    :param pid: The pid of the process to kill.
+    :param poll_interval: The polling interval used to check if the
+        process is still around.
+    :param num_polls: The number of polls to do before doing a SIGKILL.
+    """
+    # Kill the process.
+    _kill_may_race(pid, SIGTERM)
+
+    # Poll until the process has ended.
+    for i in range(num_polls):
+        # If the process isn't gone yet, continue.
+        if not process_exists(pid):
+            return
+        time.sleep(poll_interval)
+
+    # The process is still around, so terminate it violently.
+    _kill_may_race(pid, SIGKILL)
+
+
+def kill_by_pidfile(pidfile_path, poll_interval=0.1, num_polls=50):
+    """Kill a process identified by the pid stored in a file.
+
+    The pid file is removed from disk.
+    """
+    try:
+        pid = get_pid_from_file(pidfile_path)
+        if pid is None:
+            return
+        two_stage_kill(pid, poll_interval, num_polls)
+    finally:
+        remove_if_exists(pidfile_path)
+
+
+def remove_if_exists(path):
+    """Remove the given file if it exists."""
+    try:
+        os.remove(path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
+def until_no_eintr(retries, function, *args, **kwargs):
+    """Run 'function' until it doesn't raise EINTR errors.
+
+    :param retries: The maximum number of times to try running 'function'.
+    :param function: The function to run.
+    :param *args: Arguments passed to the function.
+    :param **kwargs: Keyword arguments passed to the function.
+    :return: The return value of 'function'.
+    """
+    if not retries:
+        return
+    for i in range(retries):
+        try:
+            return function(*args, **kwargs)
+        except (IOError, OSError) as e:
+            if e.errno == errno.EINTR:
+                continue
+            raise
+        except socket.error as e:
+            # In Python 2.6 we can use IOError instead.  It also has
+            # reason.errno but we might be using 2.5 here so use the
+            # index hack.
+            if e[0] == errno.EINTR:
+                continue
+            raise
+    else:
+        raise
