a808a5837ccda1ad58c407ca675d048ffdc85f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:09 +0000

    Add package privatize/0.0.10/privatize-0.0.10.tar.gz

---
+++ packages/privatize/privatize-0.0.10.tar.gz/privatize-0.0.10/privatize/udf.py
@@ -0,0 +1,27 @@
+"""
+udf.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+import numpy as np
+from refunction import Refunction
+
+
+# Ensure that pandas DataFrame columns are not changed
+def fix_pandas_columns(columns):
+    """
+    
+    Parameters
+    ----------
+    columns
+
+    Returns
+    -------
+
+    """
+
+    def _fix_pandas_columns(df, columns):
+        return np.array_equal(df.columns, columns)
+    return Refunction(_fix_pandas_columns, columns=columns)
+
