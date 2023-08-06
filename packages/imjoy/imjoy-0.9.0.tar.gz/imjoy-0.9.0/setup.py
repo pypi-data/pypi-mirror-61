f22b25bcf7153a2893996f8f2b99cf1765691637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:27 +0000

    Add package imjoy/0.9.0/imjoy-0.9.0.tar.gz

---
+++ packages/imjoy/imjoy-0.9.0.tar.gz/imjoy-0.9.0/setup.py
@@ -0,0 +1,48 @@
+"""Set up the ImJoy-Engine imjoy package."""
+import os
+from setuptools import setup, find_packages
+import json
+
+DESCRIPTION = (
+    "ImJoy Plugin Engine for running Python plugins locally "
+    "or remotely from ImJoy.io"
+)
+
+REQUIREMENTS = [
+    "python-engineio==3.9.1",
+    "python-socketio[client]==4.4.0",
+    "numpy",
+    'janus;python_version>"3.3"',
+    'pathlib;python_version<"3.4"',
+]
+ENGINE_REQUIREMENTS = ["aiohttp", "aiohttp_cors", "gputil", "pyyaml"]
+
+ROOT_DIR = os.path.dirname(__file__)
+with open(os.path.join(ROOT_DIR, "README.md"), "r") as f:
+    README = f.read()
+
+with open(os.path.join(ROOT_DIR, "imjoy", "VERSION"), "r") as f:
+    VERSION = json.load(f)["version"]
+
+setup(
+    name="imjoy",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="http://github.com/oeway/ImJoy-Engine",
+    author="Wei OUYANG",
+    author_email="oeway007@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    extras_require={
+        "engine": ENGINE_REQUIREMENTS,
+        "worker": [],
+        "jupyter": ["jupyter"],
+        "jupyter-worker": ["ipykernel"],
+    },
+    zip_safe=False,
+    entry_points={"console_scripts": ["imjoy = imjoy.__main__:main"]},
+)
