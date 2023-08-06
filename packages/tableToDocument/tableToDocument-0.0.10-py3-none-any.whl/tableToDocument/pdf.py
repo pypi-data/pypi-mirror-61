f95932a2be28a03b79a5ce021d183b9bdf9498b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:01 +0000

    Add package tableToDocument/0.0.10/tableToDocument-0.0.10-py3-none-any.whl

---
+++ packages/tableToDocument/tableToDocument-0.0.10-py3-none-any.whl/tableToDocument/pdf.py
@@ -0,0 +1,8 @@
+import toDoc
+import PyPDF2
+
+def write(toPdf):
+    with open(toPdf.path,"rb") as p:
+        pread = PyPDF2.PdfFileReader(p)
+        txt = pread.getPage(5)
+        txt.extractText()
