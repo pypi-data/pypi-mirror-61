374d2fa41c798af03d0495df54db6d5d8d0173a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:10 +0000

    Add package georipe/1.0.6.1/georipe-1.0.6.1.tar.gz

---
+++ packages/georipe/georipe-1.0.6.1.tar.gz/georipe-1.0.6.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+  name = 'georipe',
+  packages = ['georipe'],
+  version = '1.0.6.1',
+  description = 'RIPE and GEOIP mass searching reconnaissance tool',
+  author = 'USSC',
+  author_email = 'usscltd@gmail.com',
+  url = 'https://github.com/USSCltd/georipe',
+  bugtrack_url = 'https://github.com/USSCltd/georipe/issues',
+  keywords = ['geoip', 'ripe', 'whois', 'networks', 'recon', 'reconnaissance', 'world'],
+  classifiers = [],
+  scripts=['bin/geoip', 'bin/rwhois'],
+  install_requires=[
+    'argparse',
+    'netaddr',
+    'pykml',
+    'lxml',
+    'ipwhois',
+    'folium'
+  ]
+)
