589dc8fa30b9ae80416d8a3c81672d88320056f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:12 +0000

    Add package requests-ntlm2/6.0.11/requests_ntlm2-6.0.11.tar.gz

---
+++ packages/requests-ntlm2/requests_ntlm2-6.0.11.tar.gz/requests_ntlm2-6.0.11/requests_ntlm2/__init__.py
@@ -0,0 +1,13 @@
+from .adapters import HttpNtlmAdapter, HttpProxyAdapter
+from .connection import HTTPConnection, HTTPSConnection, VerifiedHTTPSConnection
+from .requests_ntlm2 import HttpNtlmAuth
+
+
+__all__ = (
+    "HttpNtlmAuth",
+    "HttpNtlmAdapter",
+    "HttpProxyAdapter",
+    "HTTPConnection",
+    "HTTPSConnection",
+    "VerifiedHTTPSConnection",
+)
