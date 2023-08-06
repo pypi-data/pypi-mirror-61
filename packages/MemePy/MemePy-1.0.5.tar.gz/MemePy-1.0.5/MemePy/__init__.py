9ec0a1cae687a27bbf7476bfa9c25dd8e32dbd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:41 +0000

    Add package MemePy/1.0.5/MemePy-1.0.5.tar.gz

---
+++ packages/MemePy/MemePy-1.0.5.tar.gz/MemePy-1.0.5/MemePy/__init__.py
@@ -0,0 +1,28 @@
+import sys
+
+from MemePy.MemeGenerator import save_meme_to_disk
+
+
+def h():
+    print("Usage: MemePy <template> <path> <*args> <*options>\n"
+          "Visit https://github.com/julianbrandt/src for detailed instructions")
+    sys.exit()
+
+
+def main(args):
+    if args.__contains__("MemePy"):
+        args.remove("MemePy")
+    if len(args) <= 2:
+        h()
+    try:
+        path = args[1]
+        template = args[0]
+        save_meme_to_disk(template, path, args[2:])
+    except ValueError as e:
+        print(str(e))
+        h()
+    except FileNotFoundError:
+        print("No such directory: " + path)
+
+if __name__ == "__main__":
+    main(sys.argv)
