d006ebb2ba00e5782b3df53e209cfae1e47f92da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:29 +0000

    Add package animals.py/1.0.0a1/animals.py-1.0.0a1-py3-none-any.whl

---
+++ packages/animals.py/animals.py-1.0.0a1-py3-none-any.whl/animals/client.py
@@ -0,0 +1,52 @@
+import requests
+import random
+
+class RequestError(Execption):
+	"""Base Exception"""
+	pass
+
+class Animals:
+	"""
+	Base Class for the module. This module uses https://some-random-api.ml/. 
+	This module can be used to get random animals image/fact. 
+	This is a simple module, which uses requests library.
+	
+	Attributes
+	---------------
+	
+	image
+		Returns the url of the animal image
+		
+		Parameters
+		------------------
+		animal: :class: `str`
+			The animal which you are going to get the url.
+			If not animal given it will give a random image.
+			
+	fact
+		Return the fact of the animal image
+		
+		Parameters
+		------------------
+		animal: :class: `str`
+			The animal which you are going to get the fact.
+			If not animal given it will give a random fact
+	"""
+	
+	def image(animal=None):
+		options = ("cat", "dog", "koala", "fox", "birb", "red_panda", "panda", "racoon", "kangaroo")
+		if not animals in options and animal != None:
+			raise RequestError(animal + "is not a valid options\nValid Options are cat, dog, koala, fox, birb, red_panda, panda, racoon, kangaroo")
+			
+		if animal == None:
+			choice = random.choice(options)
+			r = requests.get(f"https://some-random-api.ml/img/{choice}")
+			r = r.json()
+			url = r["link"]
+			return url
+			
+		else:
+			r = requests.get(f"https://some-random-api.ml/img/{animal}")
+			r = r.json()
+			url = r["link"]
+			return url
