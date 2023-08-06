51e9b76ba7226e7818d672c0605e57c6909ab7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:53 +0000

    Add package secure-aiohttp/0.0.1/secure-aiohttp-0.0.1.tar.gz

---
+++ packages/secure-aiohttp/secure-aiohttp-0.0.1.tar.gz/secure-aiohttp-0.0.1/README.md
@@ -0,0 +1,11 @@
+# secure-aiohttp
+
+## not ready, wip for now
+csrf/ssl/sql injectctions etc.
+
+
+
+
+# License
+
+`secure-aiohttp` is offered under the Apache 2 license.
---
+++ packages/secure-aiohttp/secure-aiohttp-0.0.1.tar.gz/secure-aiohttp-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="secure-aiohttp",
+    version="0.0.1",
+    author="Danil Pekarchuk",
+    author_email="danilopeka321@gmail.com",
+    description="aiohttp additional security",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pinlast/secure-aiohttp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: AsyncIO',
+      ],
+    python_requires='>=3.6',
+    license='Apache 2',
+)
