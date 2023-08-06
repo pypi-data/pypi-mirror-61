3f5c7bf0ab4a84d46789055e0ba385fbb39b6a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:45 +0000

    Add package vkbee/2.6/vkbee-2.6.tar.gz

---
+++ packages/vkbee/vkbee-2.6.tar.gz/vkbee-2.6/vkbee/exceptions.py
@@ -0,0 +1,23 @@
+TOO_MANY_RPS_CODE = 6
+
+
+class BasicError(Exception):
+    pass
+
+
+class api_error(BasicError):
+    def __init__(self, vk, method, values, error):
+        self.vk = vk
+        self.method = method
+        self.values = values
+        self.code = error["error_code"]
+        self.error = error
+
+    def try_method(self):
+        """ Отправить запрос заново """
+
+        return self.vk.method(self.method, self.values)
+
+    def __str__(self):
+        return "{} ({})".format(self.error["error_msg"], self.code)
+
