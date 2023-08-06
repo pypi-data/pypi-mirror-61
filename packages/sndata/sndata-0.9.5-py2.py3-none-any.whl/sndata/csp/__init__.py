401ef1e5365fbcad0f243fce9d55f30688101b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:59 +0000

    Add package sndata/0.9.5/sndata-0.9.5-py2.py3-none-any.whl

---
+++ packages/sndata/sndata-0.9.5-py2.py3-none-any.whl/sndata/csp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""The ``csp`` module provides access to data from the Carnegie Supernova
+Project (CSP). It includes data from the first (DR1) and third (DR3) data
+releases. For more information on CSP see https://csp.obs.carnegiescience.edu .
+"""
+
+from . import dr1, dr3
+
+survey_name = 'Carnegie Supernova Project'
+survey_abbrev = 'CSP'
