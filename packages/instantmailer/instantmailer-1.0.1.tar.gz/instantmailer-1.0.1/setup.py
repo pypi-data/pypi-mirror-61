206c28478499c66dcbf2be0661206399c9fab7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:56 +0000

    Add package instantmailer/1.0.1/instantmailer-1.0.1.tar.gz

---
+++ packages/instantmailer/instantmailer-1.0.1.tar.gz/instantmailer-1.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+from setuptools import setup
+	
+setup(
+	name="instantmailer",
+	author="mischievousdev",
+	author_email="miscdev.py@gmail.com",
+	version="1.0.1",
+	url="https://github.com/Pythonastics/instant-mailer",
+	packages=setuptools.find_packages(),
+	license="MIT",
+	keywords='gmail email wrapper'
+)
