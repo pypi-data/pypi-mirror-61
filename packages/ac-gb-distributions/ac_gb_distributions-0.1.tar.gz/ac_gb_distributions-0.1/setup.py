c45017044d1d451f244c56aa0120670047623f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:12 +0000

    Add package ac-gb-distributions/0.1/ac_gb_distributions-0.1.tar.gz

---
+++ packages/ac-gb-distributions/ac_gb_distributions-0.1.tar.gz/ac_gb_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ac_gb_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ac_gb_distributions'],
+      zip_safe=False)
