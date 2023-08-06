736e50ed97ad494e74bafc671a34780613cbffed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:15 +0000

    Add package fulcrum/1.11.0/fulcrum-1.11.0-py3.7.egg

---
+++ packages/fulcrum/fulcrum-1.11.0-py3.7.egg/fulcrum/api/endpoints.py
@@ -0,0 +1,116 @@
+from fulcrum.mixins import Findable, Deleteable, Createable, Searchable, Updateable, Media, Track, MediaCreateable
+from . import BaseAPI
+
+
+class Forms(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'forms'
+    
+    def history(self, id):
+        api_resp = api_resp = self.client.call('get', '{0}/{1}/history'.format(self.path, id))
+        return api_resp
+
+class Records(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'records'
+
+    def history(self, id):
+        api_resp = api_resp = self.client.call('get', '{0}/{1}/history'.format(self.path, id))
+        return api_resp
+
+class Webhooks(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'webhooks'
+
+
+class Photos(BaseAPI, Findable, Searchable, Media, MediaCreateable):
+    path = 'photos'
+    ext = 'jpg'
+    sizes = ['thumbnail', 'large']
+    media_upload_path = ''
+    media_form_field_name = 'photo'
+    default_content_type = 'image/jpeg'
+
+
+class Signatures(BaseAPI, Findable, Searchable, Media, MediaCreateable):
+    path = 'signatures'
+    ext = 'png'
+    sizes = ['thumbnail', 'large']
+    media_upload_path = ''
+    media_form_field_name = 'signature'
+    default_content_type = 'image/png'
+
+
+class Videos(BaseAPI, Findable, Searchable, Media, Track, MediaCreateable):
+    path = 'videos'
+    ext = 'mp4'
+    sizes = ['small', 'medium']
+    media_upload_path = '/upload'
+    media_form_field_name = 'video'
+    default_content_type = 'video/mp4'
+
+
+class Audio(BaseAPI, Findable, Searchable, Media, Track, MediaCreateable):
+    path = 'audio'
+    ext = 'mp4'
+    sizes = []
+    media_upload_path = '/upload'
+    media_form_field_name = 'audio'
+    default_content_type = 'audio/mp3'
+
+
+class Memberships(BaseAPI, Searchable):
+    path = 'memberships'
+
+    def change(self, resource_type, id, action, membership_ids):
+        change = {
+            'type': '{}_members'.format(resource_type),
+            '{}_id'.format(resource_type): id,
+            action: membership_ids
+        }
+        data = {'change': change}
+        api_resp = self.client.call('post', 'memberships/change_permissions',
+                                    data=data,
+                                    extra_headers={'Content-Type': 'application/json'})
+        return api_resp
+
+
+class Roles(BaseAPI, Searchable):
+    path = 'roles'
+
+
+class ChoiceLists(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'choice_lists'
+
+
+class ClassificationSets(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'classification_sets'
+
+
+class Projects(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'projects'
+
+
+class Changesets(BaseAPI, Findable, Createable, Searchable, Updateable):
+    path = 'changesets'
+
+    def close(self, id):
+        api_resp = api_resp = self.client.call('put', '{0}/{1}/close'.format(self.path, id))
+        return api_resp
+
+
+class ChildRecords(BaseAPI, Searchable):
+    path = 'child_records'
+
+
+class AuditLogs(BaseAPI, Searchable, Findable):
+    path = 'audit_logs'
+
+
+class Layers(BaseAPI, Findable, Deleteable, Createable, Searchable, Updateable):
+    path = 'layers'
+
+
+class Authorizations(BaseAPI, Findable, Deleteable, Searchable, Updateable):
+    path = 'authorizations'
+
+    def regenerate(self, id):
+        api_resp = self.client.call('post', '{}/{}/regenerate'.format(self.path, id))
+        return api_resp
---
+++ packages/fulcrum/fulcrum-1.11.0-py3.7.egg/fulcrum/mixins.py
@@ -0,0 +1,81 @@
+from fulcrum.utils import is_string, generate_uuid
+
+class Findable(object):
+    def find(self, id):
+        api_resp = self.client.call('get', '{0}/{1}'.format(self.path, id))
+        return api_resp
+
+
+class Deleteable(object):
+    def delete(self, id):
+        self.client.call('delete', '{0}/{1}'.format(self.path, id))
+
+
+class Createable(object):
+    def create(self, obj):
+        api_resp = self.client.call('post', self.path, data=obj, extra_headers={'Content-Type': 'application/json'})
+        return api_resp
+
+
+class Searchable(object):
+    def search(self, url_params=None):
+        api_resp = self.client.call('get', self.path, url_params=url_params)
+        return api_resp
+
+
+class Updateable(object):
+    def update(self, id, obj):
+        api_resp = self.client.call('put', '{0}/{1}'.format(self.path, id), data=obj, extra_headers={'Content-Type': 'application/json'})
+        return api_resp
+
+
+class Media(object):
+    def media(self, id, size='original'):
+        if size == 'original':
+            path = '{}/{}.{}'.format(self.path, id, self.ext)
+        else:
+            if not size in self.sizes:
+                raise ValueError('Size {} not supported'.format(size))
+            path = '{}/{}/{}.{}'.format(self.path, id, size, self.ext)
+
+        api_resp = self.client.call('get', path, json_content=False)
+        return api_resp
+
+
+class Track(object):
+    track_formats = {
+        'json': 'json',
+        'geojson': 'geojson',
+        'gpx': 'gpx',
+        'kml': 'kml',
+        'geojson_points': 'geojson?type=points',
+    }
+
+    def track(self, id, format='json'):
+        if not format in self.track_formats.keys():
+            raise ValueError('Format {} not supported'.format(format))
+        path = '{}/{}/track.{}'.format(self.path, id, self.track_formats[format])
+
+        is_json_resp = format in ('json', 'geojson', 'geojson_points')
+
+        api_resp = self.client.call('get', path, json_content=is_json_resp)
+        return api_resp
+
+
+class MediaCreateable(object):
+    def create(self, media_or_path, content_type=None, access_key=None):
+        if is_string(media_or_path):
+            media = open(media_or_path, 'rb')
+        else:
+            media = media_or_path
+
+        data = {
+            '{}[access_key]'.format(self.media_form_field_name): access_key or generate_uuid()
+        }
+
+        files = {
+            '{}[file]'.format(self.media_form_field_name): (media.name, media, content_type or self.default_content_type)
+        }
+
+        api_resp = self.client.call('post', self.path + self.media_upload_path, data=data, files=files)
+        return api_resp
