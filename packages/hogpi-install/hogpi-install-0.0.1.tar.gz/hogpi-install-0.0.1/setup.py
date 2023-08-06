f8d38b0157f7aed8ef08eb4720cb9bfbe0b58b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:59 +0000

    Add package hogpi-install/0.0.1/hogpi-install-0.0.1.tar.gz

---
+++ packages/hogpi-install/hogpi-install-0.0.1.tar.gz/hogpi-install-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hogpi-install", # Replace with your own username
+    version="0.0.1",
+    author="Jack Whitehorn",
+    author_email="jackwh.whitehorn@gmail.com",
+    description="The official tech4nature installer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['hogpi-install'],
+    entry_points='''
+        [console_scripts]
+        hogpi-install=main:cli
+    ''',
+)
+set
