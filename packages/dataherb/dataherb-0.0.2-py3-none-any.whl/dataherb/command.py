27a3ae07f22c3906479c0079ea5286f980854121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:20 +0000

    Add package dataherb/0.0.2/dataherb-0.0.2-py3-none-any.whl

---
+++ packages/dataherb/dataherb-0.0.2-py3-none-any.whl/dataherb/command.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+import click
+
+from dataherb.flora import Flora
+from dataherb.parse.model import MetaData
+
+__CWD__ = os.getcwd()
+
+logging.basicConfig()
+logger = logging.getLogger("dataherb.command")
+
+
+_FLORA = Flora()
+
+_FLORA.herb("geonames_timezone").leaves.get("dataset/geonames_timezone.csv").data
+
+@click.group()
+def dataherb():
+    click.echo("Hello {}".format(os.environ.get('USER', '')))
+    click.echo("Welcome to DataHerb.")
+
+@dataherb.command()
+def search(keywords, ids):
+    click.echo('Search Herbs in DataHerb Flora ...')
+    _FLORA.search()
+
+@dataherb.command()
+@click.confirmation_option(
+    prompt=f"Your current working directory is {__CWD__}\n"
+    "The .dataherb folder will be created right here.\n"
+    "Are you sure this is the correct path?"
+)
+def create():
+
+    md = MetaData()
+    md.create()
+
+    click.echo(
+        "The .dataherb folder and metadata.yml file has been created inside \n"
+        f"{__CWD__}"
+    )
+
+if __name__ == "__main__":
+    fl = Flora()
+    pass
