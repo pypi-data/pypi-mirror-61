7a4b7a251d1226d0fc9a57539b0a64550c03b787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:20 +0000

    Add package slate-utils/0.0.4.2/slate_utils-0.0.4.2.tar.gz

---
+++ packages/slate-utils/slate_utils-0.0.4.2.tar.gz/slate_utils-0.0.4.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="slate_utils",
+    version="0.0.4.2",
+    author="Jamie Davis",
+    author_email="jamjam@umich.edu",
+    description=("Some simple utilites to help automate tasks in Slate."),
+    license="BSD",
+    packages=["slate_utils"],
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[],
+    install_requires=["loguru", "requests", "selenium", "umdriver"],
+)
