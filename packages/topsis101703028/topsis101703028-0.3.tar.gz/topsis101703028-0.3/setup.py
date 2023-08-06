885ac3f9930b18587f25d8674b94c0f7e80d3efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:29 +0000

    Add package topsis101703028/0.3/topsis101703028-0.3.tar.gz

---
+++ packages/topsis101703028/topsis101703028-0.3.tar.gz/topsis101703028-0.3/setup.py
@@ -0,0 +1,51 @@
+from distutils.core import setup
+
+setup(
+
+  name = 'topsis101703028',
+
+  packages = ['topsis101703028'],
+
+  version = '0.3',
+
+  license='MIT',
+
+  description = 'Python package to implement TOPSIS',
+
+  author = 'Abhishek Shivaram',
+
+  author_email = 'ashivaram_be17@thapar.com',
+
+  url = 'https://github.com/shivaram-abhishek/topsis101703028',
+
+  download_url = 'https://github.com/shivaram-abhishek/topsis/archive/0.3.tar.gz',
+
+  keywords = ['TOPSIS'],
+
+  install_requires=[
+
+          'numpy'
+    
+      ],
+
+  classifiers=[
+
+    'Development Status :: 3 - Alpha',
+
+    'Intended Audience :: Developers',
+
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3',
+
+    'Programming Language :: Python :: 3.4',
+
+    'Programming Language :: Python :: 3.5',
+
+    'Programming Language :: Python :: 3.6',
+
+  ],
+
+)
