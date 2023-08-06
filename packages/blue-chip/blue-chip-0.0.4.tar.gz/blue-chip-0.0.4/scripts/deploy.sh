779f5573910b840fa353e0ab699f62665f2b9641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:48 +0000

    Add package blue-chip/0.0.4/blue-chip-0.0.4.tar.gz

---
+++ packages/blue-chip/blue-chip-0.0.4.tar.gz/blue-chip-0.0.4/scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export FLIT_PASSWORD="pypi-AgEIcHlwaS5vcmcCJGNmYmM0ZDY3LTUyYjYtNDk0Yi1iNTQwLTlhZWNlNGZmNGYzYgACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgST8aTOCfPNU6Rdsc3tZPmI8X0SS1d6-dN06I2Oa-PwA"
+export FLIT_INDEX_URL="https://upload.pypi.org/legacy/"
+flit publish
