5c0ddb0147e9105c2db22ec5d5232c9a7cc36f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:26 +0000

    Add package scout-browser/4.12.3/scout-browser-4.12.3.tar.gz

---
+++ packages/scout-browser/scout-browser-4.12.3.tar.gz/scout-browser-4.12.3/tests/commands/update/test_update_genes_cmd.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from scout.commands import cli
+
+
+def test_update_genes_wrong_omim_key(mock_app):
+    """Tests the CLI that updates genes in database"""
+
+    runner = mock_app.test_cli_runner()
+    assert runner
+
+    # Test CLI base, provide non-valid API key
+    result = runner.invoke(cli, ["update", "genes", "--api-key", "not_a_valid_key"])
+    assert (
+        "Seems like url https://data.omim.org/downloads/not_a_valid_key/morbidmap.txt does not exist"
+        in result.output
+    )
