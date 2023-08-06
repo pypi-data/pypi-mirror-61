2c77654bbb11f39130c4ea31e498985f00bc0ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:30 +0000

    Add package math3/1.0/math3-1.0.tar.gz

---
+++ packages/math3/math3-1.0.tar.gz/math3-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='math3', #模块的名称
+    version='1.0', #版本号
+    description='这是第一个发布的版本', #描述
+    author='xiaofeiyan', #作者
+    author_email='1276813302@qq.com', #邮箱
+    py_modules=['math3.demo_math01','math3.demo_math02'] #要发布的模块
+)
