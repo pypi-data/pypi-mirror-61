b321d495ec404b39c05e1094ab4b5f6fd0f70607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:06 +0000

    Add package pipdate/0.4.0/pipdate-0.4.0.tar.gz

---
+++ packages/pipdate/pipdate-0.4.0.tar.gz/pipdate-0.4.0/setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, "pipdate", "__about__.py"), "rb") as f:
+    exec(f.read(), about)
+
+
+setup(
+    name="pipdate",
+    version=about["__version__"],
+    packages=find_packages(),
+    url="https://github.com/nschloe/pipdate",
+    author=about["__author__"],
+    author_email=about["__email__"],
+    install_requires=["appdirs", "requests"],
+    python_requires=">=3.6",
+    description="pip update helpers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license=about["__license__"],
+    classifiers=[
+        about["__status__"],
+        about["__license__"],
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Version Control",
+        "Topic :: System :: Software Distribution",
+    ],
+    entry_points={
+        "console_scripts": [
+            "{} = pipdate.cli:update".format(
+                "pipdate" if sys.version_info[0] < 3 else "pipdate3"
+            )
+        ]
+    },
+)
