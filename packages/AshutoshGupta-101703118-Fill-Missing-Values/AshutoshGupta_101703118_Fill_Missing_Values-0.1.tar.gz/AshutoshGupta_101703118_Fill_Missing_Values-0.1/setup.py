257b61c45ec657c6d39c4f01c1aceedc37851fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:43 +0000

    Add package AshutoshGupta-101703118-Fill-Missing-Values/0.1/AshutoshGupta_101703118_Fill_Missing_Values-0.1.tar.gz

---
+++ packages/AshutoshGupta-101703118-Fill-Missing-Values/AshutoshGupta_101703118_Fill_Missing_Values-0.1.tar.gz/AshutoshGupta_101703118_Fill_Missing_Values-0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 
+
+@author: ashu
+"""
+#Made by Ashutosh Gupta 101703118
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AshutoshGupta_101703118_Fill_Missing_Values",
+    version="0.1",
+    author="Ashutosh Gupta",
+    author_email="agupta12_be17@thapar.edu",
+    description="A small package for filling missing values in the given dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = ['command-line', 'Missing values', 'missing-values'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
