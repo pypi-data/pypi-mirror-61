d22abca3ef6d1eeb5327f2b7dfc8c29012539925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:56 +0000

    Add package twofactorauthcli/0.1.1/twofactorauthcli-0.1.1.tar.gz

---
+++ packages/twofactorauthcli/twofactorauthcli-0.1.1.tar.gz/twofactorauthcli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['twofactorauthcli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyotp>=2.3.0,<3.0.0', 'pyyaml>=5.3,<6.0', 'tabulate>=0.8.6,<0.9.0']
+
+entry_points = \
+{'console_scripts': ['2fa = twofactorauthcli:main']}
+
+setup_kwargs = {
+    'name': 'twofactorauthcli',
+    'version': '0.1.1',
+    'description': '2FA in your computer',
+    'long_description': '# 2fa-cli\n\n2FA in your computer\n',
+    'author': 'Angel Fernandez',
+    'author_email': 'angelfernandezibanez@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/anxodio/twofactorauthcli',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
