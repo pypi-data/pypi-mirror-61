388adb3f434e1da0ca841065fbbb164c4c63c8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:08 +0000

    Add package damhur-distributions/0.1/damhur_distributions-0.1.tar.gz

---
+++ packages/damhur-distributions/damhur_distributions-0.1.tar.gz/damhur_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='damhur_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Mario Damhur',
+      author_email = 'damhur92@gmail.com',
+      zip_safe=False)
