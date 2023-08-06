9609f75943dae818c89b774a0157df5b2528e895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:40 +0000

    Add package pytube3/9.6.2/pytube3-9.6.2.tar.gz

---
+++ packages/pytube3/pytube3-9.6.2.tar.gz/pytube3-9.6.2/tests/test_request.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+from unittest import mock
+
+import pytest
+
+from pytube import request
+
+
+@mock.patch("pytube.request.urlopen")
+def test_streaming(mock_urlopen):
+    fake_stream_binary = [
+        iter(os.urandom(8 * 1024)),
+        iter(os.urandom(8 * 1024)),
+        iter(os.urandom(8 * 1024)),
+        None,
+    ]
+    response = mock.Mock()
+    response.read.side_effect = fake_stream_binary
+    mock_urlopen.return_value = response
+    response = request.stream("http://fakeassurl.gov")
+    call_count = len(list(response))
+
+    assert call_count == 3
+
+
+@mock.patch("pytube.request.urlopen")
+def test_headers(mock_urlopen):
+    response = mock.Mock()
+    response.info.return_value = {"content-length": "16384"}
+    mock_urlopen.return_value = response
+    response = request.head("http://fakeassurl.gov")
+    assert response == {"content-length": "16384"}
+
+
+@mock.patch("pytube.request.urlopen")
+def test_get(mock_urlopen):
+    response = mock.Mock()
+    response.read.return_value = "<html></html>".encode("utf-8")
+    mock_urlopen.return_value = response
+    response = request.get("http://fakeassurl.gov")
+    assert response == "<html></html>"
+
+
+def test_get_non_http():
+    with pytest.raises(ValueError):
+        request.get("file://bad")
