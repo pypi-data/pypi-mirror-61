d87e42ffb9759b7a9d98e0eae3a63f3db80e8a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:17 +0000

    Add package DjangoKit/0.2.1/DjangoKit-0.2.1-py3-none-any.whl

---
+++ packages/DjangoKit/DjangoKit-0.2.1-py3-none-any.whl/djangokit/__init__.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2018, Grigoriy Kramarenko
+# All rights reserved.
+# This file is distributed under the BSD 3-Clause License.
+#
+"""A set of extensions for Django used in the construction of complex web
+applications.
+"""
+import os
+from djangokit.utils import version
+
+VERSION = (0, 2, 1, 'final', 0)
+
+
+def get_version():
+    path = os.path.dirname(os.path.abspath(__file__))
+    return version.get_version(VERSION, path)
+
+
+__version__ = get_version()
