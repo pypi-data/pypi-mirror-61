e47105006cd2b40010334ca39fc71c77078d2a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package nevada/1.0.4/nevada-1.0.4.tar.gz

---
+++ packages/nevada/nevada-1.0.4.tar.gz/nevada-1.0.4/nevada/API/AdExtension.py
@@ -0,0 +1,121 @@
+from nevada.Common.Connector import *
+from typing import List
+import jsonpickle
+import json
+
+class CreateAdExtensionObject:
+    def __init__(self, pcChannelId, mobileChannelId, ownerId, type, userLock, schedule=None):
+        self.pcChannelId = pcChannelId
+        self.mobileChannelId = mobileChannelId
+        self.ownerId = ownerId
+        self.schedule = schedule
+        self.type = type
+        self.userLock = userLock
+
+class UpdateAdExtensionObject:
+    def __init__(self, nccAdExtensionId, schedule=None, userLock = None):
+        self.nccAdExtensionId = nccAdExtensionId
+        self.schedule = schedule
+        self.userLock = userLock
+
+class AdExtensionObject:
+    def __init__(self, json_def):
+        if type(json_def) is str:
+            json_def = json.loads(json_def)
+        s = json_def
+        self.customerId = None if 'customerId' not in s else s['customerId']
+        self.delFlag = None if 'delFlag' not in s else s['delFlag']
+        self.editTm = None if 'editTm' not in s else s['editTm']
+        self.inspectStatus = None if 'inspectStatus' not in s else s['inspectStatus']
+        self.mobileChannelId = None if 'mobileChannelId' not in s else s['mobileChannelId']
+        self.nccAdExtensionId = None if 'nccAdExtensionId' not in s else s['nccAdExtensionId']
+        self.ownerId = None if 'ownerId' not in s else s['ownerId']
+        self.pcChannelId = None if 'pcChannelId' not in s else s['pcChannelId']
+        self.regTm = None if 'regTm' not in s else s['regTm']
+        self.status = None if 'status' not in s else s['status']
+        self.statusReason = None if 'statusReason' not in s else s['statusReason']
+        self.type = None if 'type' not in s else s['type']
+        self.userLock = None if 'userLock' not in s else s['userLock']
+
+
+class AdExtension:  # 확장소재
+    def __init__(self, base_url: str, api_key: str, secret_key: str, customer_id: int):
+        self.conn = Connector(base_url, api_key, secret_key, customer_id)
+    AdExtensionObjectList = List[AdExtensionObject]
+    IdList = List[str]
+    ChangeFieldsList = List[str]
+
+    def list_by_owner_id(self, ownerId: str, format=True) -> AdExtensionObjectList:
+        result = self.conn.get('/ncc/ad-extensions', {'ownerId': ownerId})
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            adext_list = []
+            for arr in result:
+                camp = AdExtensionObject(arr)
+                adext_list.append(camp)
+            return adext_list
+        else:
+            print('Please Check the input value of format.')
+
+    def list_by_ids(self, ids: IdList, format=True) -> AdExtensionObjectList:
+        ids = ",".join(ids)
+        ids = {'ids': ids}
+        result = self.conn.get('/ncc/ad-extensions', ids)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            adext_list = []
+            for arr in result:
+                camp = AdExtensionObject(arr)
+                adext_list.append(camp)
+            return adext_list
+        else:
+            print('Please Check the input value of format.')
+
+    def get(self, adExtensionId: str, format=True) -> AdExtensionObject:
+        result = self.conn.get('/ncc/ad-extensions/' + adExtensionId)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = AdExtensionObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
+
+    def create(self, CreateAdExtensionObject: CreateAdExtensionObject, format=True) -> AdExtensionObject:
+        data = jsonpickle.encode(CreateAdExtensionObject, unpicklable=False)
+        data = json.loads(data)
+        data = CommonFunctions.dropna(data)
+        data_str = data
+        data_str = json.dumps(data_str)
+        result = self.conn.post('/ncc/ad-extensions', data_str)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = AdExtensionObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
+
+    def update(self, adExtensionId: str, fields: ChangeFieldsList,
+                             UpdateAdExtensionObject: UpdateAdExtensionObject, format=True) -> AdExtensionObject:
+        data = jsonpickle.encode(UpdateAdExtensionObject, unpicklable=False)
+        data = json.loads(data)
+        data = CommonFunctions.dropna(data)
+        data_str = data
+        data_str = json.dumps(data_str)
+        change_fields_list = ",".join(fields)
+        query = {'fields': change_fields_list}
+        result = self.conn.put('/ncc/ad-extensions/' + adExtensionId, data_str, query)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = AdExtensionObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
+
+    def delete(self, adExtensionId: str):
+        self.conn.delete('/ncc/ad-extensions/' + adExtensionId)
+        return True
---
+++ packages/nevada/nevada-1.0.4.tar.gz/nevada-1.0.4/nevada/API/AdKeyword.py
@@ -0,0 +1,146 @@
+from nevada.Common.Connector import *
+from typing import List
+import jsonpickle
+import json
+
+class CreateAdKeywordObject:
+    def __init__(self, keyword):
+        self.bidAmt = None
+        self.customerId = None
+        self.keyword = keyword
+        self.useGroupBidAmt = None
+        self.userLock = None
+
+class UpdateAdKeywordObject:
+    def __init__(self, nccAdgroupId, nccKeywordId):
+        self.bidAmt = None
+        self.links = None
+        self.nccAdgroupId = nccAdgroupId
+        self.nccKeywordId = nccKeywordId
+        self.useGroupBidAmt = None
+        self.userLock = None
+
+class AdKeywordObject:
+    def __init__(self, json_def):
+        if type(json_def) is str:
+            json_def = json.loads(json_def)
+        s = json_def
+        self.bidAmt = None if 'bidAmt' not in s else s['bidAmt']
+        self.customerId = None if 'customerId' not in s else s['customerId']
+        self.editTm = None if 'editTm' not in s else s['editTm']
+        self.inspectStatus = None if 'inspectStatus' not in s else s['inspectStatus']
+        self.keyword = None if 'keyword' not in s else s['keyword']
+        self.nccAdgroupId = None if 'nccAdgroupId' not in s else s['nccAdgroupId']
+        self.nccCampaignId = None if 'nccCampaignId' not in s else s['nccCampaignId']
+        self.nccKeywordId = None if 'nccKeywordId' not in s else s['nccKeywordId']
+        self.nccQi = None if 'nccQi' not in s else s['nccQi']
+        self.regTm = None if 'regTm' not in s else s['regTm']
+        self.status = None if 'status' not in s else s['status']
+        self.statusReason = None if 'statusReason' not in s else s['statusReason']
+        self.useGroupBidAmt = None if 'useGroupBidAmt' not in s else s['useGroupBidAmt']
+        self.userLock = None if 'userLock' not in s else s['userLock']
+
+
+class AdKeyword:
+    def __init__(self, base_url: str, api_key: str, secret_key: str, customer_id: int):
+        self.conn = Connector(base_url, api_key, secret_key, customer_id)
+
+    AdKeywordList = List[AdKeywordObject]
+    AdKeywordIdList = List[str]
+
+    def list_by_label_id(self, nccLabelId: str, format=True) -> AdKeywordList:
+        query = {'nccLabelId': nccLabelId}
+        result = self.conn.get('/ncc/keywords', query)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            adkeyword_list = []
+            for arr in result:
+                keyword = AdKeywordObject(arr)
+                adkeyword_list.append(keyword)
+            return adkeyword_list
+        else:
+            print('Please Check the input value of format.')
+
+    def list_by_ids(self, ids: AdKeywordIdList, format=True) -> AdKeywordList:
+        ids = ",".join(ids)
+        query = {'ids': ids}
+        result = self.conn.get('/ncc/keywords', query)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            adkeyword_list = []
+            for arr in result:
+                keyword = AdKeywordObject(arr)
+                adkeyword_list.append(keyword)
+            return adkeyword_list
+        else:
+            print('Please Check the input value of format.')
+
+    def list_by_group_id(self, nccAdgroupId: str = None, baseSearchId: str = None,
+                                       recordSize: int = None, selector: str = None, format=True) -> AdKeywordList:
+
+        query = {'nccAdgroupId': nccAdgroupId, 'baseSearchId': baseSearchId, 'recordSize': recordSize,
+                 'selector': selector}
+        result = self.conn.get('/ncc/keywords', query)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            adkeyword_list = []
+            for arr in result:
+                keyword = AdKeywordObject(arr)
+                adkeyword_list.append(keyword)
+            return adkeyword_list
+        else:
+            print('Please Check the input value of format.')
+
+    def get(self, nccKeywordId, format=True) -> AdKeywordObject:
+        result = self.conn.get('/ncc/keywords/' + nccKeywordId)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = AdKeywordObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
+
+    def create(self, nccAdgroupId, CreateAdKeywordObject, format=True) -> AdKeywordObject:
+        data = jsonpickle.encode(CreateAdKeywordObject, unpicklable=False)
+        data = json.loads(data)
+        data = CommonFunctions.dropna(data)
+        data = [data]
+        data_str = json.dumps(data)
+        result = self.conn.post('/ncc/keywords', data_str, {'nccAdgroupId': nccAdgroupId})
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = AdKeywordObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
+
+    def update(self, nccKeywordId, fields, UpdateAdKeywordObject, format=True) -> AdKeywordObject:
+        data = jsonpickle.encode(UpdateAdKeywordObject, unpicklable=False)
+        data = json.loads(data)
+        data = CommonFunctions.dropna(data)
+        data_str = json.dumps(data)
+
+        query = {'fields': fields}
+
+        result = self.conn.put('/ncc/keywords/' + nccKeywordId, data_str, query)
+        if format in [False, 'json']:
+            return result
+        elif format in [True, 'object', 'list']:
+            result = AdKeywordObject(result)
+            return result
+        else:
+            print('Please Check the input value of format.')
+
+    def delete(self, nccKeywordId: str):
+        self.conn.delete('/ncc/keywords/' + nccKeywordId)
+        return True
+
+    def delete_items(self, ids: AdKeywordIdList):
+        ids = ",".join(ids)
+        query = {'ids': ids}
+        self.conn.delete('/ncc/keywords', query)
