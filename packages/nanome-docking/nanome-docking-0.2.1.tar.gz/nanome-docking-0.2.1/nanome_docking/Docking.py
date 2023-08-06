4740ff544d4ff8a5ab6505872dac23a15695bbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:35 +0000

    Add package nanome-docking/0.2.1/nanome-docking-0.2.1.tar.gz

---
+++ packages/nanome-docking/nanome-docking-0.2.1.tar.gz/nanome-docking-0.2.1/nanome_docking/Docking.py
@@ -0,0 +1,187 @@
+import nanome
+from ._DockingCalculations import DockingCalculations as Smina
+from ._DockingCalculationsAutodock4 import DockingCalculations as Autodock4
+from ._DockingCalculationsRhodium import DockingCalculations as Rhodium
+from ._DockingMenu import DockingMenu
+from ._DockingMenuRhodium import DockingMenuRhodium
+import functools
+import sys
+
+from .ComplexUtils import ComplexUtils
+
+__metaclass__ = type
+class Docking(nanome.PluginInstance):
+    def __init__(self):
+        self._menu = None
+        self._calculations = None
+        self._autobox = True
+
+    # Called when Nanome connects to the Plugin, after its instantiation
+    def start(self):
+        self._menu.build_menu()
+        if self._autobox == False:
+            self._menu.disable_autobox()
+        # Request shallow complex (name, position, orientation), to display them in a list
+        self.request_complex_list(self.on_complex_list_received)
+
+    # Called when user clicks on the "Run" button in Nanome
+    def on_run(self):
+        menu = self._menu
+        # If menu doesn't have Receptor and Ligands selected, open it
+        # Else, just start docking
+        if menu.is_ready_for_docking() == False:
+            self.open_menu()
+        else:
+            self.run_docking(menu.get_receptor(), menu.get_ligands(), menu.get_site(), menu.get_params())
+
+    # Called when user click on the "Advanced Settings" button in Nanome
+    def on_advanced_settings(self):
+        nanome.util.Logs.debug("Advanced Settings")
+        self.open_menu()
+
+    # Called when a complex is added to the workspace in Nanome
+    def on_complex_added(self):
+        self.request_complex_list(self.on_complex_list_received)
+
+    # Called when a complex is removed from the workspace in Nanome
+    def on_complex_removed(self):
+        self.request_complex_list(self.on_complex_list_received)
+
+    def open_menu(self):
+        menu = self.menu
+        menu.enabled = True
+        self.update_menu(menu)
+
+    def make_plugin_usable(self):
+        self._menu.make_plugin_usable()
+
+    def on_complex_list_received(self, complexes):
+        self._menu.change_complex_list(complexes)
+
+    def combine_ligands_start_docking(self, receptor, site, params, individual_ligands):
+        self._calculations.start_docking(receptor, individual_ligands, site, **params)
+
+    def replace_conformer(self, complexes, callback, existing=True):
+        for i in range(len(complexes)):
+            complex_index = complexes[i].index
+            complexes[i] = complexes[i].convert_to_frames()
+            complexes[i].index = complex_index
+
+        rerequest_complexes = functools.partial(self.request_complexes, [complex.index for complex in complexes], callback)
+        request_docking_results = functools.partial(self.request_docking_results, callback)
+        if not existing:
+            rerequest_complexes = functools.partial(self.request_complex_list, request_docking_results)
+
+        self.update_structures_deep(complexes, rerequest_complexes)
+
+    def request_docking_results(self, callback, all_complexes):
+        docking_results_index = all_complexes[len(all_complexes)-1].index
+        self.request_complexes([docking_results_index], callback)
+
+    def set_and_convert_structures(self, has_site, params, complexes):
+            # When deep complexes data are received, unpack them and prepare ligand for docking
+            receptor = complexes[0]
+            receptor.m_workspace_to_complex = receptor.get_workspace_to_complex_matrix()
+            self._receptor = receptor
+            starting_lig_idx = 1
+            site = None
+            if has_site:
+                site = complexes[1]
+                self._site = site
+                ComplexUtils.convert_atoms_to_absolute_position(site)
+                ComplexUtils.convert_atoms_to_relative_position(site, receptor.m_workspace_to_complex)
+                starting_lig_idx = 2
+            # convert ligands to frame representation
+            ligands = complexes[starting_lig_idx:]
+            combine_ligs_and_start_docking = functools.partial(self.combine_ligands_start_docking, receptor, site, params)
+            self.replace_conformer(ligands, combine_ligs_and_start_docking)
+
+    def run_docking(self, receptor, ligands, site, params):
+        # Change the plugin to be "unusable"
+        if self._menu._run_button.unusable == True:
+            return
+        self._menu.make_plugin_usable(False)
+
+        # Request complexes to Nanome in this order: [receptor, site (if any), ligand, ligand,...]
+        request_list = [receptor.index]
+        if site != None:
+            request_list.append(site.index)
+        request_list += [x.index for x in ligands]
+
+        setup_structures = functools.partial(self.set_and_convert_structures, site != None, params)
+        self.request_complexes(request_list, setup_structures)
+
+    # Called every update tick of the Plugin
+    def update(self):
+        self._calculations.update()
+
+    def add_result_to_workspace(self, results, align=False):
+        for complex in results:
+            complex.position = self._receptor.position
+            complex.rotation = self._receptor.rotation
+
+            if align:
+                mat = complex.get_complex_to_workspace_matrix()
+                # get global atom pos
+                global_pos = [mat * atom.position for atom in complex.atoms]
+                # align bounding box
+                complex.position = self._site.position
+                complex.rotation = self._site.rotation
+                # restore atom pos
+                mat = complex.get_workspace_to_complex_matrix()
+                for (atom, pos) in zip(complex.atoms, global_pos):
+                    atom.position = mat * pos
+
+                complex.boxed = True
+
+        self.add_to_workspace(results)
+
+    def display_scoring_result(self, result):
+        self._menu.display_scoring_result(result)
+
+class SminaDocking(Docking):
+    def __init__(self):
+        super(SminaDocking, self).__init__()
+        self._calculations = Smina(self)
+        self._menu = DockingMenu(self)
+
+
+class Autodock4Docking(Docking):
+    def __init__(self):
+        super(Autodock4Docking, self).__init__()
+        self._calculations = Autodock4(self)
+        self._menu = DockingMenu(self)
+        self._autobox = False
+
+class RhodiumDocking(Docking):
+    def __init__(self):
+        super(RhodiumDocking, self).__init__()
+        self._calculations = Rhodium(self)
+        self._menu = DockingMenuRhodium(self)
+
+
+def main():
+    name = None
+    cl = None
+    for arg in sys.argv:
+        if arg == "smina":
+            name = "Smina"
+            cl = SminaDocking
+        elif arg == "autodock4":
+            name = "Autodock 4"
+            cl = Autodock4Docking
+        elif arg == "rhodium":
+            name = "Rhodium"
+            cl = RhodiumDocking
+    if name == None:
+        nanome.util.Logs.error("Please pass the docking software to use as an argument: smina|autodock4|rhodium")
+        sys.exit(1)
+
+    # Create the plugin, register Docking as the class to instantiate, and start listening
+    plugin = nanome.Plugin(name + " Docking", "Run docking using " + name + ". Lets user choose the receptor, ligands, and diverse options", "Docking", True)
+    plugin.set_plugin_class(cl)
+    plugin.run('127.0.0.1', 8888)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/nanome-docking/nanome-docking-0.2.1.tar.gz/nanome-docking-0.2.1/nanome_docking/_DockingCalculationsAutodock4.py
@@ -0,0 +1,319 @@
+import nanome
+import os
+import shutil
+import subprocess
+import tempfile
+import re
+from timeit import default_timer as timer
+
+# TMP
+from nanome._internal._structure._io._pdb.save import Options as PDBOptions
+from nanome._internal._structure._io._sdf.save import Options as SDFOptions
+
+from .ComplexUtils import ComplexUtils
+
+class DockingCalculations():
+    def __init__(self, plugin):
+        self._plugin = plugin
+        self._preparation_pending = False
+        self._grid_pending = False
+        self._docking_pending = False
+        self._bond_pending = False
+        self._running = False
+        self._pdb_options = PDBOptions()
+        self._pdb_options.write_bonds = True
+        self._sdf_options = SDFOptions()
+        self._sdf_options.write_bonds = True
+        self.requires_site = False
+
+        self.requires_site = False
+
+    def initialize(self):
+        # TODO: Read and write in a folder unique per plugin instance
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self._protein_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._protein_input_converted = tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt", dir=self.temp_dir.name)
+        self._ligands_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._ligands_input_converted = tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt", dir=self.temp_dir.name)
+        self._ligands_output = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
+        self._bond_output = tempfile.NamedTemporaryFile(delete=False, suffix=".sdf", dir=self.temp_dir.name)
+        self._autogrid_input = tempfile.NamedTemporaryFile(delete=False, suffix=".gpf", dir=self.temp_dir.name)
+        self._autodock_input = tempfile.NamedTemporaryFile(delete=False, suffix=".dpf", dir=self.temp_dir.name)
+        self._autogrid_log = tempfile.NamedTemporaryFile(delete=False, suffix=".glg", dir=self.temp_dir.name)
+        self._autodock_log = tempfile.NamedTemporaryFile(delete=False, suffix=".dlf", dir=self.temp_dir.name)
+
+    def start_docking(self, receptor, ligands, site, exhaustiveness, modes, align, replace, scoring, visual_scores, autobox):
+        self.initialize()
+        self._receptor = receptor
+        self._ligands =  ligands
+        self._combined_ligands = ComplexUtils.combine_ligands(receptor, ligands)
+        self._site = site
+        self._align = align
+        self._replace = replace
+        self._visual_scores = visual_scores
+
+        # Save all input files
+        receptor.io.to_pdb(self._protein_input.name, self._pdb_options)
+        nanome.util.Logs.debug("Saved PDB", self._protein_input.name)
+        self._combined_ligands.io.to_pdb(self._ligands_input.name, self._pdb_options)
+        nanome.util.Logs.debug("Saved PDB", self._ligands_input.name)
+
+        # Start docking process
+        self._running = False
+        self._preparation_pending = True
+        self._parameters_preparation_pending = True
+        self._grid_pending = True
+        self._docking_pending = True
+        self._bond_pending = True
+
+    def update(self):
+        if self._bond_pending == False:
+            return
+
+        if self._running == False:
+            if self._preparation_pending == True:
+                self._start_preparation()
+            elif self._parameters_preparation_pending == True:
+                self._start_parameters_preparation()
+            elif self._grid_pending == True:
+                self._start_grid()
+            elif self._docking_pending == True:
+                self._start_docking()
+            elif self._bond_pending == True:
+                self._start_bonds()
+        elif self._preparation_pending:
+            if self._check_preparation():
+                self._preparation_finished()
+        elif self._parameters_preparation_pending:
+            if self._check_parameters_preparation():
+                self._parameters_preparation_finished()
+        elif self._grid_pending:
+            if self._check_grid():
+                self._grid_finished()
+        elif self._docking_pending:
+            if self._check_docking():
+                self._docking_finished()
+        elif self._bond_pending:
+            if self._check_bonds():
+                self._bonds_finished()
+
+    def _check_process_error(self, process, check_only_errors = False):
+        (results, errors) = process.communicate()
+
+        try:
+            if len(errors) == 0:
+                if check_only_errors == True:
+                    return True
+                for line in results.splitlines():
+                    nanome.util.Logs.debug(line.decode("utf-8"))
+            else:
+                error = False
+                for line in errors.splitlines():
+                    str = line.decode("utf-8")
+                    if "warning" in str.lower():
+                        nanome.util.Logs.warning(str)
+                    elif str.trim() != "":
+                        nanome.util.Logs.error(str)
+                        error = True
+                if error == True:
+                    self._preparation_pending = False
+                    self._parameters_preparation_pending = False
+                    self._grid_pending = False
+                    self._docking_pending = False
+                    self._bond_pending = False
+                    self._running = False
+                    self._plugin.make_plugin_usable()
+                    nanome.util.Logs.error("Error. Abort docking")
+                    return True
+        except:
+            pass
+        return False
+
+    # Preparation of lig and receptor files
+
+    def _start_preparation(self):
+        # Awful situation here
+        lig_args = ['python2', os.path.join(os.path.dirname(__file__), 'prepare_ligand4.py'), '-l', self._ligands_input.name, '-o', self._ligands_input_converted.name]
+        rec_args = ['python2', os.path.join(os.path.dirname(__file__), 'prepare_receptor4.py'), '-r', self._protein_input.name, '-o', self._protein_input_converted.name]
+
+        nanome.util.Logs.debug("Prepare ligand and receptor")
+        self._start_timer = timer()
+        self._lig_process = subprocess.Popen(lig_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.temp_dir.name)
+        self._rec_process = subprocess.Popen(rec_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.temp_dir.name)
+        self._running = True
+
+    def _check_preparation(self):
+        return self._lig_process.poll() != None or self._rec_process.poll() != None
+
+    def _preparation_finished(self):
+        end = timer()
+        nanome.util.Logs.debug("Prepared ligand and receptor in", end - self._start_timer, "seconds")
+
+        if self._check_process_error(self._lig_process) or self._check_process_error(self._rec_process):
+            return
+
+        self._running = False
+        self._preparation_pending = False
+        self._parameters_preparation_pending = True
+
+    # Preparation of gpf and dpf files
+
+    def _start_parameters_preparation(self):
+        # Awful situation here
+        print("protein input converted name:", self._protein_input_converted.name)
+        grid_args = ['python2', os.path.join(os.path.dirname(__file__), 'prepare_gpf4.py'), '-l', self._ligands_input_converted.name, '-r', self._protein_input_converted.name, '-o', self._autogrid_input.name]
+        dock_args = ['python2', os.path.join(os.path.dirname(__file__), 'prepare_dpf42.py'), '-l', self._ligands_input_converted.name, '-r', self._protein_input_converted.name, '-o', self._autodock_input.name]
+
+        nanome.util.Logs.debug("Prepare grid and docking parameter files")
+        self._start_timer = timer()
+        self._grid_process = subprocess.Popen(grid_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.temp_dir.name)
+        self._dock_process = subprocess.Popen(dock_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.temp_dir.name)
+        self._running = True
+
+    def _check_parameters_preparation(self):
+        return self._grid_process.poll() != None or self._dock_process.poll() != None
+
+    def _parameters_preparation_finished(self):
+        end = timer()
+        nanome.util.Logs.debug("Prepared grid and docking parameter files in", end - self._start_timer, "seconds")
+
+        if self._check_process_error(self._grid_process) or self._check_process_error(self._dock_process):
+            return
+
+        f = open(self._autodock_input.name, 'a')
+        f.write("write_all\n")
+        f.close()
+
+        self._running = False
+        self._parameters_preparation_pending = False
+        self._grid_pending = True
+
+    # Autogrid
+
+    def _start_grid(self):
+        full_name = self._autogrid_input.name
+        full_name_log = self._autogrid_log.name
+        path = os.path.dirname(full_name)
+
+        args = ['autogrid4', '-p', full_name, '-l', full_name_log]
+
+        nanome.util.Logs.debug("Start Autogrid")
+        self._start_timer = timer()
+        self._autogrid_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=path)
+        self._running = True
+
+    def _check_grid(self):
+        return self._autogrid_process.poll() != None
+
+    def _grid_finished(self):
+        end = timer()
+        nanome.util.Logs.debug("Ran Autogrid in", end - self._start_timer, "seconds. Logs: ", self._autogrid_log.name)
+
+        if self._check_process_error(self._autogrid_process):
+            return
+
+        self._running = False
+        self._grid_pending = False
+        self._docking_pending = True
+
+    # Autodock
+
+    def _start_docking(self):
+        full_name_input = self._autodock_input.name
+        path = os.path.dirname(full_name_input)
+        full_name_log = self._autodock_log.name
+
+        args = ['autodock4', '-p', full_name_input, '-l', full_name_log]
+
+        nanome.util.Logs.debug("Start Autodock")
+        self._start_timer = timer()
+        self._autodock_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=path)
+        self._running = True
+
+    def _check_docking(self):
+        return self._autodock_process.poll() != None
+
+    def make_ligands_invisible(self):
+        for ligand in self._ligands:
+            ligand.visible = False
+        self._plugin.update_structures_shallow(self._ligands)
+
+    def _docking_finished(self):
+        end = timer()
+        nanome.util.Logs.debug("Ran Autodock in", end - self._start_timer, "seconds. Logs:", self._autodock_log.name)
+
+        if self._check_process_error(self._autodock_process):
+            return
+
+        # make ligands invisible
+        self.make_ligands_invisible()
+
+        # Conversion
+
+        with open(self._autodock_log.name) as origin_file, open(self._ligands_output.name, 'w') as destination_file:
+            print("ligand output file:", self._ligands_output.name)
+
+            for line in origin_file:
+                if line.startswith('DOCKED'):
+                    str = line[8:]
+                    if str.startswith('ROOT') or str.startswith('ENDROOT'):
+                        continue
+                    str = str.replace('+', ' ')
+                    if not str.endswith('\n'):
+                        str += '\n'
+                    str = re.sub("([0-9]{3}) HD *\n", r"\1 H \n", str)
+                    str = re.sub("([0-9]{3}) N.*A *\n", r"\1 N \n", str)
+                    str = re.sub("([0-9]{3}) O.*A *\n", r"\1 O \n", str)
+                    str = re.sub("([0-9]{3}) S.*A *\n", r"\1 S \n", str)
+                    str = re.sub("([0-9]{3}) A.* *\n", r"\1 C \n", str)
+                    str = re.sub("([0-9]{3} [A-G][a-g]) \n", r"\1\n", str)
+                    str = re.sub("([0-9]{3}) ([A-G][a-g])\n", r"\1\2\n", str)
+                    destination_file.write(str)
+
+        self._running = False
+        self._docking_pending = False
+        self._bond_pending = True
+
+    # Add Bonds
+
+    def _start_bonds(self):
+        nanome.util.Logs.debug("Start Bonds")
+        self._start_timer = timer()
+        args = ['obabel', '-ipdb', self._ligands_output.name, '-osdf', '-O' + self._bond_output.name]
+        self._obabel_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self._running = True
+
+    def _check_bonds(self):
+        poll = self._obabel_process.poll()
+        if poll == None:
+            self._obabel_process.communicate()
+        return poll != None
+
+    def _bonds_finished(self):
+        end = timer()
+        nanome.util.Logs.debug("Ran OpenBabel in", end - self._start_timer, "seconds")
+
+        # if self._check_process_error(self._obabel_process, check_only_errors=True):
+        #     return
+
+        self._running = False
+        self._bond_pending = False
+
+        docked_ligands = nanome.structure.Complex.io.from_sdf(path=self._bond_output.name)
+        nanome.util.Logs.debug("Read SDF", self._bond_output.name)
+        if len(self._combined_ligands.names) == 1:
+            docked_ligands.name = self._combined_ligands.names[0] + " (Docked)"
+        else:
+            docked_ligands.name == "Docking Results"
+            
+        docked_ligands.visible = True
+        if self._align == True:
+            docked_ligands.transform.position = self._receptor.transform.position
+            docked_ligands.transform.rotation = self._receptor.transform.rotation
+
+        nanome.util.Logs.debug("Update workspace")
+        # TODO: verify this shouldn't be here anymore (test)
+        # self._plugin.make_plugin_usable()
+        self._plugin.add_result_to_workspace([docked_ligands])
+
+        # shutil.rmtree(self.temp_dir.name)
