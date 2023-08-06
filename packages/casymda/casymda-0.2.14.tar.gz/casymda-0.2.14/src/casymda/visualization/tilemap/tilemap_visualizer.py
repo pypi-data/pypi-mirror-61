059f58b52528d43293a9467cab512862af74f6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:23 +0000

    Add package casymda/0.2.14/casymda-0.2.14.tar.gz

---
+++ packages/casymda/casymda-0.2.14.tar.gz/casymda-0.2.14/src/casymda/visualization/tilemap/tilemap_visualizer.py
@@ -0,0 +1,9 @@
+"""
+2D visualization of positions on a ScaledCanvas
+"""
+
+from casymda.visualization.entity_visualizer import EntityVisualizer
+
+
+class TilemapVisualizer(EntityVisualizer):
+    """ visualizes entity positions at a given canvas """
