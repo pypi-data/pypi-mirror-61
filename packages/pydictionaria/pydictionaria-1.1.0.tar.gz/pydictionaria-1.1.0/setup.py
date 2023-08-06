26b6354033078c2b1af2ee1ca5f6b2b58f07e8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:43 +0000

    Add package pydictionaria/1.1.0/pydictionaria-1.1.0.tar.gz

---
+++ packages/pydictionaria/pydictionaria-1.1.0.tar.gz/pydictionaria-1.1.0/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pydictionaria',
+    version='1.1.0',
+    description='',
+    long_description='',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+    ],
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='',
+    keywords='data linguistics',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'purl',
+        'python-dateutil',
+        'csvw>=1.5.4',
+        'BeautifulSoup4',
+        'attrs>=19.1',
+        'clldutils>=3.5',
+        'pycldf',
+        'pycdstar>=1.0.1',
+        'cdstarcat>=1.0.0',
+        'tqdm',
+        'transliterate==1.7.6',
+        'pyconcepticon>=1.1.1',
+        'colorlog',
+        'termcolor',
+        'pybtex',
+        'xlrd',
+    ],
+    extras_require={
+        'dev': ['flake8'],
+        'test': [
+            'tox',
+            'mock',
+            'pluggy>=0.12',
+            'pytest>=4.3',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    entry_points={
+        'console_scripts': ['dictionaria=pydictionaria.__main__:main'],
+    },
+    tests_require=[],
+    test_suite="pydictionaria")
