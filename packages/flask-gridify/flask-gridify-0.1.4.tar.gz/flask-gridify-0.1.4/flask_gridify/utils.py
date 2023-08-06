7b9f1a440d1d654fc5865b67f7e1e25e3fa9fa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:31 +0000

    Add package flask-gridify/0.1.4/flask-gridify-0.1.4.tar.gz

---
+++ packages/flask-gridify/flask-gridify-0.1.4.tar.gz/flask-gridify-0.1.4/flask_gridify/utils.py
@@ -0,0 +1,26 @@
+
+from dataclasses import dataclass
+from sqlalchemy import inspect
+import enum
+from typing import List
+class FlaskGridifyException(BaseException):
+    pass
+
+class FlaskGridifyDatatypes(enum.Enum):
+    INTEGER = 'integer'
+    TEXT = 'text'
+    BOOL = 'bool'
+
+@dataclass
+class ModelAttribute:
+    name: str
+    datatype: FlaskGridifyDatatypes
+
+def get_attributes(sqlalchemy_model_class) -> List[ModelAttribute]:
+    class_name = sqlalchemy_model_class.__name__
+    if not hasattr(sqlalchemy_model_class, '__table__'):
+        raise FlaskGridifyException(f'Class {class_name} has to attribute __table__ but FlaskGridify expects a subclass of SQLAlchemy declarative base class')
+    t = sqlalchemy_model_class.__table__
+    columns = t.columns
+    foreign_keys = t.foreign_keys
+    return [ModelAttribute(name=c.name, datatype='VARCHAR' if 'VARCHAR' in str(c.type) else c.type) for c in columns]
