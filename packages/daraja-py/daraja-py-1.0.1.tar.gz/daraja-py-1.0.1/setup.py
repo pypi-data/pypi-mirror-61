8aae8d5cf8bbef2d63605ba9151f5c2618c62388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:20 +0000

    Add package daraja-py/1.0.1/daraja-py-1.0.1.tar.gz

---
+++ packages/daraja-py/daraja-py-1.0.1.tar.gz/daraja-py-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as infile:
+    README = infile.read()
+
+# This call to setup() does all the work
+setup(
+    name="daraja-py",
+    version="1.0.1",
+    description="Lipa na mpesa library",
+    packages=find_packages(),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kamauvick/daraja-py",
+    download_url='https://github.com/kamauvick/daraja-py.git',
+    author="Victor Waichigo",
+    author_email="waichigovick@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["requests", "python-decouple"],
+)
