7d616825faad7e4dd029f9941f0900599241f5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package statusio-python/1.1/statusio-python-1.1.tar.gz

---
+++ packages/statusio-python/statusio-python-1.1.tar.gz/statusio-python-1.1/statusio/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""A library that provides a Python interface to the Status.io API"""
+from __future__ import absolute_import
+
+__author__ = 'hello@status.io'
+__version__ = '1.1'
+
+import json                                 # noqa
+
+from .api import Api                        # noqa
