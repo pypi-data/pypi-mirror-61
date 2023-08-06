bc30fddc904f3e4249f54ba7290ecbbeb90e4df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:02 +0000

    Add package tf2sku/0.1/tf2sku-0.1.tar.gz

---
+++ packages/tf2sku/tf2sku-0.1.tar.gz/tf2sku-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+version = '0.1'
+
+setup(
+    name='tf2sku',
+    packages=['tf2sku'],
+    version=version,
+    description='Python lib for creating sku for tf2 items',
+    author='Jędrzej Szadejko',
+    author_email='shadyprywatny@gmail.com',
+    license='MIT',
+    url='https://github.com/Jertusz/python-tf2-sku',
+    download_url='https://github.com/Jertusz/python-tf2-sku',
+    keywords=['steam', 'trade', 'tf2', 'teamfortress2' ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    ],
+    install_requires=[],
+)
