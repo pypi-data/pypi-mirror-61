5fbb91632afb27f6d4dde3d7d3b53d6ed801213d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:18 +0000

    Add package nester-wuhan2020/1.1.0/nester-wuhan2020-1.1.0.tar.gz

---
+++ packages/nester-wuhan2020/nester-wuhan2020-1.1.0.tar.gz/nester-wuhan2020-1.1.0/nester-wuhan2020.py
@@ -0,0 +1,15 @@
+"""
+这是“nester.py”模块，提供了一个名为print_lol()的函数，这个函数的作用是打印列表，其中有可能包含（也有skein包含）嵌套列表
+"""
+def print_lol(the_list,indent=False,level=0):
+    """
+        这个函数取一个位置参数,名为the_list  这可能是任何python列表也可以是包含含嵌套列表的列表 所指定的列表中的每个数据项会（递归地）输出到屏幕上，各数据项各占一行
+    """
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent :
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
---
+++ packages/nester-wuhan2020/nester-wuhan2020-1.1.0.tar.gz/nester-wuhan2020-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nester-wuhan2020',
+    version='1.1.0',
+    py_modules=['nester-wuhan2020'],
+    author='wuhan2020',
+    author_email='liweihong2020@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer all nested lists',
+
+)
