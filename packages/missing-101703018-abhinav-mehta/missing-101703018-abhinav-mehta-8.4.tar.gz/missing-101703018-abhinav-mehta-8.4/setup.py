9d51df87826c499736f8584b84cac87d42f5382d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:17 +0000

    Add package missing-101703018-abhinav-mehta/8.4/missing-101703018-abhinav-mehta-8.4.tar.gz

---
+++ packages/missing-101703018-abhinav-mehta/missing-101703018-abhinav-mehta-8.4.tar.gz/missing-101703018-abhinav-mehta-8.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'missing-101703018-abhinav-mehta',         # How you named your package folder (MyLib)
+  packages = ['missing-101703018-abhinav-mehta'],   # Chose the same as "name"
+  version = '8.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Handling missing values in a dataset',   # Give a short description about your library
+  author = 'Abhinav Mehta',                   # Type in your name
+  author_email = 'abhinavmehta3110@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/abhinavmehta3110',   # Provide either the link to your github or to your website
+  keywords = ['MISSING', 'Statistics'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pandas', 'numpy','sys', 'datawig' ,
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
