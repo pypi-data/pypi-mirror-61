1b27e5d7339ae5cceb675cf3894ea55818453e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:25 +0000

    Add package tfidf-matcher/0.2.1/tfidf_matcher-0.2.1.zip

---
+++ packages/tfidf-matcher/tfidf_matcher-0.2.1.zip/tfidf_matcher-0.2.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+long_description_md = open("README.md", encoding = "utf-8").read()
+
+setup(
+    name="tfidf_matcher",
+    version="0.2.1",
+    author="Louis Tsiattalou",
+    author_email="louis.tsi@gmail.com",
+    description="A small package that enables super-fast TF-IDF based string matching.",
+    long_description=long_description_md,
+    long_description_content_type="text/markdown",
+    url="https://github.com/louistsiattalou/tfidf_matcher",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires='>=3.0',
+    install_requires=["sklearn", "pandas"],
+    setup_requires=["sklearn", "pandas"]
+)
