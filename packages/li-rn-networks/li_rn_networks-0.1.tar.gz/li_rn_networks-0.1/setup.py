ecb250963235edf0a2a1e2136301a8ecec37b52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:20 +0000

    Add package li-rn-networks/0.1/li_rn_networks-0.1.tar.gz

---
+++ packages/li-rn-networks/li_rn_networks-0.1.tar.gz/li_rn_networks-0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import sys
+from setuptools import setup, find_packages
+
+
+setup(
+    name='li_rn_networks',
+    version='0.1',
+    description='neural networks. It have recurrent connections and leaky integrator neuron.',
+    long_description_content_type='text/markdown',
+    author='picker.',
+    author_email='picker@pickerlav.jp',
+    url='https://github.com/shibuiwilliam/py-kaomoji.git',
+    packages=['li_rn_networks'],
+    include_package_data=True,
+)
