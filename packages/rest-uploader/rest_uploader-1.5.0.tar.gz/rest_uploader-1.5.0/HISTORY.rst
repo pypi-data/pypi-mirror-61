71af22d62edb46a84847bfe9cdd6fe8c077ae3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:08 +0000

    Add package rest-uploader/1.5.0/rest_uploader-1.5.0.tar.gz

---
+++ packages/rest-uploader/rest_uploader-1.5.0.tar.gz/rest_uploader-1.5.0/HISTORY.rst
@@ -0,0 +1,73 @@
+=======
+History
+=======
+
+0.1.0 (2019-04-14)
+------------------
+
+* First super-unpolished and barely usable release on PyPI.
+
+
+0.3.0 (2019-06-26)
+------------------
+
+* First real release on PyPI.
+
+
+0.4.0 (2019-11-12)
+------------------
+
+* Fixed multipage OCR on PDFs
+* Added Language as a command line option
+
+
+0.5.0 (2019-11-14)
+------------------
+
+* Removed unidecode function on OCR, no longer needed
+
+
+0.6.0 (2019-11-14)
+------------------
+
+* Cleaned up PDF reading function
+* Syncronized versioning
+
+
+0.7.0 (2019-11-14)
+------------------
+
+* Nothing of consequence
+
+
+0.8.0 (2019-11-14)
+------------------
+
+* Added --version flag on cli
+
+
+1.2.0 (2019-11-14)
+------------------
+
+* Resolved failed builds related to bumpversion and
+  manual versioning out of sync
+
+
+1.3.0 (2019-12-03)
+------------------
+
+* Added special processing to turn CSVs into markdown tables
+
+
+1.4.0 (2020-02-19)
+------------------
+
+* Ignore .part files (KDE Bug)
+* Added logic to automatically assign tags to newly created notes
+* Added click option to turn off automatic tag generation
+
+
+1.5.0 (2020-02-19)
+------------------
+
+* Embarrassingly dumb bug fix for autotag argument
---
+++ packages/rest-uploader/rest_uploader-1.5.0.tar.gz/rest_uploader-1.5.0/rest_uploader/img_process.py
@@ -0,0 +1,68 @@
+import PyPDF2
+import os
+from uuid import uuid4
+from PIL import Image
+from pdf2image import convert_from_path
+from pytesseract import image_to_string, TesseractError
+from .settings import TEMP_PATH
+
+
+def set_language(language):
+    global LANGUAGE
+    LANGUAGE = language
+
+
+def open_pdf(filename):
+    try:
+        pdfFileObject = open(filename, "rb")
+        pdf_reader = PyPDF2.PdfFileReader(pdfFileObject, strict=False)
+        return pdf_reader
+    except PyPDF2.utils.PdfReadError:
+        print("PDF not fully written - no EOF Marker")
+        return None
+
+
+def pdf_valid(filename):
+    if open_pdf(filename) is None:
+        return False
+    else:
+        return True
+
+
+def pdf_page_to_image(filename, page_num=0):
+    pages = convert_from_path(filename, 250)
+    if page_num == 0:
+        tempfile = TEMP_PATH + "preview.png"
+    else:
+        tempfile = TEMP_PATH + f"{uuid4()}.png"
+    pages[page_num].save(tempfile, "PNG")
+    return tempfile
+
+
+def extract_text_from_pdf(filename):
+    pdfReader = open_pdf(filename)
+    count = pdfReader.numPages
+    text = ""
+    for i in range(count):
+        text += f"\n\n***PAGE {i+1} of {count}*** \n\n"
+        page = pdfReader.getPage(i)
+        embedded_text = page.extractText()
+        # if embedded PDF text is minimal or does not exist,
+        # run OCR the images extracted from the PDF
+        if len(embedded_text) >= 100:
+            text += embedded_text
+        else:
+            extracted_image = pdf_page_to_image(filename, i)
+            text += extract_text_from_image(extracted_image)
+            if extracted_image != TEMP_PATH + "preview.png":
+                os.remove(extracted_image)
+    return text
+
+
+def extract_text_from_image(filename):
+    try:
+        text = image_to_string(Image.open(filename), lang=LANGUAGE)
+    except TesseractError as e:
+        text = "\nCheck Tesseract OCR Configuration\n"
+        text += e.message
+    return text
