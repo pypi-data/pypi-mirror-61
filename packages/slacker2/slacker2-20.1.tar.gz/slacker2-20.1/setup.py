1e8d52554a86fc2bdee9878bcf4e1120de54dc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:40 +0000

    Add package slacker2/20.1/slacker2-20.1.tar.gz

---
+++ packages/slacker2/slacker2-20.1.tar.gz/slacker2-20.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from slacker2 import constants
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='slacker2',
+    version=constants.VERSION,
+    packages=['slacker2'],
+    description='Slack API client, fork of slacker',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Oktay Sancak, Alexander Pushkarev',
+    author_email='oktaysancak@gmail.com, alexspush@gmail.com',
+    url='http://github.com/senpay/slacker/',
+    install_requires=['requests >= 2.2.1'],
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    test_suite='tests',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='slack api'
+)
