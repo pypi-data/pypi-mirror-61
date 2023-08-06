a8aaf1f28c14651cd545ffccfd763ca861a3dff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:29 +0000

    Add package animals.py/1.0.0a1/animals.py-1.0.0a1.tar.gz

---
+++ packages/animals.py/animals.py-1.0.0a1.tar.gz/animals.py-1.0.0a1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+from setuptools import setup
+	
+setup(
+	name="animals.py",
+	author="mischievousdev",
+	author_email="miscdev.py@gmail.com",
+	version="1.0.0a1",
+	packages=setuptools.find_packages(),
+	license="MIT",
+	keywords='animals'
+)
