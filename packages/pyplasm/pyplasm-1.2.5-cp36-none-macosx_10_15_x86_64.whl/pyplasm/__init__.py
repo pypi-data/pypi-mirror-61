004af67dbd77fcf81fbbee3540b4b40973ba5fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:23 +0000

    Add package pyplasm/1.2.5/pyplasm-1.2.5-cp36-none-macosx_10_15_x86_64.whl

---
+++ packages/pyplasm/pyplasm-1.2.5-cp36-none-macosx_10_15_x86_64.whl/pyplasm/__init__.py
@@ -0,0 +1,19 @@
+import sys,types,math
+
+import OpenGL.GL   ; Gl   = OpenGL.GL
+import OpenGL.GLU  ; Glu  = OpenGL.GLU
+import OpenGL.GLUT ; Glut = OpenGL.GLUT
+
+import sys
+if (sys.version_info > (3, 0)):
+  from .xgepy import *
+  from .fenvs import *
+else:
+  from xgepy import *
+  from fenvs import *
+
+
+
+
+
+
