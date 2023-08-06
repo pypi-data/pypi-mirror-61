2f2ff4910436fbe160fc7af3864d128fa8892202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:11 +0000

    Add package setl/0.9.0/setl-0.9.0.tar.gz

---
+++ packages/setl/setl-0.9.0.tar.gz/setl-0.9.0/src/setl/projects/clean.py
@@ -0,0 +1,65 @@
+__all__ = ["ProjectCleanMixin"]
+
+import os
+import shutil
+import subprocess
+
+from typing import Iterator, Optional
+
+from .build import BuildEnv
+from .dev import ProjectDevelopMixin
+
+
+def _get_name(f: Iterator[str]) -> Optional[str]:
+    for line in f:
+        if ":" not in line:  # End of metadata.
+            break
+        k, v = line.strip().split(":", 1)
+        if k.lower() == "name":
+            return v.strip()
+    return None
+
+
+_GET_EGG_INFO_LOCATION_CODE = """
+import os
+import sys
+import pkg_resources
+
+try:
+    dist = pkg_resources.get_distribution(sys.argv[1])
+except pkg_resources.DistributionNotFound:
+    pass
+else:
+    print(getattr(dist, "egg_info", ""))
+"""
+
+
+class ProjectCleanMixin(ProjectDevelopMixin):
+    def _clean_egg_info(self, env: BuildEnv):
+        name = _get_name(self.iter_metadata_for_development(env))
+        if not name:
+            return
+
+        args = [
+            os.fspath(env.interpreter),
+            "-c",
+            _GET_EGG_INFO_LOCATION_CODE,
+            name,
+        ]
+        info = subprocess.check_output(args, text=True).strip()
+
+        if not info or not os.path.isdir(info):
+            return
+        shutil.rmtree(info)
+
+    def clean(self, env: BuildEnv):
+        # Clean up the built .egg-info. This is probably a good idea.
+        # https://github.com/pypa/setuptools/issues/1347
+        self._clean_egg_info(env)
+
+        self.setuppy(env, "clean", "--all")
+
+        # We don't just clean up here because the build environment is still
+        # active, and the caller might want to use it after the call. Set a
+        # flag so it is deleted when the context exits instead.
+        env.mark_for_cleanup()
