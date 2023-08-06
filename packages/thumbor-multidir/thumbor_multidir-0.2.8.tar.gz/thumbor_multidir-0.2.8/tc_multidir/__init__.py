2269ddaa73c330a67a8b2bc574b05ab11a1b5224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:15 +0000

    Add package thumbor-multidir/0.2.8/thumbor_multidir-0.2.8.tar.gz

---
+++ packages/thumbor-multidir/thumbor_multidir-0.2.8.tar.gz/thumbor_multidir-0.2.8/tc_multidir/__init__.py
@@ -0,0 +1,13 @@
+ # -*- coding: utf-8 -*-
+import os
+
+from ._version import __version__
+
+from thumbor.config import Config
+
+Config.define(
+        'TC_MULTIDIR_PATHS',
+        [],
+        'The list of paths where the File Loader will try to find images',
+        'File Loader'
+)
