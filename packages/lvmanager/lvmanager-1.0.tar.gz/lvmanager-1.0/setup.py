5d85e89d1817eb95ef61124e699e643002568f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:29 +0000

    Add package lvmanager/1.0/lvmanager-1.0.tar.gz

---
+++ packages/lvmanager/lvmanager-1.0.tar.gz/lvmanager-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'requirements.txt'), 'rb') as f:
+    install_requires = f.read().decode('utf-8').split('\n')
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lvmanager',
+    version="1.0",
+    description="CLI tool to manage local key/certs and token.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Sid Premkumar',
+    author_email='sid.premkumar@gmail.com',
+    url='https://sidpremkumar.com',
+    license='MIT',
+    install_requires=install_requires,
+    packages=[
+        'lvmanager',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            "lvmanager=lvmanager.main:main",
+        ],
+    },
+)
