3e02e52ee97556a1056b468850fa08d58e741634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:56 +0000

    Add package django-fontawesome-5/1.0.18/django-fontawesome-5-1.0.18.tar.gz

---
+++ packages/django-fontawesome-5/django-fontawesome-5-1.0.18.tar.gz/django-fontawesome-5-1.0.18/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-fontawesome-5',
+    version='1.0.18',
+    packages=['fontawesome_5'],
+    include_package_data=True,
+    license='BSD License',
+    description='A utility for using icons in models, forms, and templates.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/BenjjinF/django-fontawesome-5',
+    author='Benjamin Frost',
+    author_email='info@btfrost.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'Django>=2.1',
+    ],
+)
