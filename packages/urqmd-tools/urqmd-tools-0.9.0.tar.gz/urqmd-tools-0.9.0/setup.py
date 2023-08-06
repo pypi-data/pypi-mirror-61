07093181420bfd3950a6d666c8ebe7ca357707d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:40 +0000

    Add package urqmd-tools/0.9.0/urqmd-tools-0.9.0.tar.gz

---
+++ packages/urqmd-tools/urqmd-tools-0.9.0.tar.gz/urqmd-tools-0.9.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+try:
+    import pypandoc
+    LDESC = open('README.md', 'r').read()
+    LDESC = pypandoc.convert(LDESC, 'rst', format='md')
+except (ImportError, IOError, RuntimeError) as e:
+    print("Could not create long description:")
+    print(str(e))
+    LDESC = ''
+
+setup(name='urqmd-tools',
+      version = '0.9.0',
+      description = 'Python tools that facilitate working with UrQMD',
+      long_description = LDESC,
+      author = 'Philipp Klaus',
+      author_email = 'klaus@physik.uni-frankfurt.de',
+      url = 'https://github.com/pklaus/urqmd-tools',
+      license = 'GPL',
+      packages = find_packages(),
+      entry_points = {
+          'console_scripts': [
+              'urqmd-tools.pids = urqmd_tools.pids:main',
+              'urqmd-tools.parser.f14 = urqmd_tools.parser.f14:main',
+              'urqmd-tools.launcher = urqmd_tools.launcher.__init__:main',
+          ],
+      },
+      include_package_data = False,
+      zip_safe = True,
+      platforms = 'any',
+      install_requires = [
+          'attrs',
+          'cached_property',
+      ],
+      extras_require = {
+      },
+      keywords = 'UrQMD Python Physics Heavy-Ion-Collisions Transport Model',
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Visualization',
+      ]
+)
