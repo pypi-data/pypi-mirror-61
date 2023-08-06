7c09e652fd8eb845e33dd454bfd20a04c76bf7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:30 +0000

    Add package GlobalData/0.3/GlobalData-0.3.tar.gz

---
+++ packages/GlobalData/GlobalData-0.3.tar.gz/GlobalData-0.3/GlobalData/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# Author: Tami Bar (fire17@gmail.com)
+
+#__init__.py
+#from GlobalData.GlobalData import *
+#from GlobalData.Globals import *
+#from GlobalData.Universe import *
+
+from . import GlobalData, Globals, Universe
