3048b17e26315c898344c15806a116f551d34149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:26 +0000

    Add package dblpy/0.3.4/dblpy-0.3.4-py3-none-any.whl

---
+++ packages/dblpy/dblpy-0.3.4-py3-none-any.whl/dbl/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+DBL Python API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the top.gg API.
+:copyright: (c) 2019 Assanali Mukhanov & top.gg
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'dblpy'
+__author__ = 'Francis Taylor'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Assanali Mukhanov'
+__version__ = '0.3.4'
+
+from collections import namedtuple
+
+from .client import DBLClient
+from .errors import *
+from .http import HTTPClient
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major = 0, minor = 3, micro = 4, releaselevel = 'final', serial = 0)
