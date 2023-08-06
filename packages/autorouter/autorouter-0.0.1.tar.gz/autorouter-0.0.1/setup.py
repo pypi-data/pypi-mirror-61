beb06168b2a9c5147e9fa7aef80e239e1adf8bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:03 +0000

    Add package autorouter/0.0.1/autorouter-0.0.1.tar.gz

---
+++ packages/autorouter/autorouter-0.0.1.tar.gz/autorouter-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="autorouter",
+    version="0.0.1",
+    author="svinerus",
+    author_email="svinerus@gmail.com",
+    description="small client-server url router",
+    long_description="small client-server url router",
+    long_description_content_type="text/markdown",
+    url="https://github.com/svinerus/autorouter",
+    packages=setuptools.find_packages(),
+    install_requires=['websockets'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
