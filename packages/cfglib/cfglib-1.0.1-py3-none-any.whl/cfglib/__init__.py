a3b8ee01a2c3172d8ef2529977603b82158e26cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:07 +0000

    Add package cfglib/1.0.1/cfglib-1.0.1-py3-none-any.whl

---
+++ packages/cfglib/cfglib-1.0.1-py3-none-any.whl/cfglib/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for cfglib.
+
+Objects from the `config` and `spec` submodules are reexported to the root, so you can use
+e.g. `cfglib.Config`.
+"""
+
+__author__ = '''Anton Barkovsky'''
+__email__ = 'anton@swarmer.me'
+__version__ = '1.0.1'
+
+
+# pylint: disable=wildcard-import
+from .config import *
+from .spec import *
