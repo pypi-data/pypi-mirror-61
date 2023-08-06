25326bc4b29e6fb4320a7745d8fb1361e684c6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:44 +0000

    Add package pyoncoprint/0.1.1/pyoncoprint-0.1.1.tar.gz

---
+++ packages/pyoncoprint/pyoncoprint-0.1.1.tar.gz/pyoncoprint-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import io
+import setuptools
+
+with io.open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyoncoprint",
+    version="0.1.1",
+    author="Jeongbin Park",
+    author_email="jeongbin.park@charite.de",
+    description="PyOncoPrint",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/pjb7687/pyoncoprint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+    ]
+)
