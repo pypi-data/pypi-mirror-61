333125c2435cdd6939e60ea3e2da456fb32f5c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:27 +0000

    Add package PiPocketGeiger/0.4.0/PiPocketGeiger-0.4.0.tar.gz

---
+++ packages/PiPocketGeiger/PiPocketGeiger-0.4.0.tar.gz/PiPocketGeiger-0.4.0/setup.py
@@ -0,0 +1,27 @@
+import sys
+import re
+import ast
+from setuptools import setup
+
+readme_markdown = None
+with open("README.md") as f:
+    readme_markdown = f.read()
+
+setup(
+    name="PiPocketGeiger",
+    version="0.4.0",
+    url="https://github.com/MonsieurV/PiPocketGeiger",
+    license="MIT",
+    author="Yoan Tournade",
+    author_email="yoan@ytotech.com",
+    description="A library for monitoring radiation with the Radiation Watch "
+    "Pocket Geiger.",
+    long_description=readme_markdown,
+    long_description_content_type="text/markdown",
+    packages=["PiPocketGeiger"],
+    include_package_data=True,
+    zip_safe=True,
+    platforms="any",
+    install_requires=["RPi.GPIO>=0.5.0a"],
+    extras_require={"dev": ["flake8", "pylint"]},
+)
