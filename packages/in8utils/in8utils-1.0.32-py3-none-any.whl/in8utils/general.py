a2c9a8b615ba8abb42399b7da0cdc7e1cefebf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:40 +0000

    Add package in8utils/1.0.32/in8utils-1.0.32-py3-none-any.whl

---
+++ packages/in8utils/in8utils-1.0.32-py3-none-any.whl/in8utils/general.py
@@ -0,0 +1,42 @@
+import inspect
+import os
+import dotenv
+import requests
+import json
+
+from .aws import Aws
+
+
+def dumps(to_dump):
+    def dump(obj):
+        return obj.__dict__
+
+    return json.dumps(to_dump, indent=4, default=dump, ensure_ascii=False)
+
+
+class AtomicCounter:
+    def __init__(self):
+        self.counter = 0
+
+    def count(self):
+        self.counter += 1
+        return self.counter
+
+
+def load_config_to_env(ref: str, config_url: str = 'http://buscamax-data.buscamilhas.com', force_az: str = None):
+    az = Aws.get_az() or force_az
+
+    if not az:
+        try:
+            root_path = os.path.dirname(os.path.abspath((inspect.stack()[1])[1]))
+            dotenv.load_dotenv(f"{root_path}/.env")
+        except:
+            pass
+        return
+
+    ambient = 'prod' if az in ['2B', '2C', '2A'] else 'staging'
+    resp = requests.get(f"{config_url}/{ambient}/{az}/{ref}")
+    config: dict = resp.json()
+
+    for key, value in config.items():
+        os.environ[key] = str(value)
