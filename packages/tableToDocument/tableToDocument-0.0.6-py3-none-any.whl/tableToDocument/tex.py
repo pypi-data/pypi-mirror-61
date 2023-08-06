66605a342efbb2da87f0d4c439f553a2ac45a066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:45 +0000

    Add package tableToDocument/0.0.6/tableToDocument-0.0.6-py3-none-any.whl

---
+++ packages/tableToDocument/tableToDocument-0.0.6-py3-none-any.whl/tableToDocument/tex.py
@@ -0,0 +1,159 @@
+import toDoc
+import pandas as pd
+
+supportedFormats=['tex']
+table='\n\\begin{center}\n'
+endTable='\n\\end{center}'
+bd = "\\section"
+endD="\\end{document}"
+
+def write(tx):
+    if toDoc.docExists(tx.path) == False:
+        return 2
+    if toDoc.isDocAccessible(tx.path) == False:
+        return 1
+    if toDoc.isDocFile(tx.path) == False or tx.path.split('.')[-1] not in supportedFormats:
+        return 3
+    if toDoc.validFlag(tx.flag) == False:
+        return 4
+
+    with open('../example.tex',"r") as f:
+        x = f.read()
+    f.close()
+
+    if type(tx.dataType)==SupportedDataTypes.STRING:
+        final = appendToTex(x,tx.flag,tx.dataType)
+
+    if type(tx.dataType)==SupportedDataTypes.INT:
+        final = appendToTex(x,tx.flag,str(tx.dataType))
+
+    if type(tx.dataType)==SupportedDataTypes.LIST:
+        final = appendToTex(x,tx.flag,createTableFromList(tx.dataType))
+
+    if type(tx.dataType)==SupportedDataTypes.DICT:
+        final = appendToTex(x,tx.flag,createTableFromDict(tx.dataType))
+
+    if type(tx.dataType)==SupportedDataTypes.SET:
+        final = appendToTex(x,tx.flag,createTableFromSet(tx.dataType))
+
+    if type(tx.dataType)==SupportedDataTypes.DATAFRAME:
+        final = appendToTex(x,tx.flag,createTableFromDataframe(tx.dataType))
+
+    if type(tx.dataType)==SupportedDataTypes.SERIES:
+        final = appendToTex(x,tx.flag,createTableFromSeries(tx.dataType))
+
+
+    with open('../example.tex',"w") as g:
+        g.write(final)
+    g.close()
+
+
+def appendToTex(tex,flag,data):
+    bg=tex.split(bd)
+    #Appends the data to the final section at the bottom
+    if flag=='a':
+        final=bg[0]
+        for k in range(1,len(bg)-1):
+            final=final+bd+bg[k]
+        g=bg[len(bg)-1].split(endD)
+        final=final+bd+g[0]+table+data+endTable+'\n'+endD
+        return final
+
+    #Appends the data structure at the first section,at the beginning
+    if flag=='b':
+        getSectionName=bg[1].split('}',1)
+        final=bg[0]+bd+getSectionName[0]+'}'+table+data+endTable+getSectionName[1]
+        for j in range(2,len(bg)):
+            final=final+bd+bg[j]
+        return final
+
+    #Appends the data at the section #data ,at the bottom of the section
+    final=bg[0]
+    for sec in range(1,len(bg)):
+        if sec==flag:
+            if sec==len(bg)-1:
+                splitLastSection=bg[sec].split(endD)
+                final=final+bd+splitLastSection[0]+table+data+endTable+endD
+                return final
+            final=final+bd+bg[sec]+table+data+endTable
+        else:
+            final=final+bd+bg[sec]
+    return final
+
+def createTableFromList(data):
+    columnSize=1
+    for i in data:
+        if type(i)==SupportedDataTypes.LIST:
+            if len(i)>columnSize:
+                columnSize = len(i)
+    rowSize = len(data)
+
+    tb="\\begin{tabular}{ c"
+    for i in range(columnSize-1):
+        tb=tb+" |c"
+    tb=tb+"}\n"
+    for j in range(rowSize):
+        if type(data[j])==SupportedDataTypes.LIST:
+            for k in range(columnSize):
+                try:
+                    if k==columnSize-1:
+                        if j==rowSize-1:
+                            tb=tb+str(data[j][k])+"\n"
+                        else:
+                            tb=tb+str(data[j][k])+" \\\\\n"
+                    else:
+                        tb=tb+str(data[j][k])+" & "
+                except:
+                    tb=tb+"\\\\\n"
+                    break
+        else:
+            tb=tb+str(data[j])+"\\\\\n"
+
+    return tb
+
+def createTableFromDict(data):
+    tb = "\\begin{tabular}{c|c}\n"
+    tb= tb+"KEY &  VALUE \\\\\n"
+    for key,val in data.items():
+        tb=tb+str(key)+" & "+str(val)+" \\\\\n"
+    return tb
+
+def createTableFromSet(data):
+    tb = "\n\\begin{tabular}{ c}\n"
+    tb = tb +"SET"+" \\\\\n"
+    for i in data:
+        tb = tb + str(i)+" \\\\\n"
+    return tb
+
+def createTableFromDataframe(data):
+    tb = "\n\\begin{tabular}{"
+    for c in range(len(data.columns)):
+        tb = tb + " c "
+    tb = tb+" c }\n &"
+
+    for k in data.columns:
+        tb = tb +str(k)+" & "
+    tb=tb[:-2]+"\\\\\n"
+
+    for c in range(len(data)):
+        tb=tb+str(c+1)+" & "
+        for items in data.columns:
+            tb=tb+str(data[items][c])+" & "
+        tb=tb[:-2]+"\\\\\n"
+    return tb
+
+def createTableFromSeries(data):
+    tb = "\\begin{tabular}{c|c}\n"
+    tb= tb+"KEY &  VALUE \\\\\n"
+    for key,val in data.items():
+        tb=tb+str(key)+" & "+str(val)+" \\\\\n"
+    return tb
+
+class SupportedDataTypes:
+    STRING=type("a")
+    INT=type(1)
+    LIST=type([])
+    DICT=type({})
+    SET=type({""})
+    SERIES = type(pd.Series({}))
+    DATAFRAME = type(pd.DataFrame({}))
---
+++ packages/tableToDocument/tableToDocument-0.0.6-py3-none-any.whl/tableToDocument/word.py
@@ -0,0 +1,282 @@
+from chardet import detect
+from xml.etree import ElementTree as ET
+from enum import Enum
+import pandas as pd
+import toDoc
+import zipfile
+import os
+
+supportedFormats = ['doc','docx','odt','xml']
+ns3="{urn:oasis:names:tc:opendocument:xmlns:table:1.0}"
+ns6="{urn:oasis:names:tc:opendocument:xmlns:text:1.0}"
+
+def write(toWord):
+    if toDoc.docExists(toWord.path) == False:
+        return 2
+    if toDoc.isDocAccessible(toWord.path) == False:
+        return 1
+    if toDoc.isDocFile(toWord.path) == False or toWord.path.split('.')[-1] not in supportedFormats:
+        return 3
+    if toDoc.validFlag(toWord.flag) == False:
+        return 4
+
+    with zipfile.ZipFile(toWord.path,'r') as zp:
+        with zp.open('content.xml','r') as cont:
+            d = cont.read()
+
+    zp.close()
+    os.rename(toWord.path,toWord.path+'b')
+
+    root = ET.fromstring(d)
+
+    for child in root:
+        if child.tag.find('body')>0:
+                toXML(toWord.dataType,child[0],toWord.flag)
+
+    ET.dump(root)
+    tree = ET.ElementTree(root)
+    createNewZip(toWord.path+'b',toWord.path,root)
+    os.remove(toWord.path+'b')
+
+def createNewZip(old,new,newcontent):
+    zin = zipfile.ZipFile(old,'r')
+    zout = zipfile.ZipFile(new,'w')
+    for item in zin.infolist():
+        buffer = zin.read(item.filename)
+        if(item.filename =='content.xml'):
+            zout.writestr(item,ET.tostring(newcontent,method='xml'))
+            continue
+        zout.writestr(item,buffer)
+    zout.close()
+    zin.close()
+
+def append(root,text):
+    return root
+
+
+class SupportedDataTypes:
+    STRING=type("a")
+    INT=type(1)
+    LIST=type([])
+    DICT=type({})
+    SET=type({""})
+    SERIES = type(pd.Series({}))
+    DATAFRAME = type(pd.DataFrame({}))
+
+class toXML:
+    def __init__(self,data,parentXMLElement,flag):
+        self.parent = parentXMLElement
+
+        line=-1
+        if flag=='b':
+            line=0
+        if type(flag)==SupportedDataTypes.INT:
+            line=flag
+
+        if type(data)==SupportedDataTypes.STRING:
+            self.fromString(data,line)
+        if type(data)==SupportedDataTypes.INT:
+            self.fromInt(data,line)
+        if type(data)==SupportedDataTypes.LIST:
+            self.fromList(data,line)
+        if type(data)==SupportedDataTypes.DICT:
+            self.fromDictionary(data,line)
+        if type(data)==SupportedDataTypes.SET:
+            self.fromSet(data,line)
+        if type(data)==SupportedDataTypes.SERIES:
+            self.fromPandasSeries(data,line)
+        if type(data)==SupportedDataTypes.DATAFRAME:
+            self.fromPandasDataframe(data,line)
+
+
+
+    def fromString(self,string,line):
+        ad2=dict()
+        ad2[ns6+'style-name'] = 'P1'
+        ad=ns6+'p'
+        nm = ET.Element(ad,ad2)
+        nm.text = string
+        if line==-1:
+            self.parent.append(nm)
+        else:
+            self.parent.insert(line,nm)
+
+    def fromInt(self,integ,line):
+        id2=dict()
+        id2[ns6+'style-name'] = 'P1'
+        ind = ns6+'p'
+        nm = ET.Element(ind,id2)
+        nm.text = str(integ)
+        if line==-1:
+            self.parent.append(nm)
+        else:
+            self.parent.insert(line,nm)
+
+
+    def fromList(self,lst,line):
+        tb,tbColumn,tbRow,tcCell,tcP,tableNo = self.tableCreation(lst,line)
+
+        for child in self.parent:
+            if ns3+'name' in child.attrib.keys():
+                if child.attrib[ns3+'name'] == 'TBN'+str(tableNo):
+
+                    ET.SubElement(child,ns3+'table-column',tbColumn)
+                    for i in range(len(lst)):
+                        tbRow[ns3+'name']='ROW1'+str(i)
+                        ET.SubElement(child,ns3+'table-row',tbRow)
+
+
+                    for c2 in child:
+                        if ns3+'name' in c2.attrib.keys():
+                            for i in range(len(lst)):
+                                if c2.attrib[ns3+'name'] == 'ROW1'+str(i):
+                                    if type(lst[i]) == SupportedDataTypes.LIST:
+                                        for j in range(len(lst[i])):
+                                            tcCell[ns3+'name'] = 'Cell'+str(j)
+                                            ET.SubElement(c2,ns3+'table-cell',tcCell)
+                                        for c3 in c2:
+                                            for k in range(len(lst[i])):
+                                                if c3.attrib[ns3+'name'] == 'Cell'+str(k):
+                                                    ET.SubElement(c3,ns6+'p',tcP).text=str(lst[i][k])
+
+                                    else:
+                                        ET.SubElement(c2,ns3+'table-cell',tcCell)
+                                        for c3 in c2:
+                                            ET.SubElement(c3,ns6+'p',tcP).text = str(lst[i])
+
+
+    def fromDictionary(self,dct,line):
+        tb,tbColumn,tbRow,tcCell,tcP,tableNo = self.tableCreation(dct,line)
+        for child in self.parent:
+            if ns3+'name' in child.attrib.keys():
+                if child.attrib[ns3+'name'] == 'TBN'+str(tableNo):
+                    ET.SubElement(child,ns3+'table-column',tbColumn)
+                    for i in range(len(dct)):
+                        tbRow[ns3+'name']='ROW1'+str(i)
+                        ET.SubElement(child,ns3+'table-row',tbRow)
+
+                    keys = list(dct.keys())
+                    vals = list(dct.values())
+                    i=0
+                    for c2 in child:
+                        if ns3+'name' in c2.attrib.keys():
+                            if c2.attrib[ns3+'name']=='TAB1':
+                                continue
+                            ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text = str(keys[i])
+                            ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text = str(keys[i])
+                            i=i+1
+
+    def fromSet(self,st,line):
+        tb,tbColumn,tbRow,tcCell,tcP,tableNo = self.tableCreation(st,line)
+        for child in self.parent:
+            if ns3+'name' in child.attrib.keys():
+                if child.attrib[ns3+'name'] == 'TBN'+str(tableNo):
+                    ET.SubElement(child,ns3+'table-column',tbColumn)
+                    for s in st:
+                        row =ET.Element(ns3+'table-row',tbRow)
+                        c1 = ET.Element(ns3+'table-cell',tcCell)
+                        p1 = ET.Element(ns6+'p',tcP)
+                        p1.text = str(s)
+                        c1.append(p1)
+                        row.append(c1)
+                        child.append(row)
+
+    def fromPandasSeries(self,ser,line):
+        tb,tbColumn,tbRow,tcCell,tcP,tableNo = self.tableCreation(ser,line)
+        for child in self.parent:
+            if ns3+'name' in child.attrib.keys():
+                if child.attrib[ns3+'name'] == 'TBN'+str(tableNo):
+                    ET.SubElement(child,ns3+'table-column',tbColumn)
+                    for i in range(len(ser)):
+                        tbRow[ns3+'name']='ROW1'+str(i)
+                        ET.SubElement(child,ns3+'table-row',tbRow)
+
+                    keys = list(ser.index)
+                    vals = list(ser)
+                    i=0
+                    for c2 in child:
+                        if ns3+'name' in c2.attrib.keys():
+                            if c2.attrib[ns3+'name'] == 'TAB1':
+                                continue
+                            ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text = str(keys[i])
+                            ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text = str(vals[i])
+                            i=i+1
+
+    def fromPandasDataframe(self,df,line):
+        tb,tbColumn,tbRow,tcCell,tcP,tableNo = self.tableCreation(df,line)
+
+        for child in self.parent:
+            if ns3+'name' in child.attrib.keys():
+                if child.attrib[ns3+'name'] == 'TBN'+str(tableNo):
+
+                    ET.SubElement(child,ns3+'table-column',tbColumn)
+                    for i in range(len(df)+1):
+                        tbRow[ns3+'name']='ROW1'+str(i)
+                        ET.SubElement(child,ns3+'table-row',tbRow)
+
+
+                    for c2 in child:
+                        if ns3+'name' in c2.attrib.keys():
+                            for i in range(len(df)+1):
+                                if c2.attrib[ns3+'name'] == 'ROW1'+str(i):
+                                    if i==0:
+                                        ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text=""
+                                        for j in df.columns:
+                                            ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text=str(j)
+                                    else:
+                                        ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text=str(i-1)
+                                        for j in df.columns:
+                                            ET.SubElement(ET.SubElement(c2,ns3+'table-cell',tcCell),ns6+'p',tcP).text=str(df[j][i-1])
+
+
+
+    def tableCreation(self,lst,line):
+        tb=dict()
+        tb[ns3+'style-name'] = 'Table1'
+        i=0
+        for child in self.parent:
+            if ns3+'name' in child.attrib.keys():
+                if child.attrib[ns3+'name'] == 'TBN'+str(i):
+                    i=i+1
+        tb[ns3+'name']='TBN'+str(i)
+        tbl=ET.Element(ns3+'table',tb)
+
+        if line==-1:
+            self.parent.append(tbl)
+        else:
+            self.parent.insert(line,tbl)
+
+        tbColumn=dict()
+        tbColumn[ns3+'style-name'] = 'Table1.A'
+        tbColumn[ns3+'name']='TAB1'
+
+        if type(lst)==SupportedDataTypes.LIST:
+            cols= str(self.columnSize(lst))
+        elif type(lst)==SupportedDataTypes.DATAFRAME:
+            cols= str(len(lst.columns)+1)
+        elif type(lst)==SupportedDataTypes.SET:
+            cols="1"
+        else:
+            cols = "2"
+
+        tbColumn[ns3+'number-columns-repeated'] = cols
+
+        tbRow=dict()
+
+        tcCell=dict()
+        tcCell[ns3+'style-name'] = 'Table1.A1'
+        tcCell['ns0:value-type'] = 'string'
+
+        tcP=dict()
+        tcP[ns3+'style-name'] = 'P1'
+
+        return tb,tbColumn,tbRow,tcCell,tcP,i
+
+    def columnSize(self,lst):
+            maxc=1
+            for i in range(len(lst)):
+                if type(lst[i])==SupportedDataTypes.LIST:
+                    if maxc<len(lst[i]):
+                        maxc=len(lst[i])
+
+            return maxc
