0315ce89276d1e207309c2c08da68e9478dd1b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:12 +0000

    Add package aio-pika/6.4.2/aio_pika-6.4.2-py3-none-any.whl

---
+++ packages/aio-pika/aio_pika-6.4.2-py3-none-any.whl/aio_pika/exceptions.py
@@ -0,0 +1,63 @@
+import asyncio
+
+import pamqp
+from aiormq.exceptions import (
+    AMQPChannelError,
+    AMQPConnectionError,
+    AMQPError,
+    AMQPException,
+    AuthenticationError,
+    ChannelClosed,
+    ConnectionClosed,
+    DeliveryError,
+    PublishError,
+    DuplicateConsumerTag,
+    IncompatibleProtocolError,
+    InvalidFrameError,
+    MethodNotImplemented,
+    ProbableAuthenticationError,
+    ProtocolSyntaxError,
+    ChannelPreconditionFailed,
+    ChannelNotFoundEntity
+)
+
+PAMQP_EXCEPTIONS = (
+    pamqp.exceptions.PAMQPException,
+) + tuple(pamqp.specification.ERRORS.values())
+
+CONNECTION_EXCEPTIONS = (
+    RuntimeError,
+    ConnectionError,
+    AMQPError,
+) + PAMQP_EXCEPTIONS
+
+
+class MessageProcessError(AMQPError):
+    pass
+
+
+class QueueEmpty(AMQPError, asyncio.QueueEmpty):
+    pass
+
+
+__all__ = (
+    'AMQPChannelError',
+    'AMQPConnectionError',
+    'AMQPError',
+    'AMQPException',
+    'AuthenticationError',
+    'ChannelClosed',
+    'ConnectionClosed',
+    'DeliveryError',
+    'PublishError',
+    'DuplicateConsumerTag',
+    'IncompatibleProtocolError',
+    'InvalidFrameError',
+    'MessageProcessError',
+    'MethodNotImplemented',
+    'ProbableAuthenticationError',
+    'ProtocolSyntaxError',
+    'QueueEmpty',
+    'ChannelPreconditionFailed',
+    'ChannelNotFoundEntity',
+)
