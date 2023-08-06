52f885afc057f88358ef83842aad740b730d93f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package napoleontoolbox/2.3.8.83/napoleontoolbox-2.3.8.83.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.3.8.83.tar.gz/napoleontoolbox-2.3.8.83/napoleontoolbox/signal/parameters_generator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def generate_lead_lag(lookback_windows, contravariants):
+    parameters = []
+    lookback_window = max(lookback_windows)
+    for contravariant in contravariants:
+        for lag in range(3, lookback_window):
+            for lead in range(2, lag):
+                parameters.append({
+                    'lead':lead,
+                    'lag':lag,
+                    'lookback_window':lookback_window,
+                    'contravariant':contravariant
+                })
+    return parameters
+
+
+
+def generate_dd_threshold(thresholds, contravariants):
+    parameters = []
+    for contravariant in contravariants:
+        for threshold in thresholds:
+            parameters.append({
+                'contravariant':contravariant,
+                'threshold':threshold
+            })
+    return parameters
