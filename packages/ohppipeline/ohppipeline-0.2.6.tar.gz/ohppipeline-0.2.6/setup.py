821c2e430ddc16d21b79dbcb3758c63ccabaec44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:44 +0000

    Add package ohppipeline/0.2.6/ohppipeline-0.2.6.tar.gz

---
+++ packages/ohppipeline/ohppipeline-0.2.6.tar.gz/ohppipeline-0.2.6/setup.py
@@ -0,0 +1,17 @@
+import setuptools as st
+import ohppipeline
+packages = st.find_packages()
+with open("./requirements.txt","r") as f :
+    requirements = f.read().splitlines()
+
+
+st.setup(
+        name     = ohppipeline.__name__,
+        version  = ohppipeline.__version__,
+        setup_requires=[
+            "cython"
+            ],
+        install_requires=requirements,
+        packages = packages,
+        author   = ohppipeline.__author__,
+        )
