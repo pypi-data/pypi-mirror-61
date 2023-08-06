a64ed747ff6aad18fce30b3f9ae1a1645b2257d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:08 +0000

    Add package pgtoolbox/0.0.5/pgtoolbox-0.0.5.tar.gz

---
+++ packages/pgtoolbox/pgtoolbox-0.0.5.tar.gz/pgtoolbox-0.0.5/pgtoolbox/__init__.py
@@ -0,0 +1,6 @@
+# Version of the pgtoolbox package
+__version__ = '0.0.5'
+
+
+import db_toolbox
+
---
+++ packages/pgtoolbox/pgtoolbox-0.0.5.tar.gz/pgtoolbox-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pgtoolbox", # Replace with your own username
+    version="0.0.5",
+    author="Bernard Louis Alecu",
+    author_email="bernard.alecu10@gmail.com",
+    description="A python-postgres database toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires = ['pg8000>=1.13.2'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
