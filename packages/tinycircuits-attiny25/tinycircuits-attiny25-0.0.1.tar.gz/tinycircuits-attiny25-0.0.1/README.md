5f9ae1a1c2e33ff2d806beddf782ab579b29c3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:19 +0000

    Add package tinycircuits-attiny25/0.0.1/tinycircuits-attiny25-0.0.1.tar.gz

---
+++ packages/tinycircuits-attiny25/tinycircuits-attiny25-0.0.1.tar.gz/tinycircuits-attiny25-0.0.1/README.md
@@ -0,0 +1,17 @@
+# TinyCircuits ATtiny25 Python
+
+This repository holds a python package and examples meant for using the TinyCircuits [Soil Moisture Wireling](https://tinycircuits.com/collections/wireling-sensors/products/moisture-sensor-wireling) with a Raspberry Pi and Wireling Pi Hat.
+
+To install the package, use:
+
+```
+pip3 install tinycircuits-attiny25
+```
+
+or if your default is Python 3:
+
+```
+pip install tinycircuits-attiny25
+```
+
+Software written by: [TinyCircuits](https://tinycircuits.com/) for use with TinyCircuits hardware 
---
+++ packages/tinycircuits-attiny25/tinycircuits-attiny25-0.0.1.tar.gz/tinycircuits-attiny25-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tinycircuits-attiny25", 
+    version="0.0.1",
+    author="TinyCircuits",
+    author_email="info@tinycircuits.com",
+    description="Wireling ATtiny25 Python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TinyCircuits/TinyCircuits-ATtiny25-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
