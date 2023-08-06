1d085d36e2e36a3b87e6cb06a19d4c28c5c106fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:08 +0000

    Add package LMSQuery-fork/0.5.2/LMSQuery_fork-0.5.2-py3-none-any.whl

---
+++ packages/LMSQuery-fork/LMSQuery_fork-0.5.2-py3-none-any.whl/lmsquery/__init__.py
@@ -0,0 +1,6 @@
+from . import const
+from .lmsquery import LMSQuery
+from .scanLMS import scanLMS
+
+#def LMSQuery(host=const.LMS_HOST, port=const.LMS_PORT, player_id=""):
+#    return lmsquery.LMSQuery(host, port, player_id)
