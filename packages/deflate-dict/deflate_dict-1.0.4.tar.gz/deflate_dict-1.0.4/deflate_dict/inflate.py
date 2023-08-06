b0e9ef515aee02e5afd564270ea6db7080c6b586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:56 +0000

    Add package deflate-dict/1.0.4/deflate_dict-1.0.4.tar.gz

---
+++ packages/deflate-dict/deflate_dict-1.0.4.tar.gz/deflate_dict-1.0.4/deflate_dict/inflate.py
@@ -0,0 +1,42 @@
+from typing import Dict
+from .is_leaf import is_leaf
+from .dict_to_list import dict_to_list
+from .utils import has_numeric_keys
+
+
+def _to_number(value):
+    try:
+        return int(value)
+    except ValueError:
+        pass
+    try:
+        return float(value)
+    except ValueError:
+        pass
+    return value
+
+
+def restore_lists(my_dict: Dict, leave_tuples:bool)->Dict:
+    restored = {
+        _to_number(k): v if is_leaf(v) or leave_tuples and isinstance(v, tuple) else restore_lists(v, leave_tuples) for k, v in my_dict.items()
+    }
+
+    return dict_to_list(restored) if has_numeric_keys(restored) else restored
+
+
+def inflate(my_dict: Dict, sep="_", leave_tuples=False)->Dict:
+    """Return inflated Dict."""
+    items = dict()
+    for k, v in my_dict.items():
+        keys = k.split(sep)
+        sub_items = items
+        for ki in keys[:-1]:
+            try:
+                sub_items = sub_items[ki]
+            except KeyError:
+                sub_items[ki] = dict()
+                sub_items = sub_items[ki]
+
+        sub_items[keys[-1]] = v
+
+    return restore_lists(items, leave_tuples=leave_tuples)
