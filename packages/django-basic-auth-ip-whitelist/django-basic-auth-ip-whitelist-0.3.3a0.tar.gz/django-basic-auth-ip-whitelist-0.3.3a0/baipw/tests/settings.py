feaf1018003f909b48627aaaeaa70cf8d458f50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:44 +0000

    Add package django-basic-auth-ip-whitelist/0.3.3a0/django-basic-auth-ip-whitelist-0.3.3a0.tar.gz

---
+++ packages/django-basic-auth-ip-whitelist/django-basic-auth-ip-whitelist-0.3.3a0.tar.gz/django-basic-auth-ip-whitelist-0.3.3a0/baipw/tests/settings.py
@@ -0,0 +1,46 @@
+from os.path import abspath, dirname, join
+
+import django
+from django.utils.crypto import get_random_string
+
+TESTS_PATH = dirname(abspath(__file__))
+
+SECRET_KEY = get_random_string(50)
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(TESTS_PATH, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+ROOT_URLCONF = 'baipw.tests.urls'
+
+if django.VERSION < (1, 9):
+    # This is to satisfy the django-admin check command.
+    MIDDLEWARE_CLASSES = []
