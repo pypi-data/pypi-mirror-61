cd9da4e5c0528bd309bbb75676578156e0dbe8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:27 +0000

    Add package GlobalData/0.1/GlobalData-0.1.tar.gz

---
+++ packages/GlobalData/GlobalData-0.1.tar.gz/GlobalData-0.1/GlobalData/Universe.py
@@ -0,0 +1,43 @@
+# Universe.py
+
+# from Globals import *
+from GlobalData import GlobalData as G
+
+def Get(gd, x):
+    return gd.GetX(x)
+
+class obj(dict):
+    def __init__(self):
+        self.exist = True
+        self.birth = time.time()
+        self.dict = {"Eyes":3, "exist":self.exist, "birth":self.birth}
+        self.gd = GD
+        self.root =
+
+    def
+
+
+o = obj()
+print(getAllAtr(o))
+print()
+print(getDict(o))
+
+'''
+### DynamicData
+### Storage
+### Functionality
+### Load and Represent
+### Location
+### Inputs - Mouse, Keyboard, Screen
+### Draw
+###
+###
+###
+###
+###
+###
+###
+###
+###
+### SuperHearing
+'''
