12b4a1d2082459d2f6cdf7e21a3bbf7c8abd9f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:53 +0000

    Add package act-atmos/0.2.0/act_atmos-0.2.0-py3-none-any.whl

---
+++ packages/act-atmos/act_atmos-0.2.0-py3-none-any.whl/act/io/__init__.py
@@ -0,0 +1,21 @@
+"""
+===============
+act.io (act.io)
+===============
+
+.. currentmodule:: act.io
+
+This module contains procedures for reading and writing various ARM datasets.
+
+.. autosummary::
+    :toctree: generated/
+
+    armfiles.read_netcdf
+    armfiles.check_arm_standards
+    armfiles.ARMStandardsFlag
+    armfiles.create_obj_from_arm_dod
+    csvfiles.read_csv
+"""
+
+from . import armfiles
+from . import csvfiles
---
+++ packages/act-atmos/act_atmos-0.2.0-py3-none-any.whl/act/retrievals/__init__.py
@@ -0,0 +1,23 @@
+"""
+===============================
+act.retrievals (act.retrievals)
+===============================
+
+.. currentmodule:: act.retrievals
+
+This module contains various retrievals for ARM datsets.
+
+.. autosummary::
+    :toctree: generated/
+
+    calculate_stability_indicies
+    generic_sobel_cbh
+    calculate_precipitable_water
+    compute_winds_from_ppi
+"""
+
+from .stability_indices import calculate_stability_indicies
+from .cbh import generic_sobel_cbh
+from .pwv_calc import calculate_precipitable_water
+from .doppler_lidar import compute_winds_from_ppi
+from .aeri import aeri2irt
