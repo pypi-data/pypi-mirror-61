1eb6f3b777ec66590559e7515a59115042411cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:08 +0000

    Add package rest-uploader/1.5.0/rest_uploader-1.5.0-py2.py3-none-any.whl

---
+++ packages/rest-uploader/rest_uploader-1.5.0-py2.py3-none-any.whl/rest_uploader/api_token.py
@@ -0,0 +1,35 @@
+import requests
+from os import path
+from .settings import SERVER
+
+
+def get_token():
+    if path.exists(".api_token.txt"):
+        with open(".api_token.txt", "r") as f:
+            token = f.readline().rstrip()
+    else:
+        token = input("Paste your Joplin API Token:")
+        with open(".api_token.txt", "w") as f:
+            f.write(token.rstrip())
+    return token
+
+
+def get_token_suffix():
+    return "?token=" + get_token()
+
+
+# Code from a non-Joplin API
+def get_header():
+    try:
+        my_token = get_token()
+        url = SERVER + "/ping"
+        head = {"Authorization": "Bearer {}".format(my_token)}
+        try:
+            response = requests.get(url, headers=head)
+            if response.status_code != 200:
+                print("Invalid token!")
+        except ValueError:
+            print("Token expired!")
+    except FileNotFoundError:
+        print("No Token!")
+    return head
