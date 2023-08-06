5742e53dc480be7135a2d67daa8a7c1eab07f749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:36 +0000

    Add package 101703235-missing-val/0.0.1/101703235_missing_val-0.0.1.tar.gz

---
+++ packages/101703235-missing-val/101703235_missing_val-0.0.1.tar.gz/101703235_missing_val-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="101703235_missing_val", # Replace with your own username
+    version="0.0.1",
+    author="Simranpreet kaur",
+    author_email="skaur_be17@thapar.edu",
+    description="a package to deal with missing values",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/missing_values",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["requests"],
+    entry_points={"console_scripts":["missing-val=sin_miss.miss_val:main",]},
+)
