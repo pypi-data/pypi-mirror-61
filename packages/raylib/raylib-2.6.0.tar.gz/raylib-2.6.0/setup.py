36b44ffb8544eb1c248239563da5099e42d5c93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:51 +0000

    Add package raylib/2.6.0/raylib-2.6.0.tar.gz

---
+++ packages/raylib/raylib-2.6.0.tar.gz/raylib-2.6.0/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="raylib",
+    version="2.6.0",
+    description="Python CFFI bindings for Raylib",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/electronstudio/raylib-python-cffi",
+    author="Electron Studio",
+    author_email="github@electronstudio.co.uk",
+    license="LGPLv3+",
+    classifiers=[
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["raylib", "raylib.dynamic", "raylib.static"],
+    include_package_data=True,
+    install_requires=["cffi>=1.14.0","inflection"],
+    #cffi_modules=["raylib/build_mac.py:ffibuilder"], # this would build libs whenever the module is installed, but we are distributing static libs instead
+)
