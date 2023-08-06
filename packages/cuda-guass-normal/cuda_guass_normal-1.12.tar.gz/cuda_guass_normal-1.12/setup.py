d604eb7f0d64e2dda9f4a271a75aa672ace3e70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package cuda-guass-normal/1.12/cuda_guass_normal-1.12.tar.gz

---
+++ packages/cuda-guass-normal/cuda_guass_normal-1.12.tar.gz/cuda_guass_normal-1.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='cuda_guass_normal',
+      version='1.12',
+      author='shuzhouz',
+      author_email='shuzhouz@umich.edu',
+      license='MIT',
+      description='A package used in DNN trainning in ATLAS analysis',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['cuda_guass_normal'],
+      zip_safe=False,
+      python_requires='>=3.6',
+)
+
