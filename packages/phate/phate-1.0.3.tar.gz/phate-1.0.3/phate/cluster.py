7d18904d9265411dc8e910da65340d45b419bf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:05 +0000

    Add package phate/1.0.3/phate-1.0.3.tar.gz

---
+++ packages/phate/phate-1.0.3.tar.gz/phate-1.0.3/phate/cluster.py
@@ -0,0 +1,44 @@
+from sklearn import cluster, exceptions
+import warnings
+from .phate import PHATE
+
+def kmeans(phate_op, n_clusters=8, random_state=None, k=None):
+    """KMeans on the PHATE potential
+
+    Clustering on the PHATE operator as introduced in Moon et al.
+    This is similar to spectral clustering.
+
+    Parameters
+    ----------
+
+    phate_op : phate.PHATE
+        Fitted PHATE operator
+    n_clusters : int, optional (default: 8)
+        Number of clusters
+    random_state : int or None, optional (default: None)
+        Random seed for k-means
+    k : deprecated for `n_clusters`
+
+    Returns
+    -------
+    clusters : np.ndarray
+        Integer array of cluster assignments
+    """
+    if k is not None:
+        warnings.warn(
+            "k is deprecated. Please use n_clusters in future.", FutureWarning
+        )
+        n_clusters = k
+    if not isinstance(phate_op, PHATE):
+        raise TypeError("Expected phate_op to be of type PHATE. Got {}".format(phate_op))
+    if phate_op.graph is not None:
+        diff_potential = phate_op.diff_potential
+        return cluster.KMeans(n_clusters, random_state=random_state).fit_predict(
+            diff_potential
+        )
+    else:
+        raise exceptions.NotFittedError(
+            "This PHATE instance is not fitted yet. Call "
+            "'fit' with appropriate arguments before "
+            "using this method."
+        )
