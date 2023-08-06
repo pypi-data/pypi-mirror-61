5efc56f7f27acb6d14b2e412fea4742fa7a2ef02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:40 +0000

    Add package quant-trade-framework/0.0.3/quant_trade_framework-0.0.3.tar.gz

---
+++ packages/quant-trade-framework/quant_trade_framework-0.0.3.tar.gz/quant_trade_framework-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="quant_trade_framework",
+    version="0.0.3",
+    author="chuan.yang",
+    author_email="chuan.yang0606@gmail.co,",
+    description="Quant Trade Framwork",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yangchuan123/QuantTradeFramework.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
