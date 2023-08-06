bf1ad9a1909a3a2aae21a911a9f2d0d53e637977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:00 +0000

    Add package translators/4.3.0/translators-4.3.0-py3.7.egg

---
+++ packages/translators/translators-4.3.0-py3.7.egg/translators/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "4.3.0"
+__author__ = "UlionTse"
+
+from .baidu import baidu_api as baidu
+from .google import google_api as google
+from .youdao import youdao_api as youdao
+from .tencent import tencent_api as tencent
+from .alibaba import alibaba_api as alibaba
