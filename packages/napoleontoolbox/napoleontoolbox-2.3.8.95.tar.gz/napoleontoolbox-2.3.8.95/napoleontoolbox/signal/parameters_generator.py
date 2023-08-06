da75b361eb8f01457170a56238bbedc3bc2d550a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:30 +0000

    Add package napoleontoolbox/2.3.8.95/napoleontoolbox-2.3.8.95.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.3.8.95.tar.gz/napoleontoolbox-2.3.8.95/napoleontoolbox/signal/parameters_generator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def generate_lead_lag(lookback_windows, contravariants):
+    parameters = []
+    lookback_window = max(lookback_windows)
+    for contravariant in contravariants:
+        for lag in range(3, lookback_window):
+            for lead in range(2, lag):
+                parameters.append({
+                    'lead':lead,
+                    'lag':lag,
+                    'lookback_window':lookback_window,
+                    'contravariant':contravariant
+                })
+    return parameters
+
+def generate_dd_threshold(lookback_windows, thresholds, contravariants):
+    parameters = []
+    for lookback_window in lookback_windows:
+        for contravariant in contravariants:
+            for threshold in thresholds:
+                parameters.append({
+                    'lookback_window':lookback_window,
+                    'contravariant':contravariant,
+                    'threshold':threshold
+                })
+    return parameters
+
+def generate_volume_weighted_high_low_vol(lookback_windows, vol_thresholds, trend_thresholds, contravariants):
+    parameters = []
+    for lookback_window in lookback_windows:
+        for contravariant in contravariants:
+            for vol_threshold in vol_thresholds:
+                for trend_threshold in trend_thresholds:
+                    parameters.append({
+                        'lookback_window':lookback_window,
+                        'contravariant':contravariant,
+                        'vol_threshold':vol_threshold,
+                        'trend_threshold':trend_threshold
+                    })
+    return parameters
