96afb2ea4504425139514bfa4f8caf98fcf88d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:07 +0000

    Add package webviz-core-components/0.0.17/webviz_core_components-0.0.17.tar.gz

---
+++ packages/webviz-core-components/webviz_core_components-0.0.17.tar.gz/webviz_core_components-0.0.17/src/lib/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+import WebvizPluginPlaceholder from "./components/WebvizPluginPlaceholder.react";
+import ColorScales from "./components/ColorScales.react";
+
+export { WebvizPluginPlaceholder, ColorScales };
