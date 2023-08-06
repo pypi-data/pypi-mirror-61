5b590bb38f15245c3e4584fc4c34bd3530315ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package napoleontoolbox/2.3.8.76/napoleontoolbox-2.3.8.76.tar.gz

---
+++ packages/napoleontoolbox/napoleontoolbox-2.3.8.76.tar.gz/napoleontoolbox-2.3.8.76/napoleontoolbox/signal/signal_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import talib
+
+def dd_threshold(data = None, threshold=1., contravariant = -1, **kwargs):
+    ratio = data['high'][-1]/data['high'][0]
+    if ratio > threshold:
+        return contravariant
+    else :
+        return -contravariant
+
+def lead_lag_indicator(data = None, lead=3, lag=5, contravariant = -1, **kwargs):
+    output_sma_lead = talib.SMA(data.close, timeperiod=lead)
+    output_sma_lag = talib.SMA(data.close, timeperiod=lag)
+    if output_sma_lead > output_sma_lag:
+        return -contravariant
+    else :
+        return contravariant
