e466082679c260c38156928f1c5c1e082192c328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:43 +0000

    Add package autoprotocol/6.1.2/autoprotocol-6.1.2-py3-none-any.whl

---
+++ packages/autoprotocol/autoprotocol-6.1.2-py3-none-any.whl/autoprotocol/liquid_handle/tip_type.py
@@ -0,0 +1,39 @@
+"""
+Generic tip type and device class mappings for LiquidHandleMethods
+"""
+from collections import namedtuple
+
+from ..util import parse_unit
+
+
+class TipType(namedtuple("TipType", ["name", "volume"])):
+    """
+    The TipType class holds the properties of a TipType
+    """
+    def __new__(cls, name, volume):
+        """
+        Parameters
+        ----------
+        name : str
+          Full name describing a TipType.
+        volume : Unit
+          The maximum capacity of the TipType.
+
+        Returns
+        -------
+        TipType
+            A tip type compatible with LiquidHandleMethods
+
+        Raises
+        ------
+        TypeError
+            if the name is not a str
+
+        See Also
+        --------
+        :py:class: `autoprotocol.LiquidHandleMethod._get_tip_types`
+        """
+        if not isinstance(name, str):
+            raise TypeError("TipType name {} was not a str.".format(name))
+        volume = parse_unit(volume, "uL")
+        return super(TipType, cls).__new__(cls, name, volume)
---
+++ packages/autoprotocol/autoprotocol-6.1.2-py3-none-any.whl/autoprotocol/version.py
@@ -0,0 +1,2 @@
+"""Maintains current version of package"""
+__version__ = "6.1.2"
