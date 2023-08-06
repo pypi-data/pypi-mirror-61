af663a08e8239bad351a0aa5c5deb9ef665f6e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:26 +0000

    Add package mincepy/0.9.0/mincepy-0.9.0.tar.gz

---
+++ packages/mincepy/mincepy-0.9.0.tar.gz/mincepy-0.9.0/mincepy/gui/main.py
@@ -0,0 +1,22 @@
+import sys
+
+from PySide2 import QtWidgets
+
+import mincepy
+from . import views
+
+__all__ = ('run_application',)
+
+
+def run_application(default_connect_uri="mongodb://localhost/test_database",
+                    create_historian_callback=mincepy.create_historian):
+    app = QtWidgets.QApplication([])
+    widget = views.MainWindow(default_connect_uri, create_historian_callback)
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    run_application()
