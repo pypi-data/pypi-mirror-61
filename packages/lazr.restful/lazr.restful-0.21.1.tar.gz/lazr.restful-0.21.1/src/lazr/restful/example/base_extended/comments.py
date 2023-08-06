99b1bc4c7e13f792fec781ab3ac9c4e76f8afe9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:48 +0000

    Add package lazr.restful/0.21.1/lazr.restful-0.21.1.tar.gz

---
+++ packages/lazr.restful/lazr.restful-0.21.1.tar.gz/lazr.restful-0.21.1/src/lazr/restful/example/base_extended/comments.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, print_function
+
+from zope.component import adapter
+from zope.interface import implementer, Interface
+from zope.schema import List, Text
+
+from lazr.restful.example.base.interfaces import IRecipe
+
+from lazr.restful.declarations import (
+    export_as_webservice_entry, exported)
+
+
+class IHasComments(Interface):
+    export_as_webservice_entry(contributes_to=[IRecipe])
+    comments = exported(
+        List(title=u'Comments made by users', value_type=Text()))
+
+
+@implementer(IHasComments)
+@adapter(IRecipe)
+class RecipeToHasCommentsAdapter:
+
+    def __init__(self, recipe):
+        self.recipe = recipe
+
+    @property
+    def comments(self):
+        return comments_db.get(self.recipe.id, [])
+
+
+# A fake database for storing comments. Monkey-patch this to test the
+# IHasComments adapter.
+comments_db = {}
