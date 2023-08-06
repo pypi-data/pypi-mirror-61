5e2ae781bc9001870c4b78ec1c138ea37bf8b894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:41 +0000

    Add package air-quotes/0.1.2/air-quotes-0.1.2.tar.gz

---
+++ packages/air-quotes/air-quotes-0.1.2.tar.gz/air-quotes-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['air_quotes']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'air-quotes',
+    'version': '0.1.2',
+    'description': 'Mock any proposal, reasonable or otherwise',
+    'long_description': None,
+    'author': 'JB',
+    'author_email': 'jb.tellez@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
