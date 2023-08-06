1512cfc8c25dcf9645f36da09d353b7cc8ff8974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:45 +0000

    Add package compas-rcf/0.1.15/compas_rcf-0.1.15-py2.py3-none-any.whl

---
+++ packages/compas-rcf/compas_rcf-0.1.15-py2.py3-none-any.whl/compas_rcf/abb/helpers.py
@@ -0,0 +1,23 @@
+"""Helpers for scripts interacting with ABB robots or RAPID code."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Describes the valid zone data definitions.
+zone_dict = {
+    "FINE": -1,
+    "Z0": 0,
+    "Z1": 1,
+    "Z5": 5,
+    "Z10": 10,
+    "Z15": 15,
+    "Z20": 20,
+    "Z30": 30,
+    "Z40": 40,
+    "Z50": 50,
+    "Z60": 60,
+    "Z80": 80,
+    "Z100": 100,
+    "Z150": 150,
+    "Z200": 200,
+}
