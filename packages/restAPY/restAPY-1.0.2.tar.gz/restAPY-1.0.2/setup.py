8ce6627fd78d7ba69d3b6e617cb3f293d3def9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:01 +0000

    Add package restAPY/1.0.2/restAPY-1.0.2.tar.gz

---
+++ packages/restAPY/restAPY-1.0.2.tar.gz/restAPY-1.0.2/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='restAPY',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='1.0.2',
+
+    description='A python module for building restapis',
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    # The project's main homepage.
+    url='https://github.com/N-Ziermann/restAPY',
+
+    # Author details
+    author='Niklas Ziermann',
+    author_email='n-ziermann@protonmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+
+    # What does your project relate to?
+    keywords='api, restapi, json, web',
+
+    packages=[],
+
+)
