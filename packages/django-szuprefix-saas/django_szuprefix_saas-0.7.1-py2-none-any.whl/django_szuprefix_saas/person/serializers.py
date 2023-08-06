1948178c740d409f9a77b909b4f5d082c2dd675b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:38 +0000

    Add package django-szuprefix-saas/0.7.1/django_szuprefix_saas-0.7.1-py2-none-any.whl

---
+++ packages/django-szuprefix-saas/django_szuprefix_saas-0.7.1-py2-none-any.whl/django_szuprefix_saas/person/serializers.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+# author = 'denishuang'
+from __future__ import unicode_literals
+
+from django_szuprefix.api.mixins import IDAndStrFieldSerializerMixin
+from rest_framework import serializers
+
+from ..saas.mixins import PartySerializerMixin
+from . import models
+
+
+class PersonSerializer(IDAndStrFieldSerializerMixin, PartySerializerMixin, serializers.ModelSerializer):
+    class Meta:
+        model = models.Person
+        exclude = ('party', )
