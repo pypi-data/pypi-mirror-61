c8b316b3643260efc335d7c16e1f067300aac8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:43 +0000

    Add package bask/0.1.2/bask-0.1.2-py2.py3-none-any.whl

---
+++ packages/bask/bask-0.1.2-py2.py3-none-any.whl/bask/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Bayes-skopt."""
+
+__author__ = """Karlson Pfannschmidt"""
+__email__ = "kiudee@mail.upb.de"
+__version__ = "0.1.2"
+
+from .acquisition import *
+from .bayesgpr import *
+from .optimizer import *
+from .utils import *
