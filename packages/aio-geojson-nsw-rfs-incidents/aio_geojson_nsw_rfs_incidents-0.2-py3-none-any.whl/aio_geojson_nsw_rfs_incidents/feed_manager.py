f73b4fac9cf8dd14c5f07acf6ed5f707211ba3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:42 +0000

    Add package aio-geojson-nsw-rfs-incidents/0.2/aio_geojson_nsw_rfs_incidents-0.2-py3-none-any.whl

---
+++ packages/aio-geojson-nsw-rfs-incidents/aio_geojson_nsw_rfs_incidents-0.2-py3-none-any.whl/aio_geojson_nsw_rfs_incidents/feed_manager.py
@@ -0,0 +1,34 @@
+"""Feed Manager for NSW Rural Fire Service Incidents feed."""
+from typing import List, Tuple, Callable, Awaitable
+
+from aio_geojson_client.feed_manager import FeedManagerBase
+from aio_geojson_client.status_update import StatusUpdate
+from aiohttp import ClientSession
+
+from .feed import NswRuralFireServiceIncidentsFeed
+
+
+class NswRuralFireServiceIncidentsFeedManager(FeedManagerBase):
+    """Feed Manager for NSW Rural Fire Services Incidents feed."""
+
+    def __init__(self,
+                 websession: ClientSession,
+                 generate_callback: Callable[[str], Awaitable[None]],
+                 update_callback: Callable[[str], Awaitable[None]],
+                 remove_callback: Callable[[str], Awaitable[None]],
+                 coordinates: Tuple[float, float],
+                 filter_radius: float = None,
+                 filter_categories: List[str] = None,
+                 status_callback: Callable[[StatusUpdate],
+                                           Awaitable[None]] = None):
+        """Initialize the NSW Rural Fire Services Feed Manager."""
+        feed = NswRuralFireServiceIncidentsFeed(
+            websession,
+            coordinates,
+            filter_radius=filter_radius,
+            filter_categories=filter_categories)
+        super().__init__(feed,
+                         generate_callback,
+                         update_callback,
+                         remove_callback,
+                         status_async_callback=status_callback)
