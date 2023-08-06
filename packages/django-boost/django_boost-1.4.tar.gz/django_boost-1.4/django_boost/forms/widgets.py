5822209bdc3e8a7b4092f5e87d408c2804be8571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:05 +0000

    Add package django-boost/1.4/django_boost-1.4.tar.gz

---
+++ packages/django-boost/django_boost-1.4.tar.gz/django_boost-1.4/django_boost/forms/widgets.py
@@ -0,0 +1,37 @@
+from django.forms.widgets import Input, CheckboxInput, RadioSelect
+
+__all__ = ["ColorInput", "InvertCheckboxInput"]
+
+
+class ColorInput(Input):
+    """HTML <input type='color'>."""
+
+    input_type = 'color'
+    template_name = 'django/forms/widgets/input.html'
+
+
+class StarRateSelect(RadioSelect):
+    """Star styled radio select."""
+
+    template_name = "boost/forms/widgets/star_radio.html"
+    option_template_name = "boost/forms/widgets/star_radio_input.html"
+
+
+def boolean_check(v):
+    return (v is False or v is None or v == '')
+
+
+class InvertCheckboxInput(CheckboxInput):
+    """Returns false if checked, true if not checked."""
+
+    def __init__(self, attrs=None, check_test=None):
+        super().__init__(attrs)
+        self.check_test = boolean_check if check_test is None else check_test
+
+    def value_from_datadict(self, data, files, name):
+        return not super().value_from_datadict(data, files, name)
+
+
+class Toggleswitch(CheckboxInput):
+    """toggle switch styled input"""
+    template_name = "boost/forms/widgets/toggleswitch.html"
