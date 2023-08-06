ad0633d8e81ba4cdebc8f79b43cce0610599973c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:27 +0000

    Add package bcf-api-xml/0.2.2/bcf_api_xml-0.2.2-py3-none-any.whl

---
+++ packages/bcf-api-xml/bcf_api_xml-0.2.2-py3-none-any.whl/bcf_api_xml/models/Line.py
@@ -0,0 +1,18 @@
+from bcf_api_xml.models import XYZ
+from lxml import builder
+
+
+def to_xml(line):
+    e = builder.ElementMaker()
+    return e.Line(
+        e.StartPoint(*XYZ.to_xml(line["start_point"])),
+        e.EndPoint(*XYZ.to_xml(line["end_point"])),
+    )
+
+
+def to_python(self):
+    xml = self.xml
+    return {
+        "start_point": XYZ.to_python(xml.find("StartPoint")),
+        "end_point": XYZ.to_python(xml.find("EndPoint")),
+    }
