548e81ac6ad44511796c2c3f617234845d37ad60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:08 +0000

    Add package AnaTon/1.1.1/AnaTon-1.1.1.tar.gz

---
+++ packages/AnaTon/AnaTon-1.1.1.tar.gz/AnaTon-1.1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='AnaTon',
+    version='1.1.1',
+    license='MIT',
+    author='Oscar Roth Andersen',
+    author_email='oscarrothandersen@gmail.com',
+    url='https://github.com/Odder/AnaTon',
+    description='Extremely fast anagram solver',
+    packages=['anaton'],
+    python_requires='>=3.7',
+    include_package_data=True
+)
